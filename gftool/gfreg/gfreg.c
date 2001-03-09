b3e966e282d3ed8f87520e06edfb26dc9290cabe
Osamu Tatebe <tatebe@users.sourceforge.net>
2001-03-09 11:15:43 +0000

    gfreg.c (main): Added a code for command option parsing. gfgrep.c (main): Added a missing '\n' of an error message.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@26 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfreg/gfreg.c
+++ gftool/gfreg/gfreg.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "gfarm.h"
@@ -6,25 +8,88 @@
 /*
  *  Register a local file to the Gfarm Meta DB.
  *
- *  gfreg <local_filename> <gfarm_url> <index>
+ *  gfreg <local_filename> <gfarm_url> [<index>]
  */
 
 char *program_name = "gfreg";
 
+void
+usage()
+{
+    fprintf(stderr, "Usage: %s <local_filename> <gfarm_url> [<index>]\n",
+	    program_name);
+    exit(1);
+}
+
+int
 main(int argc, char * argv[])
 {
     char * filename, * gfarm_url;
-    int index;
+    int node_index = -1, total_nodes = -1;
     char * e = (char *)NULL;
     struct stat sb;
+    extern char * optarg;
+    extern int optind;
+    int c;
 
-    if (argc != 4)
-	fprintf(stderr, "Usage: %s filename gfarm_url index\n",
-		program_name), exit(1);
+    /*  Command options  */
 
-    filename = argv[1];
-    gfarm_url = argv[2];
-    index = atoi(argv[3]);
+    if (argc >= 1)
+	program_name = argv[0];
+
+    while ((c = getopt(argc, argv, "I:N:")) != -1) {
+	switch (c) {
+	case 'I':
+	    node_index = strtol(optarg, NULL, 0);
+	    break;
+	case 'N':
+	    total_nodes = strtol(optarg, NULL, 0);
+	    break;
+	case '?':
+	default:
+	    usage();
+	}
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (argc == 0) {
+	fprintf(stderr,
+		"%s: missing a local filename\n",
+		program_name);
+	usage();
+	exit(1);
+    }
+    filename = argv[0];
+    argc--;
+    argv++;
+
+    if (argc == 0) {
+	fprintf(stderr,
+		"%s: missing a Gfarm URL\n",
+		program_name);
+	usage();
+	exit(1);
+    }
+    gfarm_url = argv[0];
+    argc--;
+    argv++;
+
+    if (argc == 0) {
+	if (node_index < 0) {
+	    fprintf(stderr, "%s: missing a Gfarm index\n",
+		    program_name);
+	    usage();
+	    exit(1);
+	}
+    }
+    else {
+	node_index = strtol(argv[0], NULL, 0);
+	argc--;
+	argv++;
+    }
+
+    /* */
 
     if (stat(filename, &sb))
 	perror(filename);
@@ -35,7 +100,7 @@ main(int argc, char * argv[])
 	exit(1);
     }
 
-    e = gfs_pio_set_fragment_info_local(filename, gfarm_url, index);
+    e = gfs_pio_set_fragment_info_local(filename, gfarm_url, node_index);
     if (e != NULL) {
 	fprintf(stderr, "%s: %s\n", program_name, e);
 	exit(1);
