f93510bfc0f3afe811cde60cb6a9092089f3cd86
SODA Noriyuki <n-soda@users.sourceforge.net>
2001-06-29 10:53:24 +0000

    gfrm: tool to remove gfarm file.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@40 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ gftool/gfrm/gfrm.c
@@ -0,0 +1,62 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include "gfarm.h"
+
+char *program_name = "gfrm";
+
+void
+usage()
+{
+	fprintf(stderr, "Usage: %s [option] <gfarm_url>...\n", program_name);
+	fprintf(stderr, "option:\n");
+	fprintf(stderr, "\tcurrently no option is supported.\n");
+	exit(1);
+}
+
+int
+main(argc, argv)
+	int argc;
+	char **argv;
+{
+	extern char *optarg;
+	extern int optind;
+	char *e;
+	int i, ch;
+
+	if (argc >= 1)
+		program_name = basename(argv[0]);
+
+	while ((ch = getopt(argc, argv, "rf")) != -1) {
+		switch (ch) {
+		case '?':
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	e = gfarm_initialize();
+	if (e != NULL) {
+		fprintf(stderr, "%s: %s\n", program_name, e);
+		exit(1);
+	}
+
+	for (i = 0; i < argc; i++) {
+		e = gfs_unlink(argv[i]);
+		if (e != NULL)
+			fprintf(stderr, "%s: %s\n", argv[i], e);
+	}
+	e = gfarm_terminate();
+	if (e != NULL) {
+		fprintf(stderr, "%s: %s\n", program_name, e);
+		exit(1);
+	}
+	return (0);
+}
