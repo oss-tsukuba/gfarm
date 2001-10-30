75b179c937a8fb22424182f67897de0efb5c5bcb
SODA Noriyuki <n-soda@users.sourceforge.net>
2001-10-30 02:27:43 +0000

    simple i/o throughput benchmark of gfs_pio_read/write
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@54 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ bench/thput-gfpio/thput-gfpio.c
@@ -0,0 +1,299 @@
+#include <assert.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "gfarm.h"
+
+typedef struct gfs_file *GFS_File;
+
+char *program_name = "thput-gfpio";
+int node_index = -1, total_nodes = -1;
+
+#define MAX_BUFFER_SIZE	(1024*1024)
+
+char buffer[MAX_BUFFER_SIZE];
+
+void
+initbuffer(void)
+{
+	int i;
+
+	for (i = 0; i < MAX_BUFFER_SIZE; i++)
+		buffer[i] = i;
+}
+
+void
+writetest(char *ofile, int buffer_size, off_t file_size)
+{
+	GFS_File gf;
+	char *e = gfs_pio_create_local(ofile, &gf);
+	int rv;
+	off_t residual;
+
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] %s: %s\n", node_index, ofile, e);
+		exit(1);
+	}
+	for (residual = file_size; residual > 0; residual -= rv) {
+		e = gfs_pio_write(gf, buffer,
+			buffer_size <= residual ? buffer_size : residual,
+			&rv);
+		if (e != NULL) {
+			fprintf(stderr, "[%03d] write test: %s\n",
+				node_index, e);
+			break;
+		}
+		assert(rv ==
+		       (buffer_size <= residual ? buffer_size : residual));
+	}
+	if (residual > 0) {
+		fprintf(stderr, "[%03d] write test failed, residual = %ld\n",
+			node_index, (long)residual);
+	}
+	e = gfs_pio_close(gf);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] write test close failed: %s\n",
+			node_index, e);
+	}
+}
+
+void
+readtest(char *ifile, int buffer_size, off_t file_size)
+{
+	GFS_File gf;
+	char *e = gfs_pio_open_local(ifile, GFS_FILE_RDONLY, &gf);
+	int rv;
+	off_t residual;
+
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] %s: %s\n", node_index, ifile, e);
+		exit(1);
+	}
+	for (residual = file_size; residual > 0; residual -= rv) {
+		e = gfs_pio_read(gf, buffer,
+			buffer_size <= residual ? buffer_size : residual,
+			&rv);
+		if (e != NULL) {
+			fprintf(stderr, "[%03d] read test: %s\n",
+				node_index, e);
+			break;
+		}
+		if (rv == 0)
+			break;
+		assert(rv == buffer_size || rv == residual);
+	}
+	if (residual > 0) {
+		fprintf(stderr, "[%03d] read test failed, residual = %ld\n",
+			node_index, (long)residual);
+	}
+	e = gfs_pio_close(gf);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] read test close failed: %s\n",
+			node_index, e);
+	}
+}
+
+void
+copytest(char *ifile, char *ofile, int buffer_size, off_t file_size)
+{
+	GFS_File igf, ogf;
+	char *e;
+	int rv, osize;
+	off_t residual;
+
+	e = gfs_pio_open_local(ifile, GFS_FILE_RDONLY, &igf);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] %s: %s\n", node_index, ifile, e);
+		exit(1);
+	}
+	e = gfs_pio_create_local(ofile, &ogf);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] %s: %s\n", node_index, ofile, e);
+		exit(1);
+	}
+	for (residual = file_size; residual > 0; residual -= rv) {
+		e = gfs_pio_read(igf, buffer, buffer_size, &rv);
+		if (e != NULL) {
+			fprintf(stderr, "[%03d] copytest read: %s\n",
+				node_index, e);
+			break;
+		}
+		if (rv == 0)
+			break;
+		assert(rv == buffer_size || rv == residual);
+
+		osize = rv;
+		e = gfs_pio_write(ogf, buffer, osize, &rv);
+		if (e != NULL) {
+			fprintf(stderr, "[%03d] copytest write: %s\n",
+				node_index, e);
+			break;
+		}
+		assert(rv == osize);
+	}
+	if (residual > 0) {
+		fprintf(stderr, "[%03d] copy test failed, residual = %ld\n",
+			node_index, (long)residual);
+	}
+	e = gfs_pio_close(ogf);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] copy test write close failed: %s\n",
+			node_index, e);
+	}
+	e = gfs_pio_close(igf);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] copy test read close failed: %s\n",
+			node_index, e);
+	}
+}
+
+double
+timeval_sub(struct timeval *t1, struct timeval *t2)
+{
+	return ((t1->tv_sec + t1->tv_usec / 1000000.0) -
+		(t2->tv_sec + t2->tv_usec / 1000000.0));
+}
+
+enum testmode { TESTMODE_WRITE, TESTMODE_READ, TESTMODE_COPY };
+
+void
+test(enum testmode test_mode, char *file1, char *file2,
+     int buffer_size, off_t file_size)
+{
+	struct timeval t1, t2;
+	char *label;
+
+	gettimeofday(&t1, NULL);
+
+	switch (test_mode) {
+	case TESTMODE_WRITE:
+		writetest(file1, buffer_size, file_size);
+		label = "write";
+		break;
+	case TESTMODE_READ:
+		readtest(file1, buffer_size, file_size);
+		label = "read";
+		break;
+	case TESTMODE_COPY:
+		copytest(file1, file2, buffer_size, file_size);
+		label = "copy";
+		break;
+	default:
+		fprintf(stderr, "[%03d] ??? wrong test_mode: %d\n",
+			node_index, test_mode);
+		exit(1);
+	}
+
+	gettimeofday(&t2, NULL);
+
+	printf("[%03d] %8ld %7d %-5s %10.0f %s\n",
+	       node_index, (long)file_size, buffer_size, label,
+	       file_size / timeval_sub(&t2, &t1), gfarm_self_hostname);
+	fflush(stdout);
+}
+
+int
+main(int argc, char **argv)
+{
+	char *file1 = "gfarm:test.file1";
+	char *file2 = "gfarm:test.file2";
+	int c, buffer_size = 1024 * 1024;
+	off_t file_size = 1024;
+	enum testmode test_mode = TESTMODE_WRITE;
+	char *e;
+
+	if (argc > 0)
+		program_name = argv[0];
+	while ((c = getopt(argc, argv, "I:N:b:s:wrcS:")) != -1) {
+		switch (c) {
+		case 'I':
+			node_index = strtol(optarg, NULL, 0);
+			break;
+		case 'N':
+			total_nodes = strtol(optarg, NULL, 0);
+			break;
+		case 'b':
+			buffer_size = strtol(optarg, NULL, 0);
+			if (buffer_size > MAX_BUFFER_SIZE) {
+				fprintf(stderr, "%s: \"-b %d\" is too big\n",
+					program_name, buffer_size);
+				exit(1);
+			}
+			break;
+		case 's':
+			file_size = strtol(optarg, NULL, 0);
+			break;
+		case 'w':
+			test_mode = TESTMODE_WRITE;
+			break;
+		case 'r':
+			test_mode = TESTMODE_READ;
+			break;
+		case 'c':
+			test_mode = TESTMODE_COPY;
+			break;
+		case 'S':
+			/*
+			 * This option is provided to give scheduling hint
+			 * to `gfrsh'. Benchmark itself just ignores `optarg'.
+			 */
+			break;
+		case '?':
+		default:
+			fprintf(stderr,
+				"Usage: gfrsh %s [options]"
+				" [gfarm_url1 [gfarm_url2]]\n"
+				"options:\n"
+				"\t-b block-size\n"
+				"\t-s file-size\n"
+				"\t-w			: write test\n"
+				"\t-r			: read test\n"
+				"\t-c			: copy test\n"
+				"\t-S gfarm_url		: scheduling hint\n",
+				program_name);
+			exit(1);
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (node_index < 0) {
+		fprintf(stderr, "%s: missing node index, use -I option.\n",
+		program_name);
+		exit(1);
+	}
+	if (total_nodes <= 0) {
+		fprintf(stderr,
+			"%s: missing total node number, use -N option.\n",
+			program_name);
+		exit(1);
+	}
+	
+	e = gfarm_initialize(&argc, &argv);
+	if (e != NULL) {
+		fprintf(stderr, "[%03d] %s: gfarm_initalize(): %s\n",
+			node_index, program_name, e);
+		exit(1);
+	}
+	gfs_pio_set_local(node_index, total_nodes);
+
+	if (argc > 0)
+		file1 = argv[0];
+	if (argc > 1)
+		file2 = argv[1];
+
+	file_size *= 1024 * 1024;
+	initbuffer();
+
+	test(test_mode, file1, file2, buffer_size, file_size);
+
+	e = gfarm_terminate();
+	if (e != NULL) {
+		fprintf(stderr, "%s: gfarm_terminiate(): %s\n",
+			program_name, e);
+		exit(1);
+	}
+	return (0);
+}
