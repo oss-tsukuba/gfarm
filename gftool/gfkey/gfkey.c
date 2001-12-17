62c0cdec571e37d9baf998343feade1a3611f129
SODA Noriyuki <n-soda@users.sourceforge.net>
2001-12-17 11:04:21 +0000

    s/GFS_AUTH/GFRAM_AUTH/g
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@56 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfkey/gfkey.c
+++ gftool/gfkey/gfkey.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <libgen.h>
+#include <time.h>
 #include "gfarm_config.h"
 #include "gfarm_error.h"
 #include "gfarm_misc.h"
-#include "gfs_proto.h"
+#include "auth.h"
 
 char *program_name = "gfkey";
 
@@ -39,8 +40,8 @@ main(argc, argv)
 	int ch, do_list = 0, do_expire_report = 0;
 	char *e, *user, *home;
 	unsigned int expire;
-	char shared_key[GFS_AUTH_SHARED_KEY_LEN];
-	int mode = GFS_AUTH_SHARED_KEY_GET;
+	char shared_key[GFARM_AUTH_SHARED_KEY_LEN];
+	int mode = GFARM_AUTH_SHARED_KEY_GET;
 
 	if (argc >= 1)
 		program_name = basename(argv[0]);
@@ -48,13 +49,13 @@ main(argc, argv)
 	while ((ch = getopt(argc, argv, "cefl")) != -1) {
 		switch (ch) {
 		case 'c':
-			mode = GFS_AUTH_SHARED_KEY_CREATE;
+			mode = GFARM_AUTH_SHARED_KEY_CREATE;
 			break;
 		case 'e':
 			do_expire_report = 1;
 			break;
 		case 'f':
-			mode = GFS_AUTH_SHARED_KEY_CREATE_FORCE;
+			mode = GFARM_AUTH_SHARED_KEY_CREATE_FORCE;
 			break;
 		case 'l':
 			do_list = 1;
@@ -67,18 +68,19 @@ main(argc, argv)
 	argc -= optind;
 	argv += optind;
 	if (argc != 0 ||
-	    (mode == GFS_AUTH_SHARED_KEY_GET && !do_list && !do_expire_report))
+	    (mode == GFARM_AUTH_SHARED_KEY_GET &&
+	     !do_list && !do_expire_report))
 		usage();
 
 	gfarm_user_home_get(&user, &home);
-	e = gfs_auth_shared_key_get(&expire, shared_key, home, mode);
+	e = gfarm_auth_shared_key_get(&expire, shared_key, home, mode);
 	if (e != NULL) {
 		fprintf(stderr, "%s\n", e);
 		exit(1);
 	}
 	if (do_list) {
 		printf("%08x ", expire);
-		write_hex(stdout, shared_key, GFS_AUTH_SHARED_KEY_LEN);
+		write_hex(stdout, shared_key, GFARM_AUTH_SHARED_KEY_LEN);
 		putc('\n', stdout);
 	}
 	if (do_expire_report) {
