b7c7705f2a4af8d39a95a5a04f636d326af8e683
SODA Noriyuki <n-soda@users.sourceforge.net>
2002-05-07 01:11:37 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@98 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ makes/man.mk
@@ -0,0 +1,32 @@
+all: man-all post-all-hook
+install: man-install post-install-hook
+clean: man-clean post-clean-hook
+veryclean: man-veryclean post-very-clean-hook
+distclean: man-distclean post-distclean-hook
+
+post-all-hook:
+post-install-hook:
+post-clean-hook:
+post-very-clean-hook:
+post-distclean-hook:
+
+man-all:
+
+man-install:
+	@for i in / $(MAN); do \
+		case $$i in \
+		/)	;; \
+		*)	suffix=`expr $$i : '.*\.\([^.]*\)$$'`; \
+			( set -x; \
+			$(INSTALL_DATA) $$i $(mandir)/man$$suffix/$$i );; \
+		esac; \
+	done
+
+man-clean:
+	-rm -f $(EXTRA_CLEAN_TARGETS)
+
+man-veryclean: clean
+	-rm -f $(EXTRA_VERYCLEAN_TARGETS)
+
+man-distclean: veryclean
+	if [ -f $(srcdir)/Makefile.in ]; then rm -f Makefile; fi
