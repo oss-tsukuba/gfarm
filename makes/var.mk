03ef30a498c27fab22b22867737f3af1b541f46c
SODA Noriyuki <n-soda@users.sourceforge.net>
2002-06-14 20:54:33 +0000

    link Globus GSI and libgfsl.a, when --with-globus is specified with configure.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@115 4a5e1ece-6a47-475c-b646-731d720b41d0

--- makes/var.mk
+++ makes/var.mk
@@ -4,14 +4,20 @@ COMMON_CFLAGS = $(OPTFLAGS) $(largefile_cflags) -I$(top_objdir) -I$(INCDIR)
 COMMON_LDFLAGS = $(largefile_ldflags)
 
 INCDIR = $(top_srcdir)/include
+GFSL_DIR = $(top_srcdir)/lib/libgfsl
 GFD_DIR = $(top_srcdir)/gfarmd
 GFSD_DIR = $(top_srcdir)/gfsd
 GFARMLIB_DIR = $(top_srcdir)/gfarmlib
+
+GFSL_INCLUDES = -I$(GFSL_DIR)
+GFSL_LIBS = -L$(top_objdir)/lib/libgfsl -lgfsl
+DEPGFSLLIB = $(top_objdir)/lib/libgfsl/libgfsl.a
+DEPGFSLINC = $(GFSL_DIR)/gfarm_auth.h $(GFSL_DIR)/gfarm_gsi.h $(GFSL_DIR)/gfarm_secure_session.h
 
 NSLIB = -L$(top_objdir)/nslib -lns
 DEPNSLIB = $(top_objdir)/nslib/libns.a
 
 GFARMLIB = -L$(top_objdir)/gfarmlib -lgfarm \
-	$(metadb_client_libs) $(openssl_libs)
-DEPGFARMLIB = $(top_objdir)/gfarmlib/libgfarm.a
+	$(metadb_client_libs) $(gfsl_libs) $(openssl_libs)
+DEPGFARMLIB = $(top_objdir)/gfarmlib/libgfarm.a $(depgfsllib)
 DEPGFARMINC = $(INCDIR)/gfarm.h $(INCDIR)/gfarm_error.h $(INCDIR)/gfarm_metadb.h $(INCDIR)/gfarm_misc.h $(INCDIR)/gfs.h
