e28776a9fdd8afa56913bb5a55b561b217d6ee4b
SODA Noriyuki <n-soda@users.sourceforge.net>
2002-08-06 08:04:17 +0000

    gfarmlib -> lib/libgfarm
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/branches/metadb_dev@134 4a5e1ece-6a47-475c-b646-731d720b41d0

--- makes/var.mk
+++ makes/var.mk
@@ -9,7 +9,7 @@ INCDIR = $(top_srcdir)/include
 GFSL_DIR = $(top_srcdir)/lib/libgfsl
 GFD_DIR = $(top_srcdir)/gfarmd
 GFSD_DIR = $(top_srcdir)/gfsd
-GFARMLIB_DIR = $(top_srcdir)/gfarmlib
+GFARMLIB_DIR = $(top_srcdir)/lib/libgfarm
 
 GFSL_INCLUDES = -I$(GFSL_DIR)
 GFSL_LIBS = -L$(top_objdir)/lib/libgfsl -lgfsl
@@ -19,7 +19,7 @@ DEPGFSLINC = $(GFSL_DIR)/gfarm_auth.h $(GFSL_DIR)/gfarm_gsi.h $(GFSL_DIR)/gfarm_
 NSLIB = -L$(top_objdir)/nslib -lns
 DEPNSLIB = $(top_objdir)/nslib/libns.a
 
-GFARMLIB = -L$(top_objdir)/gfarmlib -lgfarm \
+GFARMLIB = -L$(top_objdir)/lib/libgfarm -lgfarm \
 	$(metadb_client_libs) $(gfsl_libs) $(openssl_libs)
-DEPGFARMLIB = $(top_objdir)/gfarmlib/libgfarm.a $(depgfsllib)
+DEPGFARMLIB = $(top_objdir)/lib/libgfarm/libgfarm.a $(depgfsllib)
 DEPGFARMINC = $(INCDIR)/gfarm.h $(INCDIR)/gfarm_error.h $(INCDIR)/gfarm_metadb.h $(INCDIR)/gfarm_misc.h $(INCDIR)/gfs.h
