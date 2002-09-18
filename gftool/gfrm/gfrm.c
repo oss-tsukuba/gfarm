23d9ed332ae75105a26b2b758f31891a3a0adce3
SODA Noriyuki <n-soda@users.sourceforge.net>
2002-09-18 05:51:59 +0000

    add a feature to remove replica. API:		gfs_unlink_section_replica() command:	gfrm -i <fragment> -H <hostfile>
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@146 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfrm/gfrm.c
+++ gftool/gfrm/gfrm.c
@@ -15,7 +15,10 @@ usage()
 {
 	fprintf(stderr, "Usage: %s [option] <gfarm_url>...\n", program_name);
 	fprintf(stderr, "option:\n");
-	fprintf(stderr, "\tcurrently no option is supported.\n");
+	fprintf(stderr, "\t-H <hostfile>\t"
+	    "remove replica, -i is needed, too.\n");
+	fprintf(stderr, "\t-i <fragment>\t"
+	    "remove replica, -H is needed, too.\n");
 	exit(1);
 }
 
@@ -28,14 +31,21 @@ main(argc, argv)
 	extern int optind;
 	int argc_save = argc;
 	char **argv_save = argv;
-	char *e;
-	int i, ch;
+	char *e, *hostfile = NULL, *section = NULL;
+	int i, ch, nhosts, error_line;
+	char **hosttab;
 
 	if (argc >= 1)
 		program_name = basename(argv[0]);
 
-	while ((ch = getopt(argc, argv, "rf")) != -1) {
+	while ((ch = getopt(argc, argv, "H:i:fr")) != -1) {
 		switch (ch) {
+		case 'H':
+			hostfile = optarg;
+			break;
+		case 'i':
+			section = optarg;
+			break;
 		case '?':
 		default:
 			usage();
@@ -50,8 +60,36 @@ main(argc, argv)
 		exit(1);
 	}
 
+	if (hostfile != NULL && section == NULL) {
+		fprintf(stderr,
+		    "%s: -i <fragment> option is required with -H\n",
+		    program_name);
+		exit(1);
+	}
+	e = gfarm_hostlist_read(hostfile, &nhosts, &hosttab, &error_line);
+	if (e != NULL) {
+		if (error_line != -1)
+			fprintf(stderr, "%s: line %d: %s\n",
+				hostfile, error_line, e);
+		else
+			fprintf(stderr, "%s: %s\n",
+				program_name, e);
+		exit(1);
+	}
+
+	if (section != NULL && hostfile == NULL) {
+		fprintf(stderr,
+		    "%s: -H <hostfile> option is required with -i\n",
+		    program_name);
+		exit(1);
+	}
+
 	for (i = 0; i < argc; i++) {
-		e = gfs_unlink(argv[i]);
+		if (hostfile != NULL && section != NULL)
+			e = gfs_unlink_section_replica(argv[i], section,
+			    nhosts, hosttab);
+		else
+			e = gfs_unlink(argv[i]);
 		if (e != NULL)
 			fprintf(stderr, "%s: %s\n", argv[i], e);
 	}
