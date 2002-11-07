8ed75c81446fad466da8e42ced1459a56a48dcc6
SODA Noriyuki <n-soda@users.sourceforge.net>
2002-11-07 11:08:01 +0000

    add "-I" option to schedule a section (not a whole file), for gfarmlib debugging purpose
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@186 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfsched/gfsched.c
+++ gftool/gfsched/gfsched.c
@@ -1,6 +1,11 @@
+/*
+ * $Id$
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <libgen.h>
 #include <sys/types.h>
 #include <gfarm/gfarm.h>
 
@@ -24,19 +29,31 @@ usage()
 int
 main(int argc, char * argv[])
 {
-    int argc_save = argc;
-    char **argv_save = argv;
     char * gfarm_url = (char *)NULL;
+    char * section = (char *)NULL;
     char * e = (char *)NULL;
     FILE * outp = stdout;
     int errflg = 0;
     extern int optind;
     int nhosts;
     char **hosts;
+    char * gfarm_file;
+    char * host;
     int c, i;
 
-    while ((c = getopt(argc, argv, "?")) != EOF) {
+    if (argc >= 1)
+	program_name = basename(argv[0]);
+    e = gfarm_initialize(&argc, &argv);
+    if (e != NULL) {
+	fprintf(stderr, "%s: %s\n", program_name, e);
+	exit(1);
+    }
+
+    while ((c = getopt(argc, argv, "I:?")) != EOF) {
 	switch (c) {
+	case 'I': /* This is not for casual use, but for debugging purpose. */
+	    section = optarg;
+	    break;
 	case '?':
 	default:
 	    ++errflg;
@@ -63,21 +80,37 @@ main(int argc, char * argv[])
 	}
     }
 
-    e = gfarm_initialize(&argc_save, &argv_save);
-    if (e != NULL) {
-	fprintf(stderr, "%s: %s\n", program_name, e);
-	exit(1);
+    if (section == NULL) {
+	e = gfarm_url_hosts_schedule(gfarm_url, (char *)NULL,
+				     &nhosts, &hosts);
+	if (e != NULL) {
+	    fprintf(stderr, "%s: %s\n", program_name, e);
+	    exit(1);
+	}
+
+	for (i = 0; i < nhosts; ++i)
+	    fprintf(outp, "%s\n", hosts[i]);
+    }
+    else {
+	/* This is not for casual use, but for debugging purpose. */
+
+	e = gfarm_url_make_path(gfarm_url, &gfarm_file);
+	if (e != NULL) {
+	    fprintf(stderr, "%s: %s\n", program_name, e);
+	    exit(1);
+	}
+	e = gfarm_file_section_host_schedule(gfarm_file, section, &host);
+	if (e != NULL) {
+	    fprintf(stderr, "%s: %s:%s: %s\n", program_name,
+		    gfarm_url, section, e);
+	    exit(1);
+	}
+
+	fprintf(outp, "%s\n", host);
+	free(host);
+	free(gfarm_file);
     }
 
-    e = gfarm_url_hosts_schedule(gfarm_url, (char *)NULL,
-				 &nhosts, &hosts);
-    if (e != NULL) {
-	fprintf(stderr, "%s: %s\n", program_name, e);
-	exit(1);
-    }
-
-    for (i = 0; i < nhosts; ++i)
-	fprintf(outp, "%s\n", hosts[i]);
     fclose(outp);
 
     e = gfarm_terminate();
