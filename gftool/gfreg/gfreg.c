e304f973e3050653826081e1df35b668983f228a
Osamu Tatebe <tatebe@users.sourceforge.net>
2003-01-16 15:31:46 +0000

    Fixed a functionality to register a file fragment.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@215 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfreg/gfreg.c
+++ gftool/gfreg/gfreg.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <gfarm/gfarm.h>
@@ -21,6 +22,71 @@ usage()
     fprintf(stderr, "Usage: %s <local_filename> <gfarm_url>\n",
 	    program_name);
     exit(1);
+}
+
+#define GFS_FILE_BUFSIZE 65536
+
+char *
+gfarm_url_fragment_register(char *gfarm_url, int index, int nfrags,
+			    char *filename)
+{
+	char *e, *e_save = NULL;
+	int fd;
+	size_t rv;
+	int length; /* XXX - should be size_t */
+	GFS_File gf;
+	struct stat s;
+	char buffer[GFS_FILE_BUFSIZE];
+
+	if (stat(filename, &s) == -1) {
+		perror(filename);
+		return "no such file or directory";
+	}
+
+	/*
+	 * register the fragment
+	 */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		perror(filename);
+		return "cannot open";
+	}
+	/* XXX - overwrite case */
+	e = gfs_pio_create(gfarm_url, GFARM_FILE_WRONLY,
+	    s.st_mode & GFARM_S_ALLPERM, &gf);
+	if (e != NULL) {
+		close(fd);
+		fprintf(stderr, "%s\n", e);
+		return e;
+	}
+	e = gfs_pio_set_view_index(gf, nfrags, index, NULL, 0);
+	if (e != NULL) {
+		gfs_pio_close(gf);
+		close(fd);
+		fprintf(stderr, "%s\n", e);
+		return e;
+	}
+	for (;;) {
+		rv = read(fd, buffer, sizeof(buffer));
+		if (rv <= 0)
+			break;
+		/* XXX - partial write case ? */
+		e = gfs_pio_write(gf, buffer, rv, &length);
+		if (e != NULL)
+			break;
+	}
+	e_save = e;
+	e = gfs_pio_close(gf);
+	close(fd);
+	if (e_save != NULL) {
+		fprintf(stderr, "%s\n", e_save);
+		return e_save;
+	}
+	if (e != NULL) {
+		fprintf(stderr, "%s\n", e);
+		return e;
+	}
+	return e;
 }
 
 int
@@ -120,13 +186,22 @@ main(int argc, char *argv[])
 	e = gfarm_url_program_register(gfarm_url, architecture,
 				       filename, total_nodes);
     } else {
+	int index;
 	if (node_index == NULL) {
 	    fprintf(stderr, "%s: missing -I <Gfarm index>\n",
 		    program_name);
 	    usage();
 	    exit(1);
 	}
-	e = gfs_pio_set_fragment_info_local(filename, gfarm_url, node_index);
+	if (total_nodes <= 0) {
+	    fprintf(stderr, "%s: missing -N <total num of fragments>\n",
+		    program_name);
+	    usage();
+	    exit(1);
+	}
+	index = strtol(node_index, NULL, 0);
+	e = gfarm_url_fragment_register(gfarm_url, index,
+					total_nodes, filename);
     }
     if (e != NULL) {
 	fprintf(stderr, "%s: %s\n", program_name, e);
