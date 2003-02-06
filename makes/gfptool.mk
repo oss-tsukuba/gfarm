918529824a73daf879d6e9697b785bcccdddbe53
SODA Noriyuki <n-soda@users.sourceforge.net>
2003-02-06 16:06:11 +0000

    do not call "gfreg" at "make install", because: - it is impossible to make gfarm binary package (e.g. rpm, deb) with that way. - it is inconsistent with our current INSTALL.* document.   (Our installation document says that gfmd and gfsd should be run    after "make install"). add "make gfregister" target for "gfreg".
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@258 4a5e1ece-6a47-475c-b646-731d720b41d0

--- makes/gfptool.mk
+++ makes/gfptool.mk
@@ -1,24 +1,4 @@
-install: all prog-install post-install-hook
-clean: prog-clean post-clean-hook
-veryclean: prog-veryclean post-veryclean-hook
-distclean: prog-distclean post-distclean-hook
+include $(top_srcdir)/makes/prog.mk
 
-post-install-hook:
-post-clean-hook:
-post-veryclean-hook:
-post-distclean-hook:
-
-$(PROGRAM): $(OBJS) $(DEPLIBS)
-	$(CC) -o $(PROGRAM) $(CFLAGS) $(OBJS) $(LDLIBS)
-
-prog-install:
+gfregister: $(PROGRAM)
 	-gfreg $(PROGRAM) gfarm:/bin/$(PROGRAM)
-
-prog-clean:
-	-rm -f $(OBJS) $(EXTRA_CLEAN_TARGETS)
-
-prog-veryclean: clean
-	-rm -f $(PROGRAM) $(EXTRA_VERYCLEAN_TARGETS)
-
-prog-distclean: veryclean
-	if [ -f $(srcdir)/Makefile.in ]; then rm -f Makefile; fi
