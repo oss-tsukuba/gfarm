f9b5fd79db1f7ff693bfcf17f6088540728f9793
Osamu Tatebe <tatebe@users.sourceforge.net>
2003-03-19 08:51:51 +0000

    Display more debug message for set_view_local.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@313 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/gfs_hook/hooks_common.c
+++ lib/gfs_hook/hooks_common.c
@@ -92,8 +92,10 @@ retry:
 		if (gfs_pio_get_nfragment(gf, &nf) ==
 			GFARM_ERR_FRAGMENT_INDEX_NOT_AVAILABLE ||
 		    (gfs_pio_get_node_size(&np) == NULL && nf == np)) {
+			extern int gfarm_node, gfarm_nnode;
 			_gfs_hook_debug(fprintf(stderr,
-				"GFS: set_view_local(%s)\n", path));
+				"GFS: set_view_local(%s (%d, %d))\n",
+					path, gfarm_node, gfarm_nnode));
 			if ((e = gfs_pio_set_view_local(gf, 0)) != NULL) {
 				_gfs_hook_debug(fprintf(stderr,
 					"GFS: set_view_local: %s\n", e));
