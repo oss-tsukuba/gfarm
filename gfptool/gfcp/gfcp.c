5b363f18662a7f35feedb3741ac833ba5173bfe3
Osamu Tatebe <tatebe@users.sourceforge.net>
2003-10-28 13:52:37 +0000

    Added the -p option for preserving the file status. Added the error check such that the destination is a directory or not, because the gfcp currently support a copy from a file to a file.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@833 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gfptool/gfcp/gfcp.c
+++ gfptool/gfcp/gfcp.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <gfarm/gfarm.h>
 
@@ -16,6 +17,8 @@ usage()
 {
 	fprintf(stderr, "Usage: %s [option] <input_file> <output_file>\n",
 		program_name);
+	fprintf(stderr, "option:\n");
+	fprintf(stderr, "\t-p\tpreserve file status\n");
 	exit(1);
 }
 
@@ -24,9 +27,15 @@ usage()
 int
 main(int argc, char *argv[])
 {
-	int total_nodes = -1, node_index = -1;
-	char *e, *input, *output;
+	int total_nodes, node_index;
+	int flag_preserve = 0;
+	char *e, c, *input, *output;
 	GFS_File igf, ogf;
+	struct gfs_stat gstat;
+	gfarm_mode_t mode;
+	struct gfarm_timespec gtspec[2];
+	extern char *optarg;
+	extern int optind;
 
 	e = gfarm_initialize(&argc, &argv);
 	if (e != NULL) {
@@ -36,8 +45,20 @@ main(int argc, char *argv[])
 
 	if (argc >= 1)
 		program_name = argv[0];
-	argc--;
-	argv++;
+
+	while ((c = getopt(argc, argv, "ph")) != -1) {
+		switch (c) {
+		case 'p':
+			flag_preserve = 1;
+			break;
+		case 'h':
+		case '?':
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
 
 	e = gfs_pio_get_node_rank(&node_index);
 	if (e != NULL) {
@@ -45,7 +66,7 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 	e = gfs_pio_get_node_size(&total_nodes);
-	if (total_nodes <= 0) {
+	if (e != NULL) {
 		fprintf(stderr, "%s: %s\n", program_name, e);
 		exit(1);
 	}
@@ -86,18 +107,20 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
-	e = gfs_pio_create(output, GFARM_FILE_WRONLY, 0666, &ogf);
+	e = gfs_stat(input, &gstat);
 	if (e != NULL) {
 		fprintf(stderr, "%s: node %d, cannot open %s: %s\n",
-			program_name, node_index, output, e);
+			program_name, node_index, input, e);
 		exit(1);
 	}
-	e = gfs_pio_set_view_local(ogf, GFARM_FILE_SEQUENTIAL);
-	if (e != NULL) {
-		fprintf(stderr, "%s: node %d, set_view_local(%s): %s\n",
-			program_name, node_index, output, e);
-		exit(1);
+	if (flag_preserve) {
+		mode = gstat.st_mode;
+		gtspec[0] = gstat.st_atimespec;
+		gtspec[1] = gstat.st_mtimespec;
 	}
+	else
+		mode = 0666;
+	gfs_stat_free(&gstat);
 
 	e = gfs_pio_open(input, GFARM_FILE_RDONLY, &igf);
 	if (e != NULL) {
@@ -110,6 +133,28 @@ main(int argc, char *argv[])
 		fprintf(stderr,
 			"%s: node %d, set_view_local(%s): %s\n",
 			program_name, node_index, input, e);
+		exit(1);
+	}
+
+	/* check whether the destination is a directory or not */
+	e = gfs_stat(output, &gstat);
+	if (e == NULL && GFARM_S_ISDIR(gstat.st_mode)) {
+		if (node_index == 0) {
+			fprintf(stderr, "%s: is a directory\n", output);
+		}
+		exit(1);
+	}
+
+	e = gfs_pio_create(output, GFARM_FILE_WRONLY, mode, &ogf);
+	if (e != NULL) {
+		fprintf(stderr, "%s: node %d, cannot open %s: %s\n",
+			program_name, node_index, output, e);
+		exit(1);
+	}
+	e = gfs_pio_set_view_local(ogf, GFARM_FILE_SEQUENTIAL);
+	if (e != NULL) {
+		fprintf(stderr, "%s: node %d, set_view_local(%s): %s\n",
+			program_name, node_index, output, e);
 		exit(1);
 	}
 
@@ -133,6 +178,12 @@ main(int argc, char *argv[])
 	e = gfs_pio_close(ogf);
 	if (e != NULL)
 	    fprintf(stderr, "%s: close failed: %s\n", output, e);
+
+	if (flag_preserve) {
+		e = gfs_utimes(output, gtspec);
+		if (e != NULL)
+			fprintf(stderr, "%s: utimes failed: %s\n", output, e);
+	}
 
 	e = gfarm_terminate();
 	if (e != NULL) {
