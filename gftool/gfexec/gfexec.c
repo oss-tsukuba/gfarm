b12e5057ad027568d6843b3ed0c0ad6520c5a836
Osamu Tatebe <tatebe@users.sourceforge.net>
2003-11-12 08:48:49 +0000

    save the initial argv because it may be modified in gfarm_initialize().
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@857 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfexec/gfexec.c
+++ gftool/gfexec/gfexec.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <gfarm/gfarm.h>
 
 static char progname[] = "gfexec";
@@ -11,13 +12,29 @@ static char progname[] = "gfexec";
 int
 main(int argc, char *argv[], char *envp[])
 {
-	char *e, *gfarm_url;
+	char *e, *gfarm_url, **saved_argv;
+	int i, saved_argc;
 
 	--argc, ++argv;
 	if (argc < 1) {
 		printf("usage: %s program args ...\n", progname);
 		exit(2);
 	}
+	/* save argv because it may be modified in gfarm_initialize() */
+	saved_argc = argc;
+	saved_argv = malloc(argc * sizeof(char *) + 1);
+	if (saved_argv == NULL) {
+		fprintf(stderr, "not enough memory\n");
+		exit(1);
+	}
+	for (i = 0; i < argc; ++i) {
+		saved_argv[i] = strdup(argv[i]);
+		if (saved_argv[i] == NULL) {
+			fprintf(stderr, "not enough memory\n");
+			exit(1);
+		}
+	}
+	saved_argv[i] = '\0';
 
 	e = gfarm_initialize(&argc, &argv);
 	if (e != NULL) {
@@ -30,9 +47,12 @@ main(int argc, char *argv[], char *envp[])
 		fprintf(stderr, "%s: %s\n", *argv, e);
 		exit(1);
 	}
-	e = gfs_execve(gfarm_url, argv, envp);
+	e = gfs_execve(gfarm_url, saved_argv, envp);
 	fprintf(stderr, "%s: %s\n", gfarm_url, e);
 	free(gfarm_url);
+	for (i = 0; i < saved_argc; ++i)
+		free(saved_argv[i]);
+	free(saved_argv);
 
 	e = gfarm_terminate();
 	if (e != NULL) {
