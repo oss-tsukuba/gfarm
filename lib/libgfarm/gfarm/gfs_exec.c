a5fec17e49cd287f77d91ef26249aa0ecee9bb1b
Osamu Tatebe <tatebe@users.sourceforge.net>
2003-12-04 06:35:30 +0000

    (gfs_exec) Added a fault tolerance feature in case that the program in a local spool is missing even though there is a metadata entry.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@958 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfs_exec.c
+++ lib/libgfarm/gfarm/gfs_exec.c
@@ -4,30 +4,113 @@
 
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
 #include <gfarm/gfarm.h>
 
 char *
 gfs_execve(const char *filename, char *const argv [], char *const envp[])
 {
 	char *hostname, *e;
-	char **delivered_paths;
+	char *localpath, *url, *arch, *gfarm_file;
+	struct gfs_stat gstat;
 
-	if (!gfarm_is_url(filename)) {
+	e = gfs_realpath(filename, &url);
+	if (e != NULL) {
 		execve(filename, argv, envp);
 		return gfarm_errno_to_error(errno);
 	}
+	/* check the permission */
+	e = gfs_access(url, X_OK);
+	if (e != NULL) {
+		free(url);
+		return (e);
+	}
 
-	/* XXX - If this host is an active filesystem node */
-
+	/* determine the architecture */
 	e = gfarm_host_get_canonical_self_name(&hostname);
-	if (e != NULL)
+	if (e != NULL) {
+		free(url);
 		return (e);
+	}	
+	arch = gfarm_host_info_get_architecture_by_host(hostname);
+	if (arch == NULL) {
+		free(url);
+		return ("not file system node");
+	}
 
-	e = gfarm_url_program_deliver(filename, 1, &hostname,
-				      &delivered_paths);
-	if (e != NULL)
+	/* check the metadata */
+	e = gfs_stat_section(url, arch, &gstat);
+	if (e != NULL) {
+		free(arch);
+		free(url);
 		return (e);
+	}
+	if (GFARM_S_ISDIR(gstat.st_mode)) {
+		gfs_stat_free(&gstat);
+		free(arch);
+		free(url);
+		return (GFARM_ERR_IS_A_DIRECTORY);
+	}
+	else if (!GFARM_S_ISREG(gstat.st_mode)) {
+		gfs_stat_free(&gstat);
+		free(arch);
+		free(url);
+		return ("unknown format");
+	}
+	gfs_stat_free(&gstat);
 
-	execve(delivered_paths[0], argv, envp);
+	/* determine the local pathname */
+	e = gfarm_url_make_path(url, &gfarm_file);
+	if (e != NULL) {
+		free(arch);
+		free(url);
+		return (e);
+	}
+	e = gfarm_path_localize_file_section(gfarm_file, arch, &localpath);
+	if (e != NULL) {
+		free(gfarm_file);
+		free(arch);
+		free(url);
+		return (e);
+	}
+
+	/* replicate the program if needed */
+	if (gfarm_file_section_copy_info_does_exist(
+		    gfarm_file, arch, hostname)) {
+		struct stat st;		
+		/*
+		 * FT - already exists in metadata, but need check
+		 * whether it really exists or not.
+		 */
+		if (stat(localpath, &st)) {
+			/* not exist.  delete the metadata, and try to
+			 * replicate the binary */
+			e = gfarm_file_section_copy_info_remove(
+				gfarm_file, arch, hostname);
+			if (e != NULL) {
+				free(localpath);
+				free(gfarm_file);
+				free(arch);
+				free(url);
+				return (e);
+			}
+			e = gfarm_url_section_replicate_to(url, arch, hostname);		}
+		/* the replica of the program already exists */
+		/* XXX - need integrity check */
+	}
+	else
+		e = gfarm_url_section_replicate_to(url, arch, hostname);
+
+	free(gfarm_file);
+	free(arch);
+	free(url);
+	if (e != NULL) {
+		free(localpath);
+		return (e);
+	}
+
+	execve(localpath, argv, envp);
 	return gfarm_errno_to_error(errno);
 }
