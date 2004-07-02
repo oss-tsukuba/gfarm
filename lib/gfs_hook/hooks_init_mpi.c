88ae9f6ba10f410729567f94ec073b2aeec2280f
Osamu Tatebe <tatebe@users.sourceforge.net>
2004-07-02 13:38:21 +0000

    Removed gfs_hook dependent code.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1186 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/gfs_hook/hooks_init_mpi.c
+++ lib/gfs_hook/hooks_init_mpi.c
@@ -2,11 +2,9 @@
  * $Id$
  */
 
-#include <sys/types.h>
 #include <unistd.h>
 #include <mpi.h>
 #include <gfarm/gfarm.h>
-#include "hooks_subr.h"
 
 char *
 gfs_hook_initialize(void)
@@ -17,15 +15,11 @@ gfs_hook_initialize(void)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	_gfs_hook_debug(fprintf(stderr,
-			"GFS: gfs_hook_initialize: set_local(%d, %d)\n",
-				rank, size));
-
 	e = gfarm_initialize(NULL, NULL);
 	if (e != NULL)
-		return e;
+		return (e);
 
 	gfs_pio_set_local(rank, size);
 
-	return NULL;
+	return (NULL);
 }
