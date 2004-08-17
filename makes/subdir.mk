60a487224e9cf22a5a9c123172ad6fdd67abfeda
SODA Noriyuki <n-soda@users.sourceforge.net>
2004-08-17 01:44:32 +0000

    make this work with relative srcdir
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1264 4a5e1ece-6a47-475c-b646-731d720b41d0

--- makes/subdir.mk
+++ makes/subdir.mk
@@ -14,19 +14,28 @@ post-very-clean-hook:
 post-distclean-hook:
 post-gfregister-hook:
 
+# The reason why assignments of $(top_srcdir) and $(srcdir) are needed
+# for Makefile.in case too is to prevent variable-inheritance caused by
+# non Makefile.in case.
+
 subdir-all subdir-install subdir-clean subdir-veryclean subdir-distclean subdir-gfregister subdir-man subdir-html:
 	@target=`expr $@ : 'subdir-\(.*\)'`; \
 	for dir in / $(SUBDIRS); do \
 		case $${dir} in /) continue;; esac; \
 		echo '[' making $${dir} ']'; \
+		case $(top_srcdir) in \
+		/*)	top_srcdir=$(top_srcdir); \
+			srcdir=$(srcdir)/$${dir};; \
+		*)	rel=`echo $${dir}|sed 's|[^/][^/]*|..|g'`; \
+			top_srcdir=$${rel}/$(top_srcdir); \
+			srcdir=$${rel}/$(srcdir)/$${dir};; \
+		esac; \
 		if test -f $(srcdir)/$${dir}/Makefile.in; then \
 			( cd $${dir} && \
 			  case "$(srcdir)" in \
 			  .)	$(MAKE) $${target};; \
-			  *)	$(MAKE) \
-					top_srcdir=$(top_srcdir) \
-					top_objdir=$(top_objdir) \
-					srcdir=$(srcdir)/$${dir} \
+			  *)	$(MAKE) top_srcdir=$${top_srcdir} \
+					srcdir=$${srcdir} \
 					$${target};; \
 			  esac; \
 			) || exit 1; \
@@ -35,10 +44,9 @@ subdir-all subdir-install subdir-clean subdir-veryclean subdir-distclean subdir-
 			( cd $${dir} && \
 			  case "$(srcdir)" in \
 			  .)	$(MAKE) $${target};; \
-			  *)	$(MAKE) -f $(srcdir)/$${dir}/Makefile \
-					top_srcdir=$(top_srcdir) \
-					top_objdir=$(top_objdir) \
-					srcdir=$(srcdir)/$${dir} \
+			  *)	$(MAKE) -f $${srcdir}/Makefile \
+					top_srcdir=$${top_srcdir} \
+					srcdir=$${srcdir} \
 					$${target};; \
 			  esac; \
 			) || exit 1; \
