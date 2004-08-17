77090854351c8ea3e2009b7b1d3deda369cf5f7d
SODA Noriyuki <n-soda@users.sourceforge.net>
2004-08-17 02:48:30 +0000

    provide this for scripts installation
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1272 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ makes/script.mk
@@ -0,0 +1,39 @@
+all: script-all post-all-hook
+install: all script-install post-install-hook
+clean: script-clean post-clean-hook
+veryclean: script-veryclean post-veryclean-hook
+distclean: script-distclean post-distclean-hook
+gfregister: script-gfregister post-gfregister-hook
+man: script-man post-man-hook
+html: script-html post-html-hook
+
+post-all-hook:
+post-install-hook:
+post-clean-hook:
+post-veryclean-hook:
+post-distclean-hook:
+post-gfregister-hook:
+post-man-hook:
+post-html-hook:
+
+script-all:
+
+script-install:
+	@set -x; \
+	for i in -- $(SCRIPTS); do \
+		case $$i in --) continue;; esac; \
+		$(INSTALL_SCRIPT) $$i $(bindir)/$$i; \
+	done
+
+script-clean:
+	-test -z "$(EXTRA_CLEAN_TARGETS)" || $(RM) -f $(EXTRA_CLEAN_TARGETS)
+
+script-veryclean: clean
+	-test -z "$(EXTRA_VERYCLEAN_TARGETS)" || $(RM) -f $(EXTRA_VERYCLEAN_TARGETS)
+
+script-distclean: veryclean
+	-test ! -f $(srcdir)/Makefile.in || $(RM) -f Makefile
+
+script-gfregister:
+script-man:
+script-html:
