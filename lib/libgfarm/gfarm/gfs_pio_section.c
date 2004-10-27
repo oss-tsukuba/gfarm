5366664d47acfa6a7e8bac65371d49ebf4df88ba
SODA Noriyuki <n-soda@users.sourceforge.net>
2004-10-27 13:56:27 +0000

    the hostname argument of gfs_client_connection() must be canonical hostname now.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1503 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfs_pio_section.c
+++ lib/libgfarm/gfarm/gfs_pio_section.c
@@ -553,7 +553,8 @@ gfs_pio_set_view_section(GFS_File gf, const char *section,
 	       || gf_on_demand_replication ) &&  
 	      (flags & GFARM_FILE_NOT_REPLICATE) == 0) ||
 	     (flags & GFARM_FILE_REPLICATE) != 0)) {
-		e = replicate_section_to_local(gf, vc->section, if_hostname);
+		e = replicate_section_to_local(gf, vc->section,
+		    vc->canonical_hostname);
 		/* FT - inconsistent metadata has been fixed.  try again. */
 		if (e == GFARM_ERR_INCONSISTENT_RECOVERABLE
 		    && (flags & GFARM_FILE_NOT_RETRY) == 0
