999a172a8d7d1ad80271935410940fed23ebb42b
SODA Noriyuki <n-soda@users.sourceforge.net>
2004-11-11 11:09:33 +0000

    forgot to update an argument of getopt(3)
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1569 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfreg/gfreg.c
+++ gftool/gfreg/gfreg.c
@@ -311,7 +311,7 @@ main(int argc, char *argv[])
 
 	/*  Command options  */
 
-	while ((c = getopt(argc, argv, "a:fh:D:I:N:?")) != -1) {
+	while ((c = getopt(argc, argv, "a:fh:D:I:H:N:?")) != -1) {
 		switch (c) {
 		case 'I':
 		case 'a':
