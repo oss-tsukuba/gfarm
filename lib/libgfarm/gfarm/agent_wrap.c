c3593269f0212801dd3cf1a0d5843d7dddbc96e4
Osamu Tatebe <tatebe@users.sourceforge.net>
2005-04-12 18:32:55 +0000

    use a different connection to the agent when pid is changed. cwd is expanded in a client side because it is maintained by each client.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1731 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/agent_wrap.c
+++ lib/libgfarm/gfarm/agent_wrap.c
@@ -2,10 +2,13 @@
  * $Id$
  */
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <gfarm/gfarm.h>
 #include "agent_client.h"
@@ -16,6 +19,7 @@ extern char *gfarm_client_terminate(void);
 
 static int gfarm_agent_enabled = 1;
 static struct agent_connection *agent_server;
+static pid_t agent_pid;
 
 char GFARM_AGENT_ERR_NO_AGENT[] = "no agent connection";
 
@@ -25,13 +29,22 @@ gfarm_agent_disable(void)
 	gfarm_agent_enabled = 0;
 }
 
+char *gfarm_agent_connect(char *);
+char *gfarm_agent_disconnect(void);
+
 static char *
 gfarm_agent_check(void)
 {
-	if (agent_server != NULL)
-		return (NULL);
-	else
+	if (!gfarm_agent_enabled)
 		return (GFARM_AGENT_ERR_NO_AGENT);
+
+	if (agent_server != NULL) {
+		if (agent_pid == getpid())
+			return (NULL);
+		else
+			gfarm_agent_disconnect();
+	}
+	return (gfarm_agent_connect(NULL));
 }
 
 char *
@@ -42,10 +55,16 @@ gfarm_agent_connect(char *path)
 	if (agent_server != NULL)
 		return ("already connected");
 
+	if (path == NULL)
+		path = getenv("GFARM_AGENT_SOCK");
+	if (path == NULL)
+		return (GFARM_AGENT_ERR_NO_AGENT);
+
 	memset(&peer_addr, 0, sizeof(peer_addr));
 	peer_addr.sun_family = AF_UNIX;
 	strcpy(peer_addr.sun_path, path);
 
+	agent_pid = getpid();
 	return (agent_client_connect(&peer_addr, &agent_server));
 }
 
@@ -54,26 +73,22 @@ gfarm_agent_disconnect()
 {
 	char *e;
 
-	e = gfarm_agent_check();
-	if (e != NULL)
-		return (e);
+	if (agent_server == NULL)
+		return ("already disconnected");
 
 	e = agent_client_disconnect(agent_server);
-	if (e == NULL)
+	if (e == NULL) {
 		agent_server = NULL;
+		agent_pid = 0;
+	}
 	return (e);
 }
 
 char *
 gfarm_initialize(int *argc, char ***argv)
 {
-	char *path;
-
-	if (gfarm_agent_enabled) {
-		path = getenv("GFARM_AGENT_SOCK");
-		if (path != NULL)
-			(void)gfarm_agent_connect(path);
-	}
+	if (gfarm_agent_enabled)
+		(void)gfarm_agent_connect(NULL);
 	return (gfarm_client_initialize(argc, argv));
 }
 
@@ -124,6 +139,19 @@ gfarm_path_info_remove(const char *path)
 char *
 gfs_realpath_canonical(const char *path, char **abspathp)
 {
+	char *e, cwd[PATH_MAX + 1];
+
+	/* cwd is maintained by a client */
+	if (path[0] != '/') {
+		e = gfs_getcwd(cwd, sizeof(cwd));
+		if (e != NULL)
+			return (e);
+		if (strlen(cwd) + 1 + strlen(path) + 1 > sizeof(cwd))
+			return (GFARM_ERR_NUMERICAL_RESULT_OUT_OF_RANGE);
+		strcat(cwd, "/");
+		strcat(cwd, path);
+		path = cwd;
+	}
 	if (gfarm_agent_check() == NULL)
 		return (agent_client_realpath_canonical(
 				agent_server, path, abspathp));
@@ -132,24 +160,39 @@ gfs_realpath_canonical(const char *path, char **abspathp)
 }
 
 char *
-gfs_get_ino(const char *path, long *inop)
+gfs_get_ino(const char *canonic_path, long *inop)
 {
 	char *e;
 	gfarm_int32_t ip;
 
 	if (gfarm_agent_check() == NULL) {
-		e = agent_client_get_ino(agent_server, path, &ip);
+		e = agent_client_get_ino(agent_server, canonic_path, &ip);
 		if (e == NULL)
 			*inop = (long)ip;
 		return (e);
 	}
 	else
-		return (gfs_i_get_ino(path, inop));
+		return (gfs_i_get_ino(canonic_path, inop));
 }
 
 char *
 gfs_opendir(const char *path, GFS_Dir *dirp)
 {
+	char *e, cwd[PATH_MAX + 1];
+
+	path = gfarm_url_prefix_skip(path);
+
+	/* cwd is maintained by a client */
+	if (path[0] != '/') {
+		e = gfs_getcwd(cwd, sizeof(cwd));
+		if (e != NULL)
+			return (e);
+		if (strlen(cwd) + 1 + strlen(path) + 1 > sizeof(cwd))
+			return (GFARM_ERR_NUMERICAL_RESULT_OUT_OF_RANGE);
+		strcat(cwd, "/");
+		strcat(cwd, path);
+		path = cwd;
+	}
 	if (gfarm_agent_check() == NULL)
 		return (agent_client_opendir(agent_server, path, dirp));
 	else
