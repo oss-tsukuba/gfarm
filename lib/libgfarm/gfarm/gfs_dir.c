9c17afd3375e7dc7daaaa8c16ef7a3fe780856cc
Osamu Tatebe <tatebe@users.sourceforge.net>
2005-05-03 09:11:49 +0000

    (gfs_rmdir): use gfarm_url_make_path not gfarm_url_make_path_for_creation.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1784 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfs_dir.c
+++ lib/libgfarm/gfarm/gfs_dir.c
@@ -76,10 +76,10 @@ gfs_rmdir(const char *pathname)
 	GFS_Dir dir;
 	struct gfs_dirent *entry;
 
-	e = gfarm_url_make_path_for_creation(pathname, &canonic_path);
+	e = gfarm_url_make_path(pathname, &canonic_path);
 	/* We permit missing gfarm: prefix here as a special case */
 	if (e == GFARM_ERR_GFARM_URL_PREFIX_IS_MISSING)
-		e = gfarm_canonical_path_for_creation(pathname, &canonic_path);
+		e = gfarm_canonical_path(pathname, &canonic_path);
 	if (e != NULL)
 		return (e);
 
