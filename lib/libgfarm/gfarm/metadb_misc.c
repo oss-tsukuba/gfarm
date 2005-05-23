48e12c6b16dbe075f85ddef13da5785c800285d6
Kazuaki Anami <kazuaki@soum.co.jp>
2005-05-23 03:53:31 +0000

    fixed memory leak.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@1849 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/metadb_misc.c
+++ lib/libgfarm/gfarm/metadb_misc.c
@@ -112,8 +112,10 @@ gfarm_url_fragment_number(const char *gfarm_url, int *np)
 	free(gfarm_file);
 	if (e != NULL)
 		return (e);
-	if (!GFARM_S_IS_FRAGMENTED_FILE(pi.status.st_mode))
+	if (!GFARM_S_IS_FRAGMENTED_FILE(pi.status.st_mode)) {
+		gfarm_path_info_free(&pi);
 		return (GFARM_ERR_OPERATION_NOT_PERMITTED);
+	}
 	*np = pi.status.st_nsections;
 	gfarm_path_info_free(&pi);
 	return (NULL);
