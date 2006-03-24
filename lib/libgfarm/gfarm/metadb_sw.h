36cfa811cd5e244904e997dadc8cdca1e09b695c
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-03-24 02:00:45 +0000

    add NAREGI copyright notice
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2503 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/metadb_sw.h
+++ lib/libgfarm/gfarm/metadb_sw.h
@@ -1,4 +1,18 @@
 /*
+ * Copyright (c) 2003-2006 National Institute of Advanced
+ * Industrial Science and Technology (AIST).  All rights reserved.
+ *
+ * Copyright (c) 2006 National Institute of Informatics in Japan,
+ * All rights reserved.
+ *
+ * This file or a portion of this file is licensed under the terms of
+ * the NAREGI Public License, found at
+ * http://www.naregi.org/download/index.html.
+ * If you redistribute this file, with or without modifications, you
+ * must include this notice in the file.
+ */
+
+/*
  * Metadata access switch for internal implementation
  *
  * $Id$
--- lib/libgfarm/gfarm/schedule.h
+++ lib/libgfarm/gfarm/schedule.h
@@ -1,5 +1,15 @@
 /*
+ * Copyright (c) 2003-2006 National Institute of Advanced
+ * Industrial Science and Technology (AIST).  All rights reserved.
  *
+ * Copyright (c) 2006 National Institute of Informatics in Japan,
+ * All rights reserved.
+ *
+ * This file or a portion of this file is licensed under the terms of
+ * the NAREGI Public License, found at
+ * http://www.naregi.org/download/index.html.
+ * If you redistribute this file, with or without modifications, you
+ * must include this notice in the file.
  */
 
 void gfarm_strings_expand_cyclic(int, char **, int, char **);
