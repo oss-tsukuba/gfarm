e894188fbd8f748a65ef12c8b99a0f310a26fd77
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-04-27 07:19:37 +0000

    - back out part of previous change, it was my misunderstanding. - more error check
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2568 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfutil/logutil_th.c
+++ lib/libgfarm/gfutil/logutil_th.c
@@ -7,53 +7,76 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 
 #include "gfutil.h"
 #include "logutil.h"
 
-struct gflog_specific {
+struct gflog_thread_specific {
 	char *auxiliary_info;
 };
 
-static pthread_key_t log_key;
-static pthread_once_t log_key_once = PTHREAD_ONCE_INIT;
+static pthread_key_t gflog_key;
 
 static void
-gflog_pthread_key_init(void)
+gflog_thread_fatal(const char *diag, const char *status)
 {
-	struct gflog_specific *p = malloc(sizeof(struct gflog_specific));
+	if (gflog_syslog_enabled())
+		syslog(LOG_ERR, "gfarm gflog: %s%s", diag, status);
+	else
+		fprintf(stderr, "gfarm gflog: %s%s\n", diag, status);
+	exit(2);
+}
 
-	if (p == NULL) {
-		if (gflog_syslog_enabled())
-			syslog(LOG_ERR,
-			    "gfarm gflog: cannot allocate %d bytes, aborted",
-			    sizeof(*p));
-		else
-			fprintf(stderr,
-			    "gfarm gflog: cannot allocate %d bytes, aborted\n",
-			    sizeof(*p));
-		exit(2);
-	}
+static void
+gflog_key_create(void)
+{
+	int rv = pthread_key_create(&gflog_key, free);
+
+	if (rv != 0)
+		gflog_thread_fatal("pthread_key_create(): ", strerror(rv));
+}
+
+static struct gflog_thread_specific *
+gflog_thread_specific_alloc(void)
+{
+	struct gflog_thread_specific *p;
+	int rv;
+
+	p = malloc(sizeof(*p));
+	if (p == NULL)
+		gflog_thread_fatal("cannot allocate memory", "");
 
 	/* initialization */
 	p->auxiliary_info = NULL;
 
-	pthread_key_create(&log_key, free);
-	pthread_setspecific(log_key, p);
+	rv = pthread_setspecific(gflog_key, p);
+	if (rv != 0)
+		gflog_thread_fatal("pthread_setspecific(): ", strerror(rv));
+
+	return (p);
 }
 
-static struct gflog_specific *
-gflog_pthread_getspecific(void)
+static struct gflog_thread_specific *
+gflog_thread_specific_get(void)
 {
-	pthread_once(&log_key_once, gflog_pthread_key_init);
-	return (pthread_getspecific(log_key));
+	static pthread_once_t once = PTHREAD_ONCE_INIT;
+	int rv = pthread_once(&once, gflog_key_create);
+	struct gflog_thread_specific *p;
+
+	if (rv != 0)
+		gflog_thread_fatal("pthread_once(): ", strerror(rv));
+	p = pthread_getspecific(gflog_key);
+	if (p != NULL)
+		return (p);
+	return (gflog_thread_specific_alloc());
 }
 
 char **
 gflog_log_auxiliary_info_location()
 {
-	return (&gflog_pthread_getspecific()->auxiliary_info);
+	return (&gflog_thread_specific_get()->auxiliary_info);
 }
 
 #endif
