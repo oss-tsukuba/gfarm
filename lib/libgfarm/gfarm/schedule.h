6773a7960de1a420c5bd292483c9460b554a7797
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-05-28 01:09:06 +0000

    bring latest schedule.h to v2 branch
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/branches/gfarm_v2@2611 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/schedule.h
+++ lib/libgfarm/gfarm/schedule.h
@@ -1,5 +1,34 @@
 /*
+ * Copyright (c) 2003-2006 National Institute of Advanced
+ * Industrial Science and Technology (AIST).  All rights reserved.
  *
+ * Copyright (c) 2006 National Institute of Informatics in Japan,
+ * All rights reserved.
+ *
+ * This file or a portion of this file is licensed under the terms of
+ * the NAREGI Public License, found at
+ * http://www.naregi.org/download/index.html.
+ * If you redistribute this file, with or without modifications, you
+ * must include this notice in the file.
  */
 
 void gfarm_strings_expand_cyclic(int, char **, int, char **);
+
+char *gfarm_schedule_search_idle_hosts_to_write(int, char **, int, char **);
+char *gfarm_schedule_search_idle_acyclic_hosts_to_write(
+	int, char **, int *, char **);
+char *gfarm_schedule_search_idle_by_all_to_write(int, char **);
+char *gfarm_schedule_search_idle_by_domainname_to_write(
+	const char *, int, char **);
+char *gfarm_schedule_search_idle_acyclic_by_domainname_to_write(
+	const char *, int *, char **);
+char *gfarm_schedule_search_idle_by_program_to_write(char *, int, char **);
+char *gfarm_file_section_host_schedule_to_write(char *, char *, char **);
+char *gfarm_file_section_host_schedule_by_program_to_write(
+	char *, char *, char *, char **);
+
+char *gfarm_file_section_host_schedule_with_priority_to_local_to_write(
+	char *, char *, char **);
+
+int gfarm_is_active_fsnode(void);
+int gfarm_is_active_fsnode_to_write(void);
