9c4a2fbc2f4213f2e2b99986a48f0b4b7a85286b
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-05-31 12:05:07 +0000

    initial revision
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2679 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gftool/gfhost/noopt.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. ./regress.conf
+
+hosts_meta=$localtop/RT_gfhost_hosts.$$
+not_hosts_meta=$localtop/RT_gfhost_not_hosts.$$
+
+trap 'rm -f $hosts_meta $not_hosts_meta ; exit $exit_trap' $trap_sigs
+
+if gfhost >$hosts_meta && [ -s $hosts_meta ] &&
+   gfhost -M | awk '{ print $3 }' | comm -13 - $hosts_meta >$not_hosts_meta &&
+   [ ! -s $not_hosts_meta ] 
+then
+	exit_code=$exit_pass
+fi
+
+rm -f $hosts_meta $not_hosts_meta
+exit $exit_code
