c844303b300a3f7446da90eb6dbd1e8b05727ed3
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-05-31 06:26:07 +0000

    confirm that moved directory is removable. XXX - current v2 fails in this test.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/branches/gfarm_v2@2668 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfmv/dir.sh
+++ regress/gftool/gfmv/dir.sh
@@ -3,12 +3,14 @@
 . ./regress.conf
 
 trap 'gfrmdir $gftmp/yyy $gftmp/xxx $gftmp; exit $exit_trap' $trap_sigs
-trap 'gfrmdir $gftmp/yyy $gftmp; exit $exit_code' 0
+trap 'gfrmdir $gftmp; exit $exit_code' 0
 
 if gfmkdir $gftmp &&
    gfmkdir $gftmp/xxx &&
    gfmv $gftmp/xxx $gftmp/yyy &&
    [ x"`gfls -d $gftmp/xxx`" = x"" ] &&
-   [ x"`gfls -d $gftmp/yyy`" = x"$gftmp/yyy" ]; then
+   [ x"`gfls -d $gftmp/yyy`" = x"$gftmp/yyy" ] &&
+   gfrmdir $gftmp/yyy
+then
 	exit_code=$exit_pass
 fi
