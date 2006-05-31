47bde49cae6d48e9a4ae370b0874a6c9c6b7f7be
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-05-31 06:14:39 +0000

    rename gfexport-and-cmp.sh to gfexport.sh
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/branches/gfarm_v2@2665 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfexport/0byte.sh
+++ regress/gftool/gfexport/0byte.sh
@@ -2,4 +2,4 @@
 
 . ./regress.conf
 
-$testbase/gfexport-and-cmp.sh $data/0byte
+$testbase/gfexport.sh $data/0byte
--- regress/gftool/gfexport/1byte.sh
+++ regress/gftool/gfexport/1byte.sh
@@ -2,4 +2,4 @@
 
 . ./regress.conf
 
-$testbase/gfexport-and-cmp.sh $data/1byte
+$testbase/gfexport.sh $data/1byte
