e8ae1abe723026edd442d073e54158c1c21421e0
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-05-31 05:57:01 +0000

    factoring out common code
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/branches/gfarm_v2@2663 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gftool/gfreg/gfreg.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./regress.conf
+
+case $# in
+1)	datafile=$1;;
+*)	echo "Usage: $0 <datafile>" >&2
+	exit $exit_fail;;
+esac
+
+trap 'rm -f $gftmp; exit $exit_trap' $trap_sigs
+trap 'rm -f $gftmp; exit $exit_code' 0
+
+datasize=`ls -l $datafile | awk '{print $5}'`
+
+if gfreg $datafile $gftmp && [ x"`gfls $gftmp`" = x"$gftmp" ] &&
+   [ x"`gfls -l $gftmp | awk '{print $4}'`" = x"$datasize" ]; then
+	exit_code=$exit_pass
+fi
+
