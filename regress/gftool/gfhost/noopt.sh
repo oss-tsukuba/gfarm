65305450b8e92e818c7e114e9d8ff4fbf28433fe
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-05-31 12:32:07 +0000

    output of "gfhost -M" is not guaranteed to be sorted
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2681 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfhost/noopt.sh
+++ regress/gftool/gfhost/noopt.sh
@@ -8,7 +8,8 @@ not_hosts_meta=$localtop/RT_gfhost_not_hosts.$$
 trap 'rm -f $hosts_meta $not_hosts_meta ; exit $exit_trap' $trap_sigs
 
 if gfhost >$hosts_meta && [ -s $hosts_meta ] &&
-   gfhost -M | awk '{ print $3 }' | comm -13 - $hosts_meta >$not_hosts_meta &&
+   gfhost -M | sort | awk '{ print $3 }' |
+	comm -13 - $hosts_meta >$not_hosts_meta &&
    [ ! -s $not_hosts_meta ] 
 then
 	exit_code=$exit_pass
