ba045ca71202162c46fad4d8aecb40c7eca015d6
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-06-01 08:18:07 +0000

    initial revision
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2692 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gftool/gfreg/gfreg-h.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./regress.conf
+
+trap 'gfrm $gftmp; exit $exit_trap' $trap_sigs
+
+host="`gfhost | head -1`"
+
+if gfreg -h $host $data/1byte $gftmp &&
+   gfexport $gftmp | cmp -s - $data/1byte &&
+   gfwhere $gftmp | awk '{ if ($2 == '"\"$host\""') exit 0; else exit 1 }'; then
+	exit_code=$exit_pass
+fi
+
+gfrm $gftmp
+exit $exit_code
