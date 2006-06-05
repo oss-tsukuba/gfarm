48cbbc54605616f26e7b334eca46b2d9069d24c3
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-06-05 09:13:09 +0000

    test script for "mv(rename) <source file> <dest>"
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2746 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gfs_hook/tool/mv/mv.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+. ./regress.conf
+
+srcfile=/gfarm/$USER/RT_mv_src.$$
+destfile=/gfarm/$USER/RT_mv_dest.$$
+
+case $# in
+1)	datafile=$1;;
+*)	echo "Usage: $0 <datafile>" >&2
+	exit $exit_fail;;
+esac
+
+trap 'rm -f $srcfile $destfile; exit $exit_trap' $trap_sigs
+
+datasize=`ls -l $datafile | awk '{print $5}'`
+
+if cp $datafile $srcfile && mv $srcfile $destfile &&
+   [ x"`ls $srcfile`" = x"" ] && [ x"`ls $destfile`" = x"$destfile" ] &&
+   [ x"`ls -l $destfile | awk '{print $5}'`" = x"$datasize" ]; then
+	exit_code=$exit_pass
+fi
+
+rm -f $srcfile $destfile
+exit $exit_code
