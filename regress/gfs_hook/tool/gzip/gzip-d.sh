d1ae12846c4f2b9a2b9148b8ab6dd3814aef6e15
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-06-07 13:10:36 +0000

    "gftmp" -> "hooktmp"
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2805 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gfs_hook/tool/gzip/gzip-d.sh
+++ regress/gfs_hook/tool/gzip/gzip-d.sh
@@ -4,17 +4,17 @@
 
 gunzipped_file=$localtop/RT_gzip_list.$$
 
-trap 'rm -f $gunzipped_file; gfrm -f $gftmp;  exit $exit_trap' $trap_sigs
+trap 'rm -f $gunzipped_file; gfrm -f $hooktmp;  exit $exit_trap' $trap_sigs
 
 archive="gftest-0.0.tar.gz"
 
 if gzip -cd $data/$archive >$gunzipped_file &&
-   cp $data/$archive $gftmp".gz" &&
-   gzip -d $gftmp".gz" && [ -s $gftmp ] &&
-   cmp -s $gunzipped_file $gftmp; then
+   cp $data/$archive $hooktmp".gz" &&
+   gzip -d $hooktmp".gz" && [ -s $hooktmp ] &&
+   cmp -s $gunzipped_file $hooktmp; then
 	exit_code=$exit_pass
 fi
 
 rm -f $gunzipped_file
-gfrm -f $gftmp
+gfrm -f $hooktmp
 exit $exit_code
