ebd9e4568c362464e68cb63ea6c2ac7f36dc0a34
Osamu Tatebe <tatebe@users.sourceforge.net>
2006-07-19 08:03:25 +0000

    reserve the protocol number of GFS_PROTO_READEX
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2962 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfs_proto.h
+++ lib/libgfarm/gfarm/gfs_proto.h
@@ -35,7 +35,8 @@ enum gfs_proto_command {
 	GFS_PROTO_STATFS,
 	GFS_PROTO_FSYNC,
 	GFS_PROTO_OPEN_LOCAL,
-	GFS_PROTO_FSTAT
+	GFS_PROTO_FSTAT,
+	GFS_PROTO_READEX /* reservation */
 };
 
 enum gfs_proto_error {
