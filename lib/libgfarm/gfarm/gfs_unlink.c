88c4cfb55edf464ce0c20f982bc5b46c24d6cdff
SODA Noriyuki <n-soda@users.sourceforge.net>
2006-07-24 02:34:03 +0000

    move gfarm_url_fragment_cleanup() from metadb_misc.c to gfs_unlink.c, because the latter is more appropriate place.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@2984 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfs_unlink.c
+++ lib/libgfarm/gfarm/gfs_unlink.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <openssl/evp.h>
 #include <gfarm/gfarm.h>
+
+#include "host.h"
 #include "config.h"
 #include "gfs_client.h"
 #include "gfs_pio.h"	/* gfs_profile */
@@ -436,4 +438,82 @@ gfs_unlink_every_other_replicas(const char *gfarm_file, const char *section,
 	}
 	gfarm_file_section_copy_info_free_all(ncopies, copies);
 	return (e_save);
+}
+
+char *
+gfarm_url_fragment_cleanup(char *gfarm_url, int nhosts, char **hosts)
+{
+	char *e, *gfarm_file, **canonical_hostnames;
+	int i, j, nfrags, ncopies;
+	struct gfarm_file_section_info *frags;
+	struct gfarm_file_section_copy_info *copies;
+
+	e = gfarm_url_make_path(gfarm_url, &gfarm_file);
+	if (e != NULL)
+		return (e);
+	e = gfarm_file_section_info_get_all_by_file(
+		gfarm_file, &nfrags, &frags);
+	if (e != NULL) {
+		free(gfarm_file);
+		return (NULL);
+	}
+	gfarm_file_section_info_free_all(nfrags, frags);
+	if (nfrags == nhosts) {
+		free(gfarm_file);
+		return (NULL); /* complete */
+	}
+
+	e = gfarm_host_get_canonical_names(nhosts, hosts,
+	    &canonical_hostnames);
+	if (e != NULL) {
+		free(gfarm_file);
+		return (e);
+	}
+
+	/*
+	 * do removal
+	 */
+	/* remove gfarm_file_fragment_copy_info and actual copy */
+	for (i = 0; i < nhosts; i++) {
+		char *path_section;
+		char section_string[GFARM_INT32STRLEN + 1];
+
+		sprintf(section_string, "%d", i);
+		e = gfarm_path_section(gfarm_file, section_string,
+		    &path_section);
+		if (e != NULL)
+			continue;
+		/* remove copy */
+		gfs_client_unlink_with_reconnection(canonical_hostnames[i],
+		    path_section, NULL, NULL);
+
+		e = gfarm_file_section_copy_info_get_all_by_section(
+		    gfarm_file, section_string, &ncopies, &copies);
+		if (e != NULL) {
+			free(path_section);
+			continue;
+		}
+		for (j = 0; j < ncopies; j++) {
+			/*
+			 * equivalent to
+			 * gfarm_file_section_copy_info_remove_all_by_section()
+			 */
+			gfarm_file_section_copy_info_remove(gfarm_file,
+			    section_string, copies[j].hostname);
+
+			/* remove actual copies */
+			if (strcasecmp(copies[j].hostname,
+			    canonical_hostnames[i]) == 0)
+				continue;
+			gfs_client_unlink_with_reconnection(
+			    copies[j].hostname, path_section, NULL, NULL);
+		}
+		free(path_section);
+		gfarm_file_section_copy_info_free_all(ncopies, copies);
+	}
+	gfarm_file_section_info_remove_all_by_file(gfarm_file);
+	gfarm_path_info_remove(gfarm_file);
+	gfarm_strings_free_deeply(nhosts, canonical_hostnames);
+	free(gfarm_file);
+	return (NULL);
 }
