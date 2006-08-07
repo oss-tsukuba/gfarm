32b75356458516556e51e7b334ba41e5fdef3a94
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-08-07 11:16:00 +0000

    replace "open_file_sh" with "create_file_in_mode_*_dir.sh"
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3118 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gfs_hook/feature/permission/create_file_in_mode_000_dir.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. ./regress.conf
+
+$testbase/create_file_in_mode_dir.sh 000 impossible
---
+++ regress/gfs_hook/feature/permission/create_file_in_mode_dir.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -x
+
+. ./regress.conf
+
+usage() {
+    echo "Usage: $0 <mode> <'possible' or 'impossible'>" >&2
+    exit $exit_fail
+}
+
+case $# in
+2)
+    case $2 in
+    "possible"|"impossible")
+	mode=$1; possibility=$2;;
+    *) usage;;
+    esac;;	
+*)
+    usage;; 
+esac
+
+trap 'rm -rf $hooktmp; exit $exit_trap' $trap_sigs
+
+if mkdir $hooktmp && chmod $mode $hooktmp &&
+   ([ x$possibility = x"possible" ] && touch $hooktmp/foo) ||
+   ([ x$possibility = x"impossible" ] && ! touch $hooktmp/foo 2>/dev/null)
+then
+    exit_code=$exit_pass
+fi
+
+chmod 700 $hooktmp
+rm -rf $hooktmp
+exit $exit_code
