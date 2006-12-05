5b1db84785492fc3cb1696544076db15fa25e57b
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-12-05 11:04:07 +0000

    fix the bug, removes gfarm files already exist, reported in [gfarm-developers:02487] by Yamamoto-san
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3266 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gfs_hook/tool/sh/test-d.sh
+++ regress/gfs_hook/tool/sh/test-d.sh
@@ -2,10 +2,11 @@
 
 . ./regress.conf
 
-trap 'exit $exit_trap' $trap_sigs
+trap 'rmdir $hooktmp; exit $exit_trap' $trap_sigs
 
-if [ -d $hooktop ] && rm -rf $hooktop/* && [ ! -d $hooktop/$hooktop ]; then
+if mkdir $hooktmp && [ -d $hooktmp ] && [ ! -d $hooktmp/$hooktmp ]; then
 	exit_code=$exit_pass
 fi
 
+rmdir $hooktmp
 exit $exit_code
