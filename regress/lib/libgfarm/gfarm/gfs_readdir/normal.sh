e8d9ddadeeaed6e87f0eae40450c931076a482e9
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-12-06 08:23:51 +0000

    fix the bug that returns FAIL if files exist in the gfarm top directory
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3269 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/lib/libgfarm/gfarm/gfs_readdir/normal.sh
+++ regress/lib/libgfarm/gfarm/gfs_readdir/normal.sh
@@ -2,16 +2,17 @@
 
 . ./regress.conf
 
-#trap 'gfrm -f $gftmp; exit $exit_trap' $trap_sigs
+#trap 'gfrm -rf $gftmp; exit $exit_trap' $trap_sigs
 
+if ! $testbin/../gfs_mkdir/mkdir $gftmp 0755 2>&1 1>/dev/null ||
 # 0x00000401 is GFARM_FILE_WRONLY|GFARM_FILE_TRUNC
-if ! $testbin/../gfs_pio_create/create $gftmp 0x00000401 0666; then
+    ! $testbin/../gfs_pio_create/create $gftmp/tmpfile.$$ 0x00000401 0666; then
     exit $exit_unsupported
 fi
 
-# arguments are <gfarm_url of a directory> <filename portion>
-$shell $testbase/readdir.sh `dirname $gftmp` "..."`basename $gftmp`
+# arguments are <directory> <concatinated string of filenames>
+$shell $testbase/readdir.sh $gftmp ".""..""tmpfile.$$"
 exit_code=$?
 
-gfrm $gftmp
+gfrm -r $gftmp
 exit $exit_code
