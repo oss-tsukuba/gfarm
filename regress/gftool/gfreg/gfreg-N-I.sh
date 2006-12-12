07244dff97ddded0caf7d17605c5cfda9abbeac9
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-12-12 08:44:27 +0000

    return not UNSUPPORTED but FAIL if none of the hosts is available
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3272 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfreg/gfreg-N-I.sh
+++ regress/gftool/gfreg/gfreg-N-I.sh
@@ -4,13 +4,6 @@
 
 trap 'gfrm $gftmp; exit $exit_trap' $trap_sigs
 
-if [ `gfhost -l |
-      awk '$1 != "x.xx/x.xx/x.xx" && $1 != "-.--/-.--/-.--"' |
-      wc -l` -eq 0 ]
-then
-    exit $exit_unsupported
-fi
-
 if gfreg -N 2 -I 0 $data/0byte $gftmp &&
    gfreg -N 2 -I 1 $data/1byte $gftmp &&
    gfexport $gftmp | cmp -s - $data/1byte
