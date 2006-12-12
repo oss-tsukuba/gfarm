d60005b8f49c59c6b31db465efd8ea765891c78d
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-12-12 09:03:25 +0000

    Fixed the bug that fails if all of available hosts belong to the same domain, pointed out in [gfarm-developers:02487] by Yamamoto-san.  This script specifies only the option `-D' without `-N 2', so gfrep does not replicate a file.  I'm ready to make `gfrep-D-N2_*.sh' script that gfreg replicate a file in such case.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3273 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfrep/gfrep-D.sh
+++ regress/gftool/gfrep/gfrep-D.sh
@@ -8,41 +8,46 @@ case $# in
 	exit $exit_fail;;
 esac
 
-trap 'rm -f $hosts_list; gfrm $gftmp; exit $exit_trap' $trap_sigs
+trap 'rm -f $pair_file; gfrm $gftmp; exit $exit_trap' $trap_sigs
 
-hosts_list=$localtop/RT_gfrep-D_hosts.$$
+pair_file=$localtop/RT_gfrep-D_host_and_domain.$$
 
-if ! gfhost | head -2 >$hosts_list || [ `cat $hosts_list | wc -l` -ne 2 ]; then
-    rm -f $hosts_list
-    exit $exit_unsupported
-fi
-
-pat='[^.][^.]*\.'
-
-if shost=`cat $hosts_list | sed -n '1p'` &&
-   dhost=`cat $hosts_list | sed -n '2p'` &&
-   domain=`echo $dhost | sed -n s/$pat//p` && [ -z $domain ] &&
-   shost=`cat $hosts_list | sed -n '2p'` &&
-   dhost=`cat $hosts_list | sed -n '1p'` &&
-   domain=`echo $dhost | sed -n s/$pat//p` && [ -z $domain ]; then
-    rm -f $hosts_list
-    exit $exit_unsupported
-fi
-
-if gfreg -h $shost $datafile $gftmp &&
-   gfrep -D $domain $gftmp && 
-   gfwhere $gftmp | $awk '
-      NR > 1 {
-        if ($2 == "'$shost'" && $3 == "'$dhost'"  || 
-	    $2 == "'$dhost'" && $3 == "'$shost'") 
-	   exit 0
-	else
-	   exit 1
-      }'
+# search a pair of host and domain that the host is not in the domain
+if gfhost | awk '
+    {
+	pos = index($0, ".")
+	host[pos == 0 ? "" : substr($0, pos + 1)] = $0
+    }
+    END {
+	for (domain in host) { 
+	    for (i in host) {
+		if (domain != "" && match(host[i], domain) == 0) {
+		    printf "%s %s FOUND\n", host[i], domain >"'$pair_file'"
+		    exit 0
+		}
+	    }
+	}
+	printf "%s %s NOT_FOUND\n", host[i], domain >"'$pair_file'"
+	exit 0
+    }
+' && read shost domain is_found <$pair_file
 then
+    if gfreg -h $shost $datafile $gftmp &&
+	gfrep -D $domain $gftmp &&      
+	gfwhere $gftmp | awk 'NR > 1 {
+	    if ("'$is_found'" == "NOT_FOUND" && NF == 2 && $2 == "'$shost'" ||
+		"'$is_found'" == "FOUND" && NF == 3 &&
+		    ($2 == "'$shost'" && match($3, "'$domain'") ||
+		     $3 == "'$shost'" && match($2, "'$domain'")))
+		exit 0
+	    else
+		exit 1
+	}'
+    then	
 	exit_code=$exit_pass
+    fi	
 fi
 
-rm -f $hosts_list
+rm  $pair_file
 gfrm $gftmp
 exit $exit_code
