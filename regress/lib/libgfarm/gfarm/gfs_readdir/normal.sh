1fe08ece436e6f4a1cc79d557c4e00c09e49c55f
Sotoyama Touru <sotoyama@users.sourceforge.net>
2006-12-12 08:28:31 +0000

    return not UNSUPPORTED but FAIL if make a directory or file
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3271 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/lib/libgfarm/gfarm/gfs_readdir/normal.sh
+++ regress/lib/libgfarm/gfarm/gfs_readdir/normal.sh
@@ -4,15 +4,17 @@
 
 #trap 'gfrm -rf $gftmp; exit $exit_trap' $trap_sigs
 
-if ! $testbin/../gfs_mkdir/mkdir $gftmp 0755 2>&1 1>/dev/null ||
-# 0x00000401 is GFARM_FILE_WRONLY|GFARM_FILE_TRUNC
-    ! $testbin/../gfs_pio_create/create $gftmp/tmpfile.$$ 0x00000401 0666; then
-    exit $exit_unsupported
-fi
+if $testbin/../gfs_mkdir/mkdir $gftmp 0755 2>&1 1>/dev/null &&
 
-# arguments are <directory> <concatinated string of filenames>
-$shell $testbase/readdir.sh $gftmp ".""..""tmpfile.$$"
-exit_code=$?
+    # 0x00000401 is GFARM_FILE_WRONLY|GFARM_FILE_TRUNC
+    $testbin/../gfs_pio_create/create $gftmp/tmpfile.$$ 0x00000401 0666 &&
+
+    # arguments are <directory> <concatinated string of filenames>
+    $shell $testbase/readdir.sh $gftmp ".""..""tmpfile.$$"
+
+then
+    exit_code=$?
+fi
 
 gfrm -r $gftmp
 exit $exit_code
