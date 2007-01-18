0fdf399bae74bfdb546a45366c04c3275cb885d0
SODA Noriyuki <n-soda@users.sourceforge.net>
2007-01-18 23:13:42 +0000

    tests which are known to fail with GSI
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3484 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gfs_hook/tool/scp/scp_from_gfarm.sh
+++ regress/gfs_hook/tool/scp/scp_from_gfarm.sh
@@ -13,6 +13,18 @@ elif cp $data/1byte $hooktmp &&
     scp $hooktmp $host:$localtmp &&
     ssh $host cat $localtmp | cmp -s $data/1byte -; then
     exit_code=$exit_pass
+else
+    case `gfarm.arch.guess` in
+    *-*-solaris*) # Sun SSH doesn't have this problem.
+	:;;
+    *)	case $REGRESS_AUTH in
+	gsi|gsi_auth)
+	    case $exit_code in
+	    $exit_pass) exit_code=$exit_xpass;;
+	    $exit_fail) exit_code=$exit_xfail;;
+	    esac;;
+	esac;;
+    esac
 fi
 
 rm -f $hooktmp
