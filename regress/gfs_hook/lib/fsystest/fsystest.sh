f2664777f9efd85d8693a2cd9093b12f53c87eac
Sotoyama Touru <sotoyama@users.sourceforge.net>
2007-03-05 05:27:41 +0000

    correct indent
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3597 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gfs_hook/lib/fsystest/fsystest.sh
+++ regress/gfs_hook/lib/fsystest/fsystest.sh
@@ -10,12 +10,12 @@ trap 'rm -rf $hooktmp; rm -f __testfile; exit $exit_trap' $trap_sigs
 expected=$testbase/index.`uname`.expected
 if [ ! -f $expected ]
 then
-	exit $exit_unsupported
+    exit $exit_unsupported
 fi
 if mkdir $hooktmp &&
     $testbin/fsystest $hooktmp | diff -c - $expected
 then
-	exit_code=$exit_pass
+    exit_code=$exit_pass
 fi
 
 rm -rf $hooktmp
