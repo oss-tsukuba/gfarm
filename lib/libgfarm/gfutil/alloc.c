8376241c9549f73c8b7e173e939813172080b054
SODA Noriyuki <n-soda@users.sourceforge.net>
2007-03-12 08:59:51 +0000

    merge changes in gfarm v1.x branch to v2
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/branches/gfarm_v2@3643 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ lib/libgfarm/gfutil/alloc.c
@@ -0,0 +1,63 @@
+#include <errno.h>
+#include <stdlib.h>
+
+#include <gfarm/gfarm_misc.h>
+
+size_t
+gfarm_size_add(int *overflowp, size_t a, size_t b)
+{
+	size_t sum = a + b;
+
+	if (sum < a)
+		*overflowp = 1;
+	return (sum);
+}
+
+size_t
+gfarm_size_mul(int *overflowp, size_t a, size_t b)
+{
+	size_t product = a * b;
+
+	if (b != 0 && product / b != a)
+		*overflowp = 1;
+	return (product);
+}
+
+void *
+gfarm_calloc_array(size_t number, size_t size)
+{
+	int overflow = 0;
+
+	gfarm_size_mul(&overflow, number, size);
+	if (overflow) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return (calloc(number, size));
+}
+
+void *
+gfarm_malloc_array(size_t number, size_t size)
+{
+	int overflow = 0;
+	size_t total_size = gfarm_size_mul(&overflow, number, size);
+
+	if (overflow) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return (malloc(total_size));
+}
+
+void *
+gfarm_realloc_array(void *src, size_t number, size_t size)
+{
+	int overflow = 0;
+	size_t total_size = gfarm_size_mul(&overflow, number, size);
+
+	if (overflow) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return (realloc(src, total_size));
+}
