eeab0b7d04eda0ab577148665389baf57f79b13d
SODA Noriyuki <n-soda@users.sourceforge.net>
2007-03-16 14:55:51 +0000

    This script shouldn't depend on the fact that ``cat'' command can print a file in Gfarm file system. In other words, this script shouldn't depend on gfs_hook.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3666 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/lib/libgfarm/gfarm/gfs_pio_flush/write_buffered.sh
+++ regress/lib/libgfarm/gfarm/gfs_pio_flush/write_buffered.sh
@@ -6,7 +6,7 @@ return_errmsg_file=$localtop/tst_err$$
 trap 'rm -f $return_errmsg_file; exit $exit_trap' $trap_sigs
 
 if $1 $2 $3 2>$return_errmsg_file; then
-    if [ `cat $2` = $3 ]; then
+    if [ "`gfexport $2`" = "$3" ]; then
 	exit_code=$exit_pass
     fi
 elif [ $? -eq 1 ] && echo "$4" | cmp - $return_errmsg_file; then
