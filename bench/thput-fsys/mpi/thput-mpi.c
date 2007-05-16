68bac2cb0d1a5a9a3f7ffd8ffaa0c5e79504b4b9
Osamu Tatebe <tatebe@users.sourceforge.net>
2007-05-16 16:43:47 +0000

    (README): added (thput-mpi): command-line option parsing should not be done before MPI_Init() in MPI-1.1 (thput-mpi): remove files generated by thput-mpi
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm/trunk@3737 4a5e1ece-6a47-475c-b646-731d720b41d0

--- bench/thput-fsys/mpi/thput-mpi.c
+++ bench/thput-fsys/mpi/thput-mpi.c
@@ -95,14 +95,14 @@ timerval_t tm_read_close_0, tm_read_close_1;
 char *buffer;
 
 void *
-alloc_aligned_memory(size_t size, int alignment)
+alloc_aligned_memory(size_t size, int alignment, MPI_Comm comm)
 {
 	char *p = malloc(size + alignment - 1);
 
 	if (p == NULL) {
 		fprintf(stderr, "no memory for %ld bytes\n",
 			(long)size + alignment - 1);
-		MPI_Abort(MPI_COMM_WORLD, 1);
+		MPI_Abort(comm, 1);
 	}
 	if (((long)p & (alignment - 1)) != 0)
 		p += alignment - ((long)p & (alignment - 1));
@@ -119,7 +119,7 @@ initbuffer(int buffer_size)
 }
 
 void
-writetest(char *ofile, int buffer_size, off_t file_size)
+writetest(char *ofile, int buffer_size, off_t file_size, MPI_Comm comm)
 {
 	int fd, rv;
 	off_t residual;
@@ -129,7 +129,7 @@ writetest(char *ofile, int buffer_size, off_t file_size)
 	gettimerval(&tm_write_open_1);
 	if (fd == -1) {
 		perror(ofile);
-		MPI_Abort(MPI_COMM_WORLD, 1);
+		MPI_Abort(comm, 1);
 	}
 	gettimerval(&tm_write_write_all_0);
 	for (residual = file_size; residual > 0; residual -= rv) {
@@ -168,7 +168,7 @@ writetest(char *ofile, int buffer_size, off_t file_size)
 }
 
 void
-readtest(char *ifile, int buffer_size, off_t file_size)
+readtest(char *ifile, int buffer_size, off_t file_size, MPI_Comm comm)
 {
 	int fd, rv;
 	off_t residual;
@@ -178,7 +178,7 @@ readtest(char *ifile, int buffer_size, off_t file_size)
 	gettimerval(&tm_read_open_1);
 	if (fd == -1) {
 		perror(ifile);
-		MPI_Abort(MPI_COMM_WORLD, 1);
+		MPI_Abort(comm, 1);
 	}
 	gettimerval(&tm_read_read_all_0);
 	for (residual = file_size; residual > 0; residual -= rv) {
@@ -214,7 +214,8 @@ readtest(char *ifile, int buffer_size, off_t file_size)
 }
 
 void
-copytest(char *ifile, char *ofile, int buffer_size, off_t file_size)
+copytest(char *ifile, char *ofile, int buffer_size, off_t file_size,
+	MPI_Comm comm)
 {
 	int ifd, ofd;
 	int rv, osize;
@@ -223,12 +224,12 @@ copytest(char *ifile, char *ofile, int buffer_size, off_t file_size)
 	ifd = open(ifile, O_RDONLY);
 	if (ifd == -1) {
 		perror(ifile);
-		MPI_Abort(MPI_COMM_WORLD, 1);
+		MPI_Abort(comm, 1);
 	}
 	ofd = open(ofile, O_CREAT|O_TRUNC|O_WRONLY, 0666);
 	if (ofd == -1) {
 		perror(ofile);
-		MPI_Abort(MPI_COMM_WORLD, 1);
+		MPI_Abort(comm, 1);
 	}
 	for (residual = file_size; residual > 0; residual -= rv) {
 		rv = read(ifd, buffer,
@@ -275,54 +276,71 @@ timeval_sub(struct timeval *t1, struct timeval *t2)
 #define FLAG_MEASURE_PRIMITIVES	2
 
 void
-test(int test_mode, char *file1, char *file2, int buffer_size, off_t file_size,
-     int flags)
+test_title(char *msg, int test_mode, off_t file_size, int flags)
 {
-	struct timeval t1, t2;
-	double etime, gtime;
-	char *label;
+	printf("%s with %d MB file [KByte/sec]\n", msg, (int)file_size);
+	printf("[%4s] %8s", "RANK", "bufsize");
+	if (test_mode & TESTMODE_WRITE)
+		printf(" %10s   ", "write");
+	if (test_mode & TESTMODE_READ)
+		printf(" %10s   ", "read");
+	if (test_mode & TESTMODE_COPY)
+		printf(" %10s   ", "copy");
+	printf("\n");
+	fflush(stdout);
+}
+
+void
+test(int test_mode, char *file1, char *file2, int buffer_size, off_t file_size,
+	int flags, MPI_Comm comm)
+{
+	struct timeval t1, t2, t3, t4, t5, t6;
+	double etime[3], gtime[3];
+
+	(void)unlink(file1);
+	(void)unlink(file2);
 
 	gettimeofday(&t1, NULL);
-	switch (test_mode) {
-	case TESTMODE_WRITE:
-		writetest(file1, buffer_size, file_size);
-		label = "write";
-		break;
-	case TESTMODE_READ:
-		readtest(file1, buffer_size, file_size);
-		label = "read";
-		break;
-	case TESTMODE_COPY:
-		copytest(file1, file2, buffer_size, file_size);
-		label = "copy";
-		break;
-	default:
-		fprintf(stderr, "[%03d] ??? wrong test_mode: %d\n",
-			node_index, test_mode);
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
+	/* TESTMODE_WRITE: writetest is always called. */
+	writetest(file1, buffer_size, file_size, comm);
 	gettimeofday(&t2, NULL);
+	MPI_Barrier(comm);
+	gettimeofday(&t3, NULL);
+	if (test_mode & TESTMODE_READ)
+		readtest(file1, buffer_size, file_size, comm);
+	gettimeofday(&t4, NULL);
+	MPI_Barrier(comm);
+	gettimeofday(&t5, NULL);
+	if (test_mode & TESTMODE_COPY)
+		copytest(file1, file2, buffer_size, file_size, comm);
+	gettimeofday(&t6, NULL);
 
-	etime = timeval_sub(&t2, &t1);
+	etime[0] = timeval_sub(&t2, &t1);
+	etime[1] = timeval_sub(&t4, &t3);
+	etime[2] = timeval_sub(&t6, &t5);
 
-	printf("[%03d] %lld %7d %-5s %10.0f %s\n",
-	       node_index, file_size,
-	       buffer_size, label, file_size / etime, hostname);
+	file_size /= 1024;
+	printf("[%04d] %8d %10.0f%3s",
+		node_index, buffer_size, file_size / etime[0], "");
+	if (test_mode & TESTMODE_READ)
+		printf(" %10.0f%3s", file_size / etime[1], "");
+	if (test_mode & TESTMODE_COPY)
+		printf(" %10.0f%3s", file_size / etime[2], "");
+	printf(" %s\n", hostname);
 	fflush(stdout);
 
 	if ((flags & FLAG_MEASURE_PRIMITIVES) != 0) {
-		fprintf(stderr, "[%03d] %lld %7d %-5s",
-		    node_index, file_size, buffer_size, label);
-		if (test_mode == TESTMODE_WRITE)
-			fprintf(stderr, " %11g %11g %11g %11g\n",
+		fprintf(stderr, "[%04d]", node_index);
+		if (test_mode & TESTMODE_WRITE)
+			fprintf(stderr, " write %11g %11g %11g %11g",
 			    timerval_sub(&tm_write_open_1, &tm_write_open_0),
 			    timerval_sub(&tm_write_write_1, &tm_write_write_0),
 			    timerval_sub(&tm_write_write_all_1,
 					 &tm_write_write_all_0),
 			    timerval_sub(&tm_write_close_1, &tm_write_close_0)
 			);
-		if (test_mode == TESTMODE_READ)
-			fprintf(stderr, " %11g %11g %11g %11g\n",
+		if (test_mode & TESTMODE_READ)
+			fprintf(stderr, " read %11g %11g %11g %11g",
 			    timerval_sub(&tm_read_open_1, &tm_read_open_0),
 			    timerval_sub(&tm_read_read_1, &tm_read_read_0),
 			    timerval_sub(&tm_read_read_all_1,
@@ -330,14 +348,27 @@ test(int test_mode, char *file1, char *file2, int buffer_size, off_t file_size,
 			    timerval_sub(&tm_read_close_1, &tm_read_close_0)
 			);
 		tm_write_write_measured = tm_read_read_measured = 0;
+		fprintf(stderr, "\n");
+		fflush(stderr);
 	}
 
-	MPI_Reduce(&etime, &gtime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-	if (node_index == 0)
-		printf("TOTAL %lld %7d %-5s %10.0f\n",
-		       file_size * node_size,
-		       buffer_size, label, file_size / gtime * node_size);
-	fflush(stdout);
+	MPI_Reduce(etime, gtime, 3, MPI_DOUBLE, MPI_MAX, 0, comm);
+	file_size *= node_size;
+	if (node_index == 0) {
+		test_title("TOTAL parallel I/O Bandwidth",
+			test_mode, file_size / 1024, flags);
+		printf("[ ALL] %8d %10.0f%3s",
+			buffer_size, file_size / gtime[0], "");
+		if (test_mode & TESTMODE_READ)
+			printf(" %10.0f%3s", file_size / gtime[1], "");
+		if (test_mode & TESTMODE_COPY)
+			printf(" %10.0f%3s", file_size / gtime[2], "");
+		printf("\n");
+		fflush(stdout);
+	}
+
+	(void)unlink(file1);
+	(void)unlink(file2);
 }
 
 int
@@ -352,7 +383,13 @@ main(int argc, char **argv)
 	int flags = 0;
 #define INT32STRLEN 11	/* max strlen(sprintf(s, "%d", int32)) */
 
-	while ((c = getopt(argc, argv, "b:s:wrcm")) != -1) {
+	/*
+	 * MPI_Init() is required to be called before command-line
+	 * option parsing.
+	 */
+	MPI_Init(&argc, &argv);
+
+	while ((c = getopt(argc, argv, "b:s:wrcmh?")) != -1) {
 		switch (c) {
 		case 'b':
 			buffer_size = strtol(optarg, NULL, 0);
@@ -361,18 +398,19 @@ main(int argc, char **argv)
 			file_size = strtol(optarg, NULL, 0);
 			break;
 		case 'w':
-			test_mode = TESTMODE_WRITE;
+			test_mode |= TESTMODE_WRITE;
 			break;
 		case 'r':
-			test_mode = TESTMODE_READ;
+			test_mode |= TESTMODE_READ;
 			break;
 		case 'c':
-			test_mode = TESTMODE_COPY;
+			test_mode |= TESTMODE_COPY;
 			break;
 		case 'm':
 			flags |= FLAG_MEASURE_PRIMITIVES;
 			timerval_calibrate();
 			break;
+		case 'h':
 		case '?':
 		default:
 			fprintf(stderr,
@@ -381,9 +419,9 @@ main(int argc, char **argv)
 				"options:\n"
 				"\t-b block-size\n"
 				"\t-s file-size\n"
-				"\t-w			: write test\n"
-				"\t-r			: read test\n"
-				"\t-c			: copy test\n");
+				"\t-r		: do read test additionally\n"
+				"\t-c		: do copy test additionally\n");
+			MPI_Finalize();
 			exit(1);
 		}
 	}
@@ -395,7 +433,6 @@ main(int argc, char **argv)
 	if (argc > 1)
 		file2 = argv[1];
 
-	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &node_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &node_index);
 	MPI_Get_processor_name(hostname, &hostlen);
@@ -408,12 +445,17 @@ main(int argc, char **argv)
 	sprintf(pfile1, "%s.%06d", file1, node_index);
 	sprintf(pfile2, "%s.%06d", file2, node_index);
 
-	buffer = alloc_aligned_memory(buffer_size, ALIGNMENT);
+	buffer = alloc_aligned_memory(buffer_size, ALIGNMENT, MPI_COMM_WORLD);
+
+	if (node_index == 0)
+		test_title("Individual parallel I/O Bandwidth",
+			test_mode, file_size, flags);
 
 	file_size *= 1024 * 1024;
 	initbuffer(buffer_size);
 
-	test(test_mode, pfile1, pfile2, buffer_size, file_size, flags);
+	test(test_mode, pfile1, pfile2, buffer_size, file_size, flags,
+		MPI_COMM_WORLD);
 
 	MPI_Finalize();
 	return (0);
