c8ed632d58a5d0009c3e4bbadf3ab688d8d1ca33
Osamu Tatebe <tatebe@users.sourceforge.net>
2008-07-08 23:07:42 +0000

    C++ support
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4002 4a5e1ece-6a47-475c-b646-731d720b41d0

--- include/gfarm/gfarm.h
+++ include/gfarm/gfarm.h
@@ -1,3 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <gfarm/gfarm_config.h>
 #include <gfarm/error.h>
 #include <gfarm/gfarm_misc.h>
@@ -7,3 +11,7 @@
 #include <gfarm/group_info.h>
 #include <gfarm/gfs_glob.h>
 #include <gfarm/gfs.h>
+
+#ifdef __cplusplus
+}
+#endif
