5b0323ff98d6afe00153a6df77eacbcc37502f8c
SODA Noriyuki <n-soda@users.sourceforge.net>
2008-12-22 14:55:33 +0000

    fix portability problem
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4033 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfstat/gfstat.sh
+++ regress/gftool/gfstat/gfstat.sh
@@ -5,7 +5,7 @@
 trap 'gfrm $gftmp; exit $exit_trap' $trap_sigs
 
 if gfreg $data/1byte $gftmp &&
-    [ `gfstat $gftmp | awk '/Size:/ { print $2 }'` == "1" ]; then
+    [ `gfstat $gftmp | awk '/Size:/ { print $2 }'` = "1" ]; then
     exit_code=$exit_pass
 fi
 
