1c72cf692d009194165a17539c19f9fe82caff28
Osamu Tatebe <tatebe@users.sourceforge.net>
2008-12-23 14:05:45 +0000

    gfstatus - display Gfarm file system configuration
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4046 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfstatus/gfstatus.c
+++ gftool/gfstatus/gfstatus.c
@@ -7,16 +7,15 @@
 #include <gfarm/gfarm.h>
 #include "config.h"
 #include "auth.h"
-#include "agent_wrap.h"
 
 void
-error_check(char *msg, char *e)
+error_check(char *msg, gfarm_error_t e)
 {
-	if (e == NULL)
+	if (e == GFARM_ERR_NO_ERROR)
 		return;
 
-	fprintf(stderr, "%s: %s\n", msg, e);
-	exit(1);
+	fprintf(stderr, "%s: %s\n", msg, gfarm_error_string(e));
+	exit(EXIT_FAILURE);
 }
 
 void
@@ -29,23 +28,35 @@ print_msg(char *msg, char *status)
 int
 main(int argc, char *argv[])
 {
-	char *e, *name, *arch;
+	int port;
+	char *name;
+	gfarm_error_t e;
 #ifdef HAVE_GSI
 	char *cred;
 #endif
+#if 0
+	char *arch;
 	extern int gfarm_is_active_file_system_node;
-
+#endif
 	e = gfarm_initialize(&argc, &argv);
 	error_check("gfarm_initialize", e);
 
+	print_msg("config file", gfarm_config_file);
+
+	puts("");
 	print_msg("hostname          ", gfarm_host_get_self_name());
-	e = gfarm_host_get_canonical_self_name(&name);
-	print_msg("canonical hostname", e == NULL ? name : e);
+	e = gfarm_host_get_canonical_self_name(&name, &port);
+	if (e == GFARM_ERR_NO_ERROR)
+		printf("canonical hostname: %s:%d\n", name, port);
+	else
+		printf("canonical hostname: not available\n");
+#if 0
 	e = gfarm_host_get_self_architecture(&arch);
-	print_msg("architecture name ", e == NULL ? arch : e);
+	print_msg("architecture name ",
+		  e == GFARM_ERR_NO_ERROR ? arch : gfarm_error_string(e));
 	print_msg("active fs node    ",
 		  gfarm_is_active_file_system_node ? "yes" : "no");
-
+#endif
 	puts("");
 	print_msg("global username", gfarm_get_global_username());
 	print_msg(" local username", gfarm_get_local_username());
@@ -54,48 +65,12 @@ main(int argc, char *argv[])
 	cred = gfarm_gsi_client_cred_name();
 	print_msg("credential name", cred ? cred : "no credential");
 #endif
-
-	puts("");
-	printf("gfsd server port: %d\n", gfarm_spool_server_port);
-
-	puts("");
-	e = gfarm_agent_check();
-	if (e != NULL)
-		print_msg("metadata cache server", e);
-	else {
-		print_msg("metadata cache server name",
-			  gfarm_agent_name_get());
-		printf("metadata cache server port: %d\n",
-		       gfarm_agent_port_get());
-	}
-
-	/* metadata backend database server */
-	puts("");
-	/* LDAP */
-	print_msg("LDAP server name", gfarm_ldap_server_name);
-	print_msg("LDAP server port", gfarm_ldap_server_port);
-	print_msg("LDAP base dn    ", gfarm_ldap_base_dn);
-	print_msg("LDAP bind dn    ", gfarm_ldap_bind_dn);
-	/* PGSQL */
-	print_msg("PGSQL server name", gfarm_postgresql_server_name);
-	print_msg("PGSQL server port", gfarm_postgresql_server_port);
-	print_msg("PGSQL dbname     ", gfarm_postgresql_dbname);
-	print_msg("PGSQL user       ", gfarm_postgresql_user);
-	print_msg("PGSQL connection info", gfarm_postgresql_conninfo);
-	/* LocalFS */
-	print_msg("LocalFS datadir", gfarm_localfs_datadir);
-	if (!gfarm_ldap_server_name && !gfarm_postgresql_server_name &&
-	    !gfarm_localfs_datadir)
-		print_msg("metadata backend server", "not available");
-
 	/* gfmd */
 	puts("");
-	if (gfarm_metadb_server_name) {
-		print_msg("gfmd server name", gfarm_metadb_server_name);
-		printf("gfmd server port: %d\n", gfarm_metadb_server_port);
-	}
-	else
-		print_msg("gfmd server", "not available");
+	print_msg("gfmd server name", gfarm_metadb_server_name);
+	printf("gfmd server port: %d\n", gfarm_metadb_server_port);
+	print_msg("gfmd admin user", gfarm_metadb_admin_user);
+	print_msg("gfmd admin dn  ", gfarm_metadb_admin_user_gsi_dn);
 
 	e = gfarm_terminate();
 	error_check("gfarm_terminate", e);
