oss-tsukuba/gfarm

commit 4d65c679e68b985d1e73e2f2358d8f75e805ee76

- back out the change in r4009,   by using getaddrinfo(3) and getnameinfo(3)   instead of gethostbyname(3) and getaddrbyname(3) in gfmd and most of libgfarm.   i.e. authorize peers concurrently, instead of sequentially. - add option --without-mtsafe-netdb to configure.ac for platforms which don't   have MP-safe gethostbyname(3) and getaddrbyname(3).   add this --without-mtsafe-netdb option in pkgsrc.   (XXX should determine which version it became MP-safe).
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4035 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/gfm_client.c
@@ -11,7 +11,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <time.h>
-#include "hash.h"
+
 #include <gfarm/gfarm_config.h>
 #include <gfarm/error.h>
 #include <gfarm/gfarm_misc.h>
@@ -19,6 +19,10 @@
 #include <gfarm/host_info.h>
 #include <gfarm/user_info.h>
 #include <gfarm/group_info.h>
+
+#include "hash.h"
+#include "gfnetdb.h"
+
 #include "gfp_xdr.h"
 #include "io_fd.h"
 #include "sockopt.h"
@@ -62,57 +66,45 @@ gfm_client_connection0(const char *hostname, int port,
 	struct gfm_connection *gfm_server)
 {
 	gfarm_error_t e;
-	char *host_fqdn;
 	int sock;
-	struct hostent *hp;
-	struct sockaddr_in peer_addr;
+	struct addrinfo hints, *res;
+	char sbuf[NI_MAXSERV];
 
-	hp = gethostbyname(hostname);
-	if (hp == NULL || hp->h_addrtype != AF_INET)
+	snprintf(sbuf, sizeof(sbuf), "%u", port);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_CANONNAME;
+	if (gfarm_getaddrinfo(hostname, sbuf, &hints, &res) != 0)
 		return (GFARM_ERR_UNKNOWN_HOST);
-	memset(&peer_addr, 0, sizeof(peer_addr));
-	memcpy(&peer_addr.sin_addr, hp->h_addr,
-	       sizeof(peer_addr.sin_addr));
-	peer_addr.sin_family = hp->h_addrtype;
-	peer_addr.sin_port = htons(port);
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
+	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	if (sock == -1)
 		return (gfarm_errno_to_error(errno));
 	fcntl(sock, F_SETFD, 1); /* automatically close() on exec(2) */
 
 	/* XXX - how to report setsockopt(2) failure ? */
-	gfarm_sockopt_apply_by_name_addr(sock, hp->h_name,
-	    (struct sockaddr *)&peer_addr);
+	gfarm_sockopt_apply_by_name_addr(sock,
+	    res->ai_canonname, res->ai_addr);
 
-	if (connect(sock, (struct sockaddr *)&peer_addr, sizeof(peer_addr))
-	    < 0) {
+	if (connect(sock, res->ai_addr, res->ai_addrlen) < 0) {
 		close(sock);
+		gfarm_freeaddrinfo(res);
 		return (gfarm_errno_to_error(errno));
 	}
 	e = gfp_xdr_new_socket(sock, &gfm_server->conn);
 	if (e != GFARM_ERR_NO_ERROR) {
 		close(sock);
+		gfarm_freeaddrinfo(res);
 		return (e);
-	}
-	/*
-	 * the reason why we call strdup() is because
-	 * gfarm_auth_request() may break static work area of `*hp'
-	 */
-	host_fqdn = strdup(hp->h_name);
-	if (host_fqdn == NULL) {
-		gfp_xdr_free(gfm_server->conn);
-		return (GFARM_ERR_NO_MEMORY);
 	}
 	e = gfarm_auth_request(gfm_server->conn,
-	    GFM_SERVICE_TAG, host_fqdn,
-	    (struct sockaddr *)&peer_addr, gfarm_get_auth_id_type(),
+	    GFM_SERVICE_TAG, res->ai_canonname,
+	    res->ai_addr, gfarm_get_auth_id_type(),
 	    &gfm_server->auth_method);
-	free(host_fqdn);
-	if (e != GFARM_ERR_NO_ERROR) {
+	gfarm_freeaddrinfo(res);
+	if (e != GFARM_ERR_NO_ERROR)
 		gfp_xdr_free(gfm_server->conn);
-		return (e);
-	}
 	return (GFARM_ERR_NO_ERROR);
 }
 

INSTALL.en
@@ -53,6 +53,12 @@ Options for configure:
   --with-globus-flavor=flavor option.
 
 
+  --without-mtsafe-netdb
+
+  Specifies if either getaddrinfo(3) or getnameinfo(3) library function
+  is not multi-thread safe.
+
+
   --prefix=directory
 
   Specifies a destination directory for installation.  The default is

INSTALL.ja
@@ -53,6 +53,11 @@ configure コマンドには，下記のオプションがあります．
   が，--with-globus-flavor="gcc32dbg" のようにして flavor 名を指定する
   ともできます．
 
+・--without-mtsafe-netdb
+
+  ライブラリ関数 getaddrinfo(3) あるいは getnameinfo(3) が，マルチスレッド・
+  セーフではない場合に，指定が必要です．
+
 ・--prefix=ディレクトリ
 
   インストール先のディレクトリを指定します．このオプションを省略した場

configure.ac
@@ -548,6 +548,20 @@ AC_ARG_WITH(gfarmd_username,
 [gfarmd_username="${withval}"], [gfarmd_username='nobody'])
 AC_SUBST(gfarmd_username)
 
+###
+### --without-mtsafe-netdb ... are getaddrinfo(3) and getnameinfo(3) MT-Safe?
+###
+
+AC_ARG_WITH(mtsafe_netdb,
+[  --without-mtsafe-netdb	getaddrinfo(3) and getnameinfo(3) are MT-Safe?
+				[[default=yes]]],
+[mtsafe_netdb="${withval}"], [mtsafe_netdb="yes"])
+
+case ${mtsafe_netdb} in
+no) :;;
+*)  AC_DEFINE(HAVE_MTSAFE_NETDB, 1, [getaddrinfo(3) and getnameinfo(3) are MT-Safe]);;
+esac
+
 ######
 ###### Checks for libraries.
 ######
@@ -597,7 +611,7 @@ yes)
         pthread_attr_setstacksize(&at, 64 * 1024);
     ], [
       AC_MSG_RESULT([yes])
-      AC_DEFINE(HAVE_PTHREAD_ATTR_SETSTACKSIZE, 1, [pthread_attr_setstacksize()  exists])
+      AC_DEFINE(HAVE_PTHREAD_ATTR_SETSTACKSIZE, 1, [pthread_attr_setstacksize() exists])
     ], [
       AC_MSG_RESULT([no])
     ])
@@ -1089,7 +1103,7 @@ fi
 ###### Checks for library functions.
 ######
 
-AC_CHECK_FUNCS(getdents fdatasync fdopendir poll pread pwrite snprintf gethostbyname2 getpassphrase mkdtemp setlogin strtoll strtoq setrlimit daemon getloadavg statvfs statfs)
+AC_CHECK_FUNCS(getdents fdatasync fdopendir poll pread pwrite snprintf getpassphrase mkdtemp setlogin strtoll strtoq setrlimit daemon getloadavg statvfs statfs)
 
 ### SUSv2 getpwnam_r() has 5 args, but Solaris's only has 4.
 

lib/libgfarm/gfarm/host.c
@@ -6,6 +6,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -24,6 +25,9 @@
 #include <netdb.h>
 
 #include <gfarm/gfarm.h>
+
+#include "gfnetdb.h"
+
 #include "hostspec.h"
 #include "host.h"
 
@@ -42,6 +46,15 @@ gfarm_host_info_get_by_if_hostname(const char *if_hostname,
 	if (e == GFARM_ERR_NO_ERROR)
 		return (GFARM_ERR_NO_ERROR);
 
+	/* XXX should not use gethostbyname(3), but ...*/
+	/*
+	 * This interface is never called from gfmd,
+	 * so MPSAFE-ness is not an issue for now.
+	 * Unlike gethostbyname(), getaddrinfo() doesn't return multiple
+	 * host aliases, so we cannot use getaddrino() here to see alias names.
+	 *
+	 * FIXME: This design must be revised, when we support IPv6.
+	 */
 	hp = gethostbyname(if_hostname);
 	if (hp == NULL || hp->h_addrtype != AF_INET)
 		return (GFARM_ERR_UNKNOWN_HOST);
@@ -453,29 +466,57 @@ gfarm_host_address_use(struct gfarm_hostspec *hsp)
 	return (GFARM_ERR_NO_ERROR);
 }
 
+static int
+always_match(struct gfarm_hostspec *hostspecp,
+	const char *name, struct sockaddr *addr)
+{
+	return (1);
+}
+
+/* XXX should try to connect all IP addresses. i.e. this interface is wrong. */
 static gfarm_error_t
 host_address_get(const char *name, int port,
+	int (*match)(struct gfarm_hostspec *, const char *, struct sockaddr *),
+	struct gfarm_hostspec *hostspec,
 	struct sockaddr *peer_addr, char **if_hostnamep)
 {
-	/* sizeof(struct sockaddr_in) == sizeof(struct sockaddr) */
-	struct sockaddr_in *peer_addr_in = (struct sockaddr_in *)peer_addr;
-	struct hostent *hp = gethostbyname(name);
-	char *n;
+	struct addrinfo hints, *res, *res0;
+	int error;
+	char *n, sbuf[NI_MAXSERV];
 
-	if (hp == NULL || hp->h_addrtype != AF_INET)
+	snprintf(sbuf, sizeof(sbuf), "%u", port);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM; /* XXX maybe used for SOCK_DGRAM */
+	error = gfarm_getaddrinfo(name, sbuf, &hints, &res0);
+	if (error != 0)
 		return (GFARM_ERR_UNKNOWN_HOST);
-	if (if_hostnamep != NULL) {
-		n = strdup(name); /* XXX - or strdup(hp->h_name)? */
-		if (n == NULL)
-			return (GFARM_ERR_NO_MEMORY);
-		*if_hostnamep = n;
+
+	for (res = res0; res != NULL; res = res->ai_next) {
+		if ((*match)(hostspec, name, res->ai_addr)) {
+			/* to be sure */
+			if (res0->ai_addr->sa_family != AF_INET ||
+			    res0->ai_addrlen > sizeof(*peer_addr)) {
+				gfarm_freeaddrinfo(res0);
+				return (GFARM_ERR_ADDRESS_FAMILY_NOT_SUPPORTED_BY_PROTOCOL_FAMILY);
+			}
+			if (if_hostnamep != NULL) {
+				/* XXX - or strdup(res0->ai_canonname)? */
+				n = strdup(name); 
+				if (n == NULL) {
+					gfarm_freeaddrinfo(res0);
+					return (GFARM_ERR_NO_MEMORY);
+				}
+				*if_hostnamep = n;
+			}
+			memset(peer_addr, 0, sizeof(*peer_addr));
+			memcpy(peer_addr, res0->ai_addr, sizeof(*peer_addr));
+			gfarm_freeaddrinfo(res0);
+			return (GFARM_ERR_NO_ERROR);
+		}
 	}
-	memset(peer_addr_in, 0, sizeof(*peer_addr_in));
-	peer_addr_in->sin_port = htons(port);
-	peer_addr_in->sin_family = hp->h_addrtype;
-	memcpy(&peer_addr_in->sin_addr, hp->h_addr,
-	    sizeof(peer_addr_in->sin_addr));
-	return (GFARM_ERR_NO_ERROR);
+	gfarm_freeaddrinfo(res0);
+	return (GFARM_ERR_NO_SUCH_OBJECT);
 }
 
 static gfarm_error_t
@@ -483,36 +524,9 @@ host_address_get_matched(const char *name, int port,
 	struct gfarm_hostspec *hostspec,
 	struct sockaddr *peer_addr, char **if_hostnamep)
 {
-	/* sizeof(struct sockaddr_in) == sizeof(struct sockaddr) */
-	struct sockaddr_in *peer_addr_in = (struct sockaddr_in *)peer_addr;
-	struct hostent *hp;
-	char *n;
-	int i;
-
-	if (hostspec == NULL)
-		return (host_address_get(name, port, peer_addr, if_hostnamep));
-
-	hp = gethostbyname(name);
-	if (hp == NULL || hp->h_addrtype != AF_INET)
-		return (GFARM_ERR_UNKNOWN_HOST);
-	memset(peer_addr_in, 0, sizeof(*peer_addr_in));
-	peer_addr_in->sin_port = htons(port);
-	peer_addr_in->sin_family = hp->h_addrtype;
-	for (i = 0; hp->h_addr_list[i] != NULL; i++) {
-		memcpy(&peer_addr_in->sin_addr, hp->h_addr_list[i],
-		    sizeof(peer_addr_in->sin_addr));
-		if (gfarm_hostspec_match(hostspec, name, peer_addr)) {
-			if (if_hostnamep != NULL) {
-				/* XXX - or strdup(hp->h_name)? */
-				n = strdup(name); 
-				if (n == NULL)
-					return (GFARM_ERR_NO_MEMORY);
-				*if_hostnamep = n;
-			}
-			return (GFARM_ERR_NO_ERROR);
-		}
-	}
-	return (GFARM_ERR_NO_SUCH_OBJECT);
+	return (host_address_get(name, port,
+	    hostspec == NULL ? always_match: gfarm_hostspec_match, hostspec,
+	    peer_addr, if_hostnamep));
 }
 
 static gfarm_error_t

lib/libgfarm/gfarm/hostspec.c
@@ -261,44 +261,37 @@ gfarm_hostspec_match(struct gfarm_hostspec *hostspecp,
 gfarm_error_t
 gfarm_sockaddr_to_name(struct sockaddr *addr, char **namep)
 {
-	int i, addrlen, addrfamily = addr->sa_family;
-	char *addrp, *name;
-	struct hostent *hp;
+	struct addrinfo hints, *res, *res0;
+	struct sockaddr_in *sin1, *sin2;
+	char *s, name[NI_MAXHOST];
 
-	switch (addrfamily) {
-	case AF_INET:
-		addrp = (char *)&((struct sockaddr_in *)addr)->sin_addr;
-		addrlen = sizeof(struct in_addr);
-		break;
-	default:
-		return (gfarm_errno_to_error(EAFNOSUPPORT));
-	}
-	hp = gethostbyaddr(addrp, addrlen, addrfamily);
-	if (hp == NULL)
+	if (gfarm_getnameinfo(addr, sizeof(*addr), name, sizeof(name),
+	    NULL, 0, NI_NAMEREQD) != 0)
 		return(GFARM_ERR_CANNOT_RESOLVE_AN_IP_ADDRESS_INTO_A_HOSTNAME);
-	name = strdup(hp->h_name);
-	if (name == NULL)
-		return (GFARM_ERR_NO_MEMORY);
-#ifdef HAVE_GETHOSTBYNAME2
-	hp = gethostbyname2(name, addrfamily);
-#else
-	hp = gethostbyname(name);
-#endif
-	if (hp == NULL) {
-		free(name);
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = addr->sa_family;
+	if (gfarm_getaddrinfo(name, NULL, &hints, &res0) != 0)
 		return (GFARM_ERRMSG_REVERSE_LOOKUP_NAME_IS_NOT_RESOLVABLE);
-	}
-	if (hp->h_addrtype != addrfamily || hp->h_length != addrlen) {
-		free(name);
-		return (GFARM_ERRMSG_INVALID_NAME_RECORD);
-	}
-	for (i = 0; hp->h_addr_list[i] != NULL; i++) {
-		if (memcmp(hp->h_addr_list[i], addrp, addrlen) == 0) {
-			*namep = name;
-			return (GFARM_ERR_NO_ERROR); /* success */
+	for (res = res0; res; res = res->ai_next) {
+		if (res->ai_family =! addr->sa_family)
+			continue;
+		switch (res->ai_family) {
+		case AF_INET:
+			sin1 = (struct sockaddr_in *)res->ai_addr;
+			sin2 = (struct sockaddr_in *)addr;
+			if (sin1->sin_addr.s_addr == sin2->sin_addr.s_addr) {
+				s = strdup(name);
+				gfarm_freeaddrinfo(res0);
+				if (s == NULL)
+					return (GFARM_ERR_NO_MEMORY);
+				*namep = s;
+				return (GFARM_ERR_NO_ERROR); /* success */
+			}
+			break;
 		}
 	}
-	free(name);
+	gfarm_freeaddrinfo(res0);
 	return (GFARM_ERRMSG_REVERSE_LOOKUP_NAME_DOES_NOT_MATCH);
 }
 

lib/libgfarm/gfsl/server.c
@@ -17,6 +17,7 @@
 #include <gfarm/gfarm_config.h>
 #include <gfarm/error.h>
 
+#include "gfnetdb.h"
 #include "gfutil.h"
 
 #include "tcputil.h"
@@ -137,7 +138,7 @@ main(argc, argv)
      */
     while (1) {
 	pid_t pid;
-	struct hostent *hptr;
+	char hbuf[NI_MAXHOST];
 
 	fd = accept(bindFd, (struct sockaddr *)&remote, &remLen);
 	if (fd < 0) {
@@ -145,10 +146,9 @@ main(argc, argv)
 	    (void)close(bindFd);
 	    return 1;
 	}
-	hptr = gethostbyaddr(
-		&remote.sin_addr, sizeof(remote.sin_addr), AF_INET);
-	if (hptr != NULL)
-	    fprintf(stderr, "Connected from %s\n", hptr->h_name);
+	if (gfarm_getnameinfo((struct sockaddr *)&remote, remLen,
+			      hbuf, sizeof(hbuf), NULL, 0, 0) == 0)
+	    fprintf(stderr, "Connected from %s\n", hbuf);
 	pid = fork();
 	if (pid < 0) {
 	    (void)close(fd);

lib/libgfarm/gfsl/tcputil.c
@@ -20,6 +20,7 @@
 
 #include <gfarm/error.h>
 
+#include "gfnetdb.h"
 #include "gfutil.h"
 
 #include "tcputil.h"
@@ -29,80 +30,52 @@
 #define	GFARM_OCTETS_PER_32BIT	4	/* 32/8 */
 #define	GFARM_OCTETS_PER_16BIT	2	/* 16/8 */
 
-static int	isNonBlock(int fd);
-
-static int
-isNonBlock(fd)
-     int fd;
-{
-    int stat = fcntl(fd, F_GETFL, 0);
-    if (stat < 0) {
-	gflog_error("fcntl: %s", strerror(errno));
-	return 0;
-    } else {
-	if (stat & O_NONBLOCK) {
-	    return 1;
-	} else {
-	    return 0;
-	}
-    }
-}
-
-
 int
 gfarmTCPConnectPortByHost(host, port)
      char *host;
      int port;
 {
-    struct hostent *h;
-    struct sockaddr_in sin;
-    int sock;
+    struct addrinfo hints, *res, *res0;
+    int sock, error, rv;
+    char sbuf[NI_MAXSERV];
 
-    /* XXX - thread unsafe */
-    h = gethostbyname(host);
-    if (h == NULL) {
-	gflog_warning("gethostbyname: %s: %s", host, hstrerror(h_errno));
+    snprintf(sbuf, sizeof(sbuf), "%u", port);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    error = gfarm_getaddrinfo(host, sbuf, &hints, &res0);
+    if (error != 0) {
+	gflog_warning("getaddrinfo: %s: %s", host, gai_strerror(error));
 	return (-1);
     }
-    if (h->h_addrtype != AF_INET) {
-	gflog_warning("gethostbyname: %s: unsupported address family", host);
-	return (-1);
-    }
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = h->h_addrtype;
-    memcpy(&sin.sin_addr, h->h_addr, sizeof(sin.sin_addr));
-    sin.sin_port = htons(port);
-
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-	gflog_error("socket: %s", strerror(errno));
-	return (-1);
-    }
-    fcntl(sock, F_SETFD, 1); /* automatically close() on exec(2) */
-
-    /* XXX - set socket options */
-
-    ReConnect:
-    errno = 0;
-    if (connect(sock, (struct sockaddr *)&sin, sizeof(sin)) != 0) {
-	int saved_errno = errno;
-
-	if (saved_errno == EINTR) {
-	    goto ReConnect;
-	} else if (saved_errno == EINPROGRESS) {
-	    if (isNonBlock(sock) == 0) {
-		goto Error;
-	    } else {
-		/* XXX - should wait explicitly instead of sleep blindly */
-		sleep(1);
-		return (sock);
-	    }
-	} else {
-	    Error:
-	    close(sock);
-	    gflog_error("connect: %s", strerror(saved_errno));
-	    return (-1);
+    sock = -1;
+    for (res = res0; res != NULL; res = res->ai_next) {
+	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sock < 0) {
+	    gflog_error("socket(%d, %d, %d): %s\n", (int)res->ai_family,
+		(int)res->ai_socktype, (int)res->ai_protocol, strerror(errno));
+	    continue;
 	}
+
+	while ((rv = connect(sock, res->ai_addr, res->ai_addrlen)) < 0 &&
+	       errno == EINTR)
+		;
+	if (rv < 0) {
+	    gflog_error("connect: %s\n", strerror(errno));
+	    close(sock);
+	    sock = -1;
+	    continue;
+	}
+
+	/* connected */
+
+	fcntl(sock, F_SETFD, 1); /* automatically close() on exec(2) */
+
+	/* XXX - set socket options */
+
+	break;
     }
+    gfarm_freeaddrinfo(res0);
     return (sock);
 }
 
@@ -110,38 +83,41 @@ int
 gfarmTCPBindPort(port)
      int port;
 {
-    struct sockaddr_in sin;
-    int sock;
+    struct addrinfo hints, *res;
+    int sock, e;
     int one = 1;
+    char sbuf[NI_MAXSERV];
 
-    memset((void *)&sin, 0, sizeof(struct sockaddr_in));
-
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-	gflog_error("socket: %s", strerror(errno));
+    snprintf(sbuf, sizeof(sbuf), "%u", port);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    e = gfarm_getaddrinfo(NULL, sbuf, &hints, &res);
+    if (e) {
+	glog_error("getaddrinfo(port = %u): %s\n", port, gai_strerror(e));
 	return -1;
     }
+    if (res == NULL)
+	return -1;
 
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons((unsigned short)port);
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void *)&one, sizeof(int)) != 0) {
+    sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock < 0) {
+	gflog_error("socket: %s", strerror(errno));
+    } else if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void *)&one, sizeof(int)) != 0) {
 	gflog_error("setsockopt: %s", strerror(errno));
 	close(sock);
-	return -1;
-    }
-    
-    if (bind(sock, (struct sockaddr *)&sin, sizeof(sin)) != 0) {
+	sock = -1;
+    } else if (bind(sock, res->ai_addr, res->ai_addrlen) < 0) {
 	gflog_error("bind: %s", strerror(errno));
 	close(sock);
-	return -1;
-    }
-
-    if (listen(sock, MAX_BACKLOG) != 0) {
+	sock = -1;
+    } else if (listen(sock, MAX_BACKLOG) != 0) {
 	gflog_error("listen: %s", strerror(errno));
 	close(sock);
-	return -1;
+	sock = -1;
     }
+    gfarm_freeaddrinfo(res);
 
     return sock;
 }
@@ -153,20 +129,22 @@ gfarmGetPeernameOfSocket(sock, portPtr, hostPtr)
      int *portPtr;
      char **hostPtr;
 {
-    struct sockaddr_in sin;
+    struct sockaddr sin;
     socklen_t slen = sizeof(sin);
-    struct hostent *hptr;
+    char hbuf[NI_MAXHOST];
 
     if (getpeername(sock, (struct sockaddr *)&sin, &slen) != 0) {
 	gflog_error("getpeername: %s", strerror(errno));
 	return (-1);
     }
+    if (hostPtr != NULL) {
+	if (gfarm_getnameinfo((struct sockaddr *)&sin, sin.sin_len,
+			      hbuf, sizeof(hbuf), NULL, 0, 0) != 0)
+	    rerturn (-1);
+	*hostPtr = strdup(hbuf);
+    }
     if (portPtr != NULL)
 	*portPtr = (int)ntohs(sin.sin_port);
-    if (hostPtr != NULL &&
-	(hptr = gethostbyaddr(&sin.sin_addr, sizeof(sin.sin_addr), AF_INET))
-	!= NULL)
-	*hostPtr = strdup(hptr->h_name);
     return (0);
 }
 

commit 1260f53b27f01657f3f7a556783c45609dda44dc

fix a bug such that authentication failure is ignored, which is introduced by the previous commit r4035
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4049 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/gfm_client.c
@@ -105,7 +105,7 @@ gfm_client_connection0(const char *hostname, int port,
 	gfarm_freeaddrinfo(res);
 	if (e != GFARM_ERR_NO_ERROR)
 		gfp_xdr_free(gfm_server->conn);
-	return (GFARM_ERR_NO_ERROR);
+	return (e);
 }
 
 gfarm_error_t