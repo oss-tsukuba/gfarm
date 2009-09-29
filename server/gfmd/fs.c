446117dc4765304f77c1c92f7776e9bb5cf0cc14
Osamu Tatebe <tatebe@users.sourceforge.net>
2009-09-29 03:04:06 +0000

    fix access control problem of chmod. require root privilege not admin privilege
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4261 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/fs.c
+++ server/gfmd/fs.c
@@ -694,7 +694,7 @@ gfm_server_fchown(struct peer *peer, int from_client, int skip)
 	else if (*groupname != '\0' &&
 	    (new_group = group_lookup(groupname)) == NULL)
 		e = GFARM_ERR_NO_SUCH_GROUP;
-	else if (new_user != NULL && !user_is_admin(user))
+	else if (new_user != NULL && !user_is_root(user))
 		e = GFARM_ERR_OPERATION_NOT_PERMITTED;
 	else if (new_group != NULL && !user_is_root(user) &&
 	    (user != inode_get_user(inode) || !user_in_group(user, new_group)))
