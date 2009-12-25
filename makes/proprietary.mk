899d6ad1549113db072b8740a15ab237de3c8c35
Hirano Motono <m-hirano@sra.co.jp>
2009-12-25 04:56:04 +0000

    o Refine a mechanism for proprietary source potion protection build.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4298 4a5e1ece-6a47-475c-b646-731d720b41d0

--- makes/proprietary.mk
+++ makes/proprietary.mk
@@ -1,13 +1,38 @@
-prop-link:
+PROPDIR = ./proprietary
+
+prop-dir-link:
 	if test "x$(PROPROOTDIR)" != "x"; then \
 		tsDir=`(cd $(top_srcdir); /bin/pwd)` ; \
 		crDir=`/bin/pwd` ; \
 		tDir=`echo $${crDir} | sed "s:$${tsDir}::"` ; \
-		$(RM) -f ./proprietary ; \
-		ln -s $(PROPROOTDIR)$${tDir} ./proprietary ; \
+		$(RM) -f $(PROPDIR) ; \
+		ln -s $(PROPROOTDIR)$${tDir} $(PROPDIR) ; \
 	fi
 
-prop-unlink:
-	if test -L ./proprietary; then \
-		$(RM) -f ./proprietary ; \
+prop-dir-unlink: prop-src-unlink
+	if test -L $(PROPDIR); then \
+		$(RM) -f $(PROPDIR) ; \
 	fi
+
+prop-src-link: prop-dir-link
+	if test "x$(PROPRIETARY_SRCS)" != "x"; then \
+		for i in $(PROPRIETARY_SRCS); do \
+			if test ! -r ./$${i}; then \
+				if test -r $(PROPDIR)/$${i}; then \
+					ln -s $(PROPDIR)/$${i} . ; \
+				fi ; \
+			fi ; \
+		done ; \
+	fi
+
+prop-src-unlink:
+	if test "x$(PROPRIETARY_SRCS)" != "x"; then \
+		for i in $(PROPRIETARY_SRCS); do \
+			if test -L ./$${i}; then \
+				$(RM) ./$${i} ; \
+			fi ; \
+		done ; \
+	fi
+
+prop-initialize:	prop-src-link
+prop-finalize:		prop-dir-unlink
