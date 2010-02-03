5da0f75fc6ff9f5c85ffa846fde3fb7f14ad3a5c
OKUMA Koji <koji.okuma@gmail.com>
2010-02-03 02:27:03 +0000

    fixed a bug introduced in r4303. - gflog_fatal() and gflog_fatal_errno() does not exit.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4359 4a5e1ece-6a47-475c-b646-731d720b41d0

--- include/gfarm/gflog.h
+++ include/gfarm/gflog.h
@@ -50,8 +50,9 @@ void gflog_message_errno(int, int, const char *, int, const char*,
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
 
 #define gflog_fatal(msg_no, ...) \
-	gflog_message(msg_no, LOG_ERR,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
+	do { gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__); \
+	  exit(2); } while (0)
 #define gflog_auth_error(msg_no, ...)\
 	gflog_message(msg_no, LOG_ERR,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
@@ -59,8 +60,9 @@ void gflog_message_errno(int, int, const char *, int, const char*,
 	gflog_message(msg_no, LOG_WARNING,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
 #define gflog_fatal_errno(msg_no, ...) \
-	gflog_message_errno(msg_no, LOG_ERR,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
+	do { gflog_message_errno(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__); \
+	  exit(2); } while (0)
 #define gflog_warning_errno(msg_no, ...) \
 	gflog_message_errno(msg_no, LOG_WARNING,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
