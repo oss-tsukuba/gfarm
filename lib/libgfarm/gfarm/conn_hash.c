oss-tsukuba/gfarm

commit 96288d78c707acba3df058e7648e3e5f00073175

add diagnostic debug log at error.
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4418 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/conn_hash.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include <gfarm/gfarm_config.h>
+#include <gfarm/gflog.h>
 #include <gfarm/error.h>
 #include <gfarm/gfarm_misc.h>
 
@@ -67,8 +68,13 @@ gfp_conn_hash_table_init(
 
 	hashtab = gfarm_hash_table_alloc(hashtabsize,
 	    gfp_conn_hash_index, gfp_conn_hash_equal);
-	if (hashtab == NULL)
+	if (hashtab == NULL) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+			"allocation of hashtable(%d) failed: %s",
+			hashtabsize,
+			gfarm_error_string(GFARM_ERR_NO_MEMORY));
 		return (GFARM_ERR_NO_MEMORY);
+	}
 	*hashtabp = hashtab;
 	return (GFARM_ERR_NO_ERROR);
 }
@@ -86,16 +92,26 @@ gfp_conn_hash_enter(struct gfarm_hash_table **hashtabp, int hashtabsize,
 
 	if (*hashtabp == NULL &&
 	    (e = gfp_conn_hash_table_init(hashtabp, hashtabsize))
-	    != GFARM_ERR_NO_ERROR)
+	    != GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+			"initialization of connection hashtable (%d) failed: %s",
+			hashtabsize,
+			gfarm_error_string(e));
 		return (e);
+	}
 
 	id.hostname = (char *)hostname; /* UNCONST */
 	id.port = port;
 	id.username = (char *)username; /* UNCONST */
 	entry = gfarm_hash_enter(*hashtabp, &id, sizeof(id), entrysize,
 	    &created);
-	if (entry == NULL)
+	if (entry == NULL) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+			"insertion to hashtable (%zd) failed: %s",
+			entrysize,
+			gfarm_error_string(GFARM_ERR_NO_MEMORY));
 		return (GFARM_ERR_NO_MEMORY);
+	}
 
 	if (created) {
 		idp = gfarm_hash_entry_key(entry);
@@ -109,6 +125,9 @@ gfp_conn_hash_enter(struct gfarm_hash_table **hashtabp, int hashtabsize,
 			idp->hostname = (char *)hostname; /* UNCONST */
 			idp->username = (char *)username; /* UNCONST */
 			gfarm_hash_purge(*hashtabp, &id, sizeof(id));
+			gflog_debug(GFARM_MSG_UNFIXED,
+				"allocation of hostname or username failed: %s",
+				gfarm_error_string(GFARM_ERR_NO_MEMORY));
 			return (GFARM_ERR_NO_MEMORY);
 		}
 	}
@@ -129,15 +148,25 @@ gfp_conn_hash_lookup(struct gfarm_hash_table **hashtabp, int hashtabsize,
 
 	if (*hashtabp == NULL &&
 	    (e = gfp_conn_hash_table_init(hashtabp, hashtabsize)) !=
-	    GFARM_ERR_NO_ERROR)
+	    GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+			"initialization of connection hashtable (%d) failed: %s",
+			hashtabsize,
+			gfarm_error_string(e));
 		return (e);
+	}
 
 	id.hostname = (char *)hostname; /* UNCONST */
 	id.port = port;
 	id.username = (char *)username; /* UNCONST */
 	entry = gfarm_hash_lookup(*hashtabp, &id, sizeof(id));
-	if (entry == NULL)
+	if (entry == NULL) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+			"lookup in hashtable (%s)(%d)(%s) failed: %s",
+			hostname, port, username,
+			gfarm_error_string(e));
 		return (GFARM_ERR_NO_SUCH_OBJECT);
+	}
 
 	*entry_ret = entry;
 	return (GFARM_ERR_NO_ERROR);

commit 2a908bfbc87a8cf7ea0d753f5a65b75bb4ae09cd

fix a bug which refers uninitialized variable, introduced in r4418, found by gcc.
this part of the change is suspicous, because hash lookup failure is not
an error at all, and may impact performance, but that's a different problem
and this commit doesn't touch that point.


git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4419 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/conn_hash.c
@@ -162,9 +162,8 @@ gfp_conn_hash_lookup(struct gfarm_hash_table **hashtabp, int hashtabsize,
 	entry = gfarm_hash_lookup(*hashtabp, &id, sizeof(id));
 	if (entry == NULL) {
 		gflog_debug(GFARM_MSG_UNFIXED,
-			"lookup in hashtable (%s)(%d)(%s) failed: %s",
-			hostname, port, username,
-			gfarm_error_string(e));
+			"lookup in hashtable (%s)(%d)(%s) failed",
+			hostname, port, username);
 		return (GFARM_ERR_NO_SUCH_OBJECT);
 	}
 