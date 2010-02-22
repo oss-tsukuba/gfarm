05244e886c39431e182f85a97b40a48107f1b81f
SODA Noriyuki <n-soda@users.sourceforge.net>
2010-02-22 07:00:39 +0000

    checks whether gfs_replicate_file_to_request() correctly returns GFARM_ERR_FILE_BUSY or not.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4450 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/lib/libgfarm/gfarm/gfs_replicate_file_from_to_request/file_busy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. ./regress.conf
+
+trap 'gfrm $gftmp; rm -f $localtmp exit $exit_trap' $trap_sigs
+
+GFARM_FILE_RDWR=2	# from <gfarm/gfs.h>
+
+gfhost >$localtmp
+
+if [ `sed 2q $localtmp | wc -l` -ne 2 ]; then
+    rm -f $localtmp
+    exit $exit_unsupported
+fi
+srchost=`sed -n 1p $localtmp`
+dsthost=`sed -n 2p $localtmp`
+
+if gfreg -h $srchost $data/1byte $gftmp &&
+   $testbin/file_busy $gftmp $dsthost 0 2>&1 | fgrep 'file busy'
+then
+    exit_code=$exit_pass
+fi
+
+gfrm $gftmp
+rm -f $localtmp
+exit $exit_code
