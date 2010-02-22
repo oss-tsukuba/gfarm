313eaf68aa89bdaaa7ecae965025d749e918e76c
SODA Noriyuki <n-soda@users.sourceforge.net>
2010-02-22 07:37:06 +0000

    command argument was missing
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4453 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/lib/libgfarm/gfarm/gfs_replicate_file_from_to_request/file_busy.sh
+++ regress/lib/libgfarm/gfarm/gfs_replicate_file_from_to_request/file_busy.sh
@@ -16,7 +16,8 @@ srchost=`sed -n 1p $localtmp`
 dsthost=`sed -n 2p $localtmp`
 
 if gfreg -h $srchost $data/1byte $gftmp &&
-   $testbin/file_busy $gftmp $dsthost 0 2>&1 | fgrep 'file busy'
+   $testbin/file_busy $gftmp $GFARM_FILE_RDWR $dsthost 0 2>&1 |
+	fgrep 'file busy' >/dev/null
 then
     exit_code=$exit_pass
 fi
