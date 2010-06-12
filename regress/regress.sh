1dee6b23e049de1e0d0ef1d6942063e2d21b22d0
Osamu Tatebe <tatebe@users.sourceforge.net>
2010-06-12 17:25:08 +0000

    merge r4692, r4693 and r4694  * remove debug output (-x option of sh)  * bring some of regression test enhancements from    source:gfarm/trunk/regress/account.sh@3581   * count number of $exit_xpass, $exit_xfail, $exit_unresolved, $exit_untested,     $exit_unsupported and $exit_trap, as well as $exit_pass and $exit_fail.   * record start time and end time of each test   * slightly modify output format  * bring a regression test enhancement from    source:gfarm/trunk/regress/account.sh@3581 :   * treat UNRESOLVED as an error
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.3.2@4735 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/regress.sh
+++ regress/regress.sh
@@ -2,6 +2,12 @@
 
 . ./regress.conf
 
+# constants
+account_bgfmt="--- %-60.60s %s\n"
+account_lgfmt="@:= %-60.60s %s\n"
+account_fmt="%-60.60s ... "
+account_fin="--- ------------------------------------------------------------ ----"
+
 log=log
 rm -f $log
 
@@ -10,10 +16,14 @@ case $# in
 *)	schedule=$*;;
 esac
 
-killed=0
-pass=0
-fail=0
-skip=0
+n_pass=0
+n_fail=0
+n_xpass=0
+n_xfail=0
+n_unresolved=0
+n_untested=0
+n_unsupported=0
+n_trap=0
 
 while read line; do
 	set x $line
@@ -22,50 +32,87 @@ while read line; do
 	case $1 in '#'*) continue;; esac
 
 	tst=$1
-	msg="`echo $tst | awk '{printf "%-50.50s ... ", $1}'`"
-	fin="-------------------------------------------------- --- ----"
+
+	printf -- "$account_fmt"   "$tst"
+	printf -- "$account_bgfmt" "$tst" "BEGIN" >>$log
+	date +'@@_ start at %s - %Y-%m-%d %H:%M:%S' >>$log
+
 	if [ -x $regress/$tst ]; then
-		echo "$msg BEGIN" >>$log
 
 		sh $regress/$tst >>$log 2>&1
 		exit_code=$?
 
 		case $exit_code in
 		$exit_pass)
-			echo "$msg PASS"
-			echo "$msg PASS" >>$log
-			pass=`expr $pass + 1`;;
+			echo                              "PASS"
+			printf -- "$account_lgfmt" "$tst" "PASS" >>$log
+			n_pass=`expr $n_pass + 1`;;
 		$exit_fail)
-			echo "$msg FAIL"
-			echo "$msg FAIL" >>$log
-			fail=`expr $fail + 1`;;
-		*)	case $exit_code in
-			$exit_trap)
-				echo "$msg KILLED"
-				echo "$msg KILLED" >>$log;;
-			*)	echo "$msg exit code = $exit_code"
-				echo "$msg exit code = $exit_code" >>$log;;
-			esac
-			killed=1
+			echo                              "FAIL"
+			printf -- "$account_lgfmt" "$tst" "FAIL" >>$log
+			n_fail=`expr $n_fail + 1`;;
+		$exit_xpass)
+			echo                              "XPASS"
+			printf -- "$account_lgfmt" "$tst" "XPASS" >>$log
+			n_xpass=`expr $n_xpass + 1`;;
+		$exit_xfail)
+			echo                              "XFAIL"
+			printf -- "$account_lgfmt" "$tst" "XFAIL" >>$log
+			n_xfail=`expr $n_xfail + 1`;;
+		$exit_unresolved)
+			echo                              "UNRESOLVED"
+			printf -- "$account_lgfmt" "$tst" "UNRESOLVED" >>$log
+			n_unresolved=`expr $n_unresolved + 1`;;
+		$exit_untested)
+			echo                              "UNTESTED"
+			printf -- "$account_lgfmt" "$tst" "UNTESTED" >>$log
+			n_untested=`expr $n_untested + 1`;;
+		$exit_unsupported)
+			echo                              "UNSUPPORTED"
+			printf -- "$account_lgfmt" "$tst" "UNSUPPORTED" >>$log
+			n_unsupported=`expr $n_unsupported + 1`;;
+		$exit_trap)
+			echo                              "KILLED"
+			printf -- "$account_lgfmt" "$tst" "KILLED" >>$log
+			n_trap=`expr $n_trap + 1`
+			break;;
+		*)
+			echo                              "exit($exit_code)"
+			printf -- "$account_lgfmt" "$tst" "exit($exit_code)" >>$log
+			n_trap=`expr $n_trap + 1`
 			break;;
 		esac
-		echo "$fin" >>$log
 	else
-		echo "$msg SKIPPED" >>$log
-		skip=`expr $skip + 1`
+			echo                              "SKIPPED"
+			printf -- "$account_lgfmt" "$tst" "SKIPPED" >>$log
+			n_untested=`expr $n_untested + 1`
 	fi
+
+	date +'@@~  end  at %s - %Y-%m-%d %H:%M:%S' >>$log
+	echo $account_fin >>$log
 	
 done < $schedule
 
 echo ""
-echo "Total test: `expr $pass + $fail`"
-echo "  success : $pass"
-echo "  failure : $fail"
+echo "Total test: `expr $n_pass + $n_fail`"
+echo "  success            : $n_pass"
+echo "  failure            : $n_fail"
 
-if [ $skip != 0 ]; then
-echo ""
-echo "  skipped : $skip"
+if [ $n_xpass -gt 0 ]; then
+echo "  unexpected success : $n_xpass"
+fi
+if [ $n_xfail -gt 0 ]; then
+echo "  expected failure   : $n_xfail"
+fi
+if [ $n_unresolved -gt 0 ]; then
+echo "  unresolved         : $n_unresolved"
+fi
+if [ $n_untested -gt 0 ]; then
+echo "  untested           : $n_untested"
+fi
+if [ $n_unsupported -gt 0 ]; then
+echo "  unsupported        : $n_unsupported"
 fi
 
-case $killed in 1) exit $exit_trap;; esac
-[ $fail = 0 ]
+case $n_trap in 0) :;; *) exit $exit_trap;; esac
+[ $n_fail -eq 0 -a $n_unresolved -eq 0 ]
