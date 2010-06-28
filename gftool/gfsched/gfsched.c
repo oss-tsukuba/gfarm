cb131e154ad9bb49c07140e9db6ced9a684d991e
SODA Noriyuki <n-soda@users.sourceforge.net>
2010-06-28 03:45:31 +0000

    re-implement the gfsched command. now, filesystem node status from gfmd POV can be seen by: 	gfsched -M
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4799 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfsched/gfsched.c
+++ gftool/gfsched/gfsched.c
@@ -2,20 +2,25 @@
  * $Id$
  */
 
-#include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <libgen.h>
-#include <sys/types.h>
+#include <string.h>
+#include <limits.h>
+
 #include <gfarm/gfarm.h>
 
+#include "gfm_client.h"
+#include "gfm_schedule.h"
 #include "schedule.h"
 
 /*
  *  Create a hostfile.
  *
- *  gfsched <gfarm_url> [<output_hostfile>]
- *  gfsched -f <gfarm_file> [<output_hostfile>]
+ *  gfsched [-P <path>] [-D <domain>] [-n <number>] [-w]
+ *  gfsched  -f <file>  [-D <domain>] [-n <number>] [-w]
  */
 
 char *program_name = "gfsched";
@@ -23,218 +28,175 @@ char *program_name = "gfsched";
 void
 usage(void)
 {
-    fprintf(stderr, "usage:\t%s [options] gfarm_url [output_hostfile]\n",
+	fprintf(stderr, 
+	    "Usage:\t%s [-P <path>] [-D <domain>] [-n <number>] [-Mhlw]\n",
 	    program_name);
-    fprintf(stderr,
-	    "\t%s -N <number> [-p <gfarm_url>] [<host>...]\n",
+	fprintf(stderr,
+	          "\t%s  -f <file>  [-D <domain>] [-n <number>] [-Mhlw]\n",
 	    program_name);
-    fprintf(stderr, "options:\n");
-    fprintf(stderr, "\t-I <fragment>\tschedules a node"
-	    " using the specified fragment.\n");
-    fprintf(stderr, "\t-p <gfarm_url>\tprogram URL"
-	    " to process the file.\n");
-    fprintf(stderr, "\t-w\t\twrite mode\n");
-    fflush(stderr);
+	fprintf(stderr,
+	    "options:\n");
+	fprintf(stderr, "\t-w\t\twrite mode\n");
+	exit(2);
+}
+
+long
+parse_opt_long(char *option, int option_char, char *argument_name)
+{
+	long value;
+	char *s;
+
+	errno = 0;
+	value = strtol(option, &s, 0);
+	if (s == option) {
+		fprintf(stderr, "%s: missing %s after -%c\n",
+		    program_name, argument_name, option_char);
+		usage();
+	} else if (*s != '\0') {
+		fprintf(stderr, "%s: garbage in -%c %s\n",
+		    program_name, option_char, option);
+		usage();
+	} else if (errno != 0 && (value == LONG_MIN || value == LONG_MAX)) {
+		fprintf(stderr, "%s: %s with -%c %s\n",
+		    program_name, strerror(errno), option_char, option);
+		usage();
+	}
+	return (value);
 }
 
 int
-main(int argc, char * argv[])
+main(int argc, char **argv)
 {
-    char *gfarm_url = NULL;
-    char *section = NULL;
-    char *program = NULL;
-    int write_mode = 0;
-    char *e = NULL;
-    FILE *outp = stdout;
-    int errflg = 0;
-    extern int optind;
-    int nhosts = -1;
-    char **hosts;
-    char *gfarm_file;
-    char *host;
-    int c, i;
+	gfarm_error_t e;
+	char *opt_domain = "";
+	char *opt_mount_point = NULL;
+	char *opt_file = NULL;
+	int opt_metadata_only = 0;
+	int opt_long_format = 0;
+	int opt_nhosts = 0;
+	int opt_write_mode = 0;
+	int c, i, available_nhosts, nhosts, *ports;
+	struct gfarm_host_sched_info *available_hosts;
+	char *path, **hosts;
 
-    if (argc >= 1)
-	program_name = basename(argv[0]);
+	if (argc >= 1)
+		program_name = basename(argv[0]);
 
-    e = gfarm_initialize(&argc, &argv);
-    if (e != NULL) {
-	fprintf(stderr, "%s: %s\n", program_name, e);
-	exit(1);
-    }
-
-    while ((c = getopt(argc, argv, "I:N:Up:w?")) != EOF) {
-	switch (c) {
-	case 'I':
-	    section = optarg;
-	    break;
-	case 'N':
-	    nhosts = atoi(optarg);
-	    break;
-	case 'U':
-	    gfarm_schedule_search_mode_use_loadavg();
-	    break;
-	case 'p':
-	    program = optarg;
-	    if (!gfarm_is_url(program)) {
-		fprintf(stderr, "%s: only gfarm-URL can be specified"
-			" as a parameter of -p option\n", program_name);
-		exit(2);
-	    }
-	    break;
-	case 'w':
-	    write_mode = 1;
-	    break;
-	case '?':
-	default:
-	    ++errflg;
-	}
-    }
-    if (errflg) {
-	usage();
-	exit(2);
-    }
-    if (section != NULL && nhosts != -1) {
-	fprintf(stderr, "%s: -I and -N option cannot be specified at once.\n",
-		program_name);
-	exit(2);
-    }
-
-    if (nhosts == -1) {
-	if (optind < argc) {
-	    gfarm_url = argv[optind];
-	}
-	else {
-	    usage();
-	    exit(2);
-	}
-	++optind;
-	if (optind < argc) {
-	    outp = fopen(argv[optind], "w");
-	    if (outp == NULL) {
-		perror(argv[optind]);
+	e = gfarm_initialize(&argc, &argv);
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: %s\n", program_name,
+		    gfarm_error_string(e));
 		exit(1);
-	    }
 	}
-    }
 
-    if (nhosts != -1) {
-	GFARM_MALLOC_ARRAY(hosts, nhosts);
-	if (hosts == NULL) {
-	    fprintf(stderr, "%s: %s\n", program_name, GFARM_ERR_NO_MEMORY);
-	    exit(2);
-	}
-	if (optind < argc) {
-	    for (i = optind; i < argc; i++) {
-		e = gfarm_host_get_canonical_name(argv[i], &argv[i]);
-		if (e != NULL) {
-		    fprintf(stderr, "%s: %s: %s\n", program_name, argv[i], e);
-		    exit(2);
+	while ((c = getopt(argc, argv, "D:LMP:f:ln:w")) != -1) {
+		switch (c) {
+		case 'D':
+			opt_domain = optarg;
+			break;
+		case 'L':
+			gfarm_schedule_search_mode_use_loadavg();
+			break;
+		case 'M':
+			opt_metadata_only = 1;
+			break;
+		case 'P':
+			opt_mount_point = optarg;
+			break;
+		case 'f':
+			opt_file = optarg;
+			break;
+		case 'l':
+			opt_long_format = 1;
+			break;
+		case 'n':
+			opt_nhosts = parse_opt_long(optarg, c, "<nhosts>");
+			if (opt_nhosts <= 0) {
+				fprintf(stderr, "%s: invalid value: -%c %d\n",
+				    program_name, c, opt_nhosts);
+				usage();
+			}
+			break;
+		case 'w':
+			opt_write_mode = 1;
+			break;
+		default:
+			usage();
 		}
-	    }
-	    if (program != NULL) {
-		e = "currently, hosts cannot be specified, "
-		    "if both -N and -p option are used.";
-	    }
-	    else {
-		if (write_mode)
-		    e = gfarm_schedule_search_idle_hosts_to_write(
-			argc - optind, &argv[optind],
-			nhosts, hosts);
-		else
-		    e = gfarm_schedule_search_idle_hosts(
-			argc - optind, &argv[optind],
-			nhosts, hosts);
-	    }
 	}
-	else {
-	    if (program != NULL) {
-		if (write_mode)
-		    e = gfarm_schedule_search_idle_by_program_to_write(program,
-							      nhosts, hosts);
-		else
-		    e = gfarm_schedule_search_idle_by_program(program,
-							      nhosts, hosts);
-	    }
-	    else {
-		if (write_mode)
-		    e = gfarm_schedule_search_idle_by_all_to_write(
-			nhosts, hosts);
-		else
-		    e = gfarm_schedule_search_idle_by_all(nhosts, hosts);
-	    }
-	}
-	if (e != NULL) {
-	    fprintf(stderr, "%s: %s\n", program_name, e);
-	    exit(2);
-	}
-	for (i = 0; i < nhosts; ++i)
-	    printf("%s\n", hosts[i]);
-    }
-    else if (section == NULL) {
-	if (write_mode) {
-	    fprintf(stderr, "-w option isn't supported with this operation\n");
-	    exit(1);
+	argc -= optind;
+	argv += optind;
+	if (argc != 0)
+		usage();
+
+	if (opt_mount_point != NULL && opt_file != NULL) {
+		fprintf(stderr,
+		    "%s: -P and -f option cannot be specified at once.\n",
+		    program_name);
+		usage();
 	}
 
-	/* file-affinity scheduling */
-	if (program != NULL) {
-	    e = gfarm_url_hosts_schedule_by_program(gfarm_url, program, NULL,
-						    &nhosts, &hosts);
+	if (opt_file != NULL) {
+		path = opt_file;
+		e = gfarm_schedule_hosts_domain_by_file(path,
+		    opt_write_mode ? GFARM_FILE_RDWR : GFARM_FILE_RDONLY,
+		    opt_domain,
+		    &available_nhosts, &available_hosts);
+	} else {
+		path = opt_mount_point == NULL ? "/" : opt_mount_point;
+		e = gfarm_schedule_hosts_domain_all(path, opt_domain,
+		    &available_nhosts, &available_hosts);
 	}
-	else {
-	    e = gfarm_url_hosts_schedule(gfarm_url, NULL,
-					 &nhosts, &hosts);
-	}
-	if (e != NULL) {
-	    fprintf(stderr, "%s: %s\n", program_name, e);
-	    exit(1);
-	}
-
-	for (i = 0; i < nhosts; ++i)
-	    fprintf(outp, "%s\n", hosts[i]);
-    }
-    else {
-	/* schedule a node using the specific file fragment. */
-	e = gfarm_url_make_path(gfarm_url, &gfarm_file);
-	if (e != NULL) {
-	    fprintf(stderr, "%s: %s\n", program_name, e);
-	    exit(1);
-	}
-	if (program != NULL) {
-	    if (write_mode)
-		e = gfarm_file_section_host_schedule_by_program_to_write(
-		    gfarm_file, section, program, &host);
-	    else
-		e = gfarm_file_section_host_schedule_by_program(
-		    gfarm_file, section, program, &host);
-	}
-	else {
-	    if (write_mode)
-		e = gfarm_file_section_host_schedule_to_write(
-		    gfarm_file, section, &host);
-	    else
-		e = gfarm_file_section_host_schedule(
-		    gfarm_file, section, &host);
-	}
-	if (e != NULL) {
-	    fprintf(stderr, "%s: %s:%s: %s\n", program_name,
-		    gfarm_url, section, e);
-	    exit(1);
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: metadata scheduling: %s\n",
+		    program_name, gfarm_error_string(e));
+		exit(1);
 	}
 
-	fprintf(outp, "%s\n", host);
-	free(host);
-	free(gfarm_file);
-    }
+	nhosts = opt_nhosts > 0 ? opt_nhosts : available_nhosts;
+	GFARM_MALLOC_ARRAY(hosts, nhosts);
+	GFARM_MALLOC_ARRAY(ports, nhosts);
+	if (hosts == NULL || ports == NULL) {
+		fprintf(stderr, "%s: cannot allocate memory for %d hosts.\n",
+		    program_name, nhosts);
+		exit(1);
+	}
 
-    fclose(outp);
+	if (opt_metadata_only) {
+		if (nhosts > available_nhosts)
+			nhosts = available_nhosts;
+		for (i = 0; i < nhosts; i++) {
+			hosts[i] = available_hosts[i].host;
+			ports[i] = available_hosts[i].port;
+		}
+	} else if (opt_write_mode) {
+		e = gfarm_schedule_hosts_acyclic_to_write(path,
+		    available_nhosts, available_hosts,
+		    &nhosts, hosts, ports);
+	} else {
+		e = gfarm_schedule_hosts_acyclic(path,
+		    available_nhosts, available_hosts,
+		    &nhosts, hosts, ports);
+	}
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: client side scheduling: %s\n",
+		    program_name, gfarm_error_string(e));
+		exit(1);
+	}
 
-    e = gfarm_terminate();
-    if (e != NULL) {
-	fprintf(stderr, "%s: %s\n", program_name, e);
-	exit(1);
-    }
+	for (i = 0; i < nhosts; i++) {
+		printf("%s", hosts[i]);
+		if (opt_long_format)
+			printf("\t%d", ports[i]);
+		putchar('\n');
+	}
 
-    exit(0);
+	e = gfarm_terminate();
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: %s\n",
+		    program_name, gfarm_error_string(e));
+		exit(1);
+	}
+
+	exit(0);
 }
