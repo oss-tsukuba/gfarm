da177091d4d63c3d31d27e760c90fe3c3c6609c9
SODA Noriyuki <n-soda@users.sourceforge.net>
2010-07-30 11:54:40 +0000

    fix typo in usage: delimter vs delim"i"ter
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4868 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gffindxmlattr/gffindxmlattr.c
+++ gftool/gffindxmlattr/gffindxmlattr.c
@@ -70,7 +70,7 @@ usage(char *prog_name)
 			"{expr | -f expr-file-path} path\n",
 		prog_name);
 	fprintf(stderr, "\t-d\tsearch directory depth (>= 0)\n");
-	fprintf(stderr, "\t-F\tdelimter of path and attrname (default is TAB)\n");
+	fprintf(stderr, "\t-F\tdelimiter of path and attrname (default is TAB)\n");
 	fprintf(stderr, "\t-f\tXPath script filename\n");
 	exit(2);
 }
