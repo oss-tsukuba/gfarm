0fb798e74f8ff472d9cc6e10bf49a610374b4a03
SODA Noriyuki <n-soda@users.sourceforge.net>
2011-06-28 06:07:42 +0000

    <libgen.h> is necessary to use basename(3), this problem was found by the following warning from gcc-3.3.3 on NetBSD: {{{ warning: implicit declaration of function `basename' }}}
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5396 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfjournal/gfjournal.c
+++ gftool/gfjournal/gfjournal.c
@@ -2,6 +2,7 @@
  * $Id$
  */
 
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- gftool/gfmdhost/gfmdhost.c
+++ gftool/gfmdhost/gfmdhost.c
@@ -4,6 +4,7 @@
 
 #include <assert.h>
 #include <ctype.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
