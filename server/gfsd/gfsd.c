oss-tsukuba/gfarm

commit c30b928fda5979168427847b353bce206cad9668

merge r4759 - (gfsd) if input/output error occurs, kill oneself [sf.net trac #85]
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4900 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfsd/gfsd.c
@@ -218,6 +218,8 @@ cleanup_handler(int signo)
 	_exit(2);
 }
 
+static int kill_master_gfsd;
+
 static void fatal_full(int, const char *, int, const char *,
 		const char *, ...) GFLOG_PRINTF_ARG(5, 6);
 static void
@@ -231,7 +233,7 @@ fatal_full(int msg_no, const char *file, int line_no, const char *func,
 	va_end(ap);
 
 	cleanup(0);
-	if (getpid() == back_channel_gfsd_pid) {
+	if (getpid() == back_channel_gfsd_pid || kill_master_gfsd) {
 		/*
 		 * send terminate signal to the master process.
 		 * this should be done at the end of fatal(),
@@ -301,7 +303,6 @@ accepting_fatal_errno_full(int msg_no, const char *file, int line_no,
 	accepting_fatal_full(msg_no, file, line_no, func, "%s: %s", buffer,
 			strerror(save_errno));
 }
-
 
 static int
 fd_send_message(int fd, void *buf, size_t size, int fdc, int *fdv)
@@ -400,6 +401,13 @@ gfs_server_put_reply_common(struct gfp_xdr *client, const char *diag,
 	if (e != GFARM_ERR_NO_ERROR)
 		fatal(GFARM_MSG_1000459, "%s put reply: %s",
 		    diag, gfarm_error_string(e));
+
+	/* if input/output error occurs, die */
+	if (ecode == GFARM_ERR_INPUT_OUTPUT) {
+		kill_master_gfsd = 1;
+		fatal(GFARM_MSG_UNFIXED, "%s: %s, die", diag,
+		    gfarm_error_string(e));
+	}
 }
 
 void

commit 2b07574c0445d49de4755a36e25eb22038d6a1ef

display correct error message when gfsd dies due to input/output error, which is introduced by r4900.
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5429 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfsd/gfsd.c
@@ -416,7 +416,7 @@ gfs_server_put_reply_common(struct gfp_xdr *client, const char *diag,
 	if (ecode == GFARM_ERR_INPUT_OUTPUT) {
 		kill_master_gfsd = 1;
 		fatal(GFARM_MSG_1002513, "%s: %s, die", diag,
-		    gfarm_error_string(e));
+		    gfarm_error_string(ecode));
 	}
 }
 