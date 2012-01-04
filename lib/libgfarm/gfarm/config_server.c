12db7499f1a798597c60170f28f91a84d7bd55c1
Osamu Tatebe <tatebe@users.sourceforge.net>
2012-01-04 12:49:04 +0000

    improve debug message
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5644 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/config_server.c
+++ lib/libgfarm/gfarm/config_server.c
@@ -42,7 +42,7 @@ gfarm_server_config_read(void)
 	}
 	e = gfarm_config_read_file(config, &lineno);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000014, "%s: %d: %s",
+		gflog_error(GFARM_MSG_1000014, "%s: line %d: %s",
 		    gfarm_config_file, lineno, gfarm_error_string(e));
 		return (e);
 	}
