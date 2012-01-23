oss-tsukuba/gfarm

commit a0bb81e805f09e4dd0454143ed041ce21a86b321

Use the new 'makes/build-script' utility to generate 'config-gfarm', 'config-gfarm-update', 'config-gsd' and  'gfdump.postgresql' scripts from '*.in' template files.
Developers don't have to execute 'configure' to update the scripts
any longer.  Type 'make' instead.



git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5520 4a5e1ece-6a47-475c-b646-731d720b41d0

makes/script.mk
@@ -20,11 +20,11 @@ post-gfregister-hook:
 post-man-hook:
 post-html-hook:
 
-script-all:
+script-all: $(BUILT_SCRIPTS)
 
 script-install:
 	@$(MKDIR_P) $(DESTDIR)$(bindir)
-	@for i in -- $(SCRIPTS); do \
+	@for i in -- $(SCRIPTS) $(BUILT_SCRIPTS); do \
 		case $$i in --) continue;; esac; \
 		echo \
 		$(INSTALL_SCRIPT) $$i $(DESTDIR)$(bindir)/`basename $$i`; \
@@ -33,6 +33,7 @@ script-install:
 
 script-clean:
 	-test -z "$(EXTRA_CLEAN_TARGETS)" || $(RM) -f $(EXTRA_CLEAN_TARGETS)
+	-$(RM) -f $(BUILT_SCRIPTS)
 
 script-veryclean: clean private-finalize
 	-test -z "$(EXTRA_VERYCLEAN_TARGETS)" || $(RM) -f $(EXTRA_VERYCLEAN_TARGETS)

configure.ac
@@ -1412,10 +1412,8 @@ test -d gfptool || mkdir gfptool
 test -d package || mkdir package
 test -d util || mkdir util
 
-AC_CONFIG_FILES(Makefile makes/config.mk makes/install-doc
+AC_CONFIG_FILES(Makefile makes/config.mk makes/install-doc makes/build-script
 	${ns_makefiles}
-	gftool/config-gfarm/config-gfarm gftool/config-gfarm/config-gfsd
-	gftool/config-gfarm/config-gfarm-update gftool/gfdump/gfdump.postgresql
 	package/solaris/gfmd package/solaris/gfsd
 )
 AC_OUTPUT

gftool/config-gfarm/Makefile
@@ -12,9 +12,8 @@ datadir=$(default_datadir)/gfarm/config
 
 PRIVATE_SRCS = $(PRIVATE_GFTOOL_CONFIG_GFARM_SRCS)
 
-SCRIPTS = config-gfarm config-gfarm-update config-gfsd \
-	$(srcdir)/gfarm.arch.guess \
-	$(optional_srcs)
+SCRIPTS = $(srcdir)/gfarm.arch.guess $(optional_srcs)
+BUILT_SCRIPTS = config-gfarm config-gfarm-update config-gfsd
 
 DATA =	$(srcdir)/config-gfarm.postgresql \
 	$(srcdir)/config-gfarm.ldap \
@@ -34,3 +33,12 @@ DATA =	$(srcdir)/config-gfarm.postgresql \
 include $(top_srcdir)/makes/script.mk
 include $(top_srcdir)/makes/data.mk
 include $(top_srcdir)/makes/subdir.mk
+
+config-gfarm: $(srcdir)/config-gfarm.in
+	$(SHELL) $(top_builddir)/makes/build-script $? $@
+
+config-gfarm-update: $(srcdir)/config-gfarm-update.in
+	$(SHELL) $(top_builddir)/makes/build-script $? $@
+
+config-gfsd: $(srcdir)/config-gfsd.in
+	$(SHELL) $(top_builddir)/makes/build-script $? $@

gftool/gfdump/Makefile
@@ -6,6 +6,9 @@ srcdir = .
 
 include $(top_srcdir)/makes/var.mk
 
-SCRIPTS = gfdump.postgresql
+BUILT_SCRIPTS = gfdump.postgresql
 
 include $(top_srcdir)/makes/script.mk
+
+gfdump.postgresql: $(srcdir)/gfdump.postgresql.in
+	$(SHELL) $(top_builddir)/makes/build-script $? $@

makes/build-script.in
@@ -1,0 +1,41 @@
+#!/bin/sh
+
+case $# in
+2)	:;;
+*)	echo "Usage: $0 <source_file> <destination_file>" >&2
+	exit 1;;
+esac
+src=$1
+dst=$2
+
+prefix="@prefix@"
+datarootdir="@datarootdir@"
+datadir="@datadir@"
+sysconfdir="@sysconfdir@"
+ldap_targets='@ldap_targets@'
+postgresql_targets='@postgresql_targets@'
+globus_flavor="@globus_flavor@"
+globus_location="@globus_location@"
+
+# If Globus is not configured, use "gcc32" for documentation purpose.
+: ${globus_flavor:="gcc32"}
+
+INSTALL="@abs_top_srcdir@/makes/install-sh -c"
+tmpfile=/tmp/insttmp.$$
+status=1
+
+trap 'rm -f $tmpfile; exit $status' 0 1 2 15
+
+sed	-e "s|[@]prefix[@]|$prefix|g" \
+	-e "s|[@]datadir[@]|$datadir|g" \
+	-e "s|[@]datarootdir[@]|$datarootdir|g" \
+	-e "s|[@]sysconfdir[@]|$sysconfdir|g" \
+	-e "s|[@]ldap_targets[@]|$ldap_targets|g" \
+	-e "s|[@]postgresql_targets[@]|$postgresql_targets|g" \
+	-e "s|[@]globus_flavor[@]|$globus_flavor|g" \
+	-e "s|[@]globus_location[@]|$globus_location|g" \
+	$src >$tmpfile
+
+@INSTALL_SCRIPT@ "$tmpfile" "$dst"
+status=$?
+rm -f $tmpfile

commit 1adcbd164b6048eff32025e22099d3a1a875d89e

merge r5683 from trunk - fix the following (ignored) error, which was introduced in r5520: {{{ [ making util/ptool ] test -z "" || rm -f rm -f usage: rm [-f|-i] [-dPRrvW] file ... *** Error code 1 (ignored) }}}
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@5796 4a5e1ece-6a47-475c-b646-731d720b41d0

makes/script.mk
@@ -33,7 +33,7 @@ script-install:
 
 script-clean:
 	-test -z "$(EXTRA_CLEAN_TARGETS)" || $(RM) -f $(EXTRA_CLEAN_TARGETS)
-	-$(RM) -f $(BUILT_SCRIPTS)
+	-test -z "$(BUILT_SCRIPTS)" || $(RM) -f $(BUILT_SCRIPTS)
 
 script-veryclean: clean private-finalize
 	-test -z "$(EXTRA_VERYCLEAN_TARGETS)" || $(RM) -f $(EXTRA_VERYCLEAN_TARGETS)