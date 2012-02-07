cd7e4ce5e5b1a84de6b7ce3009b7d66ef244fc2d
Sotoyama Touru <sotoyama@users.sourceforge.net>
2012-02-07 08:05:39 +0000

    implement #325 - new output format for Jenkins about regression test  * regress.sh has new option '-j' for generating such output  * add new target 'check-jenkins' runs 'regress.sh -j jenkins_classname'    to Makefile
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5881 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/regress.sh
+++ regress/regress.sh
@@ -2,14 +2,66 @@
 
 . ./regress.conf
 
+before_tst ()
+{
+	tst=$1
+	log=$2
+	jenkins_classname=$3
+
+	account_fmt="%-60.60s ... "
+	account_bgfmt="--- %-60.60s %s\n"
+
+	if [ X"$jenkins_classname" = X"" ]; then
+		printf -- "$account_fmt"   "$tst"
+	fi    
+
+	printf -- "$account_bgfmt" "$tst" "BEGIN" >>$log
+	date +'@@_ start at %s - %Y-%m-%d %H:%M:%S' >>$log
+} 
+
+after_tst ()
+{
+	tst=$1
+	log=$2
+	result=$3
+	elapsed_time=$4
+	jenkins_classname=$5
+
+	testcase_fmt_begin="    <testcase classname=\"%s\" name=\"%s\" time=\"%d\">\n"
+	failure_fmt="        <failure message=\"%s\"/>\n"
+	testcase_fmt_end="    </testcase>\n"
+	account_lgfmt="@:= %-60.60s %s\n"
+
+	if [ X"$jenkins_classname" = X"" ]; then
+		echo $result
+	else
+		printf -- "$testcase_fmt_begin" "$jenkins_classname" "$tst" $elapsed_time
+	    	case $result in
+		"FAIL"|"TRAP"|"XPASS")    
+			printf -- "$failure_fmt"    "$result"
+		esac    
+		printf -- "$testcase_fmt_end"
+	fi    
+
+	printf -- "$account_lgfmt" "$tst" "$result" >>$log
+	date +'@@~  end  at %s - %Y-%m-%d %H:%M:%S' >>$log
+	echo $account_fin >>$log
+}
+
 # constants
-account_bgfmt="--- %-60.60s %s\n"
-account_lgfmt="@:= %-60.60s %s\n"
-account_fmt="%-60.60s ... "
 account_fin="--- ------------------------------------------------------------ ----"
 
 log=log
 rm -f $log
+
+while getopts j: OPT; do
+	case $OPT in
+	"j") jenkins_classname=$OPTARG;
+	     rm -f $jenkins_file;;
+	esac
+done
+
+shift `expr $OPTIND - 1`
 
 case $# in
 0)	schedule=$regress/schedule;;
@@ -25,6 +77,11 @@ n_untested=0
 n_unsupported=0
 n_trap=0
 
+
+if [ X"$jenkins_classname" != X"" ]; then
+	echo "<testsuite>"
+fi
+
 while read line; do
 	set x $line
 	shift
@@ -33,85 +90,78 @@ while read line; do
 
 	tst=$1
 
-	printf -- "$account_fmt"   "$tst"
-	printf -- "$account_bgfmt" "$tst" "BEGIN" >>$log
-	date +'@@_ start at %s - %Y-%m-%d %H:%M:%S' >>$log
+	before_tst $tst $log "$jenkins_classname"
+	start_time=`date +%s`
 
 	if [ -x $regress/$tst ]; then
 
 		sh $regress/$tst >>$log 2>&1
 		exit_code=$?
+		elapsed_time=`expr \`date +%s\` - $start_time`
 
 		case $exit_code in
 		$exit_pass)
-			echo                              "PASS"
-			printf -- "$account_lgfmt" "$tst" "PASS" >>$log
+			after_tst $tst $log "PASS" $elapsed_time "$jenkins_classname"
 			n_pass=`expr $n_pass + 1`;;
 		$exit_fail)
-			echo                              "FAIL"
-			printf -- "$account_lgfmt" "$tst" "FAIL" >>$log
+			after_tst $tst $log "FAIL" $elapsed_time "$jenkins_classname"
 			n_fail=`expr $n_fail + 1`;;
 		$exit_xpass)
-			echo                              "XPASS"
-			printf -- "$account_lgfmt" "$tst" "XPASS" >>$log
+			after_tst $tst $log "XPASS" $elapsed_time "$jenkins_classname"
 			n_xpass=`expr $n_xpass + 1`;;
 		$exit_xfail)
-			echo                              "XFAIL"
-			printf -- "$account_lgfmt" "$tst" "XFAIL" >>$log
+			after_tst $tst $log "XFAIL" $elapsed_time "$jenkins_classname"
 			n_xfail=`expr $n_xfail + 1`;;
 		$exit_unresolved)
-			echo                              "UNRESOLVED"
-			printf -- "$account_lgfmt" "$tst" "UNRESOLVED" >>$log
+			after_tst $tst $log "UNRESOLVED" $elapsed_time "$jenkins_classname"
 			n_unresolved=`expr $n_unresolved + 1`;;
 		$exit_untested)
-			echo                              "UNTESTED"
-			printf -- "$account_lgfmt" "$tst" "UNTESTED" >>$log
+			after_tst $tst $log "UNTESTED" $elapsed_time "$jenkins_classname"
 			n_untested=`expr $n_untested + 1`;;
 		$exit_unsupported)
-			echo                              "UNSUPPORTED"
-			printf -- "$account_lgfmt" "$tst" "UNSUPPORTED" >>$log
+			after_tst $tst $log "UNSUPPORTED" $elapsed_time "$jenkins_classname"
 			n_unsupported=`expr $n_unsupported + 1`;;
 		$exit_trap)
-			echo                              "KILLED"
-			printf -- "$account_lgfmt" "$tst" "KILLED" >>$log
+			after_tst $tst $log "KILLED" $elapsed_time "$jenkins_classname"
 			n_trap=`expr $n_trap + 1`
 			break;;
 		*)
-			echo                              "exit($exit_code)"
-			printf -- "$account_lgfmt" "$tst" "exit($exit_code)" >>$log
+			echo                              
+			after_tst $tst $log "exit($exit_code)" $elapsed_time "$jenkins_classname"
 			n_trap=`expr $n_trap + 1`
 			break;;
 		esac
 	else
-			echo                              "SKIPPED"
-			printf -- "$account_lgfmt" "$tst" "SKIPPED" >>$log
+			after_tst $tst $log "SKIPPED" "0" "$jenkins_classname"
 			n_untested=`expr $n_untested + 1`
 	fi
 
-	date +'@@~  end  at %s - %Y-%m-%d %H:%M:%S' >>$log
-	echo $account_fin >>$log
 	
 done < $schedule
 
-echo ""
-echo "Total test: `expr $n_pass + $n_fail`"
-echo "  success            : $n_pass"
-echo "  failure            : $n_fail"
+if [ X"$jenkins_classname" != X"" ]; then
+	echo '</testsuite>'
+else
+	echo ""
+	echo "Total test: `expr $n_pass + $n_fail`"
+	echo "  success            : $n_pass"
+	echo "  failure            : $n_fail"
 
-if [ $n_xpass -gt 0 ]; then
-echo "  unexpected success : $n_xpass"
-fi
-if [ $n_xfail -gt 0 ]; then
-echo "  expected failure   : $n_xfail"
-fi
-if [ $n_unresolved -gt 0 ]; then
-echo "  unresolved         : $n_unresolved"
-fi
-if [ $n_untested -gt 0 ]; then
-echo "  untested           : $n_untested"
-fi
-if [ $n_unsupported -gt 0 ]; then
-echo "  unsupported        : $n_unsupported"
+	if [ $n_xpass -gt 0 ]; then
+	    echo "  unexpected success : $n_xpass"
+	fi
+	if [ $n_xfail -gt 0 ]; then
+	    echo "  expected failure   : $n_xfail"
+	fi
+	if [ $n_unresolved -gt 0 ]; then
+	    echo "  unresolved         : $n_unresolved"
+	fi
+	if [ $n_untested -gt 0 ]; then
+	    echo "  untested           : $n_untested"
+	fi
+	if [ $n_unsupported -gt 0 ]; then
+	    echo "  unsupported        : $n_unsupported"
+	fi
 fi
 
 case $n_trap in 0) :;; *) exit $exit_trap;; esac
