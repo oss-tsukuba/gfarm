36484a376cf20184b988fa5f09c0bd77f511ea96
Teppei Baba <tbarbar256@gmail.com>
2012-02-09 07:35:48 +0000

    merge r5903 from trunk (fix #333)
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@5909 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/filesystem.c
+++ lib/libgfarm/gfarm/filesystem.c
@@ -174,6 +174,7 @@ gfarm_filesystem_set_metadb_server_list(struct gfarm_filesystem *fs,
 {
 	gfarm_error_t e;
 	int i;
+	struct gfarm_metadb_server *ms;
 	struct gfarm_metadb_server **servers;
 
 	GFARM_MALLOC_ARRAY(servers, sizeof(void *) * n);
@@ -183,6 +184,11 @@ gfarm_filesystem_set_metadb_server_list(struct gfarm_filesystem *fs,
 		return (GFARM_ERR_NO_MEMORY);
 	}
 	memcpy(servers, metadb_servers, sizeof(void *) * n);
+	for (i = 0; i < fs->nservers; ++i) {
+		ms = fs->servers[i];
+		gfarm_metadb_server_free(ms);
+		free(ms);
+	}
 	free(fs->servers);
 	fs->servers = servers;
 	fs->nservers = n;
