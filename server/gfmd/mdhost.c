d1dd9983c9c609c9bb5f46af39020f793ecb1e27
Teppei Baba <tbarbar256@gmail.com>
2012-02-09 07:23:44 +0000

    fix #330: segfault in gfmd after gfmd channel disconnects
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5901 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/mdhost.c
+++ server/gfmd/mdhost.c
@@ -526,7 +526,6 @@ mdhost_disabled(struct abstract_host *h, struct peer *peer, void *closure)
 		gfm_client_connection_unset_conn(conn);
 		gfm_client_connection_free(conn);
 		mdhost_set_connection(m, NULL);
-		local_peer_readable_invoked(peer_to_local_peer(peer));
 	}
 	m->is_recieved_seqnum = 0;
 	if (m->jreader)
