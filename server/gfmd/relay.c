699b240ccef5fcd55f03a0dd1f192c974ca72e74
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-02-13 08:03:35 +0000

    add more stub functions for #287 - inter-gfmd RPC forwarding, to reduce redundant code segment for simple cases, discussed with tbaba@-san.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5923 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/relay.c
+++ server/gfmd/relay.c
@@ -3,19 +3,38 @@
 
 #include <gfarm/gfarm.h>
 
+#include "gfp_xdr.h"
+
 #include "relay.h"
 
 gfarm_error_t
-relay_put_request(struct relayed_reqeust **reqp, const char *diag,
+relay_put_request(struct relayed_request **reqp, const char *diag,
 	gfarm_int32_t command, const char *format, ...)
 {
 	return (GFARM_ERR_FUNCTION_NOT_IMPLEMENTED);
 }
 
 gfarm_error_t
-relay_get_reply(struct relayed_reqeust *req, const char *diag,
+relay_get_reply(struct relayed_request *req, const char *diag,
 	gfarm_error_t *ep, const char *format, ...)
 {
 	return (GFARM_ERR_FUNCTION_NOT_IMPLEMENTED);
 }
 
+/* returns *reqp != NULL, if !mdhost_self_is_master() case. */
+gfarm_error_t
+gfm_server_get_request_with_relay(
+	struct peer *peer, size_t *sizep,
+	int skip, struct relayed_request **reqp, const char *diag,
+	gfarm_int32_t command, const char *format, ...)
+{
+	return (GFARM_ERR_FUNCTION_NOT_IMPLEMENTED);
+}
+
+gfarm_error_t gfm_server_put_reply_with_relay(
+	struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep,
+	struct relayed_request *req, const char *diag,
+	gfarm_error_t *ep, const char *fmormat, ...)
+{
+	return (GFARM_ERR_FUNCTION_NOT_IMPLEMENTED);
+}
--- server/gfmd/relay.h
+++ server/gfmd/relay.h
@@ -1,6 +1,15 @@
-struct relayed_reqeust;
+struct relayed_request;
 
-gfarm_error_t relay_put_request(struct relayed_reqeust **, const char *,
+gfarm_error_t relay_put_request(struct relayed_request **, const char *,
 	gfarm_int32_t, const char *, ...);
-gfarm_error_t relay_get_reply(struct relayed_reqeust *, const char *,
+gfarm_error_t relay_get_reply(struct relayed_request *, const char *,
+	gfarm_error_t *, const char *, ...);
+
+struct peer;
+gfarm_error_t gfm_server_get_request_with_relay(struct peer *, size_t *,
+	int, struct relayed_request **, const char *,
+	gfarm_int32_t, const char *, ...);
+gfarm_error_t gfm_server_put_reply_with_relay(
+	struct peer *, gfp_xdr_xid_t, size_t *,
+	struct relayed_request *, const char *,
 	gfarm_error_t *, const char *, ...);
