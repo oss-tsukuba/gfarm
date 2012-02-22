10a38d1240f9399455af32e2314fd518103bd5e7
Teppei Baba <tbarbar256@gmail.com>
2012-02-22 06:47:49 +0000

    implement a part of #307: gfmd connection failover feature for public functions in gfs.h/gfs_misc.h
    add gfmd connection failover feature to gfs_statfs(), gfs_statfsnode().
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5971 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfs_statfs.c
+++ lib/libgfarm/gfarm/gfs_statfs.c
@@ -10,34 +10,56 @@
 #include "gfm_client.h"
 #include "lookup.h"
 #include "config.h"
+#include "gfs_failover.h"
+
+struct statfs_info {
+	const char *path;
+	gfarm_off_t *used, *avail, *files;
+};
+
+static gfarm_error_t
+statfs_rpc(struct gfm_connection **gfm_serverp, void *closure)
+{
+	gfarm_error_t e;
+	struct statfs_info *si = closure;
+
+	if ((e = gfarm_url_parse_metadb(&si->path, gfm_serverp))
+	    != GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+		    "gfarm_url_parse_metadb: %s",
+		    gfarm_error_string(e));
+		return (e);
+	}
+	return (gfm_client_statfs(*gfm_serverp, si->used, si->avail,
+		si->files));
+}
+
+static gfarm_error_t
+statfs_post_failover(struct gfm_connection *gfm_server, void *closure)
+{
+	if (gfm_server)
+		gfm_client_connection_free(gfm_server);
+	return (GFARM_ERR_NO_ERROR);
+}
+
+static void
+statfs_exit(struct gfm_connection *gfm_server, gfarm_error_t e,
+	void *closure)
+{
+	(void)statfs_post_failover(gfm_server, closure);
+	if (e != GFARM_ERR_NO_ERROR)
+		gflog_debug(GFARM_MSG_UNFIXED,
+		    "gfs_statfs: %s",
+		    gfarm_error_string(e));
+}
 
 gfarm_error_t
 gfs_statfs(gfarm_off_t *used, gfarm_off_t *avail, gfarm_off_t *files)
 {
-	gfarm_error_t e;
-	struct gfm_connection *gfm_server;
-	int retry = 0;
-	const char *path = GFARM_PATH_ROOT;
+	struct statfs_info si = {
+		GFARM_PATH_ROOT, used, avail, files
+	};
 
-	for (;;) {
-		if ((e = gfarm_url_parse_metadb(&path, &gfm_server))
-		    != GFARM_ERR_NO_ERROR) {
-			gflog_debug(GFARM_MSG_1002667,
-			    "gfarm_url_parse_metadb failed: %s",
-			    gfarm_error_string(e));
-			return (e);
-		}
-
-		e = gfm_client_statfs(gfm_server, used, avail, files);
-		if (gfm_client_is_connection_error(e) && ++retry <= 1) {
-			gfm_client_connection_free(gfm_server);
-			continue;
-		}
-
-		break;
-	}
-	gfm_client_connection_free(gfm_server);
-
-	return (e);
-
+	return (gfm_client_rpc_with_failover(statfs_rpc, statfs_post_failover,
+	    statfs_exit, NULL, &si));
 }
--- lib/libgfarm/gfarm/gfs_statfsnode.c
+++ lib/libgfarm/gfarm/gfs_statfsnode.c
@@ -12,6 +12,7 @@
 #include "gfm_client.h"
 #include "gfs_client.h"
 #include "lookup.h"
+#include "gfs_failover.h"
 
 gfarm_error_t
 gfs_statfsnode(char *host, int port,
@@ -22,36 +23,39 @@ gfs_statfsnode(char *host, int port,
 	gfarm_error_t e;
 	struct gfm_connection *gfm_server;
 	struct gfs_connection *gfs_server;
-	int retry = 0;
+	int nretry = 1;
 	const char *path = GFARM_PATH_ROOT;
 
-	for (;;) {
-		if ((e = gfarm_url_parse_metadb(&path, &gfm_server))
-		    != GFARM_ERR_NO_ERROR) {
-			gflog_debug(GFARM_MSG_1002668,
-			    "gfarm_url_parse_metadb failed: %s",
-			    gfarm_error_string(e));
-			return (e);
-		}
-
-		if ((e = gfs_client_connection_and_process_acquire(
-		    &gfm_server, host, port, &gfs_server, NULL))
-		    != GFARM_ERR_NO_ERROR)
-			goto free_gfm_connection;
-
-		if (e == GFARM_ERR_NO_ERROR) {
-			/* "/" is actually not used */
-			e = gfs_client_statfs(gfs_server, "/", bsize, blocks,
-				bfree, bavail, files, ffree, favail);
-			if (gfs_client_is_connection_error(e) && ++retry <= 1) {
-				gfs_client_connection_free(gfs_server);
-				continue;
-			}
-		}
-		break;
+	if ((e = gfarm_url_parse_metadb(&path, &gfm_server))
+	    != GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_1002668,
+		    "gfarm_url_parse_metadb failed: %s",
+		    gfarm_error_string(e));
+		return (e);
 	}
+
+retry:
+	if ((e = gfs_client_connection_and_process_acquire(&gfm_server,
+	    host, port, &gfs_server, NULL)) != GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+		    "gfs_client_connection_acquire_by_host: %s",
+		    gfarm_error_string(e));
+		gfm_client_connection_free(gfm_server);
+		return (e);
+	}
+
+	/* "/" is actually not used */
+	e = gfs_client_statfs(gfs_server, "/", bsize, blocks,
+		bfree, bavail, files, ffree, favail);
 	gfs_client_connection_free(gfs_server);
- free_gfm_connection:
+	if (e != GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_UNFIXED,
+		    "gfs_client_statfs: %s",
+		    gfarm_error_string(e));
+		if (gfs_client_is_connection_error(e) && nretry-- > 0)
+			goto retry;
+	}
 	gfm_client_connection_free(gfm_server);
+
 	return (e);
 }
