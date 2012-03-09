2186e035857826f15fa29c34dc24df09ae117f0d
Teppei Baba <tbarbar256@gmail.com>
2012-03-09 06:53:43 +0000

    refactored about freeing memory at writing objects to journal file.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6034 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/journal_file.c
+++ server/gfmd/journal_file.c
@@ -1592,7 +1592,7 @@ journal_file_write(struct journal_file *jf, gfarm_uint64_t seqnum,
 	if ((e = size_add_op(ope, &data_len, arg)) != GFARM_ERR_NO_ERROR) {
 		GFLOG_ERROR_WITH_SN(GFARM_MSG_1002904,
 		    "size_add_op", e, seqnum, ope);
-		goto end;
+		return (e);
 	}
 
 	journal_file_mutex_lock(jf, diag);
@@ -1639,8 +1639,6 @@ journal_file_write(struct journal_file *jf, gfarm_uint64_t seqnum,
 #endif
 unlock:
 	journal_file_mutex_unlock(jf, diag);
-end:
-	free(arg);
 	return (e);
 }
 
