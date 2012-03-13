dd2ff5f3a7b1a4bd74cf0619c64197b1727ba52a
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-03-13 15:14:46 +0000

    merge r6070, r6077, r6079, r6081, r6083, r6084 from trunk.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6085 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ gftool/gfprep/gfarm_dirtree.h
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ */
+
+typedef struct gfarm_dirtree gfarm_dirtree_t;
+
+typedef struct gfarm_dirtree_entry {
+	gfarm_off_t src_size;
+	gfarm_off_t dst_size;
+	gfarm_int64_t src_m_sec; /* mtime */
+	gfarm_int64_t dst_m_sec;
+	/* XXX gfarm_int32_t src_m_nsec; */
+	/* XXX gfarm_int32_t dst_m_nsec; */
+	gfarm_uint64_t n_pending;
+	char *subpath; /* src and dst */
+	char **src_copy;
+	char **dst_copy;
+	int src_ncopy;
+	int dst_ncopy;
+	int src_mode; /* 07777 */
+	int src_nlink;
+	unsigned char src_d_type;
+	unsigned char dst_d_type;
+	unsigned char dst_exist;
+} gfarm_dirtree_entry_t;
+
+gfarm_error_t gfarm_dirtree_open(gfarm_dirtree_t **, const char *,
+				 const char *, int, int, int);
+gfarm_error_t gfarm_dirtree_checknext(gfarm_dirtree_t *,
+				       gfarm_dirtree_entry_t **);
+gfarm_error_t gfarm_dirtree_next(gfarm_dirtree_t *, gfarm_dirtree_entry_t **);
+void gfarm_dirtree_entry_free(gfarm_dirtree_entry_t *);
+gfarm_error_t gfarm_dirtree_pending(gfarm_dirtree_t *);
+gfarm_error_t gfarm_dirtree_delete(gfarm_dirtree_t *);
+gfarm_error_t gfarm_dirtree_close(gfarm_dirtree_t *);
+gfarm_error_t gfarm_dirtree_array(gfarm_dirtree_t *, int *,
+				  gfarm_dirtree_entry_t ***);
+void gfarm_dirtree_array_free(int, gfarm_dirtree_entry_t **);
