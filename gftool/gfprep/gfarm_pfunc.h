9dde755d596606b034ea4f2aa962e5103d7b3a49
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-03-13 09:59:58 +0000

    add new commands.  * gfprep: replicate Gfarm files in parallel  * gfpcopy: copy Gfarm files and directories in parallel
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6070 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ gftool/gfprep/gfarm_pfunc.h
@@ -0,0 +1,21 @@
+/*
+ * $Id$
+ */
+
+typedef struct gfarm_pfunc gfarm_pfunc_t;
+typedef struct gfarm_pfunc_cmd gfarm_pfunc_cmd_t;
+
+gfarm_error_t gfarm_pfunc_start(gfarm_pfunc_t **, int, int, gfarm_uint64_t,
+				int, void (*)(void *), void (*)(int, void *));
+gfarm_error_t gfarm_pfunc_cmd_add(gfarm_pfunc_t *, gfarm_pfunc_cmd_t *);
+gfarm_error_t gfarm_pfunc_interrupt(gfarm_pfunc_t *);
+gfarm_error_t gfarm_pfunc_join(gfarm_pfunc_t *);
+
+gfarm_error_t gfarm_pfunc_replicate(gfarm_pfunc_t *, const char *,
+				    const char *, int,
+				    const char *, int, void *, int);
+gfarm_error_t gfarm_pfunc_remove_replica(gfarm_pfunc_t *, const char *,
+					 const char *, int, void *);
+gfarm_error_t gfarm_pfunc_copy(gfarm_pfunc_t *,
+			       const char *, const char *, int,
+			       const char *, const char *, int, void *, int);
