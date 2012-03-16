004d7df4646d7e90545652110cbe4fff1a9a4d97
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-03-16 08:46:52 +0000

    add regression tests for gfprep and gfpcopy.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6103 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gftool/gfprep/gfprep_m.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+. ./regress.conf
+
+GFPREP_DIR=`dirname $0`
+. ${GFPREP_DIR}/setup_gfprep.sh
+
+[ `gfsched -w | wc -l` -ge 2 ] || exit $exit_unsupported
+setup_test
+
+check_N $gf_dir1/dir/1byte 1
+if gfwhere $gf_dir1/dir/1byte > $local_tmpfile1 2>&1; then
+  :
+else
+  echo gfwhere failed
+  clean_test
+  exit $exit_fail
+fi
+
+if cat $local_tmpfile1 | gfprep -m -h - gfarm:${gf_dir1}; then
+  :
+else
+  echo failed: gfprep -m
+  clean_test
+  exit $exit_fail
+fi
+check_N $gf_dir1/dir/1byte 1
+
+if gfwhere $gf_dir1/dir/1byte | diff - $local_tmpfile1 2>&1 > /dev/null; then
+  echo not migrated
+  clean_test
+  exit $exit_fail
+fi
+
+clean_test
+exit $exit_pass
