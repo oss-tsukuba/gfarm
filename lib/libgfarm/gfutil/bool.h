fb22612a71a0a12fab8d84ec21ec871853776c9d
Hirano Motono <m-hirano@sra.co.jp>
2012-03-28 08:21:23 +0000

    o Added bool cuz I've been dying for it.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6182 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ lib/libgfarm/gfutil/bool.h
@@ -0,0 +1,13 @@
+/*
+ * $Id$
+ */
+#if !defined(__cplusplus)
+#if __STDC_VERSION__ >= 199901L
+#include <stdbool.h>
+#else
+typedef enum {
+	false = 0,
+	true = 1
+} bool;
+#endif /* __STDC_VERSION__ >= 199901L */
+#endif /* ! __cplusplus */
