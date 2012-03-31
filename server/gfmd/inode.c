40fe8d1dc43da9dd800d451004bac6f37b764131
Osamu Tatebe <tatebe@users.sourceforge.net>
2012-03-31 07:12:24 +0000

    fix assertion failure, pointed out by gfarm-discuss-ja:64198
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6199 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/inode.c
+++ server/gfmd/inode.c
@@ -3123,11 +3123,12 @@ inode_schedule_file_default(struct file_opening *opening,
 			nhosts = 0;
 			for (fo = ios->openings.opening_next;
 			    fo != &ios->openings; fo = fo->opening_next) {
-				assert(nhosts < n);
 				if (fo->u.f.spool_host != NULL &&
 				    host_is_disk_available(
-				    fo->u.f.spool_host, 0))
+				    fo->u.f.spool_host, 0)) {
+					assert(nhosts < n);
 					hosts[nhosts++] = fo->u.f.spool_host;
+				}
 			}
 			if (nhosts > 0) {
 				/*
