oss-tsukuba/gfarm

commit 304cae12aa9226ed94c2698b7e900a1bd9990eac

add metadata journaling function for gfmd redundancy.
  * To enable metadata journaling,
    1. add -DENABLE_JOURNAL to the compile option and recompile.
    2. add a following line in gfmd.conf
       journal_dir /var/gfarm-metadata/journal
    3. mkdir -p /var/gfarm-metadata/journal

  * Move rpc functions in subr.c into rpcsubr.c.


git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5099 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -457,7 +457,7 @@ inode_xattrs_init(struct inode *inode)
 	xattrs_init(&inode->i_xmlattrs);
 }
 
-static void
+void
 inode_xattrs_clear(struct inode *inode)
 {
 	xattrs_free_entries(&inode->i_xattrs);
@@ -948,6 +948,13 @@ inode_init_symlink(struct inode *inode, char *source_path)
 	return (GFARM_ERR_NO_ERROR);
 }
 
+void
+inode_clear_symlink(struct inode *inode)
+{
+	free(inode->u.c.s.l.source_path);
+	inode->u.c.s.l.source_path = NULL;
+}
+
 gfarm_error_t
 inode_check_file(struct inode *inode)
 {
@@ -1176,6 +1183,13 @@ inode_get_mode(struct inode *inode)
 	return (inode->i_mode);
 }
 
+void
+inode_set_mode_in_cache(struct inode *inode, gfarm_mode_t mode)
+{
+	inode->i_mode = (inode->i_mode & GFARM_S_IFMT) |
+	    (mode & GFARM_S_ALLPERM);
+}
+
 gfarm_error_t
 inode_set_mode(struct inode *inode, gfarm_mode_t mode)
 {
@@ -1186,8 +1200,7 @@ inode_set_mode(struct inode *inode, gfarm_mode_t mode)
 			"argument 'mode' is invalid");
 		return (GFARM_ERR_INVALID_ARGUMENT);
 	}
-	inode->i_mode = (inode->i_mode & GFARM_S_IFMT) |
-	    (mode & GFARM_S_ALLPERM);
+	inode_set_mode_in_cache(inode, mode);
 
 	e = db_inode_mode_modify(inode->i_number, inode->i_mode);
 	if (e != GFARM_ERR_NO_ERROR)
@@ -1206,13 +1219,19 @@ inode_get_size(struct inode *inode)
 }
 
 void
+inode_set_size_in_cache(struct inode *inode, gfarm_off_t size)
+{
+	inode->i_size = size;
+}
+
+void
 inode_set_size(struct inode *inode, gfarm_off_t size)
 {
 	gfarm_error_t e;
 
 	/* inode is file */
 	quota_update_file_resize(inode, size);
-	inode->i_size = size;
+	inode_set_size_in_cache(inode, size);
 
 	e = db_inode_size_modify(inode->i_number, inode->i_size);
 	if (e != GFARM_ERR_NO_ERROR)
@@ -1220,6 +1239,30 @@ inode_set_size(struct inode *inode, gfarm_off_t size)
 		    "db_inode_size_modify(%lld): %s",
 		    (unsigned long long)inode->i_number,
 		    gfarm_error_string(e));
+}
+
+void
+inode_set_nlink_in_cache(struct inode *inode, gfarm_uint64_t nlink)
+{
+	inode->i_nlink = nlink;
+}
+
+void
+inode_set_gen_in_cache(struct inode *inode, gfarm_uint64_t gen)
+{
+	inode->i_gen = gen;
+}
+
+void
+inode_set_user_by_name_in_cache(struct inode *inode, const char *username)
+{
+	inode->i_user = user_lookup(username);
+}
+
+void
+inode_set_group_by_name_in_cache(struct inode *inode, const char *groupname)
+{
+	inode->i_group = group_lookup(groupname);
 }
 
 gfarm_error_t
@@ -1277,6 +1320,12 @@ inode_get_ctime(struct inode *inode)
 }
 
 void
+inode_set_atime_in_cache(struct inode *inode, struct gfarm_timespec *atime)
+{
+	inode->i_atimespec = *atime;
+}
+
+void
 inode_set_atime(struct inode *inode, struct gfarm_timespec *atime)
 {
 	gfarm_error_t e;
@@ -1287,7 +1336,7 @@ inode_set_atime(struct inode *inode, struct gfarm_timespec *atime)
 	if (gfarm_timespec_cmp(&inode->i_atimespec, atime) == 0)
 		return; /* not necessary to change */
 
-	inode->i_atimespec = *atime;
+	inode_set_atime_in_cache(inode, atime);
 
 	e = db_inode_atime_modify(inode->i_number, &inode->i_atimespec);
 	if (e != GFARM_ERR_NO_ERROR)
@@ -1298,6 +1347,12 @@ inode_set_atime(struct inode *inode, struct gfarm_timespec *atime)
 }
 
 void
+inode_set_mtime_in_cache(struct inode *inode, struct gfarm_timespec *mtime)
+{
+	inode->i_mtimespec = *mtime;
+}
+
+void
 inode_set_mtime(struct inode *inode, struct gfarm_timespec *mtime)
 {
 	gfarm_error_t e;
@@ -1305,7 +1360,7 @@ inode_set_mtime(struct inode *inode, struct gfarm_timespec *mtime)
 	if (gfarm_timespec_cmp(&inode->i_mtimespec, mtime) == 0)
 		return; /* not necessary to change */
 
-	inode->i_mtimespec = *mtime;
+	inode_set_mtime_in_cache(inode, mtime);
 
 	e = db_inode_mtime_modify(inode->i_number, inode_get_mtime(inode));
 	if (e != GFARM_ERR_NO_ERROR)
@@ -1316,6 +1371,12 @@ inode_set_mtime(struct inode *inode, struct gfarm_timespec *mtime)
 }
 
 void
+inode_set_ctime_in_cache(struct inode *inode, struct gfarm_timespec *ctime)
+{
+	inode->i_ctimespec = *ctime;
+}
+
+void
 inode_set_ctime(struct inode *inode, struct gfarm_timespec *ctime)
 {
 	gfarm_error_t e;
@@ -1323,7 +1384,7 @@ inode_set_ctime(struct inode *inode, struct gfarm_timespec *ctime)
 	if (gfarm_timespec_cmp(&inode->i_ctimespec, ctime) == 0)
 		return; /* not necessary to change */
 
-	inode->i_ctimespec = *ctime;
+	inode_set_ctime_in_cache(inode, ctime);
 
 	e = db_inode_ctime_modify(inode->i_number, &inode->i_ctimespec);
 	if (e != GFARM_ERR_NO_ERROR)
@@ -3015,21 +3076,26 @@ inode_schedule_file_default(struct file_opening *opening,
 gfarm_error_t (*inode_schedule_file)(struct file_opening *, struct peer *,
 	gfarm_int32_t *, struct host ***) = inode_schedule_file_default;
 
-static gfarm_error_t
-remove_file_copy(struct inode *inode, struct host *spool_host)
+gfarm_error_t
+inode_remove_replica_in_cache(struct inode *inode, struct host *spool_host)
 {
 	struct file_copy **copyp, *copy, **foundp = NULL;
 
+	assert(inode && inode_is_file(inode));
+
 	for (copyp = &inode->u.c.s.f.copies; (copy = *copyp) != NULL;
 	    copyp = &copy->host_next) {
-		if (copy->host == spool_host)
+		if (copy->host == spool_host) {
 			foundp = copyp;
+			break;
+		}
 	}
 	if (foundp == NULL) {
-		gflog_error(GFARM_MSG_1002326,
-		    "remove_file_copy(%lld, %lld, %s): not found",
+		gflog_error(GFARM_MSG_UNFIXED,
+		    "(%lld, %lld, %s) : %s",
 		    (long long)inode_get_number(inode),
-		    (long long)inode_get_gen(inode), host_name(spool_host));
+		    (long long)inode_get_gen(inode), host_name(spool_host),
+		    gfarm_error_string(GFARM_ERR_NO_SUCH_OBJECT));
 		return (GFARM_ERR_NO_SUCH_OBJECT);
 	}
 	copy = *foundp;
@@ -3054,14 +3120,14 @@ file_replicating_new(struct inode *inode, struct host *dst,
 		return (e);
 
 	if ((e = host_replicating_new(dst, &fr)) != GFARM_ERR_NO_ERROR) {
-		remove_file_copy(inode, dst);
+		(void)inode_remove_replica_in_cache(inode, dst);
 		return (e);
 	}
 	if (irs == NULL) {
 		GFARM_MALLOC(irs);
 		if (irs == NULL) {
 			peer_replicating_free(fr);
-			remove_file_copy(inode, dst);
+			(void)inode_remove_replica_in_cache(inode, dst);
 			return (GFARM_ERR_NO_MEMORY);
 		}
 		/* make circular list `replicating_hosts' empty */
@@ -3306,6 +3372,12 @@ inode_add_replica(struct inode *inode, struct host *spool_host, int valid)
 	return (GFARM_ERR_NO_ERROR);
 }
 
+gfarm_error_t
+inode_add_file_copy_in_cache(struct inode *inode, struct host *host)
+{
+	return (inode_add_replica_internal(inode, host, 1, 0));
+}
+
 static gfarm_error_t
 remove_replica_metadata(struct inode *inode, struct host *spool_host)
 {
@@ -3355,21 +3427,13 @@ inode_remove_replica_completed(gfarm_ino_t inum, gfarm_int64_t igen,
 	struct host *host)
 {
 	struct inode *inode = inode_lookup(inum);
-	struct file_copy **copyp, *copy;
 
 	if (inode == NULL || !inode_is_file(inode))
 		return;
 	if (igen != inode->i_gen)
 		return;
 
-	for (copyp = &inode->u.c.s.f.copies; (copy = *copyp) != NULL;
-	    copyp = &copy->host_next) {
-		if (copy->host == host) {
-			*copyp = copy->host_next;
-			free(copy);
-			return;
-		}
-	}
+	(void)inode_remove_replica_in_cache(inode, host);
 }
 
 static gfarm_error_t
@@ -3526,9 +3590,9 @@ inode_is_updated(struct inode *inode, struct gfarm_timespec *mtime)
 	    gfarm_timespec_cmp(mtime, &ios->u.f.last_update) >= 0);
 }
 
-gfarm_error_t
-inode_replica_list_by_name(struct inode *inode,
-	gfarm_int32_t *np, char ***hostsp)
+static gfarm_error_t
+inode_replica_list_by_name_common(struct inode *inode,
+	int is_up, gfarm_int32_t *np, char ***hostsp)
 {
 	struct file_copy *copy;
 	gfarm_error_t e = GFARM_ERR_NO_ERROR;
@@ -3546,7 +3610,7 @@ inode_replica_list_by_name(struct inode *inode,
 		return (GFARM_ERR_INVALID_ARGUMENT);
 	}
 
-	n = inode_get_ncopy(inode);
+	n = inode_get_ncopy_common(inode, 1, is_up);
 	GFARM_MALLOC_ARRAY(hosts, n);
 	if (hosts == NULL) {
 		gflog_debug(GFARM_MSG_1001773,
@@ -3562,7 +3626,7 @@ inode_replica_list_by_name(struct inode *inode,
 		 * host_is_up() may change even while the giant lock is held.
 		 */
 		if (i < n && FILE_COPY_IS_VALID(copy) &&
-		    host_is_up(copy->host)) {
+		    (is_up == 0 || host_is_up(copy->host))) {
 			hosts[i] = strdup_log(host_name(copy->host), diag);
 			if (hosts[i] == NULL) {
 				gflog_debug(GFARM_MSG_1001774,
@@ -3585,6 +3649,20 @@ inode_replica_list_by_name(struct inode *inode,
 		*hostsp = hosts;
 	}
 	return (e);
+}
+
+gfarm_error_t
+inode_replica_list_by_name(struct inode *inode,
+	gfarm_int32_t *np, char ***hostsp)
+{
+	return (inode_replica_list_by_name_common(inode, 1, np, hostsp));
+}
+
+gfarm_error_t
+inode_replica_list_by_name_with_dead_host(struct inode *inode,
+	gfarm_int32_t *np, char ***hostsp)
+{
+	return (inode_replica_list_by_name_common(inode, 0, np, hostsp));
 }
 
 gfarm_error_t
@@ -3993,6 +4071,27 @@ inode_remove_orphan(void)
 static void
 inode_add_one(void *closure, struct gfs_stat *st)
 {
+	(void)inode_add(st);
+}
+
+void
+inode_modify(struct inode *inode, struct gfs_stat *st)
+{
+	inode->i_gen = st->st_gen;
+	inode->i_nlink = st->st_nlink;
+	inode->i_size = st->st_size;
+	inode->i_mode = st->st_mode;
+	inode_set_user_by_name_in_cache(inode, st->st_user);
+	inode_set_group_by_name_in_cache(inode, st->st_group);
+	inode->i_atimespec = st->st_atimespec;
+	inode->i_mtimespec = st->st_mtimespec;
+	inode->i_ctimespec = st->st_ctimespec;
+}
+
+/* The memory owner of `*st' is changed to inode.c */
+gfarm_error_t
+inode_add(struct gfs_stat *st)
+{
 	gfarm_error_t e;
 	struct inode *inode;
 
@@ -4028,19 +4127,12 @@ inode_add_one(void *closure, struct gfs_stat *st)
 			    gfarm_error_string(e));
 		}
 		gfs_stat_free(st);
-		return;
+		return (e);
 	}
 
-	inode->i_gen = st->st_gen;
-	inode->i_nlink = st->st_nlink;
-	inode->i_size = st->st_size;
-	inode->i_mode = st->st_mode;
-	inode->i_user = user_lookup(st->st_user);
-	inode->i_group = group_lookup(st->st_group);
-	inode->i_atimespec = st->st_atimespec;
-	inode->i_mtimespec = st->st_mtimespec;
-	inode->i_ctimespec = st->st_ctimespec;
+	inode_modify(inode, st);
 	gfs_stat_free(st);
+	return (GFARM_ERR_NO_ERROR);
 }
 
 /* The memory owner of `type' and `sum' is changed to inode.c */
@@ -4079,6 +4171,14 @@ inode_cksum_add_one(void *closure,
 static void
 symlink_add_one(void *closure, gfarm_ino_t inum, char *source_path)
 {
+	if (symlink_add(inum, source_path) != GFARM_ERR_NO_ERROR)
+		free(source_path);
+}
+
+gfarm_error_t
+symlink_add(gfarm_ino_t inum, char *source_path)
+{
+	gfarm_error_t e;
 	struct inode *inode = inode_lookup(inum);
 
 	if (inode == NULL) {
@@ -4086,24 +4186,24 @@ symlink_add_one(void *closure, gfarm_ino_t inum, char *source_path)
 		    "symlink_add_one: no inode %lld",
 		    (unsigned long long)inum);
 		symlink_defer_db_removal(inum, source_path);
-		return;
+		e = GFARM_ERR_NO_SUCH_FILE_OR_DIRECTORY;
 	} else if (!inode_is_symlink(inode)) {
 		gflog_error(GFARM_MSG_1000341,
 		    "symlink_add_one: not symlink %lld",
 		    (unsigned long long)inum);
 		symlink_defer_db_removal(inum, source_path);
-		return;
+		e = GFARM_ERR_NOT_A_SYMBOLIC_LINK;
 	} else if (inode->u.c.s.l.source_path != NULL) {
 		gflog_error(GFARM_MSG_1000342,
 		    "symlink_add_one: dup symlink %lld",
 		    (unsigned long long)inum);
 		symlink_defer_db_removal(inum, source_path);
-		return;
+		e = GFARM_ERR_INVALID_ARGUMENT;
 	} else {
 		inode->u.c.s.l.source_path = source_path;
-		return; /* to skip free(source_path); */
+		e = GFARM_ERR_NO_ERROR;
 	}
-	free(source_path);
+	return (e);
 }
 
 /* The memory owner of `hostname' is changed to inode.c */
@@ -4155,6 +4255,15 @@ dir_entry_add_one(void *closure,
 	gfarm_ino_t dir_inum, char *entry_name, int entry_len,
 	gfarm_ino_t entry_inum)
 {
+	(void)dir_entry_add(dir_inum, entry_name, entry_len, entry_inum);
+	free(entry_name);
+}
+
+gfarm_error_t
+dir_entry_add(gfarm_ino_t dir_inum, char *entry_name, int entry_len,
+	gfarm_ino_t entry_inum)
+{
+	gfarm_error_t e;
 	struct inode *dir_inode = inode_lookup(dir_inum);
 	struct inode *entry_inode = inode_lookup(entry_inum);
 	DirEntry entry;
@@ -4166,27 +4275,29 @@ dir_entry_add_one(void *closure,
 		    (unsigned long long)dir_inum);
 		dir_entry_defer_db_removal(dir_inum,
 		    entry_name, entry_len, entry_inum);
-		return;
+		e = GFARM_ERR_NO_SUCH_OBJECT;
 	} else if (!inode_is_dir(dir_inode)) {
 		gflog_error(GFARM_MSG_1000351,
 		    "dir_entry_add_one: not dir %lld",
 		    (unsigned long long)dir_inum);
 		dir_entry_defer_db_removal(dir_inum,
 		    entry_name, entry_len, entry_inum);
-		return;
+		e = GFARM_ERR_NOT_A_DIRECTORY;
 	} else if (entry_inode == NULL) {
 		gflog_error(GFARM_MSG_1000352,
 		    "dir_entry_add_one: no inode %lld",
 		    (unsigned long long)entry_inum);
 		dir_entry_defer_db_removal(dir_inum,
 		    entry_name, entry_len, entry_inum);
-		return;
+		e = GFARM_ERR_NO_SUCH_OBJECT;
 	} else if ((entry = dir_enter(dir_inode->u.c.s.d.entries,
 	    entry_name, entry_len, &created)) == NULL) {
 		gflog_error(GFARM_MSG_1000353, "dir_entry_add_one: no memory");
+		e = GFARM_ERR_NO_MEMORY;
 	} else if (!created) {
 		gflog_error(GFARM_MSG_1000354,
 		    "dir_entry_add_one: already exists ");
+		e = GFARM_ERR_ALREADY_EXISTS;
 	} else {
 		dir_entry_set_inode(entry, entry_inode);
 		inode_increment_nlink_ini(entry_inode);
@@ -4197,8 +4308,9 @@ dir_entry_add_one(void *closure,
 			/* remember parent */
 			entry_inode->u.c.s.d.parent_dir = dir_inode;
 		}
+		e = GFARM_ERR_NO_ERROR;
 	}
-	free(entry_name);
+	return (e);
 }
 
 void
@@ -4693,6 +4805,14 @@ xattr_find(struct xattrs *xattrs, const char *attrname)
 		entry = entry->next;
 	}
 	return NULL;
+}
+
+int
+inode_xattr_has_attr(struct inode *inode, int xmlMode, const char *attrname)
+{
+	struct xattrs *xattrs = xmlMode ? &inode->i_xmlattrs : &inode->i_xattrs;
+
+	return (xattr_find(xattrs, attrname) != NULL);
 }
 
 gfarm_error_t

include/gfarm/error.h
@@ -125,6 +125,7 @@ enum gfarm_errcode {
 		GFARM_ERR_NOT_A_REGULAR_FILE,
 		GFARM_ERR_IS_A_REGULAR_FILE,
 		GFARM_ERR_PATH_IS_ROOT,
+		GFARM_ERR_INTERNAL_ERROR,
 
 		GFARM_ERR_NUMBER
 };

lib/libgfarm/gfarm/config.c
@@ -724,6 +724,7 @@ char *gfarm_localfs_datadir = NULL;
 #define GFARM_GFMD_CONNECTION_CACHE_DEFAULT  8 /*  8 free connections */
 #define GFARM_RECORD_ATIME_DEFAULT 1 /* enable */
 #define GFARM_PROFILE_DEFAULT 0 /* disable */
+#define GFARM_JOURNAL_MAX_SIZE_DEFAULT		(32 * 1024 * 1024) /* 32MB */
 #define MISC_DEFAULT -1
 int gfarm_log_level = MISC_DEFAULT;
 int gfarm_log_message_verbose = MISC_DEFAULT;
@@ -748,6 +749,8 @@ int gfarm_metadb_heartbeat_interval = MISC_DEFAULT;
 int gfarm_metadb_dbq_size = MISC_DEFAULT;
 int gfarm_record_atime = MISC_DEFAULT;
 int gfarm_profile = MISC_DEFAULT;
+static char *journal_dir = NULL;
+static int journal_max_size = MISC_DEFAULT;
 
 void
 gfarm_config_clear(void)
@@ -776,6 +779,7 @@ gfarm_config_clear(void)
 		&gfarm_postgresql_conninfo,
 		&gfarm_localfs_datadir,
 		&schedule_write_target_domain,
+		&journal_dir,
 	};
 	int i;
 
@@ -852,6 +856,18 @@ void
 gfarm_set_record_atime(int boolean)
 {
 	gfarm_record_atime = boolean;
+}
+
+const char *
+gfarm_journal_dir(void)
+{
+	return (journal_dir);
+}
+
+int
+gfarm_journal_max_size(void)
+{
+	return (journal_max_size);
 }
 
 /*
@@ -1987,6 +2003,10 @@ parse_one_line(char *s, char *p, char **op)
 	} else if (strcmp(s, o = "profile") == 0) {
 		e = parse_profile(p, &gfarm_profile);
 
+	} else if (strcmp(s, o = "journal_dir") == 0) {
+		e = parse_set_var(p, &journal_dir);
+	} else if (strcmp(s, o = "journal_max_size") == 0) {
+		e = parse_set_misc_int(p, &journal_max_size);
 	} else {
 		o = s;
 		gflog_debug(GFARM_MSG_1000974,
@@ -2130,6 +2150,8 @@ gfarm_config_set_default_misc(void)
 		gfarm_record_atime = GFARM_RECORD_ATIME_DEFAULT;
 	if (gfarm_profile == MISC_DEFAULT)
 		gfarm_profile = GFARM_PROFILE_DEFAULT;
+	if (journal_max_size == MISC_DEFAULT)
+		journal_max_size = GFARM_JOURNAL_MAX_SIZE_DEFAULT;
 }
 
 void

lib/libgfarm/gfarm/config.h
@@ -81,6 +81,8 @@ extern int gf_hook_default_global;
 int gfarm_schedule_write_local_priority(void);
 char *gfarm_schedule_write_target_domain(void);
 gfarm_off_t gfarm_get_minimum_free_disk_space(void);
+const char *gfarm_journal_dir(void);
+int gfarm_journal_max_size(void);
 
 /* redirection */
 extern struct gfs_file *gf_stdout, *gf_stderr;

lib/libgfarm/gfarm/gfp_xdr.c
@@ -47,15 +47,20 @@ gfp_xdr_set(struct gfp_xdr *conn,
 	conn->cookie = cookie;
 	conn->fd = fd;
 
-	gfarm_iobuffer_set_read(conn->recvbuffer, ops->blocking_read,
-	    cookie, fd);
-	gfarm_iobuffer_set_write(conn->sendbuffer, ops->blocking_write,
-	    cookie, fd);
+	if (conn->recvbuffer)
+		gfarm_iobuffer_set_read(conn->recvbuffer, ops->blocking_read,
+		    cookie, fd);
+	if (conn->sendbuffer)
+		gfarm_iobuffer_set_write(conn->sendbuffer, ops->blocking_write,
+		    cookie, fd);
 }
 
-gfarm_error_t
-gfp_xdr_new(struct gfp_iobuffer_ops *ops, void *cookie, int fd,
-	struct gfp_xdr **connp)
+#define GFP_XDR_NEW_RECV	1
+#define GFP_XDR_NEW_SEND	2
+
+static gfarm_error_t
+gfp_xdr_new0(struct gfp_iobuffer_ops *ops, void *cookie, int fd,
+	int flags, struct gfp_xdr **connp)
 {
 	struct gfp_xdr *conn;
 
@@ -66,28 +71,61 @@ gfp_xdr_new(struct gfp_iobuffer_ops *ops, void *cookie, int fd,
 			gfarm_error_string(GFARM_ERR_NO_MEMORY));
 		return (GFARM_ERR_NO_MEMORY);
 	}
-	conn->recvbuffer = gfarm_iobuffer_alloc(GFP_XDR_BUFSIZE);
-	if (conn->recvbuffer == NULL) {
-		free(conn);
-		gflog_debug(GFARM_MSG_1000997,
-			"allocation of 'conn recvbuffer' failed: %s",
-			gfarm_error_string(GFARM_ERR_NO_MEMORY));
-		return (GFARM_ERR_NO_MEMORY);
-	}
-	conn->sendbuffer = gfarm_iobuffer_alloc(GFP_XDR_BUFSIZE);
-	if (conn->sendbuffer == NULL) {
-		gfarm_iobuffer_free(conn->recvbuffer);
-		free(conn);
-		gflog_debug(GFARM_MSG_1000998,
-			"allocation of 'conn sendbuffer' failed: %s",
-			gfarm_error_string(GFARM_ERR_NO_MEMORY));
-		return (GFARM_ERR_NO_MEMORY);
-	}
+	if ((flags & GFP_XDR_NEW_RECV) != 0) {
+		conn->recvbuffer = gfarm_iobuffer_alloc(GFP_XDR_BUFSIZE);
+		if (conn->recvbuffer == NULL) {
+			free(conn);
+			gflog_debug(GFARM_MSG_1000997,
+				"allocation of 'conn recvbuffer' failed: %s",
+				gfarm_error_string(GFARM_ERR_NO_MEMORY));
+			return (GFARM_ERR_NO_MEMORY);
+		}
+	} else
+		conn->recvbuffer = NULL;
+
+	if ((flags & GFP_XDR_NEW_SEND) != 0) {
+		conn->sendbuffer = gfarm_iobuffer_alloc(GFP_XDR_BUFSIZE);
+		if (conn->sendbuffer == NULL) {
+			gfarm_iobuffer_free(conn->recvbuffer);
+			free(conn);
+			gflog_debug(GFARM_MSG_1000998,
+				"allocation of 'conn sendbuffer' failed: %s",
+				gfarm_error_string(GFARM_ERR_NO_MEMORY));
+			return (GFARM_ERR_NO_MEMORY);
+		}
+	} else
+		conn->sendbuffer = NULL;
+
 	gfp_xdr_set(conn, ops, cookie, fd);
 
 	*connp = conn;
 	return (GFARM_ERR_NO_ERROR);
 }
+
+gfarm_error_t
+gfp_xdr_new(struct gfp_iobuffer_ops *ops, void *cookie, int fd,
+	struct gfp_xdr **connp)
+{
+	return (gfp_xdr_new0(ops, cookie, fd,
+	    GFP_XDR_NEW_RECV|GFP_XDR_NEW_SEND, connp));
+}
+
+gfarm_error_t
+gfp_xdr_new_recv_only(struct gfp_iobuffer_ops *ops, void *cookie, int fd,
+	struct gfp_xdr **connp)
+{
+	return (gfp_xdr_new0(ops, cookie, fd,
+	    GFP_XDR_NEW_RECV, connp));
+}
+
+gfarm_error_t
+gfp_xdr_new_send_only(struct gfp_iobuffer_ops *ops, void *cookie, int fd,
+	struct gfp_xdr **connp)
+{
+	return (gfp_xdr_new0(ops, cookie, fd,
+	    GFP_XDR_NEW_SEND, connp));
+}
+
 
 gfarm_error_t
 gfp_xdr_free(struct gfp_xdr *conn)
@@ -129,6 +167,24 @@ gfp_xdr_sendbuffer_check_size(struct gfp_xdr *conn, int size)
 		return (GFARM_ERR_NO_ERROR);
 }
 
+gfarm_error_t
+gfp_xdr_recvbuffer_check_size(struct gfp_xdr *conn, int size)
+{
+	if (size > gfarm_iobuffer_get_size(conn->recvbuffer)) {
+		gflog_fatal(GFARM_MSG_UNFIXED,
+		    "%s", gfarm_error_string(GFARM_ERR_INTERNAL_ERROR));
+		return (GFARM_ERR_INTERNAL_ERROR);
+	} else if (size > gfarm_iobuffer_avail_length(conn->recvbuffer))
+		return (GFARM_ERR_RESOURCE_TEMPORARILY_UNAVAILABLE);
+	else
+		return (GFARM_ERR_NO_ERROR);
+}
+
+void
+gfp_xdr_recvbuffer_clear_read_eof(struct gfp_xdr *conn)
+{
+	gfarm_iobuffer_clear_read_eof(conn->recvbuffer);
+}
 
 gfarm_error_t
 gfp_xdr_export_credential(struct gfp_xdr *conn)
@@ -175,6 +231,8 @@ gfp_xdr_recv_is_ready(struct gfp_xdr *conn)
 gfarm_error_t
 gfp_xdr_flush(struct gfp_xdr *conn)
 {
+	if (conn->sendbuffer == NULL)
+		return (GFARM_ERR_NO_ERROR);
 	gfarm_iobuffer_flush_write(conn->sendbuffer);
 	return (gfarm_iobuffer_get_error(conn->sendbuffer));
 }
@@ -207,6 +265,17 @@ gfp_xdr_purge(struct gfp_xdr *conn, int just, int len)
 		return (GFARM_ERR_UNEXPECTED_EOF);
 	}
 	return (GFARM_ERR_NO_ERROR);
+}
+
+void
+gfp_xdr_purge_all(struct gfp_xdr *conn)
+{
+	if (conn->sendbuffer)
+		while (gfarm_iobuffer_purge(conn->sendbuffer, NULL) > 0)
+			;
+	if (conn->recvbuffer)
+		while (gfarm_iobuffer_purge(conn->recvbuffer, NULL) > 0)
+			;
 }
 
 gfarm_error_t
@@ -712,6 +781,35 @@ gfp_xdr_send(struct gfp_xdr *conn, const char *format, ...)
 	return (GFARM_ERR_NO_ERROR);
 }
 
+gfarm_uint32_t
+gfp_xdr_send_calc_crc32(struct gfp_xdr *conn, gfarm_uint32_t crc, int offset,
+	size_t length)
+{
+	return (gfarm_iobuffer_calc_crc32(conn->sendbuffer, crc,
+		offset, length, 0));
+}
+
+gfarm_uint32_t
+gfp_xdr_recv_calc_crc32(struct gfp_xdr *conn, gfarm_uint32_t crc, int offset,
+	size_t length)
+{
+	return (gfarm_iobuffer_calc_crc32(conn->recvbuffer, crc,
+		offset, length, 1));
+}
+
+gfarm_uint32_t
+gfp_xdr_recv_get_crc32_ahead(struct gfp_xdr *conn, int offset)
+{
+	gfarm_uint32_t n;
+	int err;
+	int len = gfarm_iobuffer_get_read_x_ahead(conn->recvbuffer,
+		&n, sizeof(n), 1, offset, &err);
+
+	if (len != sizeof(n))
+		return (0);
+	return (ntohl(n));
+}
+
 gfarm_error_t
 gfp_xdr_recv_sized(struct gfp_xdr *conn, int just, size_t *sizep,
 	int *eofp, const char *format, ...)
@@ -759,6 +857,19 @@ gfp_xdr_recv(struct gfp_xdr *conn, int just,
 		    "invalid format character: %c(%x)", *format, *format);
 		return (GFARM_ERRMSG_GFP_XDR_RECV_INVALID_FORMAT_CHARACTER);
 	}
+	return (GFARM_ERR_NO_ERROR);
+}
+
+gfarm_error_t
+gfp_xdr_recv_ahead(struct gfp_xdr *conn, int len, size_t *availp)
+{
+	int r = gfarm_iobuffer_read_ahead(conn->recvbuffer, len);
+
+	if (r == 0) {
+		*availp = 0;
+		return (gfarm_iobuffer_get_error(conn->recvbuffer));
+	}
+	*availp = gfarm_iobuffer_avail_length(conn->recvbuffer);
 	return (GFARM_ERR_NO_ERROR);
 }
 

lib/libgfarm/gfarm/gfp_xdr.h
@@ -32,11 +32,18 @@ struct gfp_xdr;
 
 gfarm_error_t gfp_xdr_new(struct gfp_iobuffer_ops *, void *, int,
 	struct gfp_xdr **);
+gfarm_error_t gfp_xdr_new_recv_only(struct gfp_iobuffer_ops *, void *, int,
+	struct gfp_xdr **);
+gfarm_error_t gfp_xdr_new_send_only(struct gfp_iobuffer_ops *, void *, int,
+	struct gfp_xdr **);
 gfarm_error_t gfp_xdr_free(struct gfp_xdr *);
 
 void *gfp_xdr_cookie(struct gfp_xdr *);
 int gfp_xdr_fd(struct gfp_xdr *);
+int gfp_xdr_read_fd(struct gfp_xdr *);
 gfarm_error_t gfp_xdr_sendbuffer_check_size(struct gfp_xdr *, int);
+gfarm_error_t gfp_xdr_recvbuffer_check_size(struct gfp_xdr *, int);
+void gfp_xdr_recvbuffer_clear_read_eof(struct gfp_xdr *);
 void gfp_xdr_set(struct gfp_xdr *,
 	struct gfp_iobuffer_ops *, void *, int);
 
@@ -52,6 +59,7 @@ void gfarm_iobuffer_set_nonblocking_write_xxx(struct gfarm_iobuffer *,
 int gfp_xdr_recv_is_ready(struct gfp_xdr *);
 gfarm_error_t gfp_xdr_flush(struct gfp_xdr *);
 gfarm_error_t gfp_xdr_purge(struct gfp_xdr *, int, int);
+void gfp_xdr_purge_all(struct gfp_xdr *);
 gfarm_error_t gfp_xdr_vsend_size_add(size_t *, const char **, va_list *);
 gfarm_error_t gfp_xdr_vsend(struct gfp_xdr *,
 	const char **, va_list *);
@@ -62,10 +70,16 @@ gfarm_error_t gfp_xdr_vrecv(struct gfp_xdr *, int,
 
 gfarm_error_t gfp_xdr_send_size_add(size_t *, const char *, ...);
 gfarm_error_t gfp_xdr_send(struct gfp_xdr *, const char *, ...);
+gfarm_uint32_t gfp_xdr_send_calc_crc32(struct gfp_xdr *, gfarm_uint32_t,
+	int, size_t);
 gfarm_error_t gfp_xdr_recv_sized(struct gfp_xdr *, int, size_t *,
 	int *, const char *, ...);
 gfarm_error_t gfp_xdr_recv(struct gfp_xdr *, int, int *,
 	const char *, ...);
+gfarm_uint32_t gfp_xdr_recv_calc_crc32(struct gfp_xdr *, gfarm_uint32_t,
+	int, size_t);
+gfarm_uint32_t gfp_xdr_recv_get_crc32_ahead(struct gfp_xdr *, int);
+gfarm_error_t gfp_xdr_recv_ahead(struct gfp_xdr *, int, size_t *);
 gfarm_error_t gfp_xdr_vrpc_request(struct gfp_xdr *, gfarm_int32_t,
 	const char **, va_list *);
 gfarm_error_t gfp_xdr_vrpc_result_sized(struct gfp_xdr *, int, size_t *,

lib/libgfarm/gfarm/iobuffer.c
@@ -4,6 +4,7 @@
 #include <gfarm/gflog.h>
 #include <gfarm/gfarm_misc.h>
 #include "iobuffer.h"
+#include "crc32.h"
 
 /* XXX - This implementation is somewhat slow, but probably acceptable */
 
@@ -71,6 +72,8 @@ gfarm_iobuffer_alloc(int bufsize)
 void
 gfarm_iobuffer_free(struct gfarm_iobuffer *b)
 {
+	if (b == NULL)
+		return;
 	free(b->buffer);
 	free(b);
 }
@@ -258,7 +261,6 @@ gfarm_iobuffer_read(struct gfarm_iobuffer *b, int *residualp)
 		b->tail += rv;
 		*residualp -= rv;
 	}
-	return;
 }
 
 int
@@ -521,4 +523,49 @@ void
 gfarm_iobuffer_write_close_nop(struct gfarm_iobuffer *b, void *cookie, int fd)
 {
 	/* nop */
+}
+
+gfarm_uint32_t
+gfarm_iobuffer_calc_crc32(struct gfarm_iobuffer *b, gfarm_uint32_t crc,
+	int offset, int len, int head_or_tail)
+{
+	return (gfarm_crc32(crc, b->buffer + (head_or_tail ? b->head : b->tail)
+		+ offset, len));
+}
+
+int
+gfarm_iobuffer_get_read_x_ahead(struct gfarm_iobuffer *b, void *data, int len,
+	int just, int offset, int *errp)
+{
+	int rlen;
+	int head0 = b->head;
+	int tail0 = b->tail;
+	int read_eof0 = b->read_eof;
+	int error0 = b->error;
+
+	if (b->head + offset > b->tail)
+		return (0);
+	b->head += offset;
+	rlen = gfarm_iobuffer_get_read_x(b, data, len, just);
+	if (rlen == 0)
+		*errp = b->error;
+	b->head = head0;
+	b->tail = tail0;
+	b->read_eof = read_eof0;
+	b->error = error0;
+	return (rlen);
+}
+
+int
+gfarm_iobuffer_read_ahead(struct gfarm_iobuffer *b, int len)
+{
+	size_t alen = IOBUFFER_AVAIL_LENGTH(b);
+	int rlen;
+
+	if (alen > len)
+		return (alen);
+	rlen = len - alen;
+	while (rlen > 0 && gfarm_iobuffer_is_readable(b) && b->error == 0)
+		gfarm_iobuffer_read(b, &rlen);
+	return (len - rlen);
 }

lib/libgfarm/gfarm/iobuffer.h
@@ -80,6 +80,7 @@ void gfarm_iobuffer_set_read(struct gfarm_iobuffer *,
 void *gfarm_iobuffer_get_read_cookie(struct gfarm_iobuffer *);
 int gfarm_iobuffer_get_read_fd(struct gfarm_iobuffer *);
 void gfarm_iobuffer_read(struct gfarm_iobuffer *, int *);
+int gfarm_iobuffer_read_ahead(struct gfarm_iobuffer *, int);
 int gfarm_iobuffer_put(struct gfarm_iobuffer *, const void *, int);
 
 /* dequeue */
@@ -106,6 +107,8 @@ int gfarm_iobuffer_get_read(struct gfarm_iobuffer *, void *, int);
 int gfarm_iobuffer_get_read_partial_x(struct gfarm_iobuffer *, void *,int,int);
 int gfarm_iobuffer_get_read_partial_just(struct gfarm_iobuffer *, void *, int);
 int gfarm_iobuffer_get_read_partial(struct gfarm_iobuffer *, void *, int);
+int gfarm_iobuffer_get_read_x_ahead(struct gfarm_iobuffer *, void *, int, int,
+	int, int *);
 /*
  * gfarm_iobuffer_get_read{,_partial}_just() functions doesn't perform
  * read ahead for given stream, so the caller can perform read operation
@@ -117,3 +120,5 @@ int gfarm_iobuffer_get_read_partial(struct gfarm_iobuffer *, void *, int);
 
 /* default operation for gfarm_iobuffer_set_write_close() */
 void gfarm_iobuffer_write_close_nop(struct gfarm_iobuffer *, void *, int);
+gfarm_uint32_t gfarm_iobuffer_calc_crc32(struct gfarm_iobuffer *,
+	gfarm_uint32_t, int, int, int);

commit 0c6e73e0074e8753d9149273fb303a12db75ccbb

merge r6259 from the main trunk: fix #379 - metadata consistency check is broken, and that may crash gfmd. this problem was introduced in r5099, which was released as part of gfarm-2.5.0.
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6280 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -4087,15 +4087,18 @@ dir_entry_defer_db_removal(gfarm_ino_t dir_inum,
 	char *entry_name, int entry_len, gfarm_ino_t entry_inum)
 {
 	struct dir_entry_removal_todo *entry;
+	char *ename;
 
 	GFARM_MALLOC(entry);
-	if (entry == NULL) {
+	GFARM_MALLOC_ARRAY(ename, entry_len);
+	if (entry == NULL || ename == NULL) {
+		free(entry);
+		free(ename);
 		gflog_error(GFARM_MSG_1002827,
 		    "dir_entry (%llu name:%.*s) (%llu): "
 		    "no memory to record for removal",
 		    (unsigned long long)dir_inum, entry_len, entry_name,
 		    (unsigned long long)entry_inum);
-		free(entry_name);
 		return;
 	}
 	gflog_warning(GFARM_MSG_1002828, 
@@ -4103,8 +4106,10 @@ dir_entry_defer_db_removal(gfarm_ino_t dir_inum,
 	    (unsigned long long)dir_inum, entry_len, entry_name,
 	    (unsigned long long)entry_inum);
 
+	memcpy(ename, entry_name, entry_len);
+
 	entry->dir_inum = dir_inum;
-	entry->entry_name = entry_name;
+	entry->entry_name = ename;
 	entry->entry_len = entry_len;
 
 	entry->next = dir_entry_removal_list;
@@ -4350,8 +4355,11 @@ inode_cksum_add_one(void *closure,
 static void
 symlink_add_one(void *closure, gfarm_ino_t inum, char *source_path)
 {
-	if (symlink_add(inum, source_path) != GFARM_ERR_NO_ERROR)
-		free(source_path);
+	(void)symlink_add(inum, source_path);
+	/* abandon error */
+
+	/* symlink_defer_db_removal() doesn't defer access to source_path */
+	free(source_path);
 }
 
 gfarm_error_t
@@ -4435,6 +4443,8 @@ dir_entry_add_one(void *closure,
 	gfarm_ino_t entry_inum)
 {
 	(void)dir_entry_add(dir_inum, entry_name, entry_len, entry_inum);
+
+	/* abandon error */
 	free(entry_name);
 }
 