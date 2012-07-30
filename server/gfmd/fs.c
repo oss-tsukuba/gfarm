97dc54a20de44f9e7fff8ba5ebb9bfa52ccf4e21
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-07-30 09:00:20 +0000

    * GFM_PROTO_REPLICA_ADD returns GFARM_ERR_INVALID_FILE_REPLICA if an inode is not a file.  Because the unreferred file on gfsd should be moved to lost+found at gfsd -ccc.  * add an error log of gfm_client_replica_add().  * shut up the following warning by gcc. {{{  fs.c:5233:2: warning: 'e_rpc' may be used uninitialized in this function [-Wuninitialized] }}}
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6458 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/fs.c
+++ server/gfmd/fs.c
@@ -5074,7 +5074,7 @@ gfm_server_replica_add(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep,
 			e = GFARM_ERR_NO_SUCH_OBJECT;
 		} else if (!inode_is_file(inode)) {
 			gflog_debug(GFARM_MSG_UNFIXED, "not a file");
-			e = GFARM_ERR_OPERATION_NOT_PERMITTED;
+			e = GFARM_ERR_INVALID_FILE_REPLICA;
 		} else if (inode_get_gen(inode) != gen) {
 			gflog_debug(GFARM_MSG_1001978,
 			    "inode_get_gen() failed");
@@ -5228,7 +5228,8 @@ gfm_server_replica_create_file_in_lost_found(struct peer *peer,
 				db_end(diag);
 		}
 		giant_unlock();
-	}
+	} else
+		e_rpc = GFARM_ERR_NO_ERROR;
 
 	return (gfm_server_relay_put_reply(peer, xid, sizep, relay,
 	    diag, e_rpc, "ll", &inum_new, &gen_new));
