631b819e4a8d987ab41296edffaf6bba671610e4
Osamu Tatebe <tatebe@users.sourceforge.net>
2012-08-02 01:21:14 +0000

    checkpatch, still there is a warning about omp parallel reduction.  It seems checkpatch.pl does not support OpenMP pragma.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6481 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfrep/gfrep.c
+++ gftool/gfrep/gfrep.c
@@ -15,7 +15,7 @@
 #include <omp.h>
 #define LIBGFARM_NOT_MT_SAFE
 #else
-static void omp_set_num_threads(int n){ return; }
+static void omp_set_num_threads(int n) { return; }
 static int omp_get_num_threads(void){ return (1); }
 static int omp_get_thread_num(void){ return (0); }
 #endif
@@ -346,7 +346,7 @@ print_file_list(struct gfarm_list *list)
 {
 	int i;
 
-	for (i= 0; i < gfarm_list_length(list); ++i)
+	for (i = 0; i < gfarm_list_length(list); ++i)
 		print_file_info(gfarm_stringlist_elem(list, i));
 }
 
@@ -447,7 +447,7 @@ action(struct action *act, int tnum, int nth, int pi, struct file_info *fi,
 	while (1) {
 		while (fi->surplus_ncopy == 0
 		       && (file_copy_does_exist(fi, dst[di])
-			   || ! is_enough_space(
+			   || !is_enough_space(
 				   dst[di], dst_port[di], fi->filesize))
 		       && max_niter > 0) {
 			if (opt_verbose)
@@ -521,7 +521,7 @@ pfor(struct action *act, int nfinfo, struct file_info **finfo,
 	}
 	omp_set_num_threads(nthreads);
 
-#pragma omp parallel reduction(+:nerr) private(pi,tnum,nth)
+#pragma omp parallel reduction(+:nerr) private(pi, tnum, nth)
 	{
 	pi = 0;
 	tnum = omp_get_thread_num();
@@ -559,8 +559,7 @@ pfor(struct action *act, int nfinfo, struct file_info **finfo,
 			if (!opt_noexec) {
 				e = action(act, tnum, nth, pi, fi, arg);
 				errmsg = gfarm_error_string(e);
-			}
-			else {
+			} else {
 				e = GFARM_ERR_NO_ERROR;
 				errmsg = gfarm_error_string(e);
 			}
@@ -572,7 +571,7 @@ pfor(struct action *act, int nfinfo, struct file_info **finfo,
 				       fi->filesize / t / 1024 / 1024);
 			}
 #ifdef LIBGFARM_NOT_MT_SAFE
-		skip_replication:
+ skip_replication:
 #endif
 			if (e != GFARM_ERR_NO_ERROR) {
 #ifndef LIBGFARM_NOT_MT_SAFE
@@ -586,7 +585,8 @@ pfor(struct action *act, int nfinfo, struct file_info **finfo,
 			sync();
 			_exit(e == GFARM_ERR_NO_ERROR ? 0 : 1);
 		}
-		while ((rv = waitpid(pid, &s, 0)) == -1 && errno == EINTR);
+		while ((rv = waitpid(pid, &s, 0)) == -1 && errno == EINTR)
+			;
 		if (rv == -1 || (WIFEXITED(s) && WEXITSTATUS(s) != 0))
 			++nerr;
 #endif
