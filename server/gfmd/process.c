3a37133dd00523789bbbaeb537be452dd2f98ffa
Osamu Tatebe <tatebe@users.sourceforge.net>
2012-09-21 05:04:43 +0000

    undo r6651 - fix lost file replica when multiple clients open the same file with GFARM_FILE_TRUNC simultaneously (#441)
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6656 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/process.c
+++ server/gfmd/process.c
@@ -720,8 +720,6 @@ process_reopen_file(struct process *process,
 			gfarm_error_string(e));
 		return (e);
 	}
-	/* GFARM_FILE_TRUNC_PENDING should be always unset in reopen_file() */
-	fo->flag &= ~GFARM_FILE_TRUNC_PENDING;
 	if (!inode_is_file(fo->inode)) { /* i.e. is a directory */
 		gflog_debug(GFARM_MSG_1001628,
 			"inode is not file");
@@ -790,6 +788,7 @@ process_reopen_file(struct process *process,
 
 	fo->u.f.spool_opener = peer;
 	fo->u.f.spool_host = spool_host;
+	fo->flag &= ~GFARM_FILE_TRUNC_PENDING; /*spool_host will truncate it*/
 	*inump = inode_get_number(fo->inode);
 	*genp = inode_get_gen(fo->inode);
 	*modep = inode_get_mode(fo->inode);
