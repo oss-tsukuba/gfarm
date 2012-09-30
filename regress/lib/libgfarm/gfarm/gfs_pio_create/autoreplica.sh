dbf5731b1245424b59c1fd1c6be089c96c8931e6
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-09-30 03:16:12 +0000

    merge latest test script, as preparation of the fix to #456
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6698 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/lib/libgfarm/gfarm/gfs_pio_create/autoreplica.sh
+++ regress/lib/libgfarm/gfarm/gfs_pio_create/autoreplica.sh
@@ -4,7 +4,7 @@
 
 [ `gfsched -w | wc -l` -ge 2 ] || exit $exit_unsupported
 
-WAIT_TIME=2
+WAIT_TIME_LIMIT=10
 
 gfs_pio_test=`dirname $testbin`/gfs_pio_test/gfs_pio_test
 tmpf=$gftmp/foo
@@ -29,6 +29,7 @@ if echo -n 2 | gfxattr -s $gftmp gfarm.ncopy; then
 	:
 else
 	echo failed gfxattr
+	clean
 	exit $exit_fail
 fi
 
@@ -36,24 +37,35 @@ if echo 123456789 | $gfs_pio_test -c -w -W10 $tmpf; then
 	:
 else
 	echo failed $gfs_pio_test
+	clean
 	exit $exit_fail
 fi
 
-sleep $WAIT_TIME
-if gfstat $tmpf > $statf 2>&1; then
-	:
-else
-	echo failed gfstat
-	cat $statf
-	exit $exit_fail
-fi
-
-if [ `awk '/Ncopy/{print $NF}' $statf` -eq 2 ]; then
-	exit_code=$exit_pass
-else
-	echo failed gfstat
-	exit $exit_fail
-fi
+WAIT_TIME=0
+while
+	if gfstat $tmpf > $statf 2>&1; then
+		:
+	else
+		echo failed gfstat
+		cat $statf
+		clean
+		exit $exit_fail
+	fi
+	if [ `awk '/Ncopy/{print $NF}' $statf` -eq 2 ]; then
+		exit_code=$exit_pass
+		false # exit from this loop
+	else
+		true
+	fi
+do
+	WAIT_TIME=`expr $WAIT_TIME + 1`
+	if [ $WAIT_TIME -gt $WAIT_TIME_LIMIT ]; then
+		echo replication timeout
+		clean
+		exit $exit_fail
+	fi
+	sleep 1
+done
 
 clean
 exit $exit_code
