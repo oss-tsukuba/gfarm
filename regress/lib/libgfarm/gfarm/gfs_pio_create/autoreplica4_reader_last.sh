e8ab0b2da6bd3f3739351416c4fd23cfd21b8198
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-09-30 02:51:37 +0000

    add a test script for #457 - automatic replication does not work, if multiple simultaneous write-open calls are made, but last closing process doesn't actually write the file, with status code = XFAIL.
    this problem is found during fixing #456 - automatic replication stopped to work, if multiple simultaneous updates are made to a same file, since the fix to #438
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6697 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/lib/libgfarm/gfarm/gfs_pio_create/autoreplica4_reader_last.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+# https://sourceforge.net/apps/trac/gfarm/ticket/457
+
+. ./regress.conf
+
+[ `gfsched -w | wc -l` -ge 2 ] || exit $exit_unsupported
+
+WAIT_TIME_LIMIT=10
+
+gfs_pio_test=`dirname $testbin`/gfs_pio_test/gfs_pio_test
+tmpf=$gftmp/foo
+statf=$localtmp
+
+clean() {
+	gfrm -f $tmpf
+	gfrmdir $gftmp
+	rm -f $statf
+}
+
+trap 'clean; exit $exit_trap' $trap_sigs
+
+if gfmkdir $gftmp; then
+	:
+else
+	echo failed gfmkdir
+	exit $exit_fail
+fi
+
+if echo -n 2 | gfxattr -s $gftmp gfarm.ncopy; then
+	:
+else
+	echo failed gfxattr
+	clean
+	exit $exit_fail
+fi
+
+# -W1 is needed to make gfsd processes access to gfmd simultaneously
+echo 123456789 | $gfs_pio_test -c -w -W1 -P1 -W9 $tmpf &
+echo abcdefghi | $gfs_pio_test -c -w -W1 -P1 -W9 $tmpf &
+echo jklmnopqr | $gfs_pio_test -c -w -W1 -P1 -W9 $tmpf &
+                 $gfs_pio_test -c    -R1 -P2 -R9 $tmpf &
+wait
+
+WAIT_TIME=0
+while
+	if gfstat $tmpf > $statf 2>&1; then
+		:
+	else
+		echo failed gfstat
+		cat $statf
+		clean
+		exit $exit_fail
+	fi
+	if [ `awk '/Ncopy/{print $NF}' $statf` -eq 2 ]; then
+		exit_code=$exit_pass
+		false # exit from this loop
+	else
+		true
+	fi
+do
+	WAIT_TIME=`expr $WAIT_TIME + 1`
+	if [ $WAIT_TIME -gt $WAIT_TIME_LIMIT ]; then
+		echo replication timeout
+		clean
+		exit $exit_xfail
+	fi
+	sleep 1
+done
+
+clean
+exit $exit_code
