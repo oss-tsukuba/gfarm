5fc01ba34f6c084ee4231c9dee6127a8f841125d
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-10-02 09:23:58 +0000

    merge r6719 from the 2.5 branch.  * stress test for #184, #421 and #429 - multiple simultaneous updates for different files, waiting completion of automatic replication, and validation for updated replicas.  * -o and -s options are added to gfreg and gfexport. (hidden options, for test use)  * gfreg can read from stdin.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6720 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfexport/gfexport.c
+++ gftool/gfexport/gfexport.c
@@ -23,10 +23,30 @@ char *program_name = "gfexport";
 static char buffer[BUFFER_SIZE];
 
 gfarm_error_t
-gfprint(GFS_File gf, FILE *ofp)
+gfprint(GFS_File gf, FILE *ofp, gfarm_off_t size)
 {
 	gfarm_error_t e;
 	int n;
+
+	if (size > 0) {
+		for (;;) {
+			int req = sizeof buffer > size ? size : sizeof buffer;
+
+			if ((e = gfs_pio_read(gf, buffer, req, &n)) !=
+			    GFARM_ERR_NO_ERROR)
+				break;
+			if (n == 0) /* EOF */
+				break;
+			if (fwrite(buffer, 1, n, ofp) != n) {
+				e = GFARM_ERR_INPUT_OUTPUT;
+				break;
+			}
+			size -= n;
+			if (size <= 0)
+				break;
+		}
+		return (e);
+	}
 
 	while ((e = gfs_pio_read(gf, buffer, sizeof buffer, &n)) ==
 	       GFARM_ERR_NO_ERROR) {
@@ -41,7 +61,8 @@ gfprint(GFS_File gf, FILE *ofp)
 }
 
 gfarm_error_t
-gfexport(char *gfarm_url, char *host, FILE *ofp)
+gfexport(char *gfarm_url, char *host, FILE *ofp,
+	gfarm_off_t off, gfarm_off_t size)
 {
 	gfarm_error_t e, e2;
 	GFS_File gf;
@@ -53,8 +74,13 @@ gfexport(char *gfarm_url, char *host, FILE *ofp)
 	e = gfs_pio_internal_set_view_section(gf, host);
 	if (e != GFARM_ERR_NO_ERROR)
 		goto close;
+	if (off > 0) {
+		e = gfs_pio_seek(gf, off, GFARM_SEEK_SET, NULL);
+		if (e != GFARM_ERR_NO_ERROR)
+			goto close;
+	}
 
-	e = gfprint(gf, ofp);
+	e = gfprint(gf, ofp, size);
  close:
 	e2 = gfs_pio_close(gf);
 	return (e != GFARM_ERR_NO_ERROR ? e : e2);
@@ -66,6 +92,10 @@ usage()
 	fprintf(stderr, "Usage: %s [option] <input_file>\n", program_name);
 	fprintf(stderr, "option:\n");
 	fprintf(stderr, "\t-h <hostname>\n");
+#if 0
+	fprintf(stderr, "\t-o <offset>\tskip bytes at start of input\n");
+	fprintf(stderr, "\t-s <size>\tinput size\n");
+#endif
 	fprintf(stderr, "\t%s\t%s\n", "-p", "turn on profiling");
 	exit(1);
 }
@@ -76,6 +106,7 @@ main(int argc, char *argv[])
 	gfarm_error_t e;
 	char *url, *realpath = NULL, *hostname = NULL;
 	int ch;
+	gfarm_off_t off = -1, size = -1;
 
 	if (argc > 0)
 		program_name = basename(argv[0]);
@@ -86,13 +117,19 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
-	while ((ch = getopt(argc, argv, "h:p?")) != -1) {
+	while ((ch = getopt(argc, argv, "h:o:ps:?")) != -1) {
 		switch (ch) {
 		case 'h':
 			hostname = optarg;
 			break;
 		case 'p':
 			gfs_profile_set();
+			break;
+		case 'o':
+			off = atoll(optarg);
+			break;
+		case 's':
+			size = atoll(optarg);
 			break;
 		case '?':
 		default:
@@ -112,7 +149,7 @@ main(int argc, char *argv[])
 	else
 		url = argv[0];
 
-	e = gfexport(url, hostname, stdout);
+	e = gfexport(url, hostname, stdout, off, size);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "%s: %s: %s\n", program_name, url,
 		    gfarm_error_string(e));
