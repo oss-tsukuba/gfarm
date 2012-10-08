oss-tsukuba/gfarm

commit cb7a7b5f9f475740cccc1eb289dd593b58d5fcb3

changed gflog to use catgets().
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4303 4a5e1ece-6a47-475c-b646-731d720b41d0

include/gfarm/gflog.h
@@ -1,3 +1,7 @@
+#include <syslog.h>
+
+#include <gfarm/gfarm_msg_enums.h>
+
 #ifndef GFARM_DEFAULT_FACILITY
 #define GFARM_DEFAULT_FACILITY	LOG_LOCAL0
 #endif
@@ -12,19 +16,54 @@
 #define GFLOG_PRINTF_ARG(M, N)
 #endif
 
-#ifdef GFLOG_USE_STDARG /* to make <stdarg.h> optional to use <gfutil.h> */
-void gflog_vmessage(int, const char *, va_list) GFLOG_PRINTF_ARG(2, 0);
+#if __STDC_VERSION__ < 199901L
+#if __GNUC__ >= 2
+#define __func__ __FUNCTION__
+#else
+#define __func__ "<unknown>"
 #endif
-void gflog_message(int, const char *, ...) GFLOG_PRINTF_ARG(2, 3);
-void gflog_error(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_warning(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_notice(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_info(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_debug(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_warning_errno(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
+#endif
 
-void gflog_fatal(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_fatal_errno(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
+#ifdef GFLOG_USE_STDARG /* to make <stdarg.h> optional to use <gfutil.h> */
+void gflog_vmessage(int, int, const char *, int, const char *,
+		const char *, va_list) GFLOG_PRINTF_ARG(6, 0);
+#endif
+void gflog_message(int, int, const char *, int, const char*,
+		const char *, ...) GFLOG_PRINTF_ARG(6, 7);
+void gflog_message_errno(int, int, const char *, int, const char*,
+		const char *, ...) GFLOG_PRINTF_ARG(6, 7);
+
+#define gflog_error(msg_no, ...) \
+	gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_warning(msg_no, ...) \
+	gflog_message(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_notice(msg_no, ...) \
+	gflog_message(msg_no, LOG_NOTICE,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_info(msg_no, ...) \
+	gflog_message(msg_no, LOG_INFO,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_debug(msg_no, ...) \
+	gflog_message(msg_no, LOG_DEBUG,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#define gflog_fatal(msg_no, ...) \
+	gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_error(msg_no, ...)\
+	gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_warning(msg_no, ...)\
+	gflog_message(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_fatal_errno(msg_no, ...) \
+	gflog_message_errno(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_warning_errno(msg_no, ...) \
+	gflog_message_errno(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
 
 void gflog_set_priority_level(int);
 void gflog_set_identifier(const char *);
@@ -32,6 +71,8 @@ void gflog_set_auxiliary_info(char *);
 char *gflog_get_auxiliary_info(void);
 void gflog_syslog_open(int, int);
 int gflog_syslog_enabled(void);
+void gflog_catopen(const char *);
+void gflog_catclose(void);
 
 int gflog_syslog_name_to_facility(const char *);
 int gflog_syslog_name_to_priority(const char *);
@@ -40,5 +81,3 @@ int gflog_syslog_name_to_priority(const char *);
 
 int gflog_auth_set_verbose(int);
 int gflog_auth_get_verbose(void);
-void gflog_auth_error(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_auth_warning(const char *, ...) GFLOG_PRINTF_ARG(1, 2);

lib/libgfarm/gfutil/logutil.c
@@ -4,6 +4,8 @@
 #include <errno.h>
 #include <syslog.h>
 #include <string.h>
+#include <nl_types.h>
+#include <pthread.h>
 
 #include <gfarm/error.h>
 #include <gfarm/gfarm_misc.h>
@@ -11,10 +13,15 @@
 #define GFLOG_USE_STDARG
 #include <gfarm/gflog.h>
 
+#define GFARM_CATALOG_SET_NO 1
+
 static const char *log_identifier = "libgfarm";
 static char *log_auxiliary_info = NULL;
 static int log_use_syslog = 0;
 static int log_level = GFARM_DEFAULT_PRIORITY_LEVEL_TO_LOG;
+static nl_catd catd = (nl_catd)-1;
+static const char *catalog_file = "gfarm.cat";
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int
 gflog_syslog_enabled(void)
@@ -23,93 +30,74 @@ gflog_syslog_enabled(void)
 }
 
 void
-gflog_vmessage(int priority, const char *format, va_list ap)
+gflog_catopen(const char *file)
+{
+	if (file == NULL)
+		catd = catopen(catalog_file, 0);
+	else
+		catd = catopen(file, 0);
+}
+
+void
+gflog_catclose()
+{
+	catclose(catd);
+}
+
+void
+gflog_vmessage(int msg_no, int priority, const char *file, int line_no,
+		const char *func, const char *format, va_list ap)
 {
 	char buffer[2048];
+	char *catmsg = NULL;
 
 	if (priority > log_level) /* not worth reporting */
 		return;
+
+	pthread_mutex_lock(&mutex);
+
+	catmsg = catgets(catd, GFARM_CATALOG_SET_NO, msg_no, NULL);
+	catmsg = catmsg == NULL ? "" : catmsg;
 
 	vsnprintf(buffer, sizeof buffer, format, ap);
 
 	if (log_auxiliary_info != NULL) {
 		if (log_use_syslog)
-			syslog(priority, "(%s) %s",
+			syslog(priority, "[%06d] (%s L%d %s()) %s / (%s) %s",
+			    msg_no, file, line_no, func, catmsg,
 			    log_auxiliary_info, buffer);
 		else
-			fprintf(stderr, "%s: (%s) %s\n",
-			    log_identifier,
+			fprintf(stderr,
+			    "%s: [%06d] (%s L%d %s()) %s / (%s) %s\n",
+			    log_identifier, msg_no, file, line_no, func, catmsg,
 			    log_auxiliary_info, buffer);
 	} else {
 		if (log_use_syslog)
-			syslog(priority, "%s", buffer);
+			syslog(priority, "[%06d] (%s L%d %s()) %s / %s",
+			    msg_no, file, line_no, func, catmsg, buffer);
 		else
-			fprintf(stderr, "%s: %s\n", log_identifier, buffer);
+			fprintf(stderr, "%s: [%06d] (%s L%d %s()) %s / %s\n",
+			    log_identifier, msg_no, file, line_no, func, catmsg,
+			    buffer);
 	}
+
+	pthread_mutex_unlock(&mutex);
 }
 
 void
-gflog_message(int priority, const char *format, ...)
+gflog_message(int msg_no, int priority, const char *file, int line_no,
+		const char *func, const char *format, ...)
 {
 	va_list ap;
 
 	va_start(ap, format);
-	gflog_vmessage(priority, format, ap);
+	gflog_vmessage(msg_no, priority, file, line_no, func, format, ap);
 	va_end(ap);
 }
 
 void
-gflog_error(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	gflog_vmessage(LOG_ERR, format, ap);
-	va_end(ap);
-}
-
-void
-gflog_warning(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	gflog_vmessage(LOG_WARNING, format, ap);
-	va_end(ap);
-}
-
-void
-gflog_notice(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	gflog_vmessage(LOG_NOTICE, format, ap);
-	va_end(ap);
-}
-
-void
-gflog_info(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	gflog_vmessage(LOG_INFO, format, ap);
-	va_end(ap);
-}
-
-void
-gflog_debug(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	gflog_vmessage(LOG_DEBUG, format, ap);
-	va_end(ap);
-}
-
-void
-gflog_warning_errno(const char *format, ...)
+gflog_message_errno(int msg_no, int priority, const char *file, int line_no,
+		const char *func, const char *format, ...)
 {
 	int save_errno = errno;
 	char buffer[2048];
@@ -119,35 +107,8 @@ gflog_warning_errno(const char *format, ...)
 	va_start(ap, format);
 	vsnprintf(buffer, sizeof buffer, format, ap);
 	va_end(ap);
-	gflog_warning("%s: %s", buffer, strerror(save_errno));
-}
-
-void
-gflog_fatal(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	gflog_vmessage(LOG_ERR, format, ap);
-	va_end(ap);
-#if 0
-	abort();
-#endif
-	exit(2);
-}
-
-void
-gflog_fatal_errno(const char *format, ...)
-{
-	int save_errno = errno;
-	char buffer[2048];
-
-	va_list ap;
-
-	va_start(ap, format);
-	vsnprintf(buffer, sizeof buffer, format, ap);
-	va_end(ap);
-	gflog_fatal("%s: %s", buffer, strerror(save_errno));
+	gflog_message(msg_no, priority, file, line_no, func,
+			"%s, %s", buffer, strerror(save_errno));
 }
 
 void
@@ -267,26 +228,3 @@ gflog_auth_get_verbose(void)
 {
 	return (authentication_verbose);
 }
-
-void
-gflog_auth_error(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	if (authentication_verbose)
-		gflog_vmessage(LOG_ERR, format, ap);
-	va_end(ap);
-}
-
-void
-gflog_auth_warning(const char *format, ...)
-{
-	va_list ap;
-
-	va_start(ap, format);
-	if (authentication_verbose)
-		gflog_vmessage(LOG_WARNING, format, ap);
-	va_end(ap);
-}
-

commit 27aadd47d2e1d4a5da9b2e9fa9236092640b2c43

merge r6764 from the main trunk: redo r6760 differently: fix #232 - gflog_auth_error() and gflog_auth_warning() have been broken since r4303
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6765 4a5e1ece-6a47-475c-b646-731d720b41d0

include/gfarm/gflog.h
@@ -41,6 +41,9 @@ void gflog_fatal_message_errno(int, int, const char *, int, const char*,
 void gflog_assert_message(int, const char *, int, const char *,
 		const char *, ...) GFLOG_PRINTF_ARG(5, 6);
 
+#define gflog_fatal(msg_no, ...) \
+	gflog_fatal_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
 #define gflog_error(msg_no, ...) \
 	gflog_message(msg_no, LOG_ERR,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
@@ -57,15 +60,6 @@ void gflog_assert_message(int, const char *, int, const char *,
 	gflog_message(msg_no, LOG_DEBUG,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
 
-#define gflog_fatal(msg_no, ...) \
-	gflog_fatal_message(msg_no, LOG_ERR,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define gflog_auth_error(msg_no, ...)\
-	gflog_message(msg_no, LOG_ERR,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define gflog_auth_warning(msg_no, ...)\
-	gflog_message(msg_no, LOG_WARNING,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
 #define gflog_fatal_errno(msg_no, ...) \
 	gflog_fatal_message_errno(msg_no, LOG_ERR,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
@@ -84,6 +78,14 @@ void gflog_assert_message(int, const char *, int, const char *,
 #define gflog_debug_errno(msg_no, ...) \
 	gflog_message_errno(msg_no, LOG_DEBUG,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#define gflog_auth_error(msg_no, ...)\
+	gflog_auth_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_warning(msg_no, ...)\
+	gflog_auth_message(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+
 #define gflog_trace(msg_no, ...) \
 	gflog_message(msg_no, LOG_INFO,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)

lib/libgfarm/gfutil/logutil.c
@@ -429,3 +429,17 @@ gflog_auth_get_verbose(void)
 {
 	return (authentication_verbose);
 }
+
+void
+gflog_auth_message(int msg_no, int priority, const char *file, int line_no,
+	const char *func, const char *format, ...)
+{
+	va_list ap;
+
+	if (authentication_verbose) {
+		va_start(ap, format);
+		gflog_vmessage(msg_no, priority, file, line_no, func,
+		    format, ap);
+		va_end(ap);
+	}
+}