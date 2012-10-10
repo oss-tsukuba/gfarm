9c7f8b16f47bb000dfe807ae1c1f89fbaa37a016
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-10-10 05:52:56 +0000

    merge r6719 and r6787 from the 2.5 branch to the 2.5.7 branch.  * stress test for #184, #421 and #429 - multiple simultaneous updates for different files, waiting completion of automatic replication, and validation for updated replicas.  * -o and -s options are added to gfreg and gfexport. (hidden options, for test use)  * gfreg can read from stdin.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5.7@6789 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfreg/gfreg_simple.c
+++ gftool/gfreg/gfreg_simple.c
@@ -7,6 +7,7 @@
 #include <libgen.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 
@@ -27,9 +28,20 @@
 char *program_name = "gfreg";
 
 gfarm_error_t
-gfimport(FILE *ifp, GFS_File ogf)
+gfimport(FILE *ifp, GFS_File ogf, gfarm_off_t size)
 {
+	gfarm_error_t e;
 	int c;
+
+	if (size > 0) {
+		while (size > 0 && (c = getc(ifp)) != EOF) {
+			e = gfs_pio_putc(ogf, c);
+			if (e != GFARM_ERR_NO_ERROR)
+				break;
+			size--;
+		}
+		return (gfs_pio_error(ogf));
+	}
 
 	while ((c = getc(ifp)) != EOF)
 		gfs_pio_putc(ogf, c);
@@ -38,11 +50,12 @@ gfimport(FILE *ifp, GFS_File ogf)
 
 gfarm_error_t
 gfimport_to(FILE *ifp, char *gfarm_url, int mode,
-	char *host)
+	char *host, gfarm_off_t off, gfarm_off_t size)
 {
 	gfarm_error_t e, e2;
 	GFS_File gf;
 	gfarm_timerval_t t1, t2, t3, t4, t5;
+	int flags;
 
 	GFARM_TIMEVAL_FIX_INITIALIZE_WARNING(t1);
 	GFARM_TIMEVAL_FIX_INITIALIZE_WARNING(t2);
@@ -51,8 +64,11 @@ gfimport_to(FILE *ifp, char *gfarm_url, int mode,
 	GFARM_TIMEVAL_FIX_INITIALIZE_WARNING(t5);
 
 	gfs_profile(gfarm_gettimerval(&t1));
-	e = gfs_pio_create(
-		gfarm_url, GFARM_FILE_WRONLY|GFARM_FILE_TRUNC, mode, &gf);
+	if (off > 0)
+		flags = GFARM_FILE_WRONLY;
+	else
+		flags = GFARM_FILE_WRONLY|GFARM_FILE_TRUNC;
+	e = gfs_pio_create(gfarm_url, flags, mode, &gf);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "%s: %s\n", gfarm_url, gfarm_error_string(e));
 		return (e);
@@ -66,7 +82,15 @@ gfimport_to(FILE *ifp, char *gfarm_url, int mode,
 	}
 	gfs_profile(gfarm_gettimerval(&t3));
 
-	e = gfimport(ifp, gf);
+	if (off > 0) {
+		e = gfs_pio_seek(gf, off, GFARM_SEEK_SET, NULL);
+		if (e != GFARM_ERR_NO_ERROR) {
+			fprintf(stderr, "seeking %s: %s\n",
+			    gfarm_url, gfarm_error_string(e));
+			goto close;
+		}
+	}
+	e = gfimport(ifp, gf, size);
 	if (e != GFARM_ERR_NO_ERROR)
 		fprintf(stderr, "writing to %s: %s\n", gfarm_url,
 		    gfarm_error_string(e));
@@ -89,24 +113,34 @@ gfimport_to(FILE *ifp, char *gfarm_url, int mode,
 
 gfarm_error_t
 gfimport_from_to(const char *ifile, char *gfarm_url,
-	char *host)
+	char *host, gfarm_off_t off, gfarm_off_t size)
 {
 	gfarm_error_t e;
-	FILE *ifp = fopen(ifile, "r");
+	FILE *ifp;
 	struct stat st;
-	int rv;
+	int rv, save_errno;
 
+	if (strcmp(ifile, "-") == 0)
+		ifp = stdin;
+	else
+		ifp = fopen(ifile, "r");
 	if (ifp == NULL) {
 		perror(ifile);
 		return (GFARM_ERR_CANT_OPEN);
 	}
-	rv = stat(ifile, &st);
-	if (rv == -1) {
-		perror("stat");
-		return (gfarm_errno_to_error(errno));
-	}
-	e = gfimport_to(ifp, gfarm_url, st.st_mode & 0777, host);
-	fclose(ifp);
+	if (ifp != stdin) {
+		rv = stat(ifile, &st);
+		if (rv == -1) {
+			save_errno = errno;
+			fclose(ifp);
+			perror("stat");
+			return (gfarm_errno_to_error(save_errno));
+		}
+	} else
+		st.st_mode = 0400;
+	e = gfimport_to(ifp, gfarm_url, st.st_mode & 0777, host, off, size);
+	if (ifp != stdin)
+		fclose(ifp);
 	return (e);
 }
 
@@ -117,6 +151,11 @@ usage(void)
 	    program_name);
 	fprintf(stderr, "option:\n");
 	fprintf(stderr, "\t%s\n", "-h <hostname>");
+#if 0
+	fprintf(stderr, "\t%s\t%s\n", "-o <offset>",
+	    "skip bytes at start of output, not truncate the file");
+	fprintf(stderr, "\t%s\t%s\n", "-s <size>", "output size");
+#endif
 	fprintf(stderr, "\t%s\t%s\n", "-p", "turn on profiling");
 	fprintf(stderr, "\t%s\t%s\n", "-v", "verbose output");
 	exit(1);
@@ -129,6 +168,7 @@ main(int argc, char **argv)
 	int c, status = 0;
 	char *host = NULL;
 	extern int optind;
+	gfarm_off_t off = -1, size = -1;
 
 	if (argc > 0)
 		program_name = basename(argv[0]);
@@ -139,13 +179,19 @@ main(int argc, char **argv)
 		exit(1);
 	}
 
-	while ((c = getopt(argc, argv, "h:pv?")) != -1) {
+	while ((c = getopt(argc, argv, "h:o:ps:v?")) != -1) {
 		switch (c) {
 		case 'p':
 			gfs_profile_set();
 			break;
 		case 'h':
 			host = optarg;
+			break;
+		case 'o':
+			off = atoll(optarg);
+			break;
+		case 's':
+			size = atoll(optarg);
 			break;
 		case 'v':
 			gflog_auth_set_verbose(1);
@@ -160,7 +206,7 @@ main(int argc, char **argv)
 	if (argc != 2)
 		usage();
 
-	e = gfimport_from_to(argv[0], argv[1], host);
+	e = gfimport_from_to(argv[0], argv[1], host, off, size);
 	if (e != GFARM_ERR_NO_ERROR)
 		status = 1;
 
---
+++ regress/manual/server/gfmd/autoreplica/para-autoreplica.sh
@@ -0,0 +1,251 @@
+#!/bin/bash
+
+##### parameters #####
+MAXRETRY=4 # sec.
+DEBUG=1
+
+##### arguments #####
+if [ $# -lt 4 ]; then
+  name=`basename $0`
+  echo "usage: $name n_attempt size(MB) n_para gfarm.ncopy [hostname]"
+  exit 1
+fi
+n_attempt=$1
+filesize=$2
+n_para=$3
+n_copy=$4
+if [ $# -ge 5 ]; then
+  host=$5
+else
+  host=
+fi
+
+pwd=`pwd`
+name=`basename $pwd`
+if [ x${name} != xregress -o ! -f regress.conf ]; then
+  cd regress > /dev/null 2>&1
+  pwd=`pwd`
+  name=`basename $pwd`
+  if [ x${name} != xregress -o ! -f regress.conf ]; then
+    echo Please change the working directory to {Gfarm source}/regress
+    exit $exit_unsupported
+  fi
+fi
+
+[ ! -f ./regress.conf ] && exit $exit_unsupported
+. ./regress.conf
+[ -z $localtmp ] && exit $exit_unsupported
+
+TMPFILE=$localtmp
+GDIR=$gftmp
+GFILE=$GDIR/file
+
+##### functions #####
+clean_all() {
+  for ((i = 1; i <= $n_para; i++)) {
+    gfrm -f $GFILE-$i
+  }
+  gfrmdir $GDIR > /dev/null 2>&1
+  rm ${TMPFILE}* > /dev/null 2>&1
+}
+
+stop_childlen() {
+  #echo interrupted
+  for ((i = 1; i <= $n_para; i++)) {
+    if [ -n ${procs[i]} ]; then
+      kill ${procs[i]}
+    fi
+  }
+  wait
+}
+
+my_lock() {
+  if [ $FLOCK_OK -eq 1 ]; then
+    flock -x 10
+  fi
+}
+
+my_unlock() {
+  if [ $FLOCK_OK -eq 1 ]; then
+    flock -u 10
+  fi
+}
+
+my_print() {
+  my_lock
+  echo $@
+  my_unlock
+}
+
+interrupt() {
+  intr=1
+}
+
+### child process
+test_overwrite() {
+  trap 'interrupt' $trap_sigs
+  intr=0
+  N=$1  # process index
+  parent_pid=$2
+  F=$GFILE-$N
+  req_ok=0
+  size=17
+  tmp_gfwhere=${TMPFILE}-${N}-gfwhere
+  tmp_data=${TMPFILE}-${N}-data
+  tmp_data2=${TMPFILE}-${N}-data2
+
+  if [ $FLOCK_OK -eq 1 ]; then
+    exec 10>> $TMPFILE
+  fi
+  for ((i = 1; i <= $n_attempt && $intr == 0; i++)) {
+    offset=$i
+    if ((`expr $i % 1000` == 0)); then
+      my_print [$N:$i] progress
+    fi
+    dd if=/dev/urandom bs=1 count=${size} 2> /dev/null > $tmp_data
+    if cat $tmp_data | gfreg -o $offset -s ${size} - $F; then
+      :
+    else
+      kill $parent_pid
+      exit 1
+    fi
+    if false; then ### true for debug
+      my_lock
+      echo [$N:$i] start
+      od -x $tmp_data
+      gfwhere -la $F
+      gfstat $F
+      my_unlock
+    fi
+
+    gfwhere $F > $tmp_gfwhere
+    nrep=`cat $tmp_gfwhere | wc -w`
+    for ((nretry = 1; $nrep != $n_copy && $intr == 0; nretry++)) {
+      if ((nretry > 120)); then  # 120 sec.
+        my_lock
+        echo [$N:$i] timeout
+        gfstat $F
+        gfwhere -al $F
+        my_unlock
+        kill $parent_pid
+        exit 1
+      fi
+      if (($DEBUG != 0)); then
+        my_print [$N:$i] waiting $nretry : `date`
+      fi
+      sleep 1
+      if (($req_ok == 0)); then
+        if (($nretry >= $MAXRETRY)); then
+          my_lock
+          echo [$N:$i] cannot request,
+          echo gfmd.conf may need to increase simultaneous_replication_receivers
+          gfstat $F
+          gfwhere -al $F
+          my_unlock
+          kill $parent_pid
+          exit 1
+        fi
+        nrep=`gfwhere -i $F | wc -w`
+        if (($nrep == $n_copy)); then
+          req_ok=1
+        fi
+      fi
+      if (($req_ok == 1)); then
+        gfwhere $F > $tmp_gfwhere
+        nrep=`cat $tmp_gfwhere | wc -w`
+      fi
+    }
+    ### compare data
+    for h in `cat $tmp_gfwhere`; do
+      gfexport -s ${size} -o $offset -h $h $F > $tmp_data2
+      cmp $tmp_data $tmp_data2 > /dev/null 2>&1
+      if [ $? -ne 0 ]; then
+        my_lock
+        echo [$N:$i]@$h: different data
+        od -x $tmp_data
+        od -x $tmp_data2
+        gfwhere -al $F
+        my_unlock
+        kill $parent_pid
+        exit 1
+      elif false; then ### true for debug
+        my_lock
+        echo [$N:$i]@$h: same data
+        od -x $tmp_data
+        od -x $tmp_data2
+        gfwhere -al $F
+        my_unlock
+      fi
+    done
+    if (($DEBUG != 0 && $intr == 0)); then
+      my_print [$N:$i] OK
+    fi
+  }
+  exit 0
+}
+
+set_ncopy() {
+  if echo -n $1 | gfxattr -s $2 gfarm.ncopy; then
+    :
+   else
+    echo failed gfxattr -s
+    clean_test
+    exit $exit_fail
+  fi
+}
+
+clean_exit() {
+  clean_all
+  exit $1
+}
+
+##### main #####
+clean_all
+
+if [ ! -e $GDIR ]; then
+  gfmkdir $GDIR || exit $exit_fail
+fi
+trap 'clean_all; exit $exit_trap' $trap_sigs
+
+echo "craete tmpfile: $TMPFILE: $filesize MB"
+dd if=/dev/urandom of=$TMPFILE bs=1M count=$filesize > /dev/null 2>&1
+if (($? != 0)); then
+  echo dd failed
+  exit $exit_fail
+fi
+echo "register files to Gfarm: $filesize MB * $n_para"
+for ((i = 1; i <= $n_para; i++)) {
+  if [ -z $host ]; then
+    gfreg $TMPFILE $GFILE-$i || clean_exit $exit_fail
+  else
+    gfreg -h $host $TMPFILE $GFILE-$i || clean_exit $exit_fail
+  fi
+}
+
+FLOCK_OK=0
+exec 10>> $TMPFILE
+flock -e 10 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  FLOCK_OK=1
+  flock -u 10
+  exec 10>&-
+fi
+
+set_ncopy $n_copy $GDIR
+
+echo -n set gfarm.ncopy=
+gfxattr -g $GDIR gfarm.ncopy
+echo
+
+echo "start overwrite test: $n_attempt times * $n_para parallels"
+trap 'echo interrupt; stop_childlen; clean_all; exit $exit_fail' $trap_sigs
+procs=()
+for ((i = 1; i <= $n_para; i++)) {
+  test_overwrite $i $$ &
+  procs[$i]=$!
+}
+wait
+
+clean_all
+
+exit $exit_pass
