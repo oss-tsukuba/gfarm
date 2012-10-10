7612d8ca85f059f78b015a900b12d731f168ea74
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-10-10 06:04:17 +0000

    merge r6744 from the 2.5 branch to the 2.5.7 branch.  * fix #436 - gfprep and gfpcopy may work slowly  * change the behavior that gfprep and gfpcopy check real disk_avail of each gfsd. The check is not executed in scheduling on parent process, and will be executed just before sending data on each child process.  * plug memory leak in case of gfprep -x
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5.7@6790 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfprep/gfarm_pfunc.h
+++ gftool/gfprep/gfarm_pfunc.h
@@ -11,11 +11,14 @@ gfarm_error_t gfarm_pfunc_cmd_add(gfarm_pfunc_t *, gfarm_pfunc_cmd_t *);
 gfarm_error_t gfarm_pfunc_interrupt(gfarm_pfunc_t *);
 gfarm_error_t gfarm_pfunc_join(gfarm_pfunc_t *);
 
-gfarm_error_t gfarm_pfunc_replicate(gfarm_pfunc_t *, const char *,
-				    const char *, int,
-				    const char *, int, void *, int);
-gfarm_error_t gfarm_pfunc_remove_replica(gfarm_pfunc_t *, const char *,
-					 const char *, int, void *);
-gfarm_error_t gfarm_pfunc_copy(gfarm_pfunc_t *,
-			       const char *, const char *, int,
-			       const char *, const char *, int, void *, int);
+gfarm_error_t gfarm_pfunc_replicate(
+	gfarm_pfunc_t *, const char *, const char *, int, gfarm_off_t,
+	const char *, int, void *, int, int);
+
+gfarm_error_t gfarm_pfunc_remove_replica(
+	gfarm_pfunc_t *, const char *,
+	const char *, int, gfarm_off_t, void *);
+gfarm_error_t gfarm_pfunc_copy(
+	gfarm_pfunc_t *,
+	const char *, const char *, int, gfarm_off_t,
+	const char *, const char *, int, void *, int, int);
