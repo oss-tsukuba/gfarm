023836a3bd6113176e12f0e6df7bd374a044c239
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-10-16 09:34:23 +0000

    * add -D, -H and -U options.(#491 - gfpcopy-test.sh: specify hosts or domain)  * print error message if gfstat fails.  * support a pathname with spaces.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6827 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfprep/gfpcopy-test/gfpcopy-test.sh
+++ gftool/gfprep/gfpcopy-test/gfpcopy-test.sh
@@ -15,13 +15,21 @@ DIR_PATTERN="/gfpcopy-test.${HOSTNAME}.$$"
 DIR_PATTERN2="/gfpcopy-test.2.${HOSTNAME}.$$"
 TMP_FILE="/tmp/gfpcopy-test.sh.${HOSTNAME}.$$"
 
-function sig_handler() {
+OPT_DH=
+OPT_DH_VAL=
+OPT_U=
+
+function test_clean() {
     local dir
     remove_local_files > /dev/null 2>&1
     remove_gfarm_files > /dev/null 2>&1
-    dir=${LOCAL_DIR}${DIR_PATTERN2}
-    rm -rf ${dir} > /dev/null 2>&1
+    dir="${LOCAL_DIR}${DIR_PATTERN2}"
+    rm -rf "${dir}" > /dev/null 2>&1
     rm ${TMP_FILE}
+}
+
+function sig_handler() {
+    test_clean
     exit 0
 }
 
@@ -43,11 +51,11 @@ function print_result() {
 
 function create_local_files() {
     local i tmp_dir src
-    tmp_dir=${LOCAL_DIR}${DIR_PATTERN}
-    mkdir -p $tmp_dir
+    tmp_dir="${LOCAL_DIR}${DIR_PATTERN}"
+    mkdir -p "$tmp_dir"
     for i in `seq $1` ; do
-	src=${tmp_dir}"/test."${i}.dat
-	dd if=/dev/zero of=${src} \
+	src="${tmp_dir}/test.${i}.dat"
+	dd if=/dev/zero of="${src}" \
            bs=1024 count=$(($2 / 1024)) > /dev/null 2>&1
 	if [[ $? -ne 0 ]]; then
 	    echo "cannot create a test file! ("${src}")"
@@ -58,113 +66,161 @@ function create_local_files() {
 
 function remove_local_files() {
     local tmp_dir
-    tmp_dir=${LOCAL_DIR}${DIR_PATTERN}
-    rm -rf $tmp_dir
+    tmp_dir="${LOCAL_DIR}${DIR_PATTERN}"
+    rm -rf "$tmp_dir"
+}
+
+function exec_gfpcopy() {
+    local j p src dst
+    use_opt=$1
+    j=$2
+    p=$3
+    src=$4
+    dst=$5
+    if [ $use_opt -eq 1 -a -n "$OPT_DH" ]; then
+        gfpcopy $OPT_DH "$OPT_DH_VAL" $OPT_U -j $j $p "$src" "$dst"
+    else
+        gfpcopy $OPT_U -j $j $p "$src" "$dst"
+    fi
+    if [ $? -ne 0 ]; then
+        echo gfpcopy failed 1>&2
+        test_clean
+        exit 1
+    fi
 }
 
 function create_gfarm_files() {
     local src_dir dst_dir
-    src_dir="file://"${LOCAL_DIR}${DIR_PATTERN}
-    dst_dir=${GFARM_DIR}
+    src_dir="file://${LOCAL_DIR}${DIR_PATTERN}"
+    dst_dir="${GFARM_DIR}"
 
-    gfpcopy ${src_dir} ${dst_dir}
+    exec_gfpcopy 1 4 "" "${src_dir}" "${dst_dir}"
 }
 
 function remove_gfarm_files() {
     local tmp_dir
-    tmp_dir=${GFARM_DIR}${DIR_PATTERN}
-    gfrm -rf $tmp_dir
+    tmp_dir="${GFARM_DIR}${DIR_PATTERN}"
+    gfrm -rf "$tmp_dir"
 }
 
 function do_gfpcopy_togfarm() {
     local src_dir dst_dir tmp_str val time
-    src_dir="file://"${LOCAL_DIR}${DIR_PATTERN}
-    dst_dir=${GFARM_DIR}${DIR_PATTERN}
+    src_dir="file://${LOCAL_DIR}${DIR_PATTERN}"
+    dst_dir="${GFARM_DIR}${DIR_PATTERN}"
 
     print_parallels $1
 
-    tmp_str=`gfpcopy -p -j $1 ${src_dir} ${dst_dir}`
+    tmp_str=`exec_gfpcopy 1 $1 -p "${src_dir}" "${dst_dir}"`
     val=`echo ${tmp_str}  | cut -d ' ' -f 8`
     time=`echo ${tmp_str}  | cut -d ' ' -f 11`
     print_result $val $time
-    gfrm -rf $dst_dir
+    gfrm -rf "$dst_dir"
 }
 
 function do_gfpcopy_fromgfarm() {
     local src_dir dst_dir tmp_str val time
-    src_dir=${GFARM_DIR}${DIR_PATTERN}
-    dst_dir=${LOCAL_DIR}${DIR_PATTERN2}
+    src_dir="${GFARM_DIR}${DIR_PATTERN}"
+    dst_dir="${LOCAL_DIR}${DIR_PATTERN2}"
 
     print_parallels $1
 
-    tmp_str=`gfpcopy -p -j $1 ${src_dir} "file://"${dst_dir}`
+    tmp_str=`exec_gfpcopy 0 $1 -p "${src_dir}" "file://${dst_dir}"`
     val=`echo ${tmp_str}  | cut -d ' ' -f 8`
     time=`echo ${tmp_str}  | cut -d ' ' -f 11`
     print_result $val $time
-    rm -rf ${dst_dir}
+    rm -rf "${dst_dir}"
 }
 
 function do_gfprep() {
     local tmp_dir tmp_str val time
-    tmp_dir=${GFARM_DIR}${DIR_PATTERN}
+    tmp_dir="${GFARM_DIR}${DIR_PATTERN}"
 
     [ `gfsched -w | wc -l` -ge  ${NUM_REPLICA} ] || return 1
 
     print_parallels $1
 
-    tmp_str=`gfprep -p -N ${NUM_REPLICA} -j $1 ${tmp_dir}`
+    tmp_str=`gfprep -p -N ${NUM_REPLICA} -j $1 "${tmp_dir}"`
     val=`echo ${tmp_str}  | cut -d ' ' -f 8`
     time=`echo ${tmp_str}  | cut -d ' ' -f 11`
     print_result $val $time
 }
 
-function usage() {
-    echo "usage : $0 -l <local directory> -g <gfarm directory>"
-    exit 1
-}
-
-# main
-
-OPT=`getopt "hl:g:" $@` || usage $@
-set -- $OPT
-
-while [[ -- != "$1" ]]; do
-    case $1 in
-	-h)
-	    usage
-	    ;;
-	-l)
-	    LOCAL_DIR=$2; shift
-	    ;;
-	-g)
-	    GFARM_DIR=$2; shift
-	    ;;
-    esac
-    shift 
-done
-
 # default value
 : ${LOCAL_DIR:=$PWD}
 : ${GFARM_DIR:=/tmp}
 
-if [[ ! -d $LOCAL_DIR ]]; then
-    echo ${LOCAL_DIR} is not a directory.
+DEFAULT_LOCAL_DIR="$LOCAL_DIR"
+DEFAULT_GFARM_DIR="$GFARM_DIR"
+
+function usage() {
+    echo "usage: $0 [options]"
+    echo "options:"
+    echo "  -l <local dir> (default: $DEFAULT_LOCAL_DIR)"
+    echo "  -g <gfarm dir> (default: $DEFAULT_GFARM_DIR)"
+    echo "  -D <domain>    create files on the domain"
+    echo "  -H <hostlist>  create files on the hosts in hostlist"
+    echo "  -U             disable checking disk_avail before copying"
+    exit 1
+}
+
+function conflict_check() {
+    if [ -n "$OPT_DH" ]; then
+        echo -${1} option conflicts with $OPT_DH 1>&2
+        usage
+    fi
+}
+
+# main
+while getopts hl:g:D:H:U opt; do
+    case $opt in
+        l)
+            LOCAL_DIR="$OPTARG"
+            ;;
+        g)
+            GFARM_DIR="$OPTARG"
+            ;;
+        D)
+            conflict_check $opt
+            OPT_DH=-D;
+            OPT_DH_VAL="$OPTARG"
+            ;;
+        H)
+            conflict_check $opt
+            OPT_DH=-H;
+            OPT_DH_VAL="$OPTARG"
+            ;;
+        U)
+            OPT_U=-U
+            ;;
+        h | ?)
+            usage
+            ;;
+    esac
+done
+
+
+if [[ ! -d "$LOCAL_DIR" ]]; then
+    echo "${LOCAL_DIR}" is not a directory.
     exit 1
 fi
 
-tmp=`gfstat ${GFARM_DIR} 2> /dev/null | grep "Filetype: directory"`
-
-if [[ X${tmp} == X ]]; then
-    echo ${GFARM_DIR} is not a directory in the Gfarm file system.
+st=`gfstat "${GFARM_DIR}" 2>&1`
+if [[ $? -ne 0 ]]; then
+    echo $st 1>&2
+    exit 1
+fi
+tmp=`echo -n $st | grep "Filetype: directory"`
+if [[ X"${tmp}" == X ]]; then
+    echo "${GFARM_DIR} is not a directory in the Gfarm file system."
     exit 1
 fi
 
-if [[ ! ${GFARM_DIR} =~ "^gfarm:*" ]]; then
+if [[ ! ${GFARM_DIR} =~ ^gfarm:* ]]; then
     case X"${GFARM_DIR}" in
 	X/*)
-	    GFARM_DIR="gfarm://"${GFARM_DIR} ;;
+	    GFARM_DIR="gfarm://${GFARM_DIR}" ;;
 	*)
-	    GFARM_DIR="gfarm:///"${GFARM_DIR} ;;
+	    GFARM_DIR="gfarm:///${GFARM_DIR}" ;;
     esac
 fi
 
