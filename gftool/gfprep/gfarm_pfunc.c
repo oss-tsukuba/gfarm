970598fadc18d2b1c920bdf50ea44a410884ffd3
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-10-17 03:21:39 +0000

    * merge r6830 and r6831 from the 2.5 branch  * sequel to r6752 - #492 gfpcopy supports nanosecond for atime and mtime.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6832 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfprep/gfarm_pfunc.c
+++ gftool/gfprep/gfarm_pfunc.c
@@ -65,12 +65,12 @@ enum pfunc_cmdnum {
 	PFUNC_CMD_TERMINATE
 };
 
-enum pfunc_stat {
-	PFUNC_STAT_OK,
-	PFUNC_STAT_NG,
-	PFUNC_STAT_WAIT_OK,
-	PFUNC_STAT_END,
-	PFUNC_STAT_FATAL
+enum pfunc_result {
+	PFUNC_RESULT_OK,
+	PFUNC_RESULT_NG,
+	PFUNC_RESULT_WAIT_OK,
+	PFUNC_RESULT_END,
+	PFUNC_RESULT_FATAL
 };
 
 enum pfunc_mode {
@@ -170,7 +170,7 @@ pfunc_replicate_main(gfarm_pfunc_t *handle, int pfunc_mode,
 				"checking disk_avail: %s (%s:%d, %s:%d): %s\n",
 				src_url, src_host, src_port,
 				dst_host, dst_port, gfarm_error_string(e));
-			gfpara_send_int(to_parent, PFUNC_STAT_NG);
+			gfpara_send_int(to_parent, PFUNC_RESULT_NG);
 			goto free_mem;
 		}
 	}
@@ -181,7 +181,7 @@ pfunc_replicate_main(gfarm_pfunc_t *handle, int pfunc_mode,
 			"ERROR: cannot replicate: %s (%s:%d, %s:%d): %s\n",
 			src_url, src_host, src_port, dst_host, dst_port,
 			gfarm_error_string(e));
-		gfpara_send_int(to_parent, PFUNC_STAT_NG);
+		gfpara_send_int(to_parent, PFUNC_RESULT_NG);
 		goto free_mem;
 	}
 	if (pfunc_mode == PFUNC_MODE_MIGRATE) {
@@ -201,7 +201,7 @@ pfunc_replicate_main(gfarm_pfunc_t *handle, int pfunc_mode,
 		}
 	}
 end:
-	gfpara_send_int(to_parent, PFUNC_STAT_OK);
+	gfpara_send_int(to_parent, PFUNC_RESULT_OK);
 free_mem:
 	free(src_url);
 	free(src_host);
@@ -248,29 +248,50 @@ pfunc_open(const char *url, int flags, int mode, struct pfunc_file *fp)
 	return (GFARM_ERR_NO_ERROR);
 }
 
-static gfarm_error_t
-pfunc_fstat(struct pfunc_file *fp, struct stat *stp)
-{
-	int retv;
+struct pfunc_stat {
+	gfarm_int32_t mode;
+	gfarm_int64_t size;
+	gfarm_int64_t atime_sec;
+	gfarm_int32_t atime_nsec;
+	gfarm_int64_t mtime_sec;
+	gfarm_int32_t mtime_nsec;
+};
 
+static gfarm_error_t
+pfunc_fstat(struct pfunc_file *fp, struct pfunc_stat *stp)
+{
+#ifdef __GNUC__ /* to shut up gcc warning "may be used uninitialized" */
+	stp->mode = stp->size = 0;
+	stp->mtime_sec = stp->atime_sec = 0;
+	stp->mtime_nsec = stp->atime_nsec = 0;
+#endif
 	if (fp->gfarm) {
 		struct gfs_stat gst;
 		gfarm_error_t e = gfs_pio_stat(fp->gfarm, &gst);
-		if (e == GFARM_ERR_NO_ERROR) {
-			/* XXX copy all of st_*  */
-			stp->st_mode = gst.st_mode;
-			stp->st_size = gst.st_size;
-			stp->st_atime = gst.st_atimespec.tv_sec;
-			stp->st_mtime = gst.st_mtimespec.tv_sec;
-			stp->st_ctime = gst.st_ctimespec.tv_sec;
-			/* XXX tv_nsec */
-			gfs_stat_free(&gst);
-		}
-		return (e);
+
+		if (e != GFARM_ERR_NO_ERROR)
+			return (e);
+		stp->mode = gst.st_mode;
+		stp->size = gst.st_size;
+		stp->atime_sec = gst.st_atimespec.tv_sec;
+		stp->atime_nsec = gst.st_atimespec.tv_nsec;
+		stp->mtime_sec = gst.st_mtimespec.tv_sec;
+		stp->mtime_nsec = gst.st_mtimespec.tv_nsec;
+		gfs_stat_free(&gst);
+	} else {
+		int retv;
+		struct stat st;
+
+		retv = fstat(fp->fd, &st);
+		if (retv == -1)
+			return (gfarm_errno_to_error(errno));
+		stp->mode = st.st_mode;
+		stp->size = st.st_size;
+		stp->atime_sec = st.st_atime;
+		stp->atime_nsec = gfarm_stat_atime_nsec(&st);
+		stp->mtime_sec = st.st_mtime;
+		stp->mtime_nsec = gfarm_stat_mtime_nsec(&st);
 	}
-	retv = fstat(fp->fd, stp);
-	if (retv == -1)
-		return (gfarm_errno_to_error(errno));
 	return (GFARM_ERR_NO_ERROR);
 }
 
@@ -333,22 +354,29 @@ pfunc_close(struct pfunc_file *fp)
 }
 
 static gfarm_error_t
-pfunc_utimes(const char *url, struct timeval *tv)
+pfunc_utimens(const char *url, struct pfunc_stat *stp)
 {
 	if (gfprep_url_is_local(url)) {
 		int retv;
-		const char *path = url;
-		path += GFPREP_FILE_URL_PREFIX_LENGTH;
+		const char *path = url + GFPREP_FILE_URL_PREFIX_LENGTH;
+		struct timeval tv[2];
+
+		tv[0].tv_sec = stp->atime_sec;
+		tv[0].tv_usec = stp->atime_nsec / GFARM_MICROSEC_BY_NANOSEC;
+		tv[1].tv_sec = stp->mtime_sec;
+		tv[1].tv_usec = stp->mtime_nsec / GFARM_MICROSEC_BY_NANOSEC;
+		/* XXX support utimensat() */
 		retv = utimes(path, tv);
 		if (retv == -1)
 			return (gfarm_errno_to_error(errno));
 	} else if (gfprep_url_is_gfarm(url)) {
 		gfarm_error_t e;
 		struct gfarm_timespec gt[2];
-		gt[0].tv_sec = tv[0].tv_sec;
-		gt[0].tv_nsec = tv[0].tv_usec * 1000;
-		gt[1].tv_sec = tv[1].tv_sec;
-		gt[1].tv_nsec = tv[1].tv_usec * 1000;
+
+		gt[0].tv_sec = stp->atime_sec;
+		gt[0].tv_nsec = stp->atime_nsec;
+		gt[1].tv_sec = stp->mtime_sec;
+		gt[1].tv_nsec = stp->mtime_nsec;
 		e = gfs_utimes(url, gt);
 		if (e != GFARM_ERR_NO_ERROR)
 			return (e);
@@ -412,8 +440,7 @@ pfunc_copy_main(gfarm_pfunc_t *handle, int pfunc_mode,
 	int src_port, dst_port;
 	int rsize, wsize;
 	struct pfunc_file src_fp, dst_fp;
-	struct stat st;
-	struct timeval tv[2];
+	struct pfunc_stat st;
 	gfarm_off_t src_size;
 	int check_disk_avail;
 
@@ -465,7 +492,7 @@ pfunc_copy_main(gfarm_pfunc_t *handle, int pfunc_mode,
 		goto end;
 	}
 	e = pfunc_open(tmp_url, O_CREAT | O_WRONLY | O_TRUNC,
-		       st.st_mode & 07777, &dst_fp);
+		       st.mode & 07777, &dst_fp);
 	if (e != GFARM_ERR_NO_ERROR) {
 		(void) pfunc_close(&src_fp);
 		fprintf(stderr, "ERROR: copy failed: open(%s): %s\n",
@@ -473,7 +500,7 @@ pfunc_copy_main(gfarm_pfunc_t *handle, int pfunc_mode,
 		ng = 1;
 		goto end;
 	}
-	if (st.st_size > 0 && src_fp.gfarm && strcmp(src_host, "") != 0) {
+	if (st.size > 0 && src_fp.gfarm && strcmp(src_host, "") != 0) {
 		/* XXX FIXME: INTERNAL FUNCTION SHOULD NOT BE USED */
 		e = gfs_pio_internal_set_view_section(src_fp.gfarm, src_host);
 		if (e != GFARM_ERR_NO_ERROR) {
@@ -484,7 +511,7 @@ pfunc_copy_main(gfarm_pfunc_t *handle, int pfunc_mode,
 			goto close;
 		}
 	}
-	if (st.st_size > 0 && dst_fp.gfarm && strcmp(dst_host, "") != 0) {
+	if (st.size > 0 && dst_fp.gfarm && strcmp(dst_host, "") != 0) {
 		/* XXX FIXME: INTERNAL FUNCTION SHOULD NOT BE USED */
 		e = gfs_pio_internal_set_view_section(dst_fp.gfarm, dst_host);
 		if (e != GFARM_ERR_NO_ERROR) {
@@ -536,11 +563,7 @@ close:
 		ng = 1;
 		goto end;
 	}
-	tv[0].tv_sec = st.st_atime;
-	tv[0].tv_usec = 0; /* XXX */
-	tv[1].tv_sec = st.st_mtime;
-	tv[1].tv_usec = 0; /* XXX */
-	e = pfunc_utimes(tmp_url, tv);
+	e = pfunc_utimens(tmp_url, &st);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "ERROR: copy failed: utimes(%s): %s\n",
 			tmp_url, gfarm_error_string(e));
@@ -557,14 +580,14 @@ close:
 	/* XXX pfunc_mode == PFUNC_MODE_MIGRATE : unlink src_url */
 end:
 	if (ng) {
-		gfpara_send_int(to_parent, PFUNC_STAT_NG);
+		gfpara_send_int(to_parent, PFUNC_RESULT_NG);
 		e = pfunc_unlink(tmp_url);
 		if (e != GFARM_ERR_NO_ERROR)
 			fprintf(stderr,
 				"ERROR: cannot remove tmp-file: %s: %s\n",
 			tmp_url, gfarm_error_string(e));
 	} else
-		gfpara_send_int(to_parent, PFUNC_STAT_OK);
+		gfpara_send_int(to_parent, PFUNC_RESULT_OK);
 	free(tmp_url);
 	free(src_url);
 	free(dst_url);
@@ -585,7 +608,7 @@ pfunc_remove_replica_main(gfarm_pfunc_t *handle,
 	gfpara_recv_int(from_parent, &src_port);
 
 	if (handle->simulate_KBs > 0) {
-		gfpara_send_int(to_parent, PFUNC_STAT_OK);
+		gfpara_send_int(to_parent, PFUNC_RESULT_OK);
 		goto end;
 	}
 
@@ -595,9 +618,9 @@ pfunc_remove_replica_main(gfarm_pfunc_t *handle,
 			"ERROR: cannot remove replica: %s (%s:%d): %s\n",
 			src_url, src_host, src_port,
 			gfarm_error_string(e));
-		gfpara_send_int(to_parent, PFUNC_STAT_NG);
+		gfpara_send_int(to_parent, PFUNC_RESULT_NG);
 	} else
-		gfpara_send_int(to_parent, PFUNC_STAT_OK);
+		gfpara_send_int(to_parent, PFUNC_RESULT_OK);
 end:
 	free(src_url);
 	free(src_host);
@@ -615,7 +638,7 @@ pfunc_child(void *param, FILE *from_parent, FILE *to_parent)
 		fprintf(stderr, "ERROR: gfarm_initialize: %s\n",
 			gfarm_error_string(e));
 		gfpara_recv_purge(from_parent);
-		gfpara_send_int(to_parent, PFUNC_STAT_FATAL);
+		gfpara_send_int(to_parent, PFUNC_RESULT_FATAL);
 		return (0);
 	}
 	for (;;) {
@@ -642,13 +665,13 @@ pfunc_child(void *param, FILE *from_parent, FILE *to_parent)
 						  from_parent, to_parent);
 			continue;
 		case PFUNC_CMD_TERMINATE:
-			gfpara_send_int(to_parent, PFUNC_STAT_END);
+			gfpara_send_int(to_parent, PFUNC_RESULT_END);
 			goto term;
 		default:
 			fprintf(stderr,
 				"ERROR: unexpected command = %d\n", command);
 			gfpara_recv_purge(from_parent);
-			gfpara_send_int(to_parent, PFUNC_STAT_FATAL);
+			gfpara_send_int(to_parent, PFUNC_RESULT_FATAL);
 			goto term;
 		}
 	}
@@ -770,16 +793,16 @@ pfunc_recv(FILE *child_out, gfpara_proc_t *proc, void *param)
 
 	gfpara_recv_int(child_out, &status);
 	switch (status) {
-	case PFUNC_STAT_OK:
-	case PFUNC_STAT_NG:
+	case PFUNC_RESULT_OK:
+	case PFUNC_RESULT_NG:
 		if (handle->cb_end && data)
-			handle->cb_end(status == PFUNC_STAT_OK, data);
+			handle->cb_end(status == PFUNC_RESULT_OK, data);
 		return (GFPARA_NEXT);
-	case PFUNC_STAT_WAIT_OK:
+	case PFUNC_RESULT_WAIT_OK:
 		return (GFPARA_NEXT);
-	case PFUNC_STAT_END:
+	case PFUNC_RESULT_END:
 		return (GFPARA_END);
-	case PFUNC_STAT_FATAL:
+	case PFUNC_RESULT_FATAL:
 	default:
 		gfpara_recv_purge(child_out);
 		return (GFPARA_FATAL);
