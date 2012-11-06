06cf8daa5a1c859d15253b23769a9b963ba6920b
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-11-06 11:13:07 +0000

    * add regression tests for #517 - "cp -a dir1 dir2" on gfarm2fs: Invalid argument
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@7009 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/lib/libgfarm/gfarm/gfs_acl/empty_default_file.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./regress.conf
+
+empty_acl=`dirname $testbin`/empty_acl/empty_acl
+
+trap 'gfrm -f $gftmp; exit $exit_trap' $trap_sigs
+
+if gfreg data/1byte $gftmp &&
+   $empty_acl -d $gftmp
+then
+    exit_code=$exit_pass
+fi
+
+gfrm -f $gftmp
+exit $exit_code
