8a2f76bf639460518bdf74727da067fcd8d8fd7a
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-11-06 11:20:53 +0000

    merge r7009 from the 2.5 branch.  * add regression tests for #517 - "cp -a dir1 dir2" on gfarm2fs: Invalid argument
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5.7@7011 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/lib/libgfarm/gfarm/gfs_acl/empty_access_dir.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./regress.conf
+
+empty_acl=`dirname $testbin`/empty_acl/empty_acl
+
+trap 'gfrmdir $gftmp; exit $exit_trap' $trap_sigs
+
+if gfmkdir $gftmp &&
+   $empty_acl -a $gftmp
+then
+    exit_code=$exit_pass
+fi
+
+gfrmdir $gftmp
+exit $exit_code
