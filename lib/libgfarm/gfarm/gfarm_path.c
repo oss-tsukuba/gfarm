9eb48e0f2955cb322f1e794842b29b698f46caec
Osamu Tatebe <tatebe@users.sourceforge.net>
2012-11-23 13:09:09 +0000

    merge r7047 from the 2.5 branch - use gfarm2fs.url instead of gfarm2fs.path to support multiple Gfarm file systems
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7155 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfarm_path.c
+++ lib/libgfarm/gfarm/gfarm_path.c
@@ -18,24 +18,24 @@ gfarm_realpath_by_gfarm2fs(const char *path, char **pathp)
 #ifdef HAVE_SYS_XATTR_H
 	char *p, *parent = NULL;
 	const char *base;
-	static const char gfarm2fs_path[] = "gfarm2fs.path";
+	static const char gfarm2fs_url[] = "gfarm2fs.url";
 	size_t s = 0;
 	int saved_errno, base_len = 0;
 #ifdef __GNUC__ /* workaround gcc warning: may be used uninitialized */
 	base = NULL;
 #endif
 
-	s = lgetxattr(path, gfarm2fs_path, NULL, 0);
+	s = lgetxattr(path, gfarm2fs_url, NULL, 0);
 	if (s == -1) {
-		parent = gfarm_path_dir(path);
+		parent = gfarm_url_dir(path);
 		if (parent == NULL)
 			return (GFARM_ERR_NO_MEMORY);
-		s = lgetxattr(parent, gfarm2fs_path, NULL, 0);
+		s = lgetxattr(parent, gfarm2fs_url, NULL, 0);
 		if (s == -1) {
 			free(parent);
 			return (GFARM_ERR_NO_SUCH_OBJECT);
 		}
-		base = gfarm_path_dir_skip(path);
+		base = gfarm_url_dir_skip(path);
 		base_len = strlen(base) + 1; /* 1 for '/' */
 		path = parent;
 	}
@@ -46,7 +46,7 @@ gfarm_realpath_by_gfarm2fs(const char *path, char **pathp)
 	GFARM_MALLOC_ARRAY(p, s + base_len + 1); /* 1 for '\0' */
 	if (p == NULL)
 		return (GFARM_ERR_NO_MEMORY);
-	if (lgetxattr(path, gfarm2fs_path, p, s) == -1) {
+	if (lgetxattr(path, gfarm2fs_url, p, s) == -1) {
 		saved_errno = errno;
 		free(p);
 		return (gfarm_errno_to_error(saved_errno));
