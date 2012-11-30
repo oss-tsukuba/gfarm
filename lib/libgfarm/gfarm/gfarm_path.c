0b15046f36224b215ed83f781f0defe800474d6d
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-11-30 13:21:46 +0000

    merge r7218, r7219, r7220, r7221 and r7222 from the main trunk: fix #552 - installation errors on Mac OS X
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@7223 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/gfarm_path.c
+++ lib/libgfarm/gfarm/gfarm_path.c
@@ -25,12 +25,20 @@ gfarm_realpath_by_gfarm2fs(const char *path, char **pathp)
 	base = NULL;
 #endif
 
+#ifdef XATTR_NOFOLLOW /* Mac OS X */
+	s = getxattr(path, gfarm2fs_url, NULL, 0, 0, XATTR_NOFOLLOW);
+#else
 	s = lgetxattr(path, gfarm2fs_url, NULL, 0);
+#endif
 	if (s == -1) {
 		parent = gfarm_url_dir(path);
 		if (parent == NULL)
 			return (GFARM_ERR_NO_MEMORY);
+#ifdef XATTR_NOFOLLOW /* Mac OS X */
+		s = getxattr(parent, gfarm2fs_url, NULL, 0, 0, XATTR_NOFOLLOW);
+#else
 		s = lgetxattr(parent, gfarm2fs_url, NULL, 0);
+#endif
 		if (s == -1) {
 			free(parent);
 			return (GFARM_ERR_NO_SUCH_OBJECT);
@@ -46,7 +54,11 @@ gfarm_realpath_by_gfarm2fs(const char *path, char **pathp)
 	GFARM_MALLOC_ARRAY(p, s + base_len + 1); /* 1 for '\0' */
 	if (p == NULL)
 		return (GFARM_ERR_NO_MEMORY);
+#ifdef XATTR_NOFOLLOW /* Mac OS X */
+	if (getxattr(path, gfarm2fs_url, p, s, 0, XATTR_NOFOLLOW) == -1) {
+#else
 	if (lgetxattr(path, gfarm2fs_url, p, s) == -1) {
+#endif
 		saved_errno = errno;
 		free(p);
 		return (gfarm_errno_to_error(saved_errno));
