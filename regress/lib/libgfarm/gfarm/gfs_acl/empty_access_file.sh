776c1b9a3bf9bae9e4fe162b3f11b4830a96c69a
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-11-30 19:54:47 +0000

    merge r7237 from the 2.6 branch: make these test scripts work with --srcdir option of the configure command. these tests are for #517 - "cp -a dir1 dir2" on gfarm2fs: Invalid argument.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@7238 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/lib/libgfarm/gfarm/gfs_acl/empty_access_file.sh
+++ regress/lib/libgfarm/gfarm/gfs_acl/empty_access_file.sh
@@ -6,7 +6,7 @@ empty_acl=`dirname $testbin`/empty_acl/empty_acl
 
 trap 'gfrm -f $gftmp; exit $exit_trap' $trap_sigs
 
-if gfreg data/1byte $gftmp &&
+if gfreg $data/1byte $gftmp &&
    $empty_acl -a $gftmp
 then
     exit_code=$exit_pass
