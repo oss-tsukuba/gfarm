d51777a877a649f7175cb76d44fb21164b1ea168
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-12-03 16:39:33 +0000

    merge r5851 from the main trunk (for #527 - [pullup-2.6.0] #296 - fault tolerancy about gfmd failover)  * fix #319: segfault in gfsd when gfsd fails gfmd connection failover
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@7296 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfsd/gfsd.c
+++ server/gfsd/gfsd.c
@@ -434,10 +434,14 @@ reconnect_gfm_server_for_failover(const char *diag)
 	gflog_error(GFARM_MSG_1003348,
 	    "%s: gfmd may be failed over, try to reconnecting", diag);
 	free_gfm_server();
-	if ((e = connect_gfm_server_with_timeout()) != GFARM_ERR_NO_ERROR)
+	if ((e = connect_gfm_server_with_timeout()) != GFARM_ERR_NO_ERROR) {
+		gfm_client_connection_free(gfm_server);
+		/* mark gfmd reconnection failed */
+		gfm_server = NULL;
 		fatal(GFARM_MSG_UNFIXED,
 		    "%s: cannot reconnect to gfm server: %s",
 		    diag, gfarm_error_string(e));
+	}
 	++server_failover_count;
 }
 
