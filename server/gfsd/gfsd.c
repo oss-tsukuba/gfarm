12d0849590f27c8deb8c3789c62b23d2df7ac29a
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-12-05 03:15:28 +0000

    merge r6809 and r6811 from the 2.5 branch to fix the following problem: #482 - incomplete replica is left due to missing reply about GFS_PROTO_REPLICATION_REQUEST from gfsd to gfmd, as part of #485 [pullup-2.5.7.3] #408 - incomplete replica is left
    the cause is found and the fix is supplied by Kiyoshi Imai-san.
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5.7@7346 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfsd/gfsd.c
+++ server/gfsd/gfsd.c
@@ -2616,12 +2616,16 @@ try_replication(struct gfp_xdr *conn, struct gfarm_hash_entry *q,
 gfarm_error_t
 start_replication(struct gfp_xdr *conn, struct gfarm_hash_entry *q)
 {
-	gfarm_error_t gfmd_err, dst_err, src_err = GFARM_ERR_NO_ERROR;
+	gfarm_error_t gfmd_err, dst_err, src_err;
+	gfarm_error_t src_net_err = GFARM_ERR_NO_ERROR;
+	int src_net_err_count = 0;
 	struct replication_queue_data *qd = gfarm_hash_entry_data(q);
 	struct replication_request *rep;
+	static const char diag[] = "GFS_PROTO_REPLICATION_REQUEST";
 
 	do {
-		if (src_err != GFARM_ERR_NO_ERROR) {
+		if (src_net_err_count > 1) {
+			rep = qd->head;
 			/*
 			 * avoid retries, because this may take long time,
 			 * if the host is down or network is unreachable.
@@ -2629,9 +2633,18 @@ start_replication(struct gfp_xdr *conn, struct gfarm_hash_entry *q)
 			gflog_warning(GFARM_MSG_1002515,
 			    "skipping replication for %lld:%lld, "
 			    "because %s:%d is down: %s",
-			    (long long)qd->head->ino, (long long)qd->head->gen,
+			    (long long)rep->ino, (long long)rep->gen,
 			    gfp_conn_hash_hostname(q), gfp_conn_hash_port(q),
-			    gfarm_error_string(src_err));
+			    gfarm_error_string(src_net_err));
+
+			/*
+			 * XXX FIXME
+			 * src_err and dst_err should be passed separately
+			 */
+			gfmd_err = gfs_async_server_put_reply(conn, rep->xid,
+			    diag, src_net_err, "");
+			if (gfmd_err != GFARM_ERR_NO_ERROR)
+				return (gfmd_err);
 		} else {
 			gfmd_err = try_replication(conn, q,
 			    &src_err, &dst_err);
@@ -2640,6 +2653,10 @@ start_replication(struct gfp_xdr *conn, struct gfarm_hash_entry *q)
 			if (src_err == GFARM_ERR_NO_ERROR &&
 			    dst_err == GFARM_ERR_NO_ERROR)
 				return (GFARM_ERR_NO_ERROR);
+			if (IS_CONNECTION_ERROR(src_err)) {
+				src_net_err = src_err;
+				++src_net_err_count;
+			}
 		}
 
 		/*
@@ -3873,8 +3890,13 @@ back_channel_server(void)
 				}
 				break;
 			}
-			if (e != GFARM_ERR_NO_ERROR)
+			if (e != GFARM_ERR_NO_ERROR) {
+				gflog_error(GFARM_MSG_UNFIXED,
+				    "back channel disconnected "
+				    "during a reply: %s",
+				    gfarm_error_string(e));
 				break;
+			}
 		}
 
 		kill_pending_replications();
