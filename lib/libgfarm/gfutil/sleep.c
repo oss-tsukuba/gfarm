b033610a9f05046302aaa575997f1d07b303bb8d
SODA Noriyuki <n-soda@users.sourceforge.net>
2012-12-05 09:30:57 +0000

    merge r6953 and r6965 from the 2.5 branch: add gfarm_sleep() function as a preparation of the following fix: #505 [pullup-2.5.7.2] #504 - gfsd stops to work with a message "<err> ![1000551] cannot set canonical hostname of this node", when gfmd is overloaded
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5.7@7364 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ lib/libgfarm/gfutil/sleep.c
@@ -0,0 +1,15 @@
+#include <time.h>
+#include <unistd.h>
+
+#include "nanosec.h"
+#include "gfutil.h"
+
+void
+gfarm_sleep(long sec)
+{
+	struct timespec ts;
+
+	ts.tv_sec = sec;
+	ts.tv_nsec = 0;
+	gfarm_nanosleep_by_timespec(&ts);
+}
