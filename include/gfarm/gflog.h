oss-tsukuba/gfarm

commit cb7a7b5f9f475740cccc1eb289dd593b58d5fcb3

changed gflog to use catgets().
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4303 4a5e1ece-6a47-475c-b646-731d720b41d0

include/gfarm/gflog.h
@@ -1,3 +1,7 @@
+#include <syslog.h>
+
+#include <gfarm/gfarm_msg_enums.h>
+
 #ifndef GFARM_DEFAULT_FACILITY
 #define GFARM_DEFAULT_FACILITY	LOG_LOCAL0
 #endif
@@ -12,19 +16,54 @@
 #define GFLOG_PRINTF_ARG(M, N)
 #endif
 
-#ifdef GFLOG_USE_STDARG /* to make <stdarg.h> optional to use <gfutil.h> */
-void gflog_vmessage(int, const char *, va_list) GFLOG_PRINTF_ARG(2, 0);
+#if __STDC_VERSION__ < 199901L
+#if __GNUC__ >= 2
+#define __func__ __FUNCTION__
+#else
+#define __func__ "<unknown>"
 #endif
-void gflog_message(int, const char *, ...) GFLOG_PRINTF_ARG(2, 3);
-void gflog_error(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_warning(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_notice(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_info(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_debug(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_warning_errno(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
+#endif
 
-void gflog_fatal(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_fatal_errno(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
+#ifdef GFLOG_USE_STDARG /* to make <stdarg.h> optional to use <gfutil.h> */
+void gflog_vmessage(int, int, const char *, int, const char *,
+		const char *, va_list) GFLOG_PRINTF_ARG(6, 0);
+#endif
+void gflog_message(int, int, const char *, int, const char*,
+		const char *, ...) GFLOG_PRINTF_ARG(6, 7);
+void gflog_message_errno(int, int, const char *, int, const char*,
+		const char *, ...) GFLOG_PRINTF_ARG(6, 7);
+
+#define gflog_error(msg_no, ...) \
+	gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_warning(msg_no, ...) \
+	gflog_message(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_notice(msg_no, ...) \
+	gflog_message(msg_no, LOG_NOTICE,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_info(msg_no, ...) \
+	gflog_message(msg_no, LOG_INFO,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_debug(msg_no, ...) \
+	gflog_message(msg_no, LOG_DEBUG,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#define gflog_fatal(msg_no, ...) \
+	gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_error(msg_no, ...)\
+	gflog_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_warning(msg_no, ...)\
+	gflog_message(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_fatal_errno(msg_no, ...) \
+	gflog_message_errno(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_warning_errno(msg_no, ...) \
+	gflog_message_errno(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
 
 void gflog_set_priority_level(int);
 void gflog_set_identifier(const char *);
@@ -32,6 +71,8 @@ void gflog_set_auxiliary_info(char *);
 char *gflog_get_auxiliary_info(void);
 void gflog_syslog_open(int, int);
 int gflog_syslog_enabled(void);
+void gflog_catopen(const char *);
+void gflog_catclose(void);
 
 int gflog_syslog_name_to_facility(const char *);
 int gflog_syslog_name_to_priority(const char *);
@@ -40,5 +81,3 @@ int gflog_syslog_name_to_priority(const char *);
 
 int gflog_auth_set_verbose(int);
 int gflog_auth_get_verbose(void);
-void gflog_auth_error(const char *, ...) GFLOG_PRINTF_ARG(1, 2);
-void gflog_auth_warning(const char *, ...) GFLOG_PRINTF_ARG(1, 2);

commit 2226b6d6347115b2120496ef8b93ede7e1c25fd0

merge r7179 from the 2.5 branch to fix the following problems:  * #512 [pullup-2.5.7.2] #511 - log level adjustment to make ZABBIX alerts usable - step 1  * #544 [pullup-2.5.7.3] #409 - insufficient log message at sharedsecret authentication failure  * #560 [pullup-2.5.7.3] #232 - gflog_auth_error() and gflog_auth_warning() have been broken since r4303
this adjusts logs about server side authentication and gfmd.
NOTE 1: this does NOT adjust logs related to gfsd spool_check feature in gfmd (#542)
NOTE 2: this does NOT adjust logs about gfsd.
NOTE 3: the first field of the following table is the message ID, and the second field is the number of occurrence of the message which appeared in recent 3 months in an environment.
NOTE 4: message numbers which are related to #544 will be renumbered.
NOTE 5: GFARM_MSG_1002257 is changed to notice on the 2.5 branch (it's changed to warning on the main trunk), because it's usual on the 2.5 branch that gfmd-initiated replication fails.

=== server side authentication ===
change non-critical log levels inside gfarm_authorize() from <err> to <info>, because callers of gfarm_authorize() logs the error at higher priority:
||'''msg no'''||'''count'''||'''old'''||'''new'''||'''comment'''||
||1000022|| none|| err || info || fixes #409 ||
||1000023|| none|| err || info || fixes #409 ||
||1000024||16884|| err || info || fixes #409 ||
||1000025|| none|| err || info || fixes #409 ||
||1000026|| none|| err || info || fixes #409 ||
||1000027||   64|| err || info || ||
||1000028|| none|| err || info || fixes #409 ||
||1000029|| none|| err || info || fixes #409 ||
||1000030||10623|| err || info || fixes #409 ||
||1000031|| none|| err || info || ||
||1000032|| none|| err || info || fixes #409 ||
||1000033|| none|| err || info || fixes #409 ||
||1000034|| none|| err || info || fixes #409 ||
||1000035||  176|| err || info || fixes #409 ||
||1000036|| none|| err || info || fixes #409 ||
||1000037|| none|| err || info || fixes #409 ||

include/gfarm/gflog.h
@@ -60,12 +60,6 @@ void gflog_assert_message(int, const char *, int, const char *,
 #define gflog_fatal(msg_no, ...) \
 	gflog_fatal_message(msg_no, LOG_ERR,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define gflog_auth_error(msg_no, ...)\
-	gflog_message(msg_no, LOG_ERR,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define gflog_auth_warning(msg_no, ...)\
-	gflog_message(msg_no, LOG_WARNING,\
-			__FILE__, __LINE__, __func__, __VA_ARGS__)
 #define gflog_fatal_errno(msg_no, ...) \
 	gflog_fatal_message_errno(msg_no, LOG_ERR,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
@@ -84,6 +78,20 @@ void gflog_assert_message(int, const char *, int, const char *,
 #define gflog_debug_errno(msg_no, ...) \
 	gflog_message_errno(msg_no, LOG_DEBUG,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#define gflog_auth_info(msg_no, ...)\
+	gflog_auth_message(msg_no, LOG_INFO,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_notice(msg_no, ...)\
+	gflog_auth_message(msg_no, LOG_NOTICE,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_error(msg_no, ...)\
+	gflog_auth_message(msg_no, LOG_ERR,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define gflog_auth_warning(msg_no, ...)\
+	gflog_auth_message(msg_no, LOG_WARNING,\
+			__FILE__, __LINE__, __func__, __VA_ARGS__)
+
 #define gflog_trace(msg_no, ...) \
 	gflog_message(msg_no, LOG_INFO,\
 			__FILE__, __LINE__, __func__, __VA_ARGS__)

lib/libgfarm/gfarm/auth_common_gsi.c
@@ -182,7 +182,7 @@ gfarm_gsi_server_initialize(void)
 			}
 			if (gflog_auth_get_verbose() &&
 				e_major != GSS_S_COMPLETE) {
-				gflog_debug(GFARM_MSG_1002722,
+				gflog_info(GFARM_MSG_1002722,
 				    "initial acceptor certificate is not valid "
 				    "because of:");
 				gfarmGssPrintMajorStatus(e_major);

lib/libgfarm/gfarm/auth_server.c
@@ -69,19 +69,22 @@ gfarm_authorize_panic(struct gfp_xdr *conn, int switch_to,
 }
 
 static gfarm_error_t
-gfarm_auth_sharedsecret_giveup_response(
-	struct gfp_xdr *conn, int try, gfarm_int32_t error)
+gfarm_auth_sharedsecret_giveup_response(struct gfp_xdr *conn,
+	const char *hostname, const char *global_username,
+	int try, gfarm_int32_t error)
 {
 	gfarm_error_t e;
 
 	e = gfp_xdr_flush(conn);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000022, "auth_sharedsecret: cut: %s",
-		    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: cut: %s",
+		    global_username, hostname, gfarm_error_string(e));
 	} else if (try <= 1) {
 		e = GFARM_ERR_AUTHENTICATION;
-		gflog_error(GFARM_MSG_1000023, "auth_sharedsecret: scaned: %s",
-			    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: scaned: %s",
+		    global_username, hostname, gfarm_error_string(e));
 	} else {
 		switch (error) {
 		case GFARM_AUTH_ERROR_EXPIRED:
@@ -94,16 +97,17 @@ gfarm_auth_sharedsecret_giveup_response(
 			e = GFARM_ERR_AUTHENTICATION;
 			break;
 		}
-		gflog_error(GFARM_MSG_1000024,
-		    "auth_sharedsecret: gives up: %s",
-		    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: gives up: %s",
+		    global_username, hostname, gfarm_error_string(e));
 	}
 	return (e);
 }
 
 static gfarm_error_t
-gfarm_auth_sharedsecret_md5_response(
-	struct gfp_xdr *conn, struct passwd *pwd, gfarm_int32_t *errorp)
+gfarm_auth_sharedsecret_md5_response(struct gfp_xdr *conn,
+	const char *hostname, const char *global_username, 
+	struct passwd *pwd, gfarm_int32_t *errorp)
 {
 	int eof;
 	size_t len;
@@ -120,22 +124,22 @@ gfarm_auth_sharedsecret_md5_response(
 	if (e == GFARM_ERR_NO_ERROR)
 		e = gfp_xdr_flush(conn);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000025,
-		    "auth_sharedsecret: challenge: %s",
-		    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: challenge: %s",
+		    global_username, hostname, gfarm_error_string(e));
 		return (e);
 	}
 	e = gfp_xdr_recv(conn, 0, &eof, "ib",
 	    &expire, sizeof(response), &len, response);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000026,
-		    "auth_sharedsecret: response: %s",
-		    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: response: %s",
+		    global_username, hostname, gfarm_error_string(e));
 		return (e);
 	}
 	if (eof) {
-		gflog_error(GFARM_MSG_1000027, "auth_sharedsecret: "
-		    "unexpected EOF in response");
+		gflog_info(GFARM_MSG_1000027, "(%s@%s) auth_sharedsecret: "
+		    "unexpected EOF in response", global_username, hostname);
 		return (GFARM_ERR_PROTOCOL);
 	}
 	/*
@@ -145,35 +149,37 @@ gfarm_auth_sharedsecret_md5_response(
 	 */
 	if (pwd == NULL) {
 		error = GFARM_AUTH_ERROR_INVALID_CREDENTIAL;
-		gflog_debug(GFARM_MSG_1001074,
-			"Password is null");
+		gflog_debug(GFARM_MSG_1001074, "Password is null");
 		/* already logged at gfarm_authorize_sharedsecret() */
 	} else if ((e = gfarm_auth_shared_key_get(&expire_expected,
 	    shared_key_expected, pwd->pw_dir, pwd,
 	    GFARM_AUTH_SHARED_KEY_GET, 0))
 	    != GFARM_ERR_NO_ERROR && e != GFARM_ERR_EXPIRED) {
 		error = GFARM_AUTH_ERROR_INVALID_CREDENTIAL;
-		gflog_error(GFARM_MSG_1000028,
-		    "auth_sharedsecret: .gfarm_shared_key: %s",
-		    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: .gfarm_shared_key: %s",
+		    global_username, hostname, gfarm_error_string(e));
 	} else if (time(0) >= expire) {
 		/* may reach here if (e == GFARM_ERR_EXPIRED) */
 		error = GFARM_AUTH_ERROR_EXPIRED;
-		gflog_warning(GFARM_MSG_1000029,
-		    "auth_sharedsecret: key expired");
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: key expired",
+		    global_username, hostname);
 	} else {
 		/* may also reach here if (e == GFARM_ERR_EXPIRED) */
 		gfarm_auth_sharedsecret_response_data(
 		    shared_key_expected, challenge, response_expected);
 		if (expire != expire_expected) {
 			error = GFARM_AUTH_ERROR_INVALID_CREDENTIAL;
-			gflog_error(GFARM_MSG_1000030, "auth_sharedsecret: "
-			    "expire time mismatch");
+			gflog_info(GFARM_MSG_UNFIXED,
+			    "(%s@%s) auth_sharedsecret: expire time mismatch",
+			    global_username, hostname);
 		} else if (memcmp(response, response_expected,
 		    sizeof(response)) != 0) {
 			error = GFARM_AUTH_ERROR_INVALID_CREDENTIAL;
-			gflog_error(GFARM_MSG_1000031, "auth_sharedsecret: "
-			    "key mismatch");
+			gflog_info(GFARM_MSG_1000031,
+			    "(%s@%s) auth_sharedsecret: key mismatch",
+			    global_username, hostname);
 		} else { /* success */
 			error = GFARM_AUTH_ERROR_NO_ERROR;
 		}
@@ -181,17 +187,17 @@ gfarm_auth_sharedsecret_md5_response(
 	*errorp = error;
 	e = gfp_xdr_send(conn, "i", error);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000032,
-		    "auth_sharedsecret: send result: %s",
-		    gfarm_error_string(e));
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "(%s@%s) auth_sharedsecret: send result: %s",
+		    global_username, hostname, gfarm_error_string(e));
 		return (e);
 	}
 	if (error == GFARM_AUTH_ERROR_NO_ERROR) {
 		e = gfp_xdr_flush(conn);
 		if (e != GFARM_ERR_NO_ERROR) {
-			gflog_error(GFARM_MSG_1000033,
-			    "auth_sharedsecret: completion: %s",
-			    gfarm_error_string(e));
+			gflog_info(GFARM_MSG_UNFIXED,
+			    "(%s@%s) auth_sharedsecret: completion: %s",
+			    global_username, hostname, gfarm_error_string(e));
 			return (e);
 		}
 		return (GFARM_ERR_NO_ERROR); /* success */
@@ -200,7 +206,8 @@ gfarm_auth_sharedsecret_md5_response(
 }
 
 static gfarm_error_t
-gfarm_auth_sharedsecret_response(struct gfp_xdr *conn, struct passwd *pwd)
+gfarm_auth_sharedsecret_response(struct gfp_xdr *conn,
+	const char *hostname, const char *global_username, struct passwd *pwd)
 {
 	gfarm_error_t e;
 	gfarm_uint32_t request;
@@ -213,15 +220,15 @@ gfarm_auth_sharedsecret_response(struct gfp_xdr *conn, struct passwd *pwd)
 		++try;
 		e = gfp_xdr_recv(conn, 0, &eof, "i", &request);
 		if (e != GFARM_ERR_NO_ERROR) {
-			gflog_error(GFARM_MSG_1000034,
-			    "auth_sharedsecret_response: %s",
-			    gfarm_error_string(e));
+			gflog_info(GFARM_MSG_UNFIXED,
+			    "(%s@%s) auth_sharedsecret_response: %s",
+			    global_username, hostname, gfarm_error_string(e));
 			return (e);
 		}
 		if (eof) {
-			gflog_error(GFARM_MSG_1000035,
-			    "auth_sharedsecret_response: "
-			    "unexpected EOF");
+			gflog_info(GFARM_MSG_UNFIXED,
+			    "(%s@%s) auth_sharedsecret_response: "
+			    "unexpected EOF", global_username, hostname);
 			return (GFARM_ERR_PROTOCOL);
 		}
 		switch (request) {
@@ -235,26 +242,27 @@ gfarm_auth_sharedsecret_response(struct gfp_xdr *conn, struct passwd *pwd)
 			break;
 		}			
 		if (e != GFARM_ERR_NO_ERROR) {
-			gflog_error(GFARM_MSG_1000036,
-			    "auth_sharedsecret: key query: %s",
-			    gfarm_error_string(e));
+			gflog_info(GFARM_MSG_UNFIXED,
+			    "(%s@%s) auth_sharedsecret: key query: %s",
+			    global_username, hostname, gfarm_error_string(e));
 			return (e);
 		}
 		switch (request) {
 		case GFARM_AUTH_SHAREDSECRET_GIVEUP:
 			return (gfarm_auth_sharedsecret_giveup_response(
-					conn, try, error));
+			    conn, hostname, global_username, try, error));
 		case GFARM_AUTH_SHAREDSECRET_MD5:
 			e = gfarm_auth_sharedsecret_md5_response(
-				conn, pwd, &error);
+			    conn, hostname, global_username, pwd, &error);
 			if (e != GFARM_ERRMSG_AUTH_SHAREDSECRET_MD5_CONTINUE)
 				return (e);
 		default:
 			e = gfp_xdr_flush(conn);
 			if (e != GFARM_ERR_NO_ERROR) {
-				gflog_error(GFARM_MSG_1000037,
-				    "auth_sharedsecret: request "
-				    "response: %s", gfarm_error_string(e));
+				gflog_info(GFARM_MSG_UNFIXED,
+				    "(%s@%s) auth_sharedsecret: request "
+				    "response: %s", global_username, hostname,
+				    gfarm_error_string(e));
 				return (e);
 			}
 			break;
@@ -294,13 +302,13 @@ gfarm_authorize_sharedsecret(struct gfp_xdr *conn, int switch_to,
 
 	e = gfp_xdr_recv(conn, 0, &eof, "s", &global_username);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000038,
-		    "authorize_sharedsecret: reading username");
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "%s: authorize_sharedsecret: reading username", hostname);
 		return (e);
 	}
 	if (eof) {
-		gflog_error(GFARM_MSG_1000039,
-		    "authorize_sharedsecret: unexpected EOF");
+		gflog_info(GFARM_MSG_UNFIXED,
+		    "%s: authorize_sharedsecret: unexpected EOF", hostname);
 		return (GFARM_ERR_PROTOCOL);
 	}
 
@@ -319,7 +327,7 @@ gfarm_authorize_sharedsecret(struct gfp_xdr *conn, int switch_to,
 		e = (*auth_uid_to_global_user)(closure,
 		    GFARM_AUTH_METHOD_SHAREDSECRET, global_username, NULL);
 		if (e != GFARM_ERR_NO_ERROR)
-			gflog_error(GFARM_MSG_1000040,
+			gflog_notice(GFARM_MSG_1000040,
 			    "(%s@%s) authorize_sharedsecret: "
 			    "the global username isn't registered in gfmd: %s",
 			    global_username, hostname, gfarm_error_string(e));
@@ -353,13 +361,15 @@ gfarm_authorize_sharedsecret(struct gfp_xdr *conn, int switch_to,
 		}
 		if (getpwnam_r(local_username, &pwbuf, buf, getpwnam_r_bufsz,
 		    &pwd) != 0)
-			gflog_error(GFARM_MSG_1000043,
+			gflog_notice(GFARM_MSG_1000043,
 			    "(%s@%s) %s: authorize_sharedsecret: "
 			    "local account doesn't exist",
 			    global_username, hostname, local_username);
 	}
 
-	e = gfarm_auth_sharedsecret_response(conn, pwd); /* pwd may be NULL */
+	/* pwd may be NULL */
+	e = gfarm_auth_sharedsecret_response(conn,
+	    hostname, global_username, pwd);
 
 	/* if (pwd == NULL), must be (e != GFARM_ERR_NO_ERROR) here */
 	if (e != GFARM_ERR_NO_ERROR) {
@@ -474,7 +484,7 @@ gfarm_authorize(struct gfp_xdr *conn,
 
 	methods = gfarm_auth_method_get_enabled_by_name_addr(hostname, addr);
 	if (methods == 0) {
-		gflog_error(GFARM_MSG_1000046,
+		gflog_info(GFARM_MSG_1000046,
 		    "%s: refusing access", hostname);
 	} else {
 		methods &= gfarm_auth_method_get_available();
@@ -494,7 +504,7 @@ gfarm_authorize(struct gfp_xdr *conn,
 	if (e == GFARM_ERR_NO_ERROR)
 		e = gfp_xdr_flush(conn);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000048,
+		gflog_info(GFARM_MSG_1000048,
 		    "%s: %s", hostname, gfarm_error_string(e));
 		return (e);
 	}
@@ -502,18 +512,18 @@ gfarm_authorize(struct gfp_xdr *conn,
 		++try;
 		e = gfp_xdr_recv(conn, 0, &eof, "i", &method);
 		if (e != GFARM_ERR_NO_ERROR) {
-			gflog_error(GFARM_MSG_1000049,
+			gflog_info(GFARM_MSG_1000049,
 			    "%s: %s", hostname, gfarm_error_string(e));
 			return (e);
 		}
 		if (eof) {
 			if (try <= 1)
-				gflog_warning(GFARM_MSG_1000050,
-				    "%s: port scan", hostname);
+				gflog_notice(GFARM_MSG_1000050,
+				    "%s: port scan, or please try to increase "
+				    "listen_backlog setting", hostname);
 			else
-				gflog_warning(GFARM_MSG_1000051,
-				    "%s: client disappeared",
-				    hostname);
+				gflog_info(GFARM_MSG_1000051,
+				    "%s: client disappeared", hostname);
 			return (GFARM_ERR_PROTOCOL);
 		}
 		if (method == GFARM_AUTH_METHOD_NONE)
@@ -532,12 +542,12 @@ gfarm_authorize(struct gfp_xdr *conn,
 		if (e == GFARM_ERR_NO_ERROR)
 			e = gfp_xdr_flush(conn);
 		if (e != GFARM_ERR_NO_ERROR) {
-			gflog_error(GFARM_MSG_1000052,
+			gflog_info(GFARM_MSG_1000052,
 			    "%s: %s", hostname, gfarm_error_string(e));
 			return (e);
 		}
 		if (error != GFARM_AUTH_ERROR_NO_ERROR) {
-			gflog_error(GFARM_MSG_1003369,
+			gflog_warning(GFARM_MSG_1003369,
 			    "%s: incorrect auth-method request %d: %d",
 			    hostname, (int)method, (int)error);
 			return (GFARM_ERR_PROTOCOL);
@@ -555,7 +565,7 @@ gfarm_authorize(struct gfp_xdr *conn,
 				 * there is no usable auth-method
 				 * between client and server.
 				 */
-				gflog_notice(GFARM_MSG_1000054,
+				gflog_info(GFARM_MSG_1000054,
 				    "%s: authentication method "
 				    "doesn't match", hostname);
 				e = GFARM_ERR_PROTOCOL_NOT_SUPPORTED;

lib/libgfarm/gfarm/auth_server_gsi.c
@@ -60,7 +60,7 @@ gfarm_authorize_gsi_common(struct gfp_xdr *conn, int switch_to,
 
 	e = gfp_xdr_flush(conn);
 	if (e != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000712,
+		gflog_info(GFARM_MSG_1000712,
 		    "authorize_gsi: %s: protocol drain: %s",
 		    hostname, gfarm_error_string(e));
 		return (e);
@@ -135,12 +135,12 @@ gfarm_authorize_gsi_common(struct gfp_xdr *conn, int switch_to,
 	}
 	if (session == NULL) {
 		if (gflog_auth_get_verbose()) {
-			gflog_error(GFARM_MSG_1000718,
+			gflog_info(GFARM_MSG_1000718,
 			    "%s: Can't accept session because of:",
 			    hostname);
 			gfarmGssPrintMajorStatus(e_major);
 			gfarmGssPrintMinorStatus(e_minor);
-			gflog_error(GFARM_MSG_1000719,
+			gflog_info(GFARM_MSG_1000719,
 			    "GSI authentication error: %s", hostname);
 		}
 		/*
@@ -174,7 +174,7 @@ gfarm_authorize_gsi_common(struct gfp_xdr *conn, int switch_to,
 		    userinfo->distName, &global_username);
 		if (e != GFARM_ERR_NO_ERROR) {
 			error = GFARM_AUTH_ERROR_INVALID_CREDENTIAL;
-			gflog_error(GFARM_MSG_1003394,
+			gflog_notice(GFARM_MSG_1003394,
 			    "authorize_gsi: \"%s\" @ %s: user authentication: "
 			    "%s%s", userinfo->distName, hostname,
 			    gfarm_error_string(e),
@@ -221,11 +221,11 @@ gfarm_authorize_gsi_common(struct gfp_xdr *conn, int switch_to,
 	gfp_xdr_set_secsession(conn, session, GSS_C_NO_CREDENTIAL);
 	e2 = gfp_xdr_send(conn, "i", error);
 	if (e2 != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000723,
+		gflog_info(GFARM_MSG_1000723,
 		    "(%s@%s) authorize_gsi: send reply: %s",
 		    global_username, hostname, gfarm_error_string(e2));
 	} else if ((e2 = gfp_xdr_flush(conn)) != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_1000724,
+		gflog_info(GFARM_MSG_1000724,
 		    "(%s@%s) authorize_gsi: completion: %s",
 		    global_username, hostname, gfarm_error_string(e2));
 	}

lib/libgfarm/gfsl/auth.c
@@ -285,9 +285,9 @@ gfarmAuthInitialize(char *usermapFile)
 	    if (strcmp(mode, "@user@") == 0) {
 		pPtr = getpwnam(localName);
 		if (pPtr == NULL) {
-		    gflog_auth_warning(GFARM_MSG_1000650,
-			"%s: WARNING: Account doesn't exist."
-				       " Ignored.", localName);
+		    gflog_auth_info(GFARM_MSG_1000650,
+			"%s: WARNING: Account doesn't exist. Ignored.",
+			localName);
 		    continue;
 		}
 		if (pPtr->pw_uid == 0) {

lib/libgfarm/gfsl/gsi.c
@@ -132,10 +132,10 @@ gfarmGssPrintStatus(char **list, const char *diag)
 
     if (lP != NULL && *lP != NULL) {
 	while (*lP != NULL) {
-	    gflog_error(GFARM_MSG_1000607, "\t : %s", *lP++);
+	    gflog_info(GFARM_MSG_1000607, "\t : %s", *lP++);
 	}
     } else
-	gflog_error(GFARM_MSG_1000608, "GSS %s Status Error: UNKNOWN", diag);
+	gflog_info(GFARM_MSG_1000608, "GSS %s Status Error: UNKNOWN", diag);
     gfarmGssFreeCrackedStatus(list);
 }
 
@@ -564,9 +564,8 @@ gfarmGssAcceptSecurityContext(int fd, gss_cred_id_t cred, gss_ctx_id_t *scPtr,
     do {
 	tknStat = gfarmGssReceiveToken(fd, itPtr, GFARM_GSS_TIMEOUT_INFINITE);
 	if (tknStat <= 0) {
-	    gflog_auth_error(GFARM_MSG_1000616,
-		"gfarmGssAcceptSecurityContext(): "
-			     "failed to receive response");
+	    gflog_auth_info(GFARM_MSG_1000616,
+		"gfarmGssAcceptSecurityContext(): failed to receive response");
 	    majStat = GSS_S_DEFECTIVE_TOKEN|GSS_S_CALL_INACCESSIBLE_READ;
 	    minStat = GFSL_DEFAULT_MINOR_ERROR;
 	    break;
@@ -593,9 +592,8 @@ gfarmGssAcceptSecurityContext(int fd, gss_cred_id_t cred, gss_ctx_id_t *scPtr,
 	    tknStat = gfarmGssSendToken(fd, otPtr);
 	    gss_release_buffer(&minStat2, otPtr);
 	    if (tknStat <= 0) {
-		gflog_auth_error(GFARM_MSG_1000617,
-		    "gfarmGssAcceptSecurityContext(): "
-				 "failed to send response");
+		gflog_auth_info(GFARM_MSG_1000617,
+		    "gfarmGssAcceptSecurityContext(): failed to send response");
 		majStat = GSS_S_DEFECTIVE_TOKEN|GSS_S_CALL_INACCESSIBLE_WRITE;
 		minStat = GFSL_DEFAULT_MINOR_ERROR;
 	    }

lib/libgfarm/gfsl/session.c
@@ -392,9 +392,9 @@ negotiateConfigParam(int fd, gss_ctx_id_t sCtx, int which,
 	   
 	    if (gfarmReadInt32(fd, param, NUM_NEGO_PARAM,
 			       GFARM_GSS_TIMEOUT_INFINITE) != NUM_NEGO_PARAM) {
-		gflog_auth_error(GFARM_MSG_1000662,
+		gflog_auth_info(GFARM_MSG_1000662,
 		    "gfarmSecSession:negotiateConfigParam(): "
-				 "negotiation failure with the initiator");
+		    "negotiation failure with the initiator");
 		goto Done;
 	    }
 	    iQOP = param[NEGO_PARAM_QOP];
@@ -465,9 +465,9 @@ negotiateConfigParam(int fd, gss_ctx_id_t sCtx, int which,
 	    param[NEGO_PARAM_OTHER_CONFIG] = retConf;
 
 	    if (gfarmWriteInt32(fd, param, NUM_NEGO_PARAM) != NUM_NEGO_PARAM) {
-		gflog_auth_error(GFARM_MSG_1000663,
+		gflog_auth_info(GFARM_MSG_1000663,
 		    "gfarmSecSession:negotiateConfigParam(): "
-				 "initiator disappered");
+		    "initiator disappered");
 		goto Done;
 	    }
 
@@ -486,7 +486,7 @@ negotiateConfigParam(int fd, gss_ctx_id_t sCtx, int which,
 	    if (gfarmWriteInt32(fd, param, NUM_NEGO_PARAM) != NUM_NEGO_PARAM) {
 		gflog_auth_error(GFARM_MSG_1000664,
 		    "gfarmSecSession:negotiateConfigParam(): "
-				 "acceptor disappered");
+		    "acceptor disappered");
 		goto Done;
 	    }
 
@@ -1171,7 +1171,7 @@ gfarmSecSessionAccept(int fd, gss_cred_id_t cred,
      */
     entry = gfarmAuthGetUserEntry(initiatorDistName);
     if (entry == NULL) {
-	gflog_auth_error(GFARM_MSG_1000672,
+	gflog_auth_notice(GFARM_MSG_1000672,
 	    "%s: not registered in mapfile", initiatorDistName);
 	majStat = GSS_S_UNAUTHORIZED;
 	/* Send NACK. */

lib/libgfarm/gfsl/tcputil.c
@@ -250,7 +250,7 @@ gfarmReadInt8(int fd, gfarm_int8_t *buf, int len, int timeoutMsec)
 	}
 	cur = read(fd, buf + sum, len - sum);
 	if (cur < 0) {
-	    gflog_error(GFARM_MSG_1000640, "read: %s", strerror(errno));
+	    gflog_info(GFARM_MSG_1000640, "read: %s", strerror(errno));
 	    return sum;
 	} else if (cur == 0) {
 	    break;
@@ -310,7 +310,7 @@ gfarmWriteInt8(int fd, gfarm_int8_t *buf, int len)
     do {
 	cur = gfarm_send_no_sigpipe(fd, buf + sum, len - sum);
 	if (cur < 0) {
-	    gflog_error(GFARM_MSG_1000641, "write: %s", strerror(errno));
+	    gflog_info(GFARM_MSG_1000641, "write: %s", strerror(errno));
 	    return sum;
 	}
 	sum += cur;

server/gfmd/gfmd.c
@@ -874,7 +874,7 @@ peer_authorize(struct peer *peer)
 	if (e != GFARM_ERR_NO_ERROR) {
 		gfarm_sockaddr_to_string(&addr,
 		    addr_string, GFARM_SOCKADDR_STRLEN);
-		gflog_warning(GFARM_MSG_1000185,
+		gflog_info(GFARM_MSG_1000185,
 		    "gfarm_sockaddr_to_name(%s): %s",
 		    gfarm_error_string(e), addr_string);
 		hostname = strdup_log(addr_string, diag);
@@ -893,7 +893,7 @@ peer_authorize(struct peer *peer)
 		    sync_protocol_watcher);
 		giant_unlock();
 	} else {
-		gflog_warning(GFARM_MSG_1002474,
+		gflog_notice(GFARM_MSG_1002474,
 		    "host %s: authorize: %s", hostname, gfarm_error_string(e));
 		free(hostname);
 	}
@@ -907,8 +907,7 @@ try_auth(void *arg)
 	gfarm_error_t e;
 
 	if ((e = peer_authorize(peer)) != GFARM_ERR_NO_ERROR) {
-		gflog_warning(GFARM_MSG_1000188,
-		    "peer_authorize: %s", gfarm_error_string(e));
+		/* peer_authorize() itself records the error log */
 		giant_lock();
 		/* db_begin()/db_end() is not necessary in this case */
 		peer_free(peer);

server/gfmd/inode.c
@@ -3486,7 +3486,7 @@ inode_replicated(struct file_replicating *fr,
 		} else if (
 		    src_errcode != GFARM_ERR_NO_ERROR ||
 		    dst_errcode != GFARM_ERR_NO_ERROR)
-			gflog_error(GFARM_MSG_1002257,
+			gflog_notice(GFARM_MSG_1002257,
 			    "error at %lld:%lld replication to %s: "
 			    "src=%d dst=%d",
 			    (long long)inode_get_number(inode),

server/gfmd/peer.c
@@ -584,7 +584,7 @@ peer_free_request(struct peer *peer)
 	 */
 	rv = shutdown(fd, SHUT_RDWR);
 	if (rv == -1)
-		gflog_warning(GFARM_MSG_1002766,
+		gflog_info(GFARM_MSG_1002766,
 		    "%s(%s) : shutdown(%d): %s", BACK_CHANNEL_DIAG(peer),
 		    peer_get_hostname(peer), fd, strerror(errno));
 
@@ -907,7 +907,7 @@ peer_free(struct peer *peer)
 		 */
 		err = peer_get_numeric_name(peer, hostbuf, sizeof(hostbuf));
 		if (err != 0)
-			gflog_error(GFARM_MSG_1003276,
+			gflog_info(GFARM_MSG_1003276,
 			    "unable to convert peer address to string: %s",
 			    strerror(err));
 	}