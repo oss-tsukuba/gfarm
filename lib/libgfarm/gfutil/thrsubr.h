c2b652bbd64031cd5ee021ddc28f903961e071a9
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-12-07 12:13:30 +0000

    merge r6239 from the main trunk (for #565 - Gfarm kernel module)  : delete unused define macros
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@7441 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfutil/thrsubr.h
+++ lib/libgfarm/gfutil/thrsubr.h
@@ -18,10 +18,4 @@ void gfarm_cond_destroy(pthread_cond_t *, const char *, const char *);
 #define GFARM_MUTEX_INITIALIZER(name)   __MUTEX_INITIALIZER(name)
 #else /* __KERNEL__ */
 #define GFARM_MUTEX_INITIALIZER(name)   PTHREAD_MUTEX_INITIALIZER
-#define GFARM_COND_INITIALIZER(name)   PTHREAD_COND_INITIALIZER
 #endif /* __KERNEL__ */
-
-#define GFARM_DEFINE_MUTEX(name) \
-		gfarm_mutex_t name = GFARM_MUTEX_INITIALIZER(name)
-#define GFARM_DEFINE_CONDVAR(name) \
-		 gfarm_condvar_t name = GFARM_CONDVAR_INITIALIZER(name)
