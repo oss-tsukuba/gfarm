34f8b1be9688a9912ec66adcdc52071910c54d03
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2012-12-13 04:55:47 +0000

    merge r6455 from the main trunk  * Also test 'gfgroup -m' (without any user name) in addition with 'gfgroup -c'.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@7501 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfgroup/gfgroup.0-nusers.sh
+++ regress/gftool/gfgroup/gfgroup.0-nusers.sh
@@ -9,14 +9,36 @@ else
 fi
 
 tmpgroup=tmpgrp-`hostname`-$$
+user1=`gfuser | sed -n 1p`
 
 trap 'gfgroup -d "$tmpgroup" 2>/dev/null; exit $exit_trap' $trap_sigs
 
-if gfgroup -c "$tmpgroup"; then
-    if [ X"`gfgroup -l $tmpgroup`" = X"$tmpgroup:" ]; then
-	exit_code=$exit_pass
+#
+# Test "gfgroup -m GROUP" with no user name.
+#
+gfgroup_m_result=false
+if gfgroup -c "$tmpgroup" "$user1"; then
+    if gfgroup -m "$tmpgroup"; then
+	if [ X"`gfgroup -l $tmpgroup`" = X"$tmpgroup:" ]; then
+	    gfgroup_m_result=true
+	fi
     fi
     gfgroup -d "$tmpgroup" 2>/dev/null
+fi
+
+#
+# Test "gfgroup -c GROUP" with no user name.
+#
+gfgroup_c_result=false
+if gfgroup -c "$tmpgroup"; then
+    if [ X"`gfgroup -l $tmpgroup`" = X"$tmpgroup:" ]; then
+	gfgroup_c_result=true
+    fi
+    gfgroup -d "$tmpgroup" 2>/dev/null
+fi
+
+if [ $gfgroup_m_result = true -a $gfgroup_c_result = true ]; then
+    exit_code=$exit_pass
 fi
 
 exit $exit_code
