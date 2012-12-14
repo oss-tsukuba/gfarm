a7fe700687c8b648db4d2618c685a688574588fe
Osamu Tatebe <tatebe@users.sourceforge.net>
2012-12-14 16:15:29 +0000

    sleep after setting extended attribute to wait for flushing it to the backend database
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@7509 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfxattr/gfxattr-long-value.sh
+++ regress/gftool/gfxattr/gfxattr-long-value.sh
@@ -35,7 +35,8 @@ if test $? -ne 0; then
     cleanup
     exit $exit_fail
 fi
-
+# wait to flush the extended attribute to the backend database
+sleep 2
 gfxattr -g -f ${attr_got} ${dir} ${attrname}
 diff -c ${attr_src} ${attr_got}
 if test $? -ne 0; then
