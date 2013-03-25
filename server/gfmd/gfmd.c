oss-tsukuba/gfarm

commit 71fdb707b5424dfbcd2b775d56d780210555c156

IMPORTANT NOTE: this change introduce protocol incompatibility. thus, all gfmd, gfsd and clients have to be updated.
 * implement #496 - making client protocol header compatible with server-side asynchronous protocol
 * as part of #287 (inter-gfmd RPC forwarding), [[BR]]
   fix protocol inconsistency about GFM_PROTO_REMOTE_RPC. [[BR]]
   before this change, gfmdc_server_remote_rpc() returns an error code of wrapper side in an error case, [[BR]]
   but relay.c doesn't return any error code of wrapper side (only wrapped side RPC error is returned). [[BR]]
   after this change, GFM_PROTO_REMOTE_RPC always returns an error of wrapper side. [[BR]]
   (i.e. relay.c is fixed). [[BR]]
   as the result, wrapping side error code parameter is added to [[BR]]
   async_server_vput_wrapped_reply_unlocked(), async_server_vput_wrapped_reply() [[BR]]
   and async_client_vrecv_wrapped_result(). [[BR]]


git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7910 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/gfmd.c
@@ -44,6 +44,7 @@
 #include "repattr.h"
 
 #include "subr.h"
+#include "rpcsubr.h"
 #include "thrpool.h"
 #include "callout.h"
 #include "journal_file.h"	/* for enum journal_operation */
@@ -857,6 +858,7 @@ protocol_switch(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep,
 		e = gfm_server_process_set(peer, xid, sizep,
 		    from_client, skip);
 		break;
+#if 0
 	case GFJ_PROTO_LOCK_REGISTER:
 		e = gfj_server_lock_register(peer, xid, sizep,
 		    from_client, skip); break;
@@ -879,6 +881,7 @@ protocol_switch(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep,
 	case GFJ_PROTO_HOSTINFO:
 		e = gfj_server_hostinfo(peer, xid, sizep,
 		    from_client, skip); break;
+#endif
 	case GFM_PROTO_XATTR_SET:
 		e = gfm_server_setxattr(peer, xid, sizep, from_client, skip, 0);
 		break;
@@ -955,6 +958,9 @@ protocol_switch(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep,
 		    from_client, skip, level, request, requestp, on_errorp);
 		break;
 	}
+	if (skip && request != GFM_PROTO_COMPOUND_ON_ERROR)
+		(void)gfm_server_put_reply(peer, xid, sizep, "skipping",
+		    GFARM_ERR_RPC_REQUEST_IGNORED, "");
 
 	if (!*suspendedp &&
 	    ((level == 0 && request != GFM_PROTO_COMPOUND_BEGIN)
@@ -1132,6 +1138,10 @@ protocol_main(void *arg)
 {
 	struct local_peer *local_peer = arg;
 	struct peer *peer = local_peer_to_peer(local_peer);
+	gfarm_error_t e;
+	enum gfp_xdr_msg_type msg_type;
+	gfp_xdr_xid_t xid;
+	size_t size;
 
 	/*
 	 * the reason why we call peer_readable_invoked() here is
@@ -1141,8 +1151,27 @@ protocol_main(void *arg)
 	local_peer_readable_invoked(local_peer);
 
 	do {
+		e = gfp_xdr_recv_async_header(peer_get_conn(peer), 0, 1,
+		    &msg_type, &xid, &size);
+		if (e != GFARM_ERR_NO_ERROR) {
+			if (e != GFARM_ERR_UNEXPECTED_EOF)
+				gflog_notice(GFARM_MSG_UNFIXED,
+				    "receiving rpc header from a client: %s",
+				    gfarm_error_string(e));
+			/* mark this peer finished */
+			peer_record_protocol_error(peer);
+			return (NULL);
+		}
+		if (msg_type != GFP_XDR_TYPE_REQUEST) {
+			gflog_warning(GFARM_MSG_UNFIXED,
+			    "receiving unexpected rpc header type: %d",
+			    (int)msg_type);
+			/* mark this peer finished */
+			peer_record_protocol_error(peer);
+			return (NULL);
+		}
 		/* (..., 0, NULL) means sync protocol */
-		if (protocol_service(peer, 0, NULL))
+		if (protocol_service(peer, xid, &size))
 			return (NULL); /* end of gfmd protocol session */
 	} while (gfp_xdr_recv_is_ready(peer_get_conn(peer)));
 

commit 1aac4ccb5e85c36920453f44f18ce2ec5b4478fc

fix descriptor leak which was introduced in r7910 as part of #496 - making client protocol header compatible with server-side asynchronous protocol
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7913 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/gfmd.c
@@ -991,6 +991,28 @@ compound_state_init(struct compound_state *cs)
 }
 
 /*
+ * finish foreground protocol handling.
+ *
+ * PREREQUISITE: giant_lock
+ */
+void
+protocol_finish(struct peer *peer, const char *diag)
+{
+	if (db_begin(diag) == GFARM_ERR_NO_ERROR) {
+		/*
+		 * the following internally calls
+		 * inode_close*() and closing must be
+		 * done regardless of the result of db_begin().
+		 * because not closing may cause
+		 * descriptor leak.
+		 */
+		peer_free(peer);
+
+		db_end(diag);
+	}
+}
+
+/*
  * this interface is exported for a use from a private extension too.
  * sizep != NULL, if this is an inter-gfmd-relayed request.
  */
@@ -1003,7 +1025,6 @@ protocol_service(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep)
 	gfarm_int32_t request;
 	int from_client;
 	int suspended = 0;
-	int transaction = 0;
 	static const char diag[] = "protocol_service";
 
 	from_client = peer_get_auth_id_type(peer) == GFARM_AUTH_ID_TYPE_USER;
@@ -1015,8 +1036,6 @@ protocol_service(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep)
 		giant_lock();
 		peer_fdpair_clear(peer);
 		if (peer_had_protocol_error(peer)) {
-			if (db_begin(diag) == GFARM_ERR_NO_ERROR)
-				transaction = 1;
 			/*
 			 * gfmd_channel and back_channel will be
 			 * freed by their own thread
@@ -1026,18 +1045,8 @@ protocol_service(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep)
 #ifdef COMPAT_GFARM_2_3
 			     request != GFM_PROTO_SWITCH_BACK_CHANNEL &&
 #endif
-			     request != GFM_PROTO_SWITCH_ASYNC_BACK_CHANNEL)) {
-				/*
-				 * the following internally calls
-				 * inode_close*() and closing must be
-				 * done regardless of the result of db_begin().
-				 * because not closing may cause
-				 * descriptor leak.
-				 */
-				peer_free(peer);
-			}
-			if (transaction)
-				db_end(diag);
+			     request != GFM_PROTO_SWITCH_ASYNC_BACK_CHANNEL))
+				protocol_finish(peer, diag);
 			giant_unlock();
 			return (1); /* finish */
 		}
@@ -1051,8 +1060,6 @@ protocol_service(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep)
 		if (peer_had_protocol_error(peer)) {
 			giant_lock();
 			peer_fdpair_clear(peer);
-			if (db_begin(diag) == GFARM_ERR_NO_ERROR)
-				transaction = 1;
 			/*
 			 * gfmd_channel and back_channel will be
 			 * freed by their own thread
@@ -1062,19 +1069,8 @@ protocol_service(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep)
 #ifdef COMPAT_GFARM_2_3
 			     request != GFM_PROTO_SWITCH_BACK_CHANNEL &&
 #endif
-			     request != GFM_PROTO_SWITCH_ASYNC_BACK_CHANNEL)) {
-				/*
-				 * the following internally calls
-				 * inode_close*() and closing must be
-				 * done regardless of the result of db_begin().
-				 * because not closing may cause
-				 * descriptor leak.
-				 */
-
-				peer_free(peer);
-			}
-			if (transaction)
-				db_end(diag);
+			     request != GFM_PROTO_SWITCH_ASYNC_BACK_CHANNEL))
+				protocol_finish(peer, diag);
 			giant_unlock();
 			return (1); /* finish */
 		}
@@ -1109,17 +1105,7 @@ protocol_service(struct peer *peer, gfp_xdr_xid_t xid, size_t *sizep)
 				"failed to process GFM_PROTO_SWITCH_BACK_"
 				"CHANNEL request: %s", gfarm_error_string(e));
 			giant_lock();
-			if (db_begin(diag) == GFARM_ERR_NO_ERROR)
-				transaction = 1;
-			/*
-			 * the following internally calls inode_close*() and
-			 * closing must be done regardless of the result of
-			 * db_begin().  because not closing may cause
-			 * descriptor leak.
-			 */
-			peer_free(peer);
-			if (transaction)
-				db_end(diag);
+			protocol_finish(peer, diag);
 			giant_unlock();
 		}
 		return (1); /* finish */
@@ -1158,16 +1144,18 @@ protocol_main(void *arg)
 				gflog_notice(GFARM_MSG_UNFIXED,
 				    "receiving rpc header from a client: %s",
 				    gfarm_error_string(e));
-			/* mark this peer finished */
-			peer_record_protocol_error(peer);
+			giant_lock();
+			protocol_finish(peer, "client EOF");
+			giant_unlock();
 			return (NULL);
 		}
 		if (msg_type != GFP_XDR_TYPE_REQUEST) {
 			gflog_warning(GFARM_MSG_UNFIXED,
 			    "receiving unexpected rpc header type: %d",
 			    (int)msg_type);
-			/* mark this peer finished */
-			peer_record_protocol_error(peer);
+			giant_lock();
+			protocol_finish(peer, "client protocol error");
+			giant_unlock();
 			return (NULL);
 		}
 		/* (..., 0, NULL) means sync protocol */