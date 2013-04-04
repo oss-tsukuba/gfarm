91545700f0162e89908ee7df993c621d118874ac
Motoyuki Kasahara <m-kasahr@users.sourceforge.net>
2013-04-04 02:14:24 +0000

    Modify behavior of 'gfkey -f'. Now it nerver complains about invalid format of the existing shared key.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7990 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/auth_common.c
+++ lib/libgfarm/gfarm/auth_common.c
@@ -196,7 +196,7 @@ gfarm_error_t
 gfarm_auth_shared_key_get(unsigned int *expirep, char *shared_key,
 	char *home, struct passwd *pwd, int create, int period)
 {
-	gfarm_error_t e = GFARM_ERR_NO_ERROR;
+	gfarm_error_t e;
 	FILE *fp = NULL;
 	static char keyfile_basename[] = "/" GFARM_AUTH_SHARED_KEY_BASENAME;
 	char *keyfilename, *allocbuf = NULL;
@@ -246,9 +246,22 @@ gfarm_auth_shared_key_get(unsigned int *expirep, char *shared_key,
 			    "seteuid(%d)", (int)pwd->pw_uid);
 	}
 
-	if ((fp = fopen(keyfilename, "r+")) == NULL) {
-		e = GFARM_ERRMSG_SHAREDSECRET_KEY_FILE_NOT_EXIST;
-		goto create;
+	if (create != GFARM_AUTH_SHARED_KEY_CREATE_FORCE &&
+	    (fp = fopen(keyfilename, "r+")) != NULL) {
+		if (skip_space(fp) || read_hex(fp, &expire, sizeof(expire))) {
+			fclose(fp);
+			free(allocbuf);
+			e = GFARM_ERRMSG_SHAREDSECRET_INVALID_EXPIRE_FIELD;
+			goto finish;
+		}
+		expire = ntohl(expire);
+		if (skip_space(fp) ||
+		    read_hex(fp, shared_key, GFARM_AUTH_SHARED_KEY_LEN)) {
+			fclose(fp);
+			free(allocbuf);
+			e = GFARM_ERRMSG_SHAREDSECRET_INVALID_KEY_FIELD;
+			goto finish;
+		}
 	}
 	if (skip_space(fp) || read_hex(fp, &expire, sizeof(expire))) {
 		fclose(fp);
