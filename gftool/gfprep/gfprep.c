6306d3a0401303206b642b8a85ff46a14fb93e31
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2013-04-05 16:49:53 +0000

    merge r8011 from the 2.5 branch plug possibility of memory leak.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@8013 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfprep/gfprep.c
+++ gftool/gfprep/gfprep.c
@@ -3585,6 +3585,7 @@ main(int argc, char *argv[])
 				if (gfprep_check_busy_and_wait(
 					    src_url, entry, n_desire,
 					    n_dst_select, dst_select_array)) {
+					free(src_select_array);
 					free(dst_select_array);
 					free(dst_exist_array);
 					gfprep_debug(
