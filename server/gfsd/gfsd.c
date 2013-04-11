847aeee4a6259ebcac91d6ab3b861793cf70d62e
SODA Noriyuki <n-soda@users.sourceforge.net>
2013-04-11 10:50:24 +0000

    merge r8022 from the 2.5 branch: fix #667 - [pullup-2.5.7.4] #666 - busy loop and delay of opening a file due to race condition between GFM_PROTO_REOPEN + O_CREATE and failed GFM_PROTO_REOPEN -> GFM_PROTO_REPLICA_LOST
    this revises the fix for #455 - "lost all replicas" symptom due to race condition between GFM_PROTO_REOPEN + O_CREATE and failed GFM_PROTO_REOPEN -> GFM_PROTO_REPLICA_LOST
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5.7@8023 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfsd/gfsd.c
+++ server/gfsd/gfsd.c
@@ -1341,7 +1341,7 @@ gfs_server_open_common(struct gfp_xdr *client, char *diag,
 					e = GFARM_ERR_FILE_MIGRATED;
 					break;
 				} else if (e == GFARM_ERR_FILE_BUSY) {
-					/* sourceforge.net #455 */
+					/* sourceforge.net #455, #666 */
 					gflog_debug(GFARM_MSG_1003559,
 					    "possible race against "
 					    "reopen with O_CREAT "
