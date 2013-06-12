9c2a12ea90d46e211130bfc865713b656b4a78c4
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2013-06-12 06:14:11 +0000

    remove unnecessary headers (merged from the 2.6 branch)
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@8230 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/fsngroup.c
+++ server/gfmd/fsngroup.c
@@ -3,51 +3,31 @@
  */
 
 #include <assert.h>
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#include <time.h>
-
-/* for host_addr_lookup() */
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-
-#include <pthread.h>
 
 #include <gfarm/gfarm.h>
 
-#include "internal_host_info.h"
-
-#include "gfutil.h"
 #include "hash.h"
 #include "thrsubr.h"
 
-#include "metadb_common.h"	/* gfarm_host_info_free_except_hostname() */
-#include "gfp_xdr.h"
-#include "gfm_proto.h" /* GFM_PROTO_SCHED_FLAG_* */
-#include "gfs_proto.h" /* GFS_PROTOCOL_VERSION */
 #include "auth.h"
-#include "config.h"
+#include "gfm_proto.h"
+#include "gfutil.h"
+#include "gfp_xdr.h"
 #include "repattr.h"
 
-#include "callout.h"
-#include "subr.h"
-#include "rpcsubr.h"
 #include "db_access.h"
-#include "host.h"
-#include "mdhost.h"
-#include "user.h"
-#include "peer.h"
-#include "inode.h"
-#include "abstract_host.h"
-#include "abstract_host_impl.h"
-#include "dead_file_copy.h"
-#include "back_channel.h"
-#include "relay.h"
 #include "fsngroup.h"
+#include "host.h"
+#include "inode.h"
+#include "peer.h"
+#include "relay.h"
+#include "rpcsubr.h"
+#include "subr.h"
+#include "user.h"
 
 struct fsngroup_tuple {
 	char *hostname;
