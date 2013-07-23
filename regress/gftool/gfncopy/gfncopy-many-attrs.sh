46751115f784925d945c1a30081ccc374aea5fb0
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2013-07-23 03:11:08 +0000

    merge r8372 from the 2.6 branch. regression tests for new gfncopy (#712) (copied from regress/gftool/gfrepattr/*)
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@8373 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gftool/gfncopy/gfncopy-many-attrs.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+. ./regress.conf
+
+large_num=4100
+
+dir=${gftmp}
+
+pat="`awk 'BEGIN{printf "hoge00000:2"; for (i = 1; i < '${large_num}'; i++) printf ",hoge%05d:2",i; exit}'`"
+
+setup() {
+    gfmkdir ${dir}
+    gfncopy -r ${dir}
+}
+
+cleanup() {
+    gfncopy -r ${dir}
+    gfrmdir ${dir}
+    rm -f ${localtmp}
+}
+
+trap 'cleanup; exit $exit_trap' $trap_sigs
+
+setup
+
+gfncopy -C -S "${pat}" ${dir}
+if [ $? -ne 0 ]; then
+    cleanup
+    exit $exit_fail
+fi
+
+gfncopy ${dir} | tr , '\012' | sort >$localtmp
+
+awk '
+BEGIN{for (i = 0; i < '${large_num}'; i++) printf "hoge%05d:2\n",i; exit}
+' | diff -c - $localtmp
+if [ $? -ne 0 ]; then
+    cleanup
+    exit $exit_fail
+fi
+
+gfncopy -r ${dir}
+st=$?
+
+cleanup
+
+if [ ${st} -eq 0 ]; then
+    exit $exit_pass
+else
+    exit $exit_fail
+fi
