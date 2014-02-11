268762028f44d255dd584941f8c6d6c87129a360
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2014-02-11 13:48:18 +0000

    merge r8877 from the 2.5 branch. print the progress of replica_check when gfmd catches SIGUSR2.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@8880 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/replica_check.h
+++ server/gfmd/replica_check.h
@@ -2,10 +2,11 @@
  * $Id$
  */
 
-void replica_check_start();
-void replica_check_signal_host_up();
-void replica_check_signal_host_down();
-void replica_check_signal_update_xattr();
-void replica_check_signal_rename();
-void replica_check_signal_rep_request_failed();
-void replica_check_signal_rep_result_failed();
+void replica_check_start(void);
+void replica_check_signal_host_up(void);
+void replica_check_signal_host_down(void);
+void replica_check_signal_update_xattr(void);
+void replica_check_signal_rename(void);
+void replica_check_signal_rep_request_failed(void);
+void replica_check_signal_rep_result_failed(void);
+void replica_check_info(void);
