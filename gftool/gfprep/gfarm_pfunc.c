23f26327162f85a816bf10a953301ed0230f831e
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2014-02-27 15:28:48 +0000

    merge r8911 from the 2.5 branch.  * fix #747 - gfpcopy: an invalid file remains after gfs_pio_close() or close() failed.  * fix a wrong message.
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@8912 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfprep/gfarm_pfunc.c
+++ gftool/gfprep/gfarm_pfunc.c
@@ -616,7 +616,7 @@ close:
 	e = pfunc_close(&src_fp);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "ERROR: copy failed: close(%s): %s\n",
-			tmp_url, gfarm_error_string(e));
+			src_url, gfarm_error_string(e));
 		result = PFUNC_RESULT_NG;
 	}
 	e = pfunc_close(&dst_fp);
@@ -625,6 +625,8 @@ close:
 			tmp_url, gfarm_error_string(e));
 		result = PFUNC_RESULT_NG;
 	}
+	if (result == PFUNC_RESULT_NG)
+		goto end;
 
 	/* handle->skip_existing: This is race condition here. */
 
@@ -633,12 +635,14 @@ close:
 		fprintf(stderr, "ERROR: copy failed: utime(%s): %s\n",
 			tmp_url, gfarm_error_string(e));
 		result = PFUNC_RESULT_NG;
+		goto end;
 	}
 	e = pfunc_rename(tmp_url, dst_url);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "ERROR: copy failed: rename(%s -> %s): %s\n",
 			tmp_url, dst_url, gfarm_error_string(e));
 		result = PFUNC_RESULT_NG;
+		goto end;
 	}
 	/* XXX pfunc_mode == PFUNC_MODE_MIGRATE : unlink src_url */
 end:
