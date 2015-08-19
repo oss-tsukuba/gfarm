4984b85c055017f1271d7d5ee3dbdac12ffa6b2d
SODA Noriyuki <n-soda@users.sourceforge.net>
2015-08-19 07:40:17 +0000

    implement #897 - make gfmv() behave like mv(1)
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@9853 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfmv/file.sh
+++ regress/gftool/gfmv/file.sh
@@ -2,15 +2,36 @@
 
 . ./regress.conf
 
-trap 'gfrm $gftmp/xxx $gftmp/1byte; gfrmdir $gftmp; exit $exit_trap' $trap_sigs
+trap 'gfrm $gftmp/1byte $gftmp/xxx $gftmp/yyy $gftmp/dir1/1byte $gftmp/dir1/xxx $gftmp/dir1/yyy $gftmp/dir2/1byte $gftmp/dir2/xxx $gftmp/dir2/yyy; gfrmdir $gftmp/dir1 $gftmp/dir2 $gftmp; exit $exit_trap' $trap_sigs
 
 if gfmkdir $gftmp &&
+
    gfreg $data/1byte $gftmp/1byte &&
    gfmv $gftmp/1byte $gftmp/xxx &&
-   gfexport $gftmp/xxx | cmp -s - $data/1byte; then
+   gfexport $gftmp/xxx | cmp -s - $data/1byte &&
+
+   gfreg $data/1byte $gftmp/1byte &&
+   gfreg $data/1byte $gftmp/yyy &&
+   if gfmv $gftmp/1byte $gftmp/xxx $gftmp/yyy; then false; else true; fi &&
+
+   gfmkdir $gftmp/dir1 &&
+   gfmv $gftmp/1byte $gftmp/xxx $gftmp/yyy $gftmp/dir1 &&
+   gfls $gftmp/dir1/1byte &&
+   gfls $gftmp/dir1/xxx &&
+   gfls $gftmp/dir1/yyy &&
+
+   gfmkdir $gftmp/dir2 &&
+   gfreg $data/1byte $gftmp/dir2/1byte &&
+   gfchmod 0500 $gftmp/dir2/1byte &&
+   echo n | gfmv -i $gftmp/dir1/'*' $gftmp/dir2 &&
+   gfls $gftmp/dir1/1byte &&
+   gfls $gftmp/dir2/1byte &&
+   gfls $gftmp/dir2/xxx &&
+   gfls $gftmp/dir2/yyy
+then
 	exit_code=$exit_pass
 fi
 
-gfrm $gftmp/xxx
-gfrmdir $gftmp
+gfrm $gftmp/dir1/1byte $gftmp/dir2/1byte $gftmp/dir2/xxx $gftmp/dir2/yyy
+gfrmdir $gftmp/dir1 $gftmp/dir2 $gftmp
 exit $exit_code
