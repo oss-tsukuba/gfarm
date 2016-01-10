0b8f928e91a0969436669bc2307111d5984576e0
Osamu Tatebe <tatebe@users.sourceforge.net>
2016-01-10 02:55:03 +0000

    sequel to r10023 - stop to export gfarm_metadb_version variables
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10026 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/gfmd.c
+++ server/gfmd/gfmd.c
@@ -1626,9 +1626,6 @@ main(int argc, char **argv)
 
 	if (config_file == NULL)
 		config_file = GFMD_CONFIG;
-	gfarm_metadb_version_major = gfarm_major_version();
-	gfarm_metadb_version_minor = gfarm_minor_version();
-	gfarm_metadb_version_teeny = gfarm_teeny_version();
 	e = gfarm_server_initialize(config_file, &argc, &argv);
 	if (e != GFARM_ERR_NO_ERROR) {
 		gflog_debug(GFARM_MSG_1001486,
