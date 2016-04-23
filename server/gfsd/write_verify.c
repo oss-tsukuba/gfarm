1dee11029591b5264a021290ba946333c08f40fd
SODA Noriyuki <n-soda@users.sourceforge.net>
2016-04-23 05:03:11 +0000

    fix the following error when using rdtsc on i386: {{{ write_verify.c:654: error: request for member 'tv_sec' in something not a structure or union }}} pointed out by tatebe@-san:
    this is part of #954 - profile and progress logging support for write_verify
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10138 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfsd/write_verify.c
+++ server/gfsd/write_verify.c
@@ -651,7 +651,7 @@ write_verify_calc_report(gfarm_int64_t ino, gfarm_uint64_t gen,
 	const char *result, const char *aux)
 {
 	const char *a1, *a2, *a3;
-	time_t t2sec = t2->tv_sec;
+	time_t t2sec = gfarm_timerval_second(t2);
 	struct tm *tp = localtime(&t2sec);
 	double t = gfarm_timerval_sub(t2, t1);
 	char end_time[TIMEBUF_SIZE];
