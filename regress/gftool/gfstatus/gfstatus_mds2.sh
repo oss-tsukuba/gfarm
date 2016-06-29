2a5c7577470b2dc1dff589dc46f28fefb59c4a8a
Osamu Tatebe <tatebe@users.sourceforge.net>
2016-06-29 09:52:28 +0000

    just check exit code.  server or port may be changed due to failover
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10173 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfstatus/gfstatus_mds2.sh
+++ regress/gftool/gfstatus/gfstatus_mds2.sh
@@ -9,21 +9,7 @@ if [ "$GFARM_TEST_MDS2" = "" ]; then
 	exit $exit_unsupported
 fi
 
-host=${GFARM_TEST_MDS2%:*}
-port=${GFARM_TEST_MDS2#*:}
-out_host=`gfstatus -P gfarm://$GFARM_TEST_MDS2 | grep "gfmd server name"`
-out_host=${out_host#gfmd server name: }
-out_port=`gfstatus -P gfarm://$GFARM_TEST_MDS2 | grep "gfmd server port"`
-out_port=${out_port#gfmd server port: }
-
-echo "host=$host port=$port out_host=$out_host out_port=$out_port"
-
-if [ "$host" != "$out_host" ]; then
-	echo host is not equal to out_host
-	exit $exit_fail
-fi
-if [ "$port" != "$out_port" ]; then
-	echo port is not equal to out_port
+if ! gfstatus -P gfarm://$GFARM_TEST_MDS2; then
 	exit $exit_fail
 fi
 
