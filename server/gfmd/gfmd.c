f4fb6dc837dccbc00b59990e5da529499898ee13
SODA Noriyuki <n-soda@users.sourceforge.net>
2016-07-14 01:48:32 +0000

    fix #961 - SIGINFO and SIGPWR are same value on Linux/alpha
    This patch is provided by Michael Cree via Dmitry Smirnov.
    problem first reported in https://bugs.debian.org/827021
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10177 4a5e1ece-6a47-475c-b646-731d720b41d0

--- server/gfmd/gfmd.c
+++ server/gfmd/gfmd.c
@@ -1420,7 +1420,9 @@ sigs_handler(void *p)
 		case SIGXCPU:
 		case SIGXFSZ:
 #ifdef SIGPWR
+#if !defined(SIGINFO) || (SIGPWR != SIGINFO) /* see SF.net #961 */
 		case SIGPWR:
+#endif
 #endif
 			/* terminate gfmd */
 			break;
