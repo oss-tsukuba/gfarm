3e28605f0b8864851ee6d4446b06b4e93a451fdf
SODA Noriyuki <n-soda@users.sourceforge.net>
2017-02-24 03:52:46 +0000

    implement lower priority job enqueue function to gfmd thread pool, and make the replica remover thread use it, to avoid that a thread which holds the giant_lock sleeps while adding a gfmd-initiated replication request to the back_channel job queue.
    note that this is a theoretical problem which was found by code reading.
    currently the bottleneck of massive removal is to write the journal file
    (found by tatebe-san).
    
    this is workaround against #986 - possible long giant_lock at massive number of file removable,
    and adds the metadb_server_remover_queue_length directive to gfmd.conf
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.7@10447 4a5e1ece-6a47-475c-b646-731d720b41d0

--- lib/libgfarm/gfarm/config.h
+++ lib/libgfarm/gfarm/config.h
@@ -61,6 +61,7 @@ extern int gfarm_metadb_max_descriptors;
 extern int gfarm_metadb_stack_size;
 extern int gfarm_metadb_thread_pool_size;
 extern int gfarm_metadb_job_queue_length;
+extern int gfarm_metadb_remover_queue_length;
 extern int gfarm_metadb_heartbeat_interval;
 extern int gfarm_metadb_dbq_size;
 extern int gfarm_metadb_server_back_channel_sndbuf_limit;
@@ -78,8 +79,10 @@ extern int gfarm_replicainfo_enabled;
 #define GFARM_METADB_THREAD_POOL_SIZE_DEFAULT	16  /* quadcore, quadsocket */
 #if 0
 #define GFARM_METADB_JOB_QUEUE_LENGTH_DEFAULT	160 /* THREAD_POOL * 10 */
+#define GFARM_METADB_REMOVER_QUEUE_LENGTH_DEFAULT	16
 #else /* XXX FIXME: until bcworkq is implemented */
 #define GFARM_METADB_JOB_QUEUE_LENGTH_DEFAULT	16000
+#define GFARM_METADB_REMOVER_QUEUE_LENGTH_DEFAULT	160
 #endif
 #define GFARM_METADB_HEARTBEAT_INTERVAL_DEFAULT 180 /* 3 min */
 #define GFARM_METADB_DBQ_SIZE_DEFAULT	65536
--- server/gfmd/thrpool.h
+++ server/gfmd/thrpool.h
@@ -1,5 +1,8 @@
 struct thread_pool;
 struct thread_pool *thrpool_new(int, int, const char *);
 void thrpool_add_job(struct thread_pool *, void *(*)(void *), void *);
+void thrpool_add_job_low_priority(struct thread_pool *,
+	void *(*)(void *), void *);
+void thrpool_set_jobq_low_priority_limit(struct thread_pool *, int);
 
 void thrpool_info(void);
