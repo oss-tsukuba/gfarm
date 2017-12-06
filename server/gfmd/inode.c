oss-tsukuba/gfarm

commit 1e9143ec9b99e8bd5e4925c9078bdafb7e8f9587

merge r10660 from the 2.7 branch:
implement #1012 - replication log in gfmd


git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10671 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h> /* sprintf */
 #include <ctype.h>
+#include <time.h>
 #include <sys/time.h>
 #include <pthread.h>
 #include <errno.h>
@@ -4707,6 +4708,9 @@ file_replicating_new(struct inode *inode, struct host *dst,
 	fr->igen = inode_get_gen(inode);
 	fr->cleanup = deferred_cleanup;
 
+	/* record this always, because gfarm_ctxp->profile may be changed */
+	gfarm_gettime(&fr->queue_time);
+
 	*frp = fr;
 	return (GFARM_ERR_NO_ERROR);
 }
@@ -4774,6 +4778,24 @@ inode_replicated(struct file_replicating *fr,
 	int transaction = 0;
 	gfarm_error_t e = GFARM_ERR_NO_ERROR;
 	static const char diag[] = "inode_replicated";
+
+	/* log even if generation updated */
+	if (src_errcode == GFARM_ERR_NO_ERROR &&
+	    dst_errcode == GFARM_ERR_NO_ERROR &&
+	    gfarm_ctxp->profile) {
+		struct timespec now;
+		double t;
+
+		gfarm_gettime(&now);
+		t = now.tv_sec - fr->queue_time.tv_sec +
+		    (double)(now.tv_nsec - fr->queue_time.tv_nsec)
+		    / GFARM_SECOND_BY_NANOSEC;
+		gflog_info(GFARM_MSG_UNFIXED, "inode %lld:%lld to %s: "
+		    "size %lld time %g second speed %g Mb/s",
+		    (long long)inode_get_number(inode),
+		    (long long)fr->igen, host_name(fr->dst), (long long)size,
+		     t, t != 0 ? size / t : 0.0);
+	}
 
 	if (db_begin(diag) == GFARM_ERR_NO_ERROR)
 		transaction = 1;

server/gfmd/abstract_host.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 #include <sys/time.h>
 
 #include <pthread.h>

server/gfmd/back_channel.c
@@ -7,6 +7,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/time.h>
 #include <sys/socket.h>
 

server/gfmd/fsngroup.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/types.h>
 
 #include <gfarm/gfarm.h>

server/gfmd/group.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <time.h>
 #include <sys/types.h>
 
 #include <gfarm/gfarm.h>

server/gfmd/job.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 #include <sys/types.h>
 
 #include <gfarm/gfarm.h>

server/gfmd/peer.c
@@ -18,6 +18,7 @@
 #include <signal.h> /* for sig_atomic_t */
 #include <netinet/in.h>
 #include <netdb.h> /* for NI_MAXHOST, NI_NUMERICHOST, etc */
+#include <time.h>
 
 #include <gfarm/gflog.h>
 #include <gfarm/error.h>

server/gfmd/peer.h
@@ -153,6 +153,9 @@ struct file_replicating {
 
 	/* GFS_PROTO_REPLICATION_CKSUM_REQUEST */
 	gfarm_int32_t cksum_request_flags;
+
+	/* for gfs_profile() */
+	struct timespec queue_time;
 };
 
 void file_replicating_set_handle(struct file_replicating *, gfarm_int64_t);

commit 080b90858c4935ff0a0310f3bfd9b965d746fad1

merge r10735 from the 2.7 branch:
fix #1024 - "Mb/s" in #1012 should be "byte/s"

problem introduced in r10671 for #1012 - replication log in gfmd
pointed out by KANEYAMA Hidetomo-san


git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10736 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -4815,7 +4815,7 @@ inode_replicated(struct file_replicating *fr,
 		    (double)(now.tv_nsec - fr->queue_time.tv_nsec)
 		    / GFARM_SECOND_BY_NANOSEC;
 		gflog_info(GFARM_MSG_1005039, "inode %lld:%lld to %s: "
-		    "size %lld time %g second speed %g Mb/s",
+		    "size %lld time %g second speed %g byte/s",
 		    (long long)inode_get_number(inode),
 		    (long long)fr->igen, host_name(fr->dst), (long long)size,
 		     t, t != 0 ? size / t : 0.0);