oss-tsukuba/gfarm

commit a3886d2a6bbf525910af11e62146c6c2576a8177

add fail over feature to gfs_pio_stat() for gfm_connection related to GFS_File. this feature enables continuous access to GFS_File after calling gfs_pio_stat() at gfmd failover occurred.
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@5589 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/gfs_pio.c
@@ -12,22 +12,35 @@
 #include <unistd.h>	/* [FRWX]_OK */
 #include <errno.h>
 #include <openssl/evp.h>
+#include <pthread.h>
 
 #define GFARM_INTERNAL_USE
 #include <gfarm/gfarm.h>
 
 #include "timer.h"
 #include "gfutil.h"
+#include "queue.h"
+#include "thrsubr.h"
 
 #include "liberror.h"
+#include "filesystem.h"
 #include "gfs_profile.h"
 #include "gfm_client.h"
 #include "gfs_proto.h"	/* GFS_PROTO_FSYNC_* */
 #include "gfs_io.h"
 #include "gfs_pio.h"
 #include "gfp_xdr.h"
+#include "gfs_failover.h"
+#include "gfs_file_list.h"
 
 #include "config.h" /* XXX FIXME this shouldn't be needed here */
+
+
+struct gfs_file_list {
+	GFARM_HCIRCLEQ_HEAD(gfs_file) files;
+	pthread_mutex_t mutex;
+};
+
 
 /*
  * GFARM_ERRMSG_GFS_PIO_IS_EOF is used as mark of EOF,
@@ -128,7 +141,7 @@ gfs_pio_fileno(GFS_File gf)
 
 static gfarm_error_t
 gfs_file_alloc(struct gfm_connection *gfm_server, gfarm_int32_t fd, int flags,
-	GFS_File *gfp)
+	char *url, gfarm_ino_t ino, GFS_File *gfp)
 {
 	GFS_File gf;
 	char *buffer;
@@ -170,9 +183,12 @@ gfs_file_alloc(struct gfm_connection *gfm_server, gfarm_int32_t fd, int flags,
 	gf->p = 0;
 	gf->length = 0;
 	gf->offset = 0;
+	gf->ino = ino;
+	gf->url = url;
 
 	gf->view_context = NULL;
 	gfs_pio_set_view_default(gf);
+	gfs_pio_file_list_add(gfm_client_connection_file_list(gfm_server), gf);
 
 	*gfp = gf;
 	return (GFARM_ERR_NO_ERROR);
@@ -182,6 +198,7 @@ static void
 gfs_file_free(GFS_File gf)
 {
 	free(gf->buffer);
+	free(gf->url);
 	/* do not touch gf->pi here */
 	free(gf);
 }
@@ -206,7 +223,7 @@ gfs_pio_create(const char *url, int flags, gfarm_mode_t mode, GFS_File *gfp)
 	struct gfm_connection *gfm_server;
 	int fd, type;
 	gfarm_timerval_t t1, t2;
-
+	char *real_url;
 	/* for gfarm_file_trace */
 	int src_port;
 	gfarm_ino_t inum;
@@ -216,13 +233,14 @@ gfs_pio_create(const char *url, int flags, gfarm_mode_t mode, GFS_File *gfp)
 	gfs_profile(gfarm_gettimerval(&t1));
 
 	if ((e = gfm_create_fd(url, flags, mode, &gfm_server, &fd, &type,
-	    &inum, &gen)) == GFARM_ERR_NO_ERROR) {
+	    &inum, &gen, &real_url)) == GFARM_ERR_NO_ERROR) {
 		if (type != GFS_DT_REG) {
 			e = type == GFS_DT_DIR ? GFARM_ERR_IS_A_DIRECTORY :
 			    type == GFS_DT_LNK ? GFARM_ERR_IS_A_SYMBOLIC_LINK :
 			    GFARM_ERR_OPERATION_NOT_PERMITTED;
 		} else
-			e = gfs_file_alloc(gfm_server, fd, flags, gfp);
+			e = gfs_file_alloc(gfm_server, fd, flags, real_url,
+			    inum, gfp);
 		if (e != GFARM_ERR_NO_ERROR) {
 			(void)gfm_close_fd(gfm_server, fd); /* ignore result */
 			gfm_client_connection_free(gfm_server);
@@ -263,19 +281,23 @@ gfs_pio_open(const char *url, int flags, GFS_File *gfp)
 	struct gfm_connection *gfm_server;
 	int fd, type;
 	gfarm_timerval_t t1, t2;
+	gfarm_ino_t ino;
+	char *real_url = NULL;
 
 	GFARM_TIMEVAL_FIX_INITIALIZE_WARNING(t1);
 	gfs_profile(gfarm_gettimerval(&t1));
 
-	if ((e = gfm_open_fd(url, flags, &gfm_server, &fd, &type)) ==
-	    GFARM_ERR_NO_ERROR) {
+	if ((e = gfm_open_fd_with_ino(url, flags, &gfm_server, &fd, &type,
+	    &real_url, &ino)) == GFARM_ERR_NO_ERROR) {
 		if (type != GFS_DT_REG) {
 			e = type == GFS_DT_DIR ? GFARM_ERR_IS_A_DIRECTORY :
 			    type == GFS_DT_LNK ? GFARM_ERR_IS_A_SYMBOLIC_LINK :
 			    GFARM_ERR_OPERATION_NOT_PERMITTED;
 		} else
-			e = gfs_file_alloc(gfm_server, fd, flags, gfp);
+			e = gfs_file_alloc(gfm_server, fd, flags, real_url, ino,
+			    gfp);
 		if (e != GFARM_ERR_NO_ERROR) {
+			free(real_url);
 			(void)gfm_close_fd(gfm_server, fd); /* ignore result */
 			gfm_client_connection_free(gfm_server);
 			gflog_debug(GFARM_MSG_1001297,
@@ -316,6 +338,7 @@ gfs_pio_close(GFS_File gf)
 {
 	gfarm_error_t e, e_save;
 	gfarm_timerval_t t1, t2;
+	int failed_over = 0;
 
 	GFARM_TIMEVAL_FIX_INITIALIZE_WARNING(t1);
 	gfs_profile(gfarm_gettimerval(&t1));
@@ -335,15 +358,23 @@ gfs_pio_close(GFS_File gf)
 			gflog_error(GFARM_MSG_1003268,
 			    "ignore %s error at pio close operation",
 			    gfarm_error_string(e));
+			failed_over = 1;
 			e = GFARM_ERR_NO_ERROR;
 		}
 		if (e_save == GFARM_ERR_NO_ERROR)
 			e_save = e;
 	}
 
-	e = gfm_close_fd(gf->gfm_server, gfs_pio_fileno(gf));
-	if (e_save == GFARM_ERR_NO_ERROR)
-		e_save = e;
+	gfs_pio_file_list_remove(
+	    gfm_client_connection_file_list(gf->gfm_server), gf);
+
+	/* do not call gfm_close_fd when fd is read-only and gfmd failed over */
+	if (!failed_over || (gf->open_flags & GFARM_FILE_RDONLY) == 0) {
+		e = gfm_close_fd(gf->gfm_server, gfs_pio_fileno(gf));
+		if (e_save == GFARM_ERR_NO_ERROR)
+			e_save = e;
+	}
+
 	gfm_client_connection_free(gf->gfm_server);
 	gfs_file_free(gf);
 
@@ -1401,6 +1432,75 @@ gfs_pio_stat(GFS_File gf, struct gfs_stat *st)
 		}
 	}
 	return (e);
+}
+
+#define GFS_FILE_LIST_MUTEX "gfs_file_list.mutex"
+
+struct gfs_file_list *
+gfs_pio_file_list_alloc()
+{
+	struct gfs_file_list *gfl;
+
+	GFARM_MALLOC(gfl);
+	if (gfl == NULL)
+		return (NULL);
+	GFARM_HCIRCLEQ_INIT(gfl->files, hcircleq);
+	gfarm_mutex_init(&gfl->mutex, "gfs_pio_file_list_alloc",
+	    GFS_FILE_LIST_MUTEX);
+
+	return (gfl);
+}
+
+void
+gfs_pio_file_list_free(struct gfs_file_list *gfl)
+{
+	if (gfl == NULL)
+		return;
+	gfarm_mutex_destroy(&gfl->mutex, "gfs_pio_file_list_free",
+	    GFS_FILE_LIST_MUTEX);
+	free(gfl);
+}
+
+void
+gfs_pio_file_list_add(struct gfs_file_list *gfl, GFS_File gf)
+{
+	gfarm_mutex_lock(&gfl->mutex, "gfs_pio_file_list_add",
+	    GFS_FILE_LIST_MUTEX);
+
+	GFARM_HCIRCLEQ_INSERT_TAIL(gfl->files, gf, hcircleq);
+
+	gfarm_mutex_unlock(&gfl->mutex, "gfs_pio_file_list_add",
+	    GFS_FILE_LIST_MUTEX);
+}
+
+void
+gfs_pio_file_list_remove(struct gfs_file_list *gfl, GFS_File gf)
+{
+	gfarm_mutex_lock(&gfl->mutex, "gfs_pio_file_list_remove",
+	    GFS_FILE_LIST_MUTEX);
+
+	GFARM_HCIRCLEQ_REMOVE(gf, hcircleq);
+
+	gfarm_mutex_unlock(&gfl->mutex, "gfs_pio_file_list_remove",
+	    GFS_FILE_LIST_MUTEX);
+}
+
+void
+gfs_pio_file_list_foreach(struct gfs_file_list *gfl,
+	int (*func)(struct gfs_file *, void *), void *closure)
+{
+	GFS_File gf;
+
+	gfarm_mutex_lock(&gfl->mutex, "gfs_pio_file_list_foreach",
+	    GFS_FILE_LIST_MUTEX);
+
+	GFARM_HCIRCLEQ_FOREACH(gf, gfl->files, hcircleq) {
+		if (func(gf, closure) == 0)
+			break;
+	}
+
+	gfarm_mutex_unlock(&gfl->mutex, "gfs_pio_file_list_foreach",
+	    GFS_FILE_LIST_MUTEX);
 }
 
 void

gftool/gfexport/Makefile
@@ -9,7 +9,7 @@ include $(top_srcdir)/makes/var.mk
 SRCS = gfexport.c
 OBJS = gfexport.o
 PROGRAM = gfexport
-CFLAGS = $(COMMON_CFLAGS) -I$(GFARMLIB_SRCDIR) $(openssl_includes)
+CFLAGS = $(COMMON_CFLAGS) -I$(GFUTIL_SRCDIR) -I$(GFARMLIB_SRCDIR) $(openssl_includes)
 LDLIBS = $(COMMON_LDFLAGS) $(GFARMLIB) $(LIBS)
 DEPLIBS = $(DEPGFARMLIB)
 
@@ -19,4 +19,4 @@ include $(top_srcdir)/makes/prog.mk
 
 ###
 
-$(OBJS): $(DEPGFARMINC) $(GFARMLIB_SRCDIR)/gfs_profile.h $(GFARMLIB_SRCDIR)/host.h $(GFARMLIB_SRCDIR)/config.h $(GFARMLIB_SRCDIR)/gfs_pio.h
+$(OBJS): $(DEPGFARMINC) $(GFUTIL_SRCDIR)/queue.h $(GFARMLIB_SRCDIR)/gfs_profile.h $(GFARMLIB_SRCDIR)/host.h $(GFARMLIB_SRCDIR)/config.h $(GFARMLIB_SRCDIR)/gfs_pio.h

gftool/gfexport/gfexport.c
@@ -5,6 +5,8 @@
 
 #include <gfarm/gfarm.h>
 
+#include "queue.h" /* for gfs_file */
+
 #include "gfs_profile.h"
 #include "host.h"
 #include "config.h"

gftool/gfreg/gfreg_simple.c
@@ -12,6 +12,8 @@
 
 #include <gfarm/gfarm.h>
 
+#include "queue.h" /* for gfs_file */
+
 #include "gfutil.h"
 #include "timer.h"
 #include "gfs_profile.h"

lib/libgfarm/gfarm/Makefile
@@ -63,6 +63,7 @@ SRCS = \
 	gfs_pio.c \
 	gfs_pio_section.c \
 	gfs_pio_local.c gfs_pio_remote.c \
+	gfs_pio_failover.c \
 	gfs_profile.c \
 	gfs_chmod.c \
 	gfs_chown.c \
@@ -145,6 +146,7 @@ OBJS = \
 	gfs_pio.lo \
 	gfs_pio_section.lo \
 	gfs_pio_local.lo gfs_pio_remote.lo \
+	gfs_pio_failover.lo \
 	gfs_profile.lo \
 	gfs_chmod.lo \
 	gfs_chown.lo \
@@ -214,7 +216,7 @@ error.lo: $(GFUTIL_SRCDIR)/hash.h
 conn_cache.lo: $(GFUTIL_SRCDIR)/hash.h $(GFUTIL_SRCDIR)/lru_cache.h conn_hash.h conn_cache.h
 crc32.lo: crc32.h
 filesystem.lo: filesystem.h metadb_server.h $(GFUTIL_SRCDIR)/hash.h
-gfm_client.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/hash.h $(GFUTIL_SRCDIR)/gfnetdb.h $(GFUTIL_SRCDIR)/lru_cache.h gfp_xdr.h io_fd.h sockopt.h sockutil.h host.h auth.h config.h conn_cache.h gfm_proto.h gfj_client.h xattr_info.h gfm_client.h
+gfm_client.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/hash.h $(GFUTIL_SRCDIR)/gfnetdb.h $(GFUTIL_SRCDIR)/lru_cache.h gfp_xdr.h io_fd.h sockopt.h sockutil.h host.h auth.h config.h conn_cache.h gfm_proto.h gfj_client.h xattr_info.h gfs_file_list.h gfm_client.h
 gfm_schedule.lo: gfm_client.h gfm_schedule.h
 gfp_xdr.lo: $(GFUTIL_SRCDIR)/gfutil.h liberror.h iobuffer.h gfp_xdr.h
 gfp_xdr_server.lo: $(GFUTIL_SRCDIR)/id_table.h gfp_xdr.h
@@ -224,15 +226,16 @@ gfs_dirplus.lo: $(GFUTIL_SRCDIR)/gfutil.h config.h gfm_client.h gfs_io.h
 gfs_dirplusxattr.lo: $(GFUTIL_SRCDIR)/gfutil.h config.h gfm_client.h gfs_io.h gfs_dirplusxattr.h
 gfs_dircache.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/hash.h config.h gfs_dir.h gfs_dircache.h gfs_attrplus.h
 gfs_attrplus.lo: $(GFUTIL_SRCDIR)/gfutil.h gfm_client.h config.h lookup.h gfs_attrplus.h
-gfs_exec.lo: gfs_pio.h
+gfs_exec.lo: $(GFUTIL_SRCDIR)/queue.h gfs_pio.h
 gfs_io.lo: gfs_io.h
 gfs_link.lo: $(GFUTIL_SRCDIR)/gfutil.h gfm_client.h config.h lookup.h
-gfs_pio.lo: $(GFUTIL_SRCDIR)/timer.h $(GFUTIL_SRCDIR)/gfutil.h liberror.h gfs_proto.h gfm_client.h gfs_proto.h gfs_io.h gfs_pio.h config.h
-gfs_pio_global.lo: gfs_proto.h gfs_pio.h
-gfs_pio_local.lo: gfs_proto.h gfs_pio.h
+gfs_pio.lo: $(GFUTIL_SRCDIR)/timer.h $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/queue.h liberror.h gfs_proto.h gfm_client.h gfs_proto.h gfs_io.h gfs_file_list.h gfs_pio.h config.h
+gfs_pio_global.lo: $(GFUTIL_SRCDIR)/queue.h gfs_proto.h gfs_pio.h
+gfs_pio_local.lo: $(GFUTIL_SRCDIR)/queue.h gfs_proto.h gfs_pio.h
 gfs_pio_misc.lo: gfs_proto.h gfs_misc.h
-gfs_pio_remote.lo: host.h config.h gfs_proto.h gfs_client.h gfs_pio.h
-gfs_pio_section.lo: gfs_pio.h host.h config.h schedule.h gfs_client.h gfs_proto.h $(GFUTIL_SRCDIR)/timer.h
+gfs_pio_remote.lo: $(GFUTIL_SRCDIR)/queue.h host.h config.h gfs_proto.h gfs_client.h gfs_pio.h
+gfs_pio_section.lo: $(GFUTIL_SRCDIR)/queue.h gfs_pio.h host.h config.h schedule.h gfs_client.h gfs_proto.h $(GFUTIL_SRCDIR)/timer.h gfs_file_list.h gfs_failover.h
+gfs_pio_failover.lo: $(GFUTIL_SRCDIR)/queue.h config.h gfm_client.h gfs_client.h gfs_io.h gfs_pio.h gfs_misc.h filesystem.h gfs_file_list.h gfs_failover.h
 gfs_proto.lo: gfs_proto.h
 gfs_quota.lo: config.h quota_info.h
 gfs_readlink.lo: $(GFUTIL_SRCDIR)/gfutil.h gfm_client.h config.h lookup.h
@@ -240,7 +243,7 @@ gfs_realpath.lo: gfm_client.h lookup.h
 gfs_replica.lo: $(GFUTIL_SRCDIR)/gfutil.h gfm_client.h host.h config.h lookup.h
 gfs_replica_info.lo: gfm_proto.h gfm_client.h lookup.h
 gfs_replicate.lo: config.h host.h gfm_client.h gfs_client.h schedule.h gfs_misc.h
-gfs_stat.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/timer.h gfs_profile.h gfm_client.h config.h lookup.h gfs_misc.h
+gfs_stat.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/timer.h gfs_profile.h gfm_client.h config.h lookup.h gfs_misc.h gfs_failover.h
 gfs_statsw.lo: gfs_dircache.h
 gfs_symlink.lo: $(GFUTIL_SRCDIR)/gfutil.h gfm_client.h config.h lookup.h
 gfs_unlink.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/timer.h gfs_profile.h
@@ -266,6 +269,6 @@ sockutil.lo: $(GFUTIL_SRCDIR)/gfnetdb.h sockutil.h
 sockopt.lo: hostspec.h param.h sockopt.h
 stringlist.lo:
 timespec.lo: timespec.h
-url.lo: config.h gfs_pio.h
+url.lo: $(GFUTIL_SRCDIR)/queue.h config.h gfs_pio.h
 
 include $(optional_rule)

lib/libgfarm/gfarm/config.h
@@ -122,3 +122,5 @@ struct gfs_connection;
 struct gfm_connection;
 gfarm_error_t gfarm_client_process_set(struct gfs_connection *,
 	struct gfm_connection *);
+gfarm_error_t gfarm_client_process_reset(struct gfs_connection *,
+	struct gfm_connection *);

lib/libgfarm/gfarm/config_client.c
@@ -467,9 +467,11 @@ gfarm_initialize(int *argcp, char ***argvp)
 	return (GFARM_ERR_NO_ERROR);
 }
 
-gfarm_error_t
-gfarm_client_process_set(struct gfs_connection *gfs_server,
-	struct gfm_connection *gfm_server)
+static gfarm_error_t
+gfarm_client_process_set_or_reset(struct gfs_connection *gfs_server,
+	struct gfm_connection *gfm_server,
+	gfarm_error_t (*process_set_or_reset_op)(struct gfs_connection *,
+	    gfarm_int32_t, const char *, size_t, gfarm_pid_t))
 {
 	gfarm_int32_t key_type;
 	const char *key;
@@ -484,8 +486,24 @@ gfarm_client_process_set(struct gfs_connection *gfs_server,
 			gfarm_error_string(e));
 		return (e);
 	}
-	return (gfs_client_process_set(gfs_server,
-	    key_type, key, key_size, pid));
+	return (process_set_or_reset_op(gfs_server, key_type, key,
+	    key_size, pid));
+}
+
+gfarm_error_t
+gfarm_client_process_set(struct gfs_connection *gfs_server,
+	struct gfm_connection *gfm_server)
+{
+	return (gfarm_client_process_set_or_reset(gfs_server, gfm_server,
+	    gfs_client_process_set));
+}
+
+gfarm_error_t
+gfarm_client_process_reset(struct gfs_connection *gfs_server,
+	struct gfm_connection *gfm_server)
+{
+	return (gfarm_client_process_set_or_reset(gfs_server, gfm_server,
+	    gfs_client_process_reset));
 }
 
 /*

lib/libgfarm/gfarm/conn_cache.c
@@ -27,6 +27,8 @@ struct gfp_cached_connection {
 	struct gfp_conn_hash_id id;
 
 	void *connection_data;
+
+	void (*dispose_connection_data)(void *);
 };
 
 /*
@@ -54,6 +56,13 @@ gfp_cached_connection_set_data(struct gfp_cached_connection *connection,
 	void *p)
 {
 	connection->connection_data = p;
+}
+
+void
+gfp_cached_connection_set_dispose_data(struct gfp_cached_connection *connection,
+	void (*dispose_connection_data)(void *))
+{
+	connection->dispose_connection_data = dispose_connection_data;
 }
 
 const char *
@@ -130,6 +139,7 @@ gfp_uncached_connection_new(const char *hostname, int port,
 	gfarm_lru_init_uncached_entry(&connection->lru_entry);
 
 	connection->connection_data = NULL;
+	connection->dispose_connection_data = NULL;
 	*connectionp = connection;
 	return (GFARM_ERR_NO_ERROR);
 }
@@ -142,6 +152,9 @@ gfp_uncached_connection_dispose(struct gfp_cached_connection *connection)
 
 	free(idp->hostname);
 	free(idp->username);
+	if (connection->dispose_connection_data && connection->connection_data)
+		connection->dispose_connection_data(
+		    connection->connection_data);
 	free(connection);
 }
 
@@ -336,6 +349,7 @@ gfp_cached_connection_acquire(struct gfp_conn_cache *cache,
 		    = connection;
 		connection->hash_entry = entry;
 		connection->connection_data = NULL;
+		connection->dispose_connection_data = NULL;
 	}
 	gfarm_mutex_unlock(&cache->mutex, diag, diag_what);
 	*connectionp = connection;

commit 8355c34a782dbf5ab0309756c228bdd736c5daa6

fix memory leak in error case, which was introduced by r5589
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.7@10737 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/gfs_pio.c
@@ -446,7 +446,7 @@ gfs_pio_create_igen(const char *url, int flags, gfarm_mode_t mode,
 	struct gfm_connection *gfm_server;
 	int fd, type;
 	gfarm_timerval_t t1, t2;
-	char *real_url;
+	char *real_url = NULL;
 	/* for gfarm_file_trace */
 	int src_port;
 	gfarm_ino_t inum;
@@ -468,6 +468,7 @@ gfs_pio_create_igen(const char *url, int flags, gfarm_mode_t mode,
 			e = gfs_file_alloc(gfm_server, fd, flags, real_url,
 			    inum, gen, cip, gfp);
 		if (e != GFARM_ERR_NO_ERROR) {
+			free(real_url);
 			/* ignore result */
 			(void)gfm_close_fd(gfm_server, fd, NULL, NULL);
 			gfm_client_connection_free(gfm_server);