77fe8ddf2f0ab868b0efd39df25fb5b0244e63e5
SODA Noriyuki <n-soda@users.sourceforge.net>
2018-02-08 05:25:07 +0000

    merge r10657 from the 2.7 branch:
    remove obsolete gfdf.c.
    this is preparation of #1011 - gfdf(1) -i option to print number of inodes 
    
    
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10771 4a5e1ece-6a47-475c-b646-731d720b41d0

--- gftool/gfdf/gfdf.c
+++ gftool/gfdf/gfdf.c
@@ -1,158 +1,357 @@
-#include <stdio.h>
+/*
+ * $Id$
+ */
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <libgen.h>
+#include <string.h>
 
 #include <gfarm/gfarm.h>
 
+#include "liberror.h"
+#include "gfm_client.h"
+#include "lookup.h"
+#include "gfarm_path.h"
+
 char *program_name = "gfdf";
 
-void
-inode_title(void)
+enum sort_order {
+	SO_NAME,
+	SO_SIZE
+} option_sort_order = SO_NAME;
+static int option_reverse_sort = 0;
+static int option_formatting_flags = 0;
+
+struct formatter {
+	char *summary_title_format;
+	char *summary_data_format;
+	char *nodes_title_format;
+	char *nodes_data_format;
+	char *nodes_separator;
+	size_t (*number_to_string)(char *, size_t, long long);
+	size_t (*blocks_to_string)(char *, size_t, long long);
+};
+
+#define PRECISE_TITLE_FORMAT	"%13s %13s %13s %4s"
+#define PRECISE_DATA_FORMAT	"%13s %13s %13s %3.0f%%"
+
+#define READABLE_TITLE_FORMAT	"%9s %6s %6s %4s"
+#define READABLE_DATA_FORMAT	"%9s %6s %6s %3.0f%%"
+
+static size_t
+precise_number(char *buf, size_t len, long long number)
 {
-	printf("%12s%12s%12s%7s %s\n",
-	    "Inodes", "IUsed", "IAvail", "%IUsed", "Host");
+	return (snprintf(buf, len, "%13lld", number));
 }
 
-void
-size_title(void)
+static size_t
+readable_number(char *buf, size_t len, long long number)
 {
-	printf("%12s%12s%12s%9s %s\n",
-	    "1K-blocks", "Used", "Avail", "Capacity", "Host");
+	return (gfarm_humanize_signed_number(buf, len, number,
+	    option_formatting_flags));
 }
 
-char *
-inode_print(char *host)
+static size_t
+readable_blocks(char *buf, size_t len, long long number)
 {
-	char *e;
-	gfarm_int32_t bsize;
-	file_offset_t blocks, bfree, bavail, files, ffree, favail;
-
-	e = gfs_statfsnode(host, &bsize,
-	    &blocks, &bfree, &bavail, &files, &ffree, &favail);
-	if (e != NULL) {
-		fprintf(stderr, "%s: %s\n", host, e);
-		return (e);
-	}
-	printf("%12.0f%12.0f%12.0f  %3.0f%%  %s\n", 
-	    (double)files, (double)files - ffree, (double)favail,
-	    (double)(files - ffree)/(files - (ffree - favail))*100.0, host);
-	return (NULL);
+	return (gfarm_humanize_signed_number(buf, len, number * 1024,
+	    option_formatting_flags));
 }
 
-char *
-size_print(char *host)
-{
-	char *e;
-	gfarm_int32_t bsize;
-	file_offset_t blocks, bfree, bavail, files, ffree, favail;
+const struct formatter precise_formatter = {
+	PRECISE_TITLE_FORMAT	" %13s\n",
+	PRECISE_DATA_FORMAT	" %13s\n",
+	PRECISE_TITLE_FORMAT	" %s\n",
+	PRECISE_DATA_FORMAT	" %s\n",
+	"----------------------------------------------",
+	precise_number,
+	precise_number
+};
 
-	e = gfs_statfsnode(host, &bsize,
-	    &blocks, &bfree, &bavail, &files, &ffree, &favail);
-	if (e != NULL) {
-		fprintf(stderr, "%s: %s\n", host, e);
-		return (e);
-	}
-	printf("%12.0f%12.0f%12.0f   %3.0f%%   %s\n", 
-	    (double)(blocks * bsize) / 1024.0,
-	    (double)(blocks - bfree) * bsize / 1024.,
-	    (double)bavail * bsize / 1024.0,
-	    (double)(blocks - bfree)/(blocks - (bfree - bavail))*100.0,
-	    host);
-	return (NULL);
-}
+const struct formatter readable_formatter = {
+	READABLE_TITLE_FORMAT	" %6s\n",
+	READABLE_DATA_FORMAT	" %6s\n",
+	READABLE_TITLE_FORMAT	" %s\n",
+	READABLE_DATA_FORMAT	" %s\n",
+	"----------------------------",
+	readable_number,
+	readable_blocks
+};
 
-#define EXIT_INVALID_ARGUMENT	2
+const struct formatter *formatter = &precise_formatter;
 
-void
+static void
 usage(void)
 {
-	fprintf(stderr, "Usage:\t%s -h <host>\n", program_name);
-	fprintf(stderr, "\t%s -H <hostfile>\n", program_name);
-	exit(EXIT_INVALID_ARGUMENT);
+	fprintf(stderr, "Usage: %s [-ahHnrSV] [-P path] [-D domain]\n",
+		program_name);
+	exit(1);
+}
+
+gfarm_error_t
+display_statfs(const char *path, const char *dummy, int *ndisplayed)
+{
+	gfarm_error_t e;
+	gfarm_off_t used, avail, files;
+	char capbuf[GFARM_INT64STRLEN];
+	char usedbuf[GFARM_INT64STRLEN];
+	char availbuf[GFARM_INT64STRLEN];
+	char filesbuf[GFARM_INT64STRLEN];
+
+	*ndisplayed = 0;
+	/* XXX FIXME: should implement and use gfs_statvfs */
+	e = gfs_statfs(&used, &avail, &files);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+
+	(*formatter->blocks_to_string)(capbuf, sizeof capbuf,
+	    (long long)used + avail);
+	(*formatter->blocks_to_string)(usedbuf, sizeof usedbuf,
+	    (long long)used);
+	(*formatter->blocks_to_string)(availbuf, sizeof availbuf,
+	    (long long)avail);
+	(*formatter->number_to_string)(filesbuf, sizeof filesbuf,
+	    (long long)files);
+
+	printf(formatter->summary_title_format,
+	       "1K-blocks", "Used", "Avail", "Use%", "Files");
+	printf(formatter->summary_data_format,
+	       capbuf, usedbuf, availbuf,
+	       (double)used / (used + avail) * 100,
+	       filesbuf);
+
+	*ndisplayed = 1;
+	return (GFARM_ERR_NO_ERROR);
+}
+
+static int
+compare_hostname(const void *s1, const void *s2)
+{
+	const struct gfarm_host_sched_info *h1 = s1;
+	const struct gfarm_host_sched_info *h2 = s2;
+
+	return (strcoll(h1->host, h2->host));
+}
+
+static int
+compare_hostname_r(const void *s1, const void *s2)
+{
+	return (-compare_hostname(s1, s2));
+}
+
+static int
+compare_available_capacity(const void *s1, const void *s2)
+{
+	const struct gfarm_host_sched_info *h1 = s1;
+	const struct gfarm_host_sched_info *h2 = s2;
+	gfarm_uint64_t a1, a2;
+
+	a1 = h1->disk_avail;
+	a2 = h2->disk_avail;
+
+	if (a1 < a2)
+		return (-1);
+	else if (a1 > a2)
+		return (1);
+	else
+		return (0);
+}
+
+static int
+compare_available_capacity_r(const void *s1, const void *s2)
+{
+	return (-compare_available_capacity(s1, s2));
+}
+
+/* XXX FIXME: should traverse all mounted metadata servers */
+gfarm_error_t
+schedule_host_domain(const char *path, const char *domain,
+	int *nhostsp, struct gfarm_host_sched_info **hostsp)
+{
+	gfarm_error_t e;
+	struct gfm_connection *gfm_server;
+	int (*compare)(const void *, const void *);
+
+	if ((e = gfm_client_connection_and_process_acquire_by_path(path,
+	    &gfm_server)) != GFARM_ERR_NO_ERROR)
+		return (e);
+
+	e = gfm_client_schedule_host_domain(gfm_server, domain,
+	    nhostsp, hostsp);
+	gfm_client_connection_free(gfm_server);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+
+#ifdef __GNUC__ /* workaround gcc warning: unused variable */
+	compare = NULL;
+#endif
+	switch (option_sort_order) {
+	case SO_NAME:
+		compare = !option_reverse_sort ?
+		    compare_hostname : compare_hostname_r;
+		break;
+	case SO_SIZE:
+		compare = !option_reverse_sort ?
+		    compare_available_capacity : compare_available_capacity_r;
+		break;
+	}
+	qsort(*hostsp, *nhostsp, sizeof(**hostsp), compare);
+	return (e);
+}
+
+gfarm_error_t
+display_statfs_nodes(const char *path, const char *domain, int *ndisplayed)
+{
+	gfarm_error_t e;
+	int nhosts, i;
+	struct gfarm_host_sched_info *hosts;
+	gfarm_uint64_t used, avail;
+	gfarm_uint64_t total_used = 0, total_avail = 0;
+	char capbuf[GFARM_INT64STRLEN];
+	char usedbuf[GFARM_INT64STRLEN];
+	char availbuf[GFARM_INT64STRLEN];
+
+	*ndisplayed = 0;
+	e = schedule_host_domain(path, domain, &nhosts, &hosts);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+	printf(formatter->nodes_title_format,
+	       "1K-blocks", "Used", "Avail", "Use%", "Host");
+	for (i = 0; i < nhosts; ++i) {
+		used = hosts[i].disk_used;
+		avail = hosts[i].disk_avail;
+		(*formatter->blocks_to_string)(capbuf, sizeof capbuf,
+		    (long long)used + avail);
+		(*formatter->blocks_to_string)(usedbuf, sizeof usedbuf,
+		    (long long)used);
+		(*formatter->blocks_to_string)(availbuf, sizeof availbuf,
+		    (long long)avail);
+		printf(formatter->nodes_data_format,
+		       capbuf, usedbuf, availbuf,
+		       (double)used / (used + avail) * 100,
+		       hosts[i].host);
+		total_used += used;
+		total_avail += avail;
+	}
+	if (nhosts > 0) {
+		puts(formatter->nodes_separator);
+		(*formatter->blocks_to_string)(capbuf, sizeof capbuf,
+		    (long long)total_used + total_avail);
+		(*formatter->blocks_to_string)(usedbuf, sizeof usedbuf,
+		    (long long)total_used);
+		(*formatter->blocks_to_string)(availbuf, sizeof availbuf,
+		    (long long)total_avail);
+		printf(formatter->nodes_data_format,
+		       capbuf, usedbuf, availbuf,
+		       (double)total_used / (total_used + total_avail) * 100,
+		       "");
+	} else {
+		fprintf(stderr, "%s\n",
+		    gfarm_error_string(GFARM_ERR_NO_FILESYSTEM_NODE));
+	}
+
+	gfarm_host_sched_info_free(nhosts, hosts);
+	*ndisplayed = nhosts;
+ 
+	return (GFARM_ERR_NO_ERROR);
+}
+
+gfarm_error_t
+display_nodes(const char *path, const char *domain, int *ndisplayed)
+{
+	gfarm_error_t e;
+	int nhosts, i;
+	struct gfarm_host_sched_info *hosts;
+
+	*ndisplayed = 0;
+	e = schedule_host_domain(path, domain, &nhosts, &hosts);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+
+	for (i = 0; i < nhosts; ++i)
+		printf("%s\n", hosts[i].host);
+
+	gfarm_host_sched_info_free(nhosts, hosts);
+	*ndisplayed = nhosts;
+	return (e);
 }
 
 int
-main(int argc, char **argv)
+main(int argc, char *argv[])
 {
-	int status = EXIT_SUCCESS;
-	void (*title)(void) = size_title;
-	char *(*print)(char *) = size_print;
-	char *e, **hosts = NULL;
-	int c, i, lineno, ninfos, nhosts = 0;
-	gfarm_stringlist hostlist;
-	struct gfarm_host_info *infos;
+	gfarm_error_t e;
+	const char *domain = "", *path = ".";
+	char *p = NULL;
+	gfarm_error_t (*statfs)(const char *, const char *, int *) =
+		display_statfs_nodes;
+	int ndisplayed;
+	int c;
 
-	if ((e = gfarm_initialize(&argc, &argv)) != NULL) {
-		fprintf(stderr, "%s: gfarm initialize: %s\n", program_name, e);
-		exit(EXIT_FAILURE);
+	if (argc > 0)
+		program_name = basename(argv[0]);
+
+	e = gfarm_initialize(&argc, &argv);
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: %s\n", program_name,
+		    gfarm_error_string(e));
+		exit(1);
 	}
 
-	gfarm_stringlist_init(&hostlist);
-
-	while ((c = getopt(argc, argv, "H:h:i?")) != -1) {
+	while ((c = getopt(argc, argv, "ahHnrD:P:SV?")) != -1) {
 		switch (c) {
-		case 'H':
-			e = gfarm_hostlist_read(optarg, &nhosts, &hosts,
-			    &lineno);
-			if (e != NULL) {
-				if (lineno != -1) {
-					fprintf(stderr,
-					    "%s: %s: line %d: %s\n",
-					    program_name, optarg, lineno, e);
-				} else {
-					fprintf(stderr, "%s: %s: %s\n",
-					    program_name, optarg, e);
-				}
-				exit(EXIT_INVALID_ARGUMENT);
-			}
+		case 'a':
+			statfs = display_statfs;
 			break;
 		case 'h':
-			gfarm_stringlist_add(&hostlist, optarg);
+			formatter = &readable_formatter;
+			option_formatting_flags = GFARM_HUMANIZE_BINARY;
 			break;
-		case 'i':
-			title = inode_title;
-			print = inode_print;
+		case 'H':
+			formatter = &readable_formatter;
+			option_formatting_flags = 0;
 			break;
+		case 'n':
+			statfs = display_nodes;
+			break;
+		case 'r':
+			option_reverse_sort = 1;
+			break;
+		case 'D':
+			domain = optarg;
+			break;
+		case 'P':
+			path = optarg;
+			break;
+		case 'S':
+			option_sort_order = SO_SIZE;
+			break;
+		case 'V':
+			fprintf(stderr, "Gfarm version %s\n", gfarm_version());
+			exit(0);
 		case '?':
-			/*FALLTHROUGH*/
 		default:
 			usage();
 		}
 	}
-	if (optind < argc)
-		usage();
-
-	(*title)();
-
-	if (nhosts == 0 && gfarm_stringlist_length(&hostlist) == 0) {
-		/* list all filesystem node */
-		e = gfarm_host_info_get_all(&ninfos, &infos);
-		if (e != NULL) {
-			fprintf(stderr, "%s: %s\n", program_name, e);
-			status = EXIT_FAILURE;
-		} else {
-			for (i = 0; i < ninfos; i++) {
-				if ((*print)(infos[i].hostname) != NULL)
-					status = EXIT_FAILURE;
-			}
-		}
-	} else {
-		for (i = 0; i < nhosts; i++) {
-			if ((*print)(hosts[i]) != NULL)
-				status = EXIT_FAILURE;
-		}
-		for (i = 0; i < gfarm_stringlist_length(&hostlist); i++) {
-			if (print(gfarm_stringlist_elem(&hostlist, i)) != NULL)
-				status = EXIT_FAILURE;
-		}
+	if (gfarm_realpath_by_gfarm2fs(path, &p) == GFARM_ERR_NO_ERROR)
+		path = p;
+	e = statfs(path, domain, &ndisplayed);
+	free(p);
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: %s\n", program_name,
+		    gfarm_error_string(e));
+		exit(1);
 	}
-	gfarm_strings_free_deeply(nhosts, hosts);
-	gfarm_stringlist_free(&hostlist);
 
-
-	if ((e = gfarm_terminate()) != NULL) {
-		fprintf(stderr, "%s: gfarm terminate: %s\n", program_name, e);
-		exit(EXIT_FAILURE);
+	e = gfarm_terminate();
+	if (e != GFARM_ERR_NO_ERROR) {
+		fprintf(stderr, "%s: %s\n", program_name,
+		    gfarm_error_string(e));
+		exit(1);
 	}
-	return (status);
+	if (ndisplayed == 0)
+		exit(1);
+	exit (0);
 }
