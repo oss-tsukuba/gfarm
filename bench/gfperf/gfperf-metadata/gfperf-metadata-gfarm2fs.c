oss-tsukuba/gfarm

commit aa0ad0138fb4c7250c640aa99a8e917e0bc0d77a

define "gfarm.ncopy" and "gfarm.replicainfo" in <gfarm/gfs.h>
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/replica_check_2@10942 4a5e1ece-6a47-475c-b646-731d720b41d0

bench/gfperf/gfperf-metadata/gfperf-metadata-gfarm2fs.c
@@ -446,10 +446,11 @@ do_posix_setxattr(struct directory_names *names)
 	set_number(&r, names->n);
 	set_start(&r);
 #ifdef __APPLE_CC__
-	e = setxattr(names->names[0], XATTR_KEY, value, val_len,
+	e = setxattr(names->names[0], GFARM_EA_NCOPY, value, val_len,
 		     0, XATTR_CREATE);
 #else
-	e = setxattr(names->names[0], XATTR_KEY, value, val_len, XATTR_CREATE);
+	e = setxattr(names->names[0], GFARM_EA_NCOPY, value, val_len,
+		     XATTR_CREATE);
 #endif
 	if (e != 0) {
 		saved_errno = errno;
@@ -460,10 +461,10 @@ do_posix_setxattr(struct directory_names *names)
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
 #ifdef __APPLE_CC__
-		e = setxattr(names->names[i], XATTR_KEY, value, val_len,
+		e = setxattr(names->names[i], GFARM_EA_NCOPY, value, val_len,
 			     0, XATTR_CREATE);
 #else
-		e = setxattr(names->names[i], XATTR_KEY, value, val_len,
+		e = setxattr(names->names[i], GFARM_EA_NCOPY, value, val_len,
 			     XATTR_CREATE);
 #endif
 		if (e != 0) {
@@ -496,10 +497,10 @@ do_posix_getxattr(struct directory_names *names)
 	set_number(&r, names->n);
 	set_start(&r);
 #ifdef __APPLE_CC__
-	e = getxattr(names->names[0], XATTR_KEY, buf, sizeof(buf),
+	e = getxattr(names->names[0], GFARM_EA_NCOPY, buf, sizeof(buf),
 		     0, XATTR_NOFOLLOW);
 #else
-	e = getxattr(names->names[0], XATTR_KEY, buf, sizeof(buf));
+	e = getxattr(names->names[0], GFARM_EA_NCOPY, buf, sizeof(buf));
 #endif
 	if (e < 0) {
 		saved_errno = errno;
@@ -510,10 +511,11 @@ do_posix_getxattr(struct directory_names *names)
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
 #ifdef __APPLE_CC__
-		e = getxattr(names->names[i], XATTR_KEY, buf, sizeof(buf),
-			     0, XATTR_NOFOLLOW);
+		e = getxattr(names->names[i], GFARM_EA_NCOPY,
+			     buf, sizeof(buf), 0, XATTR_NOFOLLOW);
 #else
-		e = getxattr(names->names[i], XATTR_KEY, buf, sizeof(buf));
+		e = getxattr(names->names[i], GFARM_EA_NCOPY,
+			     buf, sizeof(buf));
 #endif
 		if (e < 0) {
 			saved_errno = errno;
@@ -544,9 +546,9 @@ do_posix_removexattr(struct directory_names *names)
 	set_number(&r, names->n);
 	set_start(&r);
 #ifdef __APPLE_CC__
-	e = removexattr(names->names[0], XATTR_KEY, XATTR_NOFOLLOW);
+	e = removexattr(names->names[0], GFARM_EA_NCOPY, XATTR_NOFOLLOW);
 #else
-	e = removexattr(names->names[0], XATTR_KEY);
+	e = removexattr(names->names[0], GFARM_EA_NCOPY);
 #endif
 	if (e != 0) {
 		saved_errno = errno;
@@ -557,9 +559,10 @@ do_posix_removexattr(struct directory_names *names)
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
 #ifdef __APPLE_CC__
-		e = removexattr(names->names[i], XATTR_KEY, XATTR_NOFOLLOW);
+		e = removexattr(names->names[i], GFARM_EA_NCOPY
+				XATTR_NOFOLLOW);
 #else
-		e = removexattr(names->names[i], XATTR_KEY);
+		e = removexattr(names->names[i], GFARM_EA_NCOPY);
 #endif
 		if (e != 0) {
 			saved_errno = errno;

bench/gfperf/gfperf-autoreplica/gfperf-autoreplica-main.c
@@ -36,8 +36,6 @@ static char *wait_time = NULL;
 static char *group_name = "unknown";
 static int stop_waiting = 60;
 
-#define NCOPY_KEY "gfarm.ncopy"
-
 static
 gfarm_error_t
 set_ncopy()
@@ -51,9 +49,9 @@ set_ncopy()
 
 	num_len = snprintf(num_str, sizeof(num_str), "%d", replicate);
 
-	gfs_removexattr(testdir, NCOPY_KEY);
+	gfs_removexattr(testdir, GFARM_EA_NCOPY);
 
-	e = gfs_setxattr(testdir, NCOPY_KEY, num_str, num_len,
+	e = gfs_setxattr(testdir, GFARM_EA_NCOPY, num_str, num_len,
 			 GFS_XATTR_CREATE);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "can not set gfarm.ncopy! (%s)\n",
@@ -72,7 +70,7 @@ del_ncopy()
 	if (replicate <= 1)
 		return (GFARM_ERR_NO_ERROR);
 
-	e = gfs_removexattr(testdir, NCOPY_KEY);
+	e = gfs_removexattr(testdir, GFARM_EA_NCOPY);
 	if (e != GFARM_ERR_NO_ERROR)
 		return (e);
 

bench/gfperf/gfperf-metadata/gfperf-metadata-libgfarm.c
@@ -461,8 +461,8 @@ do_libgfarm_setxattr(struct directory_names *names)
 
 	set_number(&r, names->n);
 	set_start(&r);
-	e = gfs_setxattr(names->names[0], XATTR_KEY, value, val_len,
-			 GFS_XATTR_CREATE);
+	e = gfs_setxattr(names->names[0], GFARM_EA_NCOPY,
+			 value, val_len, GFS_XATTR_CREATE);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "setxattr: %s\n",
 			gfarm_error_string(e));
@@ -470,8 +470,8 @@ do_libgfarm_setxattr(struct directory_names *names)
 	}
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
-		e = gfs_setxattr(names->names[i], XATTR_KEY, value, val_len,
-				 GFS_XATTR_CREATE);
+		e = gfs_setxattr(names->names[i], GFARM_EA_NCOPY,
+				 value, val_len, GFS_XATTR_CREATE);
 		if (e != GFARM_ERR_NO_ERROR) {
 			fprintf(stderr, "setxattr: %s\n",
 				gfarm_error_string(e));
@@ -503,7 +503,7 @@ do_libgfarm_getxattr(struct directory_names *names)
 	set_number(&r, names->n);
 	set_start(&r);
 	size = sizeof(value);
-	e = gfs_getxattr(names->names[0], XATTR_KEY, value, &size);
+	e = gfs_getxattr(names->names[0], GFARM_EA_NCOPY, value, &size);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "getxattr: %s\n",
 			gfarm_error_string(e));
@@ -512,7 +512,8 @@ do_libgfarm_getxattr(struct directory_names *names)
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
 		size = sizeof(value);
-		e = gfs_getxattr(names->names[i], XATTR_KEY, value, &size);
+		e = gfs_getxattr(names->names[i], GFARM_EA_NCOPY,
+				 value, &size);
 		if (e != GFARM_ERR_NO_ERROR) {
 			fprintf(stderr, "getxattr: %s\n",
 				gfarm_error_string(e));
@@ -541,7 +542,7 @@ do_libgfarm_removexattr(struct directory_names *names)
 
 	set_number(&r, names->n);
 	set_start(&r);
-	e = gfs_removexattr(names->names[0], XATTR_KEY);
+	e = gfs_removexattr(names->names[0], GFARM_EA_NCOPY);
 	if (e != GFARM_ERR_NO_ERROR) {
 		fprintf(stderr, "removexattr: %s\n",
 			gfarm_error_string(e));
@@ -549,7 +550,7 @@ do_libgfarm_removexattr(struct directory_names *names)
 	}
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
-		e = gfs_removexattr(names->names[i], XATTR_KEY);
+		e = gfs_removexattr(names->names[i], GFARM_EA_NCOPY);
 		if (e != GFARM_ERR_NO_ERROR) {
 			fprintf(stderr, "removexattr: %s\n",
 				gfarm_error_string(e));

bench/gfperf/gfperf-metadata/gfperf-metadata.h
@@ -11,7 +11,6 @@
 
 #define MKDIR_MODE 0755
 #define CHMOD_MODE 0700
-#define XATTR_KEY "gfarm.ncopy"
 #define BUF_SIZE 1024
 
 #define UNIT_OPS "ops"

gftool/gfncopy/gfncopy.c
@@ -252,9 +252,6 @@ get_xattr(const char *url, char *key, char **value_p)
 	return (e);
 }
 
-#define XATTR_NCOPY	"gfarm.ncopy"
-#define XATTR_REPATTR	GFARM_REPATTR_NAME
-
 static gfarm_error_t
 get_replica_spec(
 	const char *url, const char *root_url, int *ncopy_p, char **repattr_p)
@@ -273,7 +270,7 @@ get_replica_spec(
 		else
 			e_repattr = GFARM_ERR_NO_SUCH_OBJECT;
 	} else {
-		e_repattr = get_xattr(root_url, XATTR_REPATTR, &repattr);
+		e_repattr = get_xattr(root_url, GFARM_EA_REPATTR, &repattr);
 		if (e_repattr == GFARM_ERR_NO_ERROR ||
 		    e_repattr == GFARM_ERR_NO_SUCH_OBJECT) {
 			support_repattr = 1;
@@ -305,7 +302,7 @@ get_replica_spec(
 
 		if (strcmp(tmpurl, root_url) == 0)
 			is_root = 1;
-		e_ncopy = get_xattr(tmpurl, XATTR_NCOPY, &ncopy_str);
+		e_ncopy = get_xattr(tmpurl, GFARM_EA_NCOPY, &ncopy_str);
 		if (e_ncopy == GFARM_ERR_NO_ERROR) {
 			ncopy = atoi(ncopy_str);
 			free(ncopy_str);
@@ -316,7 +313,8 @@ get_replica_spec(
 			return (e_ncopy);
 		}
 		if (support_repattr) {
-			e_repattr = get_xattr(tmpurl, XATTR_REPATTR, &repattr);
+			e_repattr =
+			    get_xattr(tmpurl, GFARM_EA_REPATTR, &repattr);
 			if (e_repattr == GFARM_ERR_NO_ERROR) {
 				found = 1;
 			} else if (e_repattr != GFARM_ERR_NO_SUCH_OBJECT) {
@@ -458,13 +456,13 @@ print_replica_spec_main(int ncopy, const char *repattr)
 {
 	if (ncopy >= 0) {
 		if (opt_verb)
-			printf("%s=", XATTR_NCOPY);
+			printf("%s=", GFARM_EA_NCOPY);
 		printf("%d\n", ncopy);
 	}
 
 	if (repattr != NULL) {
 		if (opt_verb)
-			printf("%s=", XATTR_REPATTR);
+			printf("%s=", GFARM_EA_REPATTR);
 		printf("%s\n", repattr);
 	}
 }
@@ -685,11 +683,11 @@ set_ncopy(
 		return (GFARM_ERR_INVALID_ARGUMENT);
 	}
 
-	e = gfncopy_setxattr(url, XATTR_NCOPY, str, strlen(str), set_flags);
+	e = gfncopy_setxattr(url, GFARM_EA_NCOPY, str, strlen(str), set_flags);
 	if (e != GFARM_ERR_NO_ERROR) {
 		if (opt_verb)
 			ERR("%s: setxattr(%s): %s",
-			    url, XATTR_NCOPY, gfarm_error_string(e));
+			    url, GFARM_EA_NCOPY, gfarm_error_string(e));
 		else
 			fprintf(stderr, "%s\n", gfarm_error_string(e));
 	}
@@ -786,12 +784,12 @@ set_repattr(
 	if (e != GFARM_ERR_NO_ERROR)
 		return (e);
 
-	e = gfncopy_setxattr(url, XATTR_REPATTR, repattr,
+	e = gfncopy_setxattr(url, GFARM_EA_REPATTR, repattr,
 	    strlen(repattr) + 1, set_flags);
 	if (e != GFARM_ERR_NO_ERROR) {
 		if (opt_verb)
 			ERR("%s: setxattr(%s): %s",
-			    url, XATTR_REPATTR, gfarm_error_string(e));
+			    url, GFARM_EA_REPATTR, gfarm_error_string(e));
 		else
 			fprintf(stderr, "%s\n", gfarm_error_string(e));
 	}
@@ -814,23 +812,23 @@ remove_replica_spec(
 		return (GFARM_ERR_INVALID_ARGUMENT);
 	}
 
-	e = gfncopy_removexattr(url, XATTR_NCOPY);
+	e = gfncopy_removexattr(url, GFARM_EA_NCOPY);
 	if (e == GFARM_ERR_NO_SUCH_OBJECT) {
 		VERB("%s: removexattr %s: %s",
-		    url, XATTR_NCOPY, gfarm_error_string(e));
+		    url, GFARM_EA_NCOPY, gfarm_error_string(e));
 		e = GFARM_ERR_NO_ERROR;
 	} else if (e != GFARM_ERR_NO_ERROR)
 		ERR("%s: removexattr(%s): %s",
-		    url, XATTR_NCOPY, gfarm_error_string(e));
+		    url, GFARM_EA_NCOPY, gfarm_error_string(e));
 
-	e2 = gfncopy_removexattr(url, XATTR_REPATTR);
+	e2 = gfncopy_removexattr(url, GFARM_EA_REPATTR);
 	if (e2 == GFARM_ERR_NO_SUCH_OBJECT) {
 		VERB("%s: removexattr %s: %s",
-		    url, XATTR_REPATTR, gfarm_error_string(e2));
+		    url, GFARM_EA_REPATTR, gfarm_error_string(e2));
 		e2 = GFARM_ERR_NO_ERROR;
 	} else if (e2 != GFARM_ERR_NO_ERROR)
 		ERR("%s: removexattr(%s): %s",
-		    url, XATTR_REPATTR, gfarm_error_string(e2));
+		    url, GFARM_EA_REPATTR, gfarm_error_string(e2));
 
 	return (e == GFARM_ERR_NO_ERROR ? e2 : e);
 }
@@ -1019,8 +1017,8 @@ main(int argc, char **argv)
 		opt_mode = MODE_GET;
 
 	gfs_stat_cache_enable(1);
-	gfarm_xattr_caching_pattern_add(XATTR_NCOPY);
-	gfarm_xattr_caching_pattern_add(XATTR_REPATTR);
+	gfarm_xattr_caching_pattern_add(GFARM_EA_NCOPY);
+	gfarm_xattr_caching_pattern_add(GFARM_EA_REPATTR);
 
 	if (opt_nofollow || opt_mode == MODE_WAIT) {
 		gfncopy_stat = gfs_lstat_cached;

include/gfarm/gfs.h
@@ -244,8 +244,20 @@ gfarm_error_t gfs_access(const char *, int);
 #define GFS_W_OK	2
 #define GFS_R_OK	4
 
-/* gfarm xattr prefix */
+/* gfarm xattr */
 #define GFARM_EA_PREFIX		"gfarm."
+#define GFARM_EA_PREFIX_LEN	6
+
+#define GFARM_EA_NCOPY_TYPE	"ncopy"
+#define GFARM_EA_NCOPY		GFARM_EA_PREFIX GFARM_EA_NCOPY_TYPE
+#define GFARM_EA_REPATTR_TYPE	"replicainfo"
+#define GFARM_EA_REPATTR	GFARM_EA_PREFIX GFARM_EA_REPATTR_TYPE
+
+/* Key name of virtual extended attribute for dirquota */
+#define GFARM_EA_DIRECTORY_QUOTA_TYPE	"directory_quota"
+#define GFARM_EA_DIRECTORY_QUOTA	\
+	GFARM_EA_PREFIX GFARM_EA_DIRECTORY_QUOTA_TYPE
+
 
 /* 5th argument (flags) of gfs_setxattr() and gfs_fsetxattr() */
 #define GFS_XATTR_CREATE    0x1     /* set value, fail if attr already exists */
@@ -432,9 +444,6 @@ gfarm_error_t gfs_acl_from_text_with_default(const char *, gfarm_acl_t *,
 #ifdef GFARM_INTERNAL_USE /* internal use only */
 gfarm_error_t gfs_acl_delete_mode(gfarm_acl_t);
 #endif
-
-/* Key name of virtual extended attribute for dirquota */
-#define GFARM_EA_DIRECTORY_QUOTA	GFARM_EA_PREFIX "directory_quota"
 
 /*
  * Key names of extended attribute for gfarm_root.*

lib/libgfarm/gfarm/repattr.h
@@ -2,9 +2,6 @@
  * $Id$
  */
 
-#define GFARM_REPATTR_TYPE	"replicainfo"
-#define GFARM_REPATTR_NAME	"gfarm." GFARM_REPATTR_TYPE
-
 struct gfarm_repattr;
 typedef struct gfarm_repattr *gfarm_repattr_t;
 

regress/server/gfmd/db_journal/db_journal_test.c
@@ -3022,7 +3022,7 @@ t_apply_xattr_add_cached(void)
 {
 	struct inode *i;
 	struct db_xattr_arg m;
-	const char *attrname = "gfarm.ncopy";
+	const char *attrname = GFARM_EA_NCOPY;
 	const char *value = "2";
 	void *value1;
 	size_t value_len;
@@ -3076,7 +3076,7 @@ t_apply_xattr_modify_cached(void)
 {
 	struct inode *i;
 	struct db_xattr_arg m;
-	const char *attrname = "gfarm.ncopy";
+	const char *attrname = GFARM_EA_NCOPY;
 	const char *value = "3";
 	void *value1;
 	size_t value_len;
@@ -3126,7 +3126,7 @@ t_apply_xattr_remove_cached(void)
 {
 	struct inode *i;
 	struct db_xattr_arg m;
-	const char *attrname = "gfarm.ncopy";
+	const char *attrname = GFARM_EA_NCOPY;
 	void *value;
 	size_t value_len;
 
@@ -3152,7 +3152,7 @@ t_apply_xattr_removeall(void)
 	struct inode *i;
 	struct db_xattr_arg m;
 	const char *attrname1 = "attrname";
-	const char *attrname2 = "gfarm.ncopy";
+	const char *attrname2 = GFARM_EA_NCOPY;
 	const char *value1 = "value";
 	const char *value2 = "2";
 	void *value;

commit 38a486cd79a20414235f70d05588b13d1d93cfe0

merge r10991 from the replica_check_2 branch (via r10993 from the 2.7 branch):
fix a typo on macOS, introduced in r10942 (r10963 on the 2.6 branch)


git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@10994 4a5e1ece-6a47-475c-b646-731d720b41d0

bench/gfperf/gfperf-metadata/gfperf-metadata-gfarm2fs.c
@@ -559,7 +559,7 @@ do_posix_removexattr(struct directory_names *names)
 	set_middle(&r);
 	for (i = 1; i <= names->n; i++) {
 #ifdef __APPLE_CC__
-		e = removexattr(names->names[i], GFARM_EA_NCOPY
+		e = removexattr(names->names[i], GFARM_EA_NCOPY,
 				XATTR_NOFOLLOW);
 #else
 		e = removexattr(names->names[i], GFARM_EA_NCOPY);