oss-tsukuba/gfarm

commit fd5545521fbbc5e785f3c26ba7fde1ac8bb52815

report close error since FUSE client does not report close error at close time
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.7@10977 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/fs.c
@@ -758,10 +758,41 @@ gfm_server_fhopen(struct peer *peer, int from_client, int skip)
 	return (gfm_server_put_reply(peer, diag, e, "i", mode));
 }
 
+/* FUSE client does not report close error */
+static gfarm_error_t
+save_inum_for_close_check(struct process *process, struct peer *peer,
+	gfarm_int32_t fd, gfarm_ino_t *inump, gfarm_uint64_t *igenp,
+	const char *diag)
+{
+	struct file_opening *fo = NULL;
+	gfarm_error_t e;
+
+	e = process_get_file_opening(process, peer, fd, &fo, diag);
+	if (e != GFARM_ERR_NO_ERROR)
+		return (e);
+	if (inump != NULL)
+		*inump = inode_get_number(fo->inode);
+	if (igenp != NULL)
+		*igenp = inode_get_gen(fo->inode);
+	return (e);
+}
+
+static void
+close_error_check(struct peer *peer, gfarm_ino_t inum, gfarm_uint64_t igen,
+	gfarm_error_t e, const char *diag)
+{
+	if (e == GFARM_ERR_NO_ERROR)
+		return;
+	gflog_warning(GFARM_MSG_UNFIXED, "%s (%s@%s) %llu:%llu: %s", diag,
+		peer_get_username(peer), peer_get_hostname(peer),
+		(unsigned long long)inum, (unsigned long long)igen,
+		gfarm_error_string(e));
+}
+
 static gfarm_error_t
 gfm_server_close_gen(struct peer *peer, int from_client, int skip, int getgen)
 {
-	gfarm_error_t e, e2;
+	gfarm_error_t e, e2, e_save = GFARM_ERR_INVALID_ARGUMENT;
 	struct host *spool_host = NULL;
 	struct process *process;
 	gfarm_int32_t fd = -1;
@@ -769,6 +800,8 @@ gfm_server_close_gen(struct peer *peer, int from_client, int skip, int getgen)
 	char *trace_log = NULL;
 	static const char diag[] = "GFM_PROTO_CLOSE";
 	gfarm_uint64_t gen = 0;
+	gfarm_ino_t saved_inum;
+	gfarm_uint64_t saved_igen;
 
 	if (skip)
 		return (GFARM_ERR_NO_ERROR);
@@ -789,6 +822,9 @@ gfm_server_close_gen(struct peer *peer, int from_client, int skip, int getgen)
 			"peer_fdpair_get_current() failed: %s",
 			gfarm_error_string(e));
 	} else {
+		e_save = save_inum_for_close_check(process, peer, fd,
+			&saved_inum, &saved_igen, diag);
+
 		if (db_begin(diag) == GFARM_ERR_NO_ERROR)
 			transaction = 1;
 		/*
@@ -809,6 +845,12 @@ gfm_server_close_gen(struct peer *peer, int from_client, int skip, int getgen)
 		e2 = gfm_server_put_reply(peer, diag, e, "l", gen);
 	else
 		e2 = gfm_server_put_reply(peer, diag, e, "");
+	/*
+	 * report close error.  note that generation number does not
+	 * chenge if error happens.
+	 */
+	if (e_save == GFARM_ERR_NO_ERROR)
+		close_error_check(peer, saved_inum, saved_igen, e, diag);
 	if (gfarm_ctxp->file_trace && trace_log != NULL) {
 		gflog_trace(GFARM_MSG_1003295, "%s", trace_log);
 		free(trace_log);

commit 17d36c6d3d65e0a6f0467734a21a96f63dcc0e11

fix the following warnings: {{{ fs.c:803: warning: 'saved_inum' may be used uninitialized in this function fs.c:804: warning: 'saved_igen' may be used uninitialized in this function }}} observed with gcc version 4.4.7 20120313 (Red Hat 4.4.7-16) on CentOS 6.6. this problem was introduced in r10977
git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.7@10982 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/fs.c
@@ -803,6 +803,11 @@ gfm_server_close_gen(struct peer *peer, int from_client, int skip, int getgen)
 	gfarm_ino_t saved_inum;
 	gfarm_uint64_t saved_igen;
 
+#ifdef __GNUC__ /* workaround gcc warning: may be used uninitialized */
+	saved_inum = 0;
+	saved_igen = 0;
+#endif
+
 	if (skip)
 		return (GFARM_ERR_NO_ERROR);
 	giant_lock();