8c3218c9affe352325b650186d05bba2f3a4d830
Osamu Tatebe <tatebe@users.sourceforge.net>
2019-03-19 08:58:57 +0000

    merge r11084 from the 2.7 branch with slightly fix regression test for gfdf -a
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@11096 4a5e1ece-6a47-475c-b646-731d720b41d0

---
+++ regress/gftool/gfdf/gfdf-a.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+. ./regress.conf
+
+allout=$(gfdf -a | tail -1)
+read allblocks allused allavail allrest <<< $allout
+
+out=$(gfdf | tail -1)
+read blocks used avail rest <<< $out
+
+if [ $allblocks = $blocks ] &&
+   [ $allused = $used ] && [ $allavail = $avail ]; then
+	exit_code=$exit_pass
+else
+	echo gfdf -a: $allout
+	echo gfdf -R: $out
+fi
+
+exit $exit_code
