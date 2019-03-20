e6941733b87ec2081343fa7639c83a977e9b77ed
Osamu Tatebe <tatebe@users.sourceforge.net>
2019-03-20 01:28:21 +0000

    merge r11099 from the 2.7 branch - sequel to r11084 - race condition between gfdf -a and gfdf -R
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@11100 4a5e1ece-6a47-475c-b646-731d720b41d0

--- regress/gftool/gfdf/gfdf-a.sh
+++ regress/gftool/gfdf/gfdf-a.sh
@@ -8,12 +8,19 @@ read allblocks allused allavail allrest <<< $allout
 out=$(gfdf | tail -1)
 read blocks used avail rest <<< $out
 
+allout2=$(gfdf -a | tail -1)
+read allblocks2 allused2 allavail2 allrest2 <<< $allout2
+
 if [ $allblocks = $blocks ] &&
    [ $allused = $used ] && [ $allavail = $avail ]; then
+	exit_code=$exit_pass
+elif [ $allblocks2 = $blocks ] &&
+   [ $allused2 = $used ] && [ $allavail2 = $avail ]; then
 	exit_code=$exit_pass
 else
 	echo gfdf -a: $allout
 	echo gfdf -R: $out
+	echo gfdf -a: $allout2
 fi
 
 exit $exit_code
