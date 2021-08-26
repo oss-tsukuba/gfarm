b3ad1d8418c446d115890cc97a8a1f38282484e1
Motonori Hirano <m-hirano@sra.co.jp>
2021-08-26 17:50:45 +0900

    Added valgrind support.

--- regress/manual/lib/libgfarm/gfarm/tls-test/tools/testscripts/lib/funcs.sh
+++ regress/manual/lib/libgfarm/gfarm/tls-test/tools/testscripts/lib/funcs.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+gen_command_line() {
+    if test ! -z "${1}"; then
+        cmd=`echo ${1} | awk '{ print $1 }'`
+	file ${cmd} | grep 'POSIX shell script' > /dev/null 2>&1
+	if test $? -eq 0; then
+	    dir=`dirname ${cmd}`
+	    newcmd=`echo ${cmd} | sed "s:${dir}::"`
+	    newcmd="${dir}/.libs/${newcmd}"
+	    echo "${1}" | sed "s:${cmd}:${newcmd}:"
+	else
+	    echo "${1}"
+	fi
+	return 0
+    fi
+    echo "true"
+    return 1
+}
+
 puts_error() {
 	echo "ERR: $1" 1>&2
 }
@@ -17,6 +35,9 @@ puts_error() {
 #   0: Test succeeded.
 #   1: Test failed.
 #
+# enhancement for valgrind check:
+#   __RUN_VALGRIND__:
+#	An enviromnment variable to determine run valgrind or not
 run_test() {
 	test_id=$1
 	server_cmd=$2
@@ -34,16 +55,33 @@ run_test() {
 	expected_result_csv="`dirname $0`/expected-test-result.csv"
 	result_server=0
 	s_exit_file="server_exit_status.txt"
-	tmp_output_file="/tmp/.tls_test_output"
 
-	# Save server log to /tmp.
-	if [ -n "${expected_server_log_reg}" ]; then
-		output_file=$tmp_output_file
-	else
-		output_file=/dev/null
+	do_valg=0
+	valgcmd=""
+	if [ ! -z "${__RUN_VALGRIND__}" ]; then
+	    do_valg=1
+	    valgcmd="valgrind --leak-check=full --leak-resolution=high --show-leak-kinds=all"
 	fi
 
-	sh -c "rm -f ./${s_exit_file}; sync; ${server_cmd} > $output_file 2>&1; \
+	no_out=0
+	if [ ${do_valg} -eq 1 ]; then
+	    client_output_file="${test_id}-cli.out"
+	    server_output_file="${test_id}-srv.out"
+	else
+	    # Save server log to /tmp.
+	    if [ -n "${expected_server_log_reg}" ]; then
+		client_output_file="/dev/null"
+		server_output_file="/tmp/.tls_test_output"
+	    else
+		client_output_file="/dev/null"
+		server_output_file="/dev/null"
+		no_out=1
+	    fi
+	fi
+
+	server_cmd=`gen_command_line "${server_cmd}"`
+	sh -c "rm -f ./${s_exit_file}; sync; \
+		${valgcmd} ${server_cmd} > \"${server_output_file}\" 2>&1; \
 		echo \$? > ./${s_exit_file}; sync" &
 	child_pid=$!
 	while :
@@ -69,8 +107,10 @@ run_test() {
 	done
 
 	if [ ${result_server} -ne 0 ]; then
-		rm -f ./${s_exit_file}
-		rm -f ${tmp_output_file}
+		rm -f "./${s_exit_file}"
+		if [ ${no_out} -eq 0 -a ${do_valg} -eq 0 ]; then
+		    rm -f "${server_output_file}"
+		fi
 		echo "server fatal fail"
 		case ${result_server} in
 			2) echo "tls_context error";;
@@ -80,7 +120,8 @@ run_test() {
 		return ${_r}
 	fi
 
-	sh -c "${client_cmd} > /dev/null 2>&1"
+	client_cmd=`gen_command_line "${client_cmd}"`
+	sh -c "${valgcmd} ${client_cmd} > \"${client_output_file}\" 2>&1"
 	client_exitstatus=$?
 
 	if [ ${client_exitstatus} -ne 2 -a ${client_exitstatus} -ne 3 ]; then
@@ -106,7 +147,7 @@ run_test() {
 	     -a "${client_exitstatus}" = "${expected_client_result}" ]; then
 		if [ -n "${expected_server_log_reg}" ]; then
 			# Check server log.
-			log=`grep -E "${expected_server_log_reg}" $tmp_output_file`
+			log=`grep -E "${expected_server_log_reg}" "${server_output_file}"`
 			if [ ${debug_flag} -eq 1 ]; then
 				echo "actual server log:'${log}'"
 			fi
@@ -129,8 +170,17 @@ run_test() {
 		shutdown_server ${child_pid}
 	fi
 
+	if [ ${do_valg} -eq 1 ]; then
+	    srvwarn=`grep tls_funcs.h "${server_output_file}" | wc -l`
+	    cliwarn=`grep tls_funcs.h "${client_output_file}" | wc -l`
+	    echo "		server leaks: ${srvwarn}"
+	    echo "		client leaks: ${cliwarn}"
+	fi
+
 	rm -f ./${s_exit_file}
-	rm -f ${tmp_output_file}
+	if [ ${no_out} -eq 0 -a ${do_valg} -eq 0 ]; then
+	    rm -f "${server_output_file}"
+	fi
 
 	return ${_r}
 }
