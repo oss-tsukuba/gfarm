f3de91a0fc161b1e6374914c6bbb2b952540cbe0
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2021-12-29 13:21:46 +0900

    docker/dev: update "make COPY_FILES".

--- docker/dev/common/copy-updated-files-to-container.sh
+++ docker/dev/common/copy-updated-files-to-container.sh
@@ -1,44 +1,63 @@
 #!/bin/sh
 
 set -eu
+#set -x
 
-DOCKER=$(make -s ECHO_DOCKER)
+BASEDIR="$PWD"
+#DOCKER=$(make -s ECHO_DOCKER)
 COMPOSE=$(make -s ECHO_COMPOSE)
 ROOTDIR=$(make -s ECHO_ROOTDIR)
-GFARM_WORKDIR=/home/user1/gfarm
 SERVICES=$($COMPOSE ps --services)
 GFARM_SRCDIR=$(cd $(realpath .) && cd $ROOTDIR/../.. && realpath .)
+GFARM2FS_SRCDIR="${GFARM_SRCDIR}/gfarm2fs"
 
-docker_cp_to() {
-    FILE="$1"
-    SERVICE="$2"
-    SERVICE_ID="$3"
-    TODIR="$4"
-    $DOCKER cp "$FILE" "$SERVICE_ID":"$TODIR/$FILE"
-    echo "UPDATE: $SERVICE:$TODIR/$FILE"
-}
+USERNAME="user1"
+HOMEDIR="/home/${USERNAME}"
+GFARM_WORKDIR="${HOMEDIR}/gfarm"
+GFARM2FS_WORKDIR="${HOMEDIR}/gfarm2fs"
+SCP_HOST="gfclient1"  # in .ssh/config
 
 updated_files() {
-    cd $GFARM_SRCDIR
     git status -s | grep -v '^??' | awk '{print $2}'
 }
 
-copy_to_service() {
-    SERVICE="$1"
-    FILES="$2"
-    SERVICE_ID=$($COMPOSE ps -q "$SERVICE")
+scp0() {
+    SRC="$1"
+    DST="$2"
+    scp -q "$SRC" "$DST"
+    echo "COPY TO: $DST"
+}
 
-    cd $GFARM_SRCDIR
-    for f in $FILES; do
+scp_to_container() {
+    HOST="$1"
+
+    cd "$GFARM_SRCDIR"
+    for f in $(updated_files); do
         f=$(echo $f | sed -e "s/[\r\n]\+//g")
-        docker_cp_to "$f" "$SERVICE" "$SERVICE_ID" "$GFARM_WORKDIR" &
+        scp0 "$f" "$HOST:$GFARM_WORKDIR/$f" &
+    done
+    cd "$GFARM2FS_SRCDIR"
+    for f in $(updated_files); do
+        f=$(echo $f | sed -e "s/[\r\n]\+//g")
+        scp0 "$f" "$HOST:$GFARM2FS_WORKDIR/$f" &
     done
     wait
 }
 
-FILES=$(updated_files)
+rsync_fromto() {
+    FROM="$1"
+    TO="$2"
+
+    cd "$BASEDIR"
+    $COMPOSE exec -u "$USERNAME" "$FROM" bash -c "rsync -a ${GFARM_WORKDIR}/ ${TO}:${GFARM_WORKDIR}/"
+    $COMPOSE exec -u "$USERNAME" "$FROM" bash -c "rsync -a ${GFARM2FS_WORKDIR}/ ${TO}:${GFARM2FS_WORKDIR}/"
+    echo "synchronized: $TO"
+}
+
+scp_to_container "$SCP_HOST"
 
 for s in $SERVICES; do
-    copy_to_service "$s" "$FILES" &
+    [ $s = "client1" ] && continue
+    rsync_fromto client1 $s &
 done
 wait
