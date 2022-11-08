oss-tsukuba/gfarm

commit b18e8fe0fd3b0f9c7de22a63d6385d749ffe5040

implement #1136: support "auth sasl"

lib/libgfarm/gfarm/auth_config.c
@@ -48,6 +48,10 @@ struct gfarm_auth_method_name_value {
 	  GFARM_AUTH_METHOD_KERBEROS },
 	{ 'k', "kerberos_auth",
 	  GFARM_AUTH_METHOD_KERBEROS_AUTH },
+	{ 'A', "sasl",
+	  GFARM_AUTH_METHOD_SASL },
+	{ 'a', "sasl_auth",
+	  GFARM_AUTH_METHOD_SASL_AUTH },
 };
 
 enum gfarm_auth_config_command { GFARM_AUTH_ENABLE, GFARM_AUTH_DISABLE };
@@ -336,8 +340,19 @@ gfarm_auth_method_kerberos_available(void)
 #endif
 }
 
-gfarm_int32_t
-gfarm_auth_method_get_available(void)
+gfarm_error_t
+gfarm_auth_method_sasl_available(void)
+{
+#if defined(HAVE_CYRUS_SASL) && defined(HAVE_TLS_1_3)
+	/* XXX TODO(?) check whether JWT is available or not? */
+	return (GFARM_ERR_NO_ERROR);
+#else
+	return (GFARM_ERR_PROTOCOL_NOT_SUPPORTED);
+#endif
+}
+
+static gfarm_int32_t
+gfarm_auth_method_get_available(int is_server)
 {
 	int i;
 	gfarm_int32_t methods;
@@ -364,12 +379,34 @@ gfarm_auth_method_get_available(void)
 				break; /* available */
 #endif
 			continue; /* not available */
+		case GFARM_AUTH_METHOD_SASL:
+		case GFARM_AUTH_METHOD_SASL_AUTH:
+#if defined(HAVE_CYRUS_SASL) && defined(HAVE_TLS_1_3)
+			if (is_server ?
+			    gfarm_auth_server_method_sasl_available() :
+			    gfarm_auth_client_method_sasl_available())
+				break; /* available */
+#else
+			continue; /* not available */
+#endif
 		default:
 			break; /* available */
 		}
 		methods |= 1 << i;
 	}
 	return (methods);
+}
+
+gfarm_int32_t
+gfarm_auth_server_method_get_available(void)
+{
+	return (gfarm_auth_method_get_available(1));
+}
+
+gfarm_int32_t
+gfarm_auth_client_method_get_available(void)
+{
+	return (gfarm_auth_method_get_available(0));
 }
 
 void

configure.ac
@@ -521,6 +521,55 @@ AC_SUBST(globus_cflags)
 AC_SUBST(globus_targets)
 
 ###
+### --disable-cyrus-sasl
+###
+
+AC_ARG_ENABLE(cyrus-sasl,
+AS_HELP_STRING([--disable-cyrus-sasl],[disable Cyrus SASL [default=enable]]), [], [])
+
+if test x"${enable_cyrus_sasl}" != x"no"; then
+  AC_CHECK_LIB(sasl2, sasl_client_init)
+  AC_MSG_CHECKING(Cyrus SASL library available)
+  if test x"${ac_cv_lib_sasl2_sasl_client_init}" = x"yes"; then
+    AC_MSG_RESULT([yes])
+    enable_cyrus_sasl=yes
+  else
+    AC_MSG_RESULT([no])
+    if test "x${enable_cyrus_sasl}" = "xyes"; then
+      AC_MSG_ERROR([cannot enable SASL support.])
+      exit 1
+    fi
+    enable_cyrus_sasl=no
+  fi
+fi
+
+if test x"${enable_cyrus_sasl}" = x"yes"; then
+  cyrus_sasl_includes= # assumes standard include path, for now
+  cyrus_sasl_libs=-lsasl2
+
+  cyrus_sasl_srcs='${CYRUS_SASL_SRCS}'
+  cyrus_sasl_objs='${CYRUS_SASL_OBJS}'
+  cyrus_sasl_cflags='${CYRUS_SASL_CFLAGS}'
+  cyrus_sasl_targets='${CYRUS_SASL_TARGETS}'
+  AC_DEFINE(HAVE_CYRUS_SASL, 1, [Cyrus SASL exists])
+else
+  cyrus_sasl_includes=
+  cyrus_sasl_libs=
+
+  cyrus_sasl_srcs=
+  cyrus_sasl_objs=
+  cyrus_sasl_cflags=
+  cyrus_sasl_targets=
+fi
+
+AC_SUBST(cyrus_sasl_includes)
+AC_SUBST(cyrus_sasl_libs)
+AC_SUBST(cyrus_sasl_srcs)
+AC_SUBST(cyrus_sasl_objs)
+AC_SUBST(cyrus_sasl_cflags)
+AC_SUBST(cyrus_sasl_targets)
+
+###
 ### --without-mtsafe-netdb ... are getaddrinfo(3) and getnameinfo(3) MT-Safe?
 ###
 

gftool/gfstatus/gfstatus.c
@@ -375,6 +375,13 @@ main(int argc, char *argv[])
 		  "not available"
 #endif
 	    );
+	print_msg("client auth sasl    ",
+#if defined(HAVE_CYRUS_SASL) && defined(HAVE_TLS_1_3)
+		  "available"
+#else
+		  "not available"
+#endif
+	    );
 
 	puts("");
 	print_msg("hostname          ", gfarm_host_get_self_name());

lib/libgfarm/gfarm/Makefile
@@ -32,9 +32,11 @@ KERBEROS_OBJS = auth_client_kerberos.lo auth_server_kerberos.lo
 KERBEROS_CFLAGS = -I$(GFSL_SRCDIR) \
 	-DLIBGFSL_KERBEROS='"$(libdir)/libgfsl_kerberos.so"'
 
+CYRUS_SASL_SRCS = auth_client_sasl.c  auth_server_sasl.c  auth_common_sasl.c
+CYRUS_SASL_OBJS = auth_client_sasl.lo auth_server_sasl.lo auth_common_sasl.lo
 
-TLS_SRCS = auth_client_tls.c  auth_server_tls.c  io_tls.c
-TLS_OBJS = auth_client_tls.lo auth_server_tls.lo io_tls.lo
+TLS_SRCS = auth_client_tls.c  auth_server_tls.c  io_tls.c  $(cyrus_sasl_srcs)
+TLS_OBJS = auth_client_tls.lo auth_server_tls.lo io_tls.lo $(cyrus_sasl_objs)
 
 SRCS = \
 	context.c \
@@ -229,6 +231,7 @@ $(OBJS): $(DEPGFARMINC)
 $(GSS_OBJS): $(DEPGFSLINC)
 $(GLOBUS_OBJS): $(DEPGFSLINC)
 $(KERBEROS_OBJS): $(DEPGFSLINC)
+$(CYRUS_SASL_OBJS): $(DEPGFSLINC)
 auth_client.lo: $(GFUTIL_SRCDIR)/gfutil.h $(GFUTIL_SRCDIR)/gfevent.h context.h liberror.h gfp_xdr.h auth.h
 auth_client_kerberos.lo: auth.h gss.h
 auth_client_gsi.lo: auth.h gss.h

lib/libgfarm/gfarm/auth.h
@@ -25,6 +25,8 @@ enum gfarm_auth_method {
 	GFARM_AUTH_METHOD_TLS_CLIENT_CERTIFICATE,
 	GFARM_AUTH_METHOD_KERBEROS,
 	GFARM_AUTH_METHOD_KERBEROS_AUTH,
+	GFARM_AUTH_METHOD_SASL,
+	GFARM_AUTH_METHOD_SASL_AUTH,
 
 	GFARM_AUTH_METHOD_NUMBER
 };
@@ -96,6 +98,15 @@ enum gfarm_auth_tls_client_certificate_request {
 	GFARM_AUTH_TLS_CLIENT_CERTIFICATE_CLIENT_TYPE
 };
 
+/*
+ * GFARM_AUTH_METHOD_SASL* dependent constants.
+ */
+enum gfarm_auth_sasl_step_type {
+	GFARM_AUTH_SASL_STEP_ERROR,
+	GFARM_AUTH_SASL_STEP_DONE,
+	GFARM_AUTH_SASL_STEP_CONTINUE,
+};
+
 /* auth_client */
 
 struct gfp_xdr;
@@ -158,7 +169,8 @@ void gfarm_auth_config_set_mark(void);
 /* this i/f have to be changed, if we support more than 31 auth methods */
 gfarm_int32_t gfarm_auth_method_get_enabled_by_name_addr(
 	const char *, struct sockaddr *);
-gfarm_int32_t gfarm_auth_method_get_available(void);
+gfarm_int32_t gfarm_auth_client_method_get_available(void);
+gfarm_int32_t gfarm_auth_server_method_get_available(void);
 
 gfarm_error_t gfarm_auth_cred_type_parse(char *, enum gfarm_auth_cred_type *);
 enum gfarm_auth_cred_type gfarm_auth_server_cred_type_get(const char *);
@@ -248,6 +260,30 @@ gfarm_error_t gfarm_auth_request_kerberos_auth_multiplexed(
 	const char *, struct passwd *, int, void (*)(void *), void *, void **);
 gfarm_error_t gfarm_auth_result_kerberos_auth_multiplexed(void *);
 
+/* auth_client_sasl */
+gfarm_error_t gfarm_auth_request_sasl(struct gfp_xdr *,
+	const char *, const char *, enum gfarm_auth_id_type, const char *,
+	struct passwd *);
+gfarm_error_t gfarm_auth_request_sasl_multiplexed(
+	struct gfarm_eventqueue *,
+	struct gfp_xdr *, const char *, const char *, enum gfarm_auth_id_type,
+	const char *, struct passwd *, int, void (*)(void *), void *, void **);
+gfarm_error_t gfarm_auth_result_sasl_multiplexed(void *);
+
+/* auth_client_sasl_auth */
+gfarm_error_t gfarm_auth_request_sasl_auth(struct gfp_xdr *,
+	const char *, const char *, enum gfarm_auth_id_type, const char *,
+	struct passwd *);
+gfarm_error_t gfarm_auth_request_sasl_auth_multiplexed(
+	struct gfarm_eventqueue *,
+	struct gfp_xdr *, const char *, const char *, enum gfarm_auth_id_type,
+	const char *, struct passwd *, int, void (*)(void *), void *, void **);
+gfarm_error_t gfarm_auth_result_sasl_auth_multiplexed(void *);
+
+int gfarm_auth_client_method_sasl_available(void);
+int gfarm_sasl_log(void *, int, const char *);
+int gfarm_sasl_addr_string(int, char *, size_t, char *, size_t, const char *);
+
 /* auth_client_tls_sharedsecret */
 gfarm_error_t gfarm_auth_request_tls_sharedsecret(struct gfp_xdr *,
 	const char *, const char *, enum gfarm_auth_id_type, const char *,
@@ -269,6 +305,8 @@ gfarm_error_t gfarm_auth_request_tls_client_certificate_multiplexed(
 	const char *, struct passwd *, int, void (*)(void *), void *, void **);
 gfarm_error_t gfarm_auth_result_tls_client_certificate_multiplexed(void *);
 
+gfarm_error_t gfarm_tls_server_cert_is_ok(
+	struct gfp_xdr *, const char *, const char *);
 
 /* auth_server_sharedsecret */
 gfarm_error_t gfarm_authorize_sharedsecret_common(struct gfp_xdr *,
@@ -327,6 +365,23 @@ gfarm_error_t gfarm_authorize_kerberos_auth(struct gfp_xdr *, char *, char *,
 	    enum gfarm_auth_method, enum gfarm_auth_id_type, const char *,
 	    char **), void *,
 	enum gfarm_auth_id_type *, char **);
+
+/* auth_server_sasl */
+gfarm_error_t gfarm_authorize_sasl(struct gfp_xdr *, char *, char *,
+	gfarm_error_t (*)(void *,
+	    enum gfarm_auth_method, enum gfarm_auth_id_type, const char *,
+	    char **), void *,
+	enum gfarm_auth_id_type *, char **);
+
+/* auth_server_sasl_auth */
+gfarm_error_t gfarm_authorize_sasl_auth(struct gfp_xdr *, char *, char *,
+	gfarm_error_t (*)(void *,
+	    enum gfarm_auth_method, enum gfarm_auth_id_type, const char *,
+	    char **), void *,
+	enum gfarm_auth_id_type *, char **);
+
+void gfarm_sasl_server_init(void);
+int gfarm_auth_server_method_sasl_available(void);
 
 /* auth_server_tls_sharedsecret */
 gfarm_error_t gfarm_authorize_tls_sharedsecret(struct gfp_xdr *,

lib/libgfarm/gfarm/auth_client.c
@@ -91,6 +91,16 @@ static const struct gfarm_auth_client_method {
 	  gfarm_auth_request_gsi_multiplexed,
 	  gfarm_auth_result_gsi_multiplexed },
 #endif /* HAVE_GSI */
+#if defined(HAVE_CYRUS_SASL) && defined(HAVE_TLS_1_3)
+	{ GFARM_AUTH_METHOD_SASL_AUTH,
+	  gfarm_auth_request_sasl_auth,
+	  gfarm_auth_request_sasl_auth_multiplexed,
+	  gfarm_auth_result_sasl_auth_multiplexed },
+	{ GFARM_AUTH_METHOD_SASL,
+	  gfarm_auth_request_sasl,
+	  gfarm_auth_request_sasl_multiplexed,
+	  gfarm_auth_result_sasl_multiplexed },
+#endif /* defined(HAVE_CYRUS_SASL) && defined(HAVE_TLS_1_3) */
 #ifdef HAVE_KERBEROS
 	{ GFARM_AUTH_METHOD_KERBEROS_AUTH,
 	  gfarm_auth_request_kerberos_auth,
@@ -101,6 +111,7 @@ static const struct gfarm_auth_client_method {
 	  gfarm_auth_request_kerberos_multiplexed,
 	  gfarm_auth_result_kerberos_multiplexed },
 #endif /* HAVE_KERBEROS */
+
 	{ GFARM_AUTH_METHOD_NONE,	  NULL, NULL, NULL }	/* sentinel */
 };
 
@@ -323,7 +334,7 @@ gfarm_auth_request(struct gfp_xdr *conn,
 		    name);
 		return (GFARM_ERRMSG_AUTH_METHOD_NOT_AVAILABLE_FOR_THE_HOST);
 	}
-	methods &= gfarm_auth_method_get_available();
+	methods &= gfarm_auth_client_method_get_available();
 	if (methods == 0) {
 		gflog_debug(GFARM_MSG_1001042,
 			"No usable auth method configured");
@@ -1080,7 +1091,7 @@ gfarm_auth_request_multiplexed(struct gfarm_eventqueue *q,
 			name);
 		return (GFARM_ERRMSG_AUTH_METHOD_NOT_AVAILABLE_FOR_THE_HOST);
 	}
-	methods &= gfarm_auth_method_get_available();
+	methods &= gfarm_auth_client_method_get_available();
 	if (methods == 0) {
 		gflog_debug(GFARM_MSG_1001069,
 			"No usable auth method configured");

lib/libgfarm/gfarm/auth_client_tls.c
@@ -13,8 +13,8 @@
 #include "io_tls.h"
 #include "auth.h"
 
-static gfarm_error_t
-server_cert_is_ok(struct gfp_xdr *conn, const char *service_tag,
+gfarm_error_t
+gfarm_tls_server_cert_is_ok(struct gfp_xdr *conn, const char *service_tag,
 	const char *hostname)
 {
 	gfarm_error_t e;
@@ -58,13 +58,14 @@ gfarm_auth_request_tls_sharedsecret(struct gfp_xdr *conn,
 	struct passwd *pwd)
 {
 	gfarm_error_t e;
+
 	e = gfp_xdr_tls_alloc(conn, gfp_xdr_fd(conn), GFP_XDR_TLS_INITIATE);
 	if (e != GFARM_ERR_NO_ERROR) {
 		/* is this case graceful? */
 		return (e);
 	}
 
-	e = server_cert_is_ok(conn, service_tag, hostname);
+	e = gfarm_tls_server_cert_is_ok(conn, service_tag, hostname);
 
 	e = gfarm_auth_request_sharedsecret_common(
 	    conn, service_tag, hostname, self_type, user, pwd,
@@ -167,7 +168,7 @@ gfarm_auth_request_tls_client_certificate(struct gfp_xdr *conn,
 		return (e);
 	}
 
-	e = server_cert_is_ok(conn, service_tag, hostname);
+	e = gfarm_tls_server_cert_is_ok(conn, service_tag, hostname);
 	if (e == GFARM_ERR_NO_ERROR) {
 		req = GFARM_AUTH_TLS_CLIENT_CERTIFICATE_CLIENT_TYPE;
 		arg = self_type;
@@ -295,7 +296,7 @@ gfarm_auth_request_tls_client_certificate_multiplexed(
 		return (e);
 	}
 
-	e = server_cert_is_ok(conn, service_tag, hostname);
+	e = gfarm_tls_server_cert_is_ok(conn, service_tag, hostname);
 	if (e == GFARM_ERR_NO_ERROR) {
 		req = GFARM_AUTH_TLS_CLIENT_CERTIFICATE_CLIENT_TYPE;
 		arg = self_type;

commit f3b9dd322323de050c41a9590a2cdd105fc1eed9

fix a bug in code to detect whether SASL is configured or not
this was introduced in commit b18e8fe0fd3b0f9c7de22a63d6385d749ffe5040

lib/libgfarm/gfarm/auth_config.c
@@ -402,9 +402,8 @@ gfarm_auth_method_get_available(int is_server,
 				if (gfarm_auth_client_method_sasl_available())
 					break; /* available */
 			}
-#else
-			continue; /* not available */
 #endif
+			continue; /* not available */
 		default:
 			break; /* available */
 		}