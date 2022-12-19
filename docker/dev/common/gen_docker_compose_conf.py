oss-tsukuba/gfarm

commit 23fe090b46e1a66c597f2eb9729bb8fed2f360b5

docker/dev: remove unnecessary extra_hosts (These extra_hosts can cause authconfig to fail)

docker/dev/common/gen_docker_compose_conf.py
@@ -102,8 +102,6 @@ print('''\
 
 for h in hosts:
     print("    - {}:{}".format(h.hostname, str(h.ipaddr)))
-    if h.hostname != h.name:
-        print("    - {}:{}".format(h.name, str(h.ipaddr)))
 
 print('''\
 

commit e7a0d30bd2e4e1007ebf2264eb1ac5db8d4f6302

docker/dev: fix an error at "make reborn" in fedroa33
commit 23fe090b46e1a66c597f2eb9729bb8fed2f360b5 introduced this problem.
non-FQDN hostname resolution failed since the commit,
because fedroa33 is using systemd-resolved

docker/dev/common/gen_docker_compose_conf.py
@@ -3,6 +3,7 @@
 from ipaddress import IPv4Network, IPv6Network
 from os import environ
 import sys
+import re
 
 num_gfmds = int(environ['GFDOCKER_NUM_GFMDS'])
 num_gfsds = int(environ['GFDOCKER_NUM_GFSDS'])
@@ -102,6 +103,12 @@ print('''\
 
 for h in hosts:
     print("    - {}:{}".format(h.hostname, str(h.ipaddr)))
+
+if hostname_suffix != '':
+    print('''\
+  dns_search:
+    - {search_domain}
+'''.format(search_domain=re.sub('^.', '', hostname_suffix)), end='')
 
 print('''\
 