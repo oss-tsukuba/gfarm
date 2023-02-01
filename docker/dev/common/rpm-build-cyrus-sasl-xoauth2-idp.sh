005eaff188b4346483ae7f2deeb532fb5827b35f
SODA Noriyuki <n-soda@users.sourceforge.net>
2023-02-01 12:43:26 +0900

    docker/dev: make cyrus-sasl-xoauth2-idp buildable on dist/opensuse/pkg

--- docker/dev/common/rpm-build-cyrus-sasl-xoauth2-idp.sh
+++ docker/dev/common/rpm-build-cyrus-sasl-xoauth2-idp.sh
@@ -3,4 +3,16 @@
 set -eux
 
 GFDOCKER_SCRIPT_PATH="`dirname $0`"
-"${GFDOCKER_SCRIPT_PATH}/rpm-build.sh" -O cyrus-sasl-xoauth2-idp
+name=cyrus-sasl-xoauth2-idp
+srcdir=/work/gfarm/${name}
+specpath="${name}.spec"
+
+# OpenSUSE does not have cyrus-sasl-lib RPM
+if type lsb_release >/dev/null &&
+   [ "$(lsb_release -i | awk '{print $NF}')" = "openSUSE" ]
+then
+  sed 's/, cyrus-sasl-lib//' "${srcdir}/${specpath}" >"/tmp/${name}.spec"
+  specpath="/tmp/${name}.spec"
+fi
+
+"${GFDOCKER_SCRIPT_PATH}/rpm-build.sh" -O -s "${specpath}" ${name}
--- docker/dev/common/rpm-build-scitokens-cpp.sh
+++ docker/dev/common/rpm-build-scitokens-cpp.sh
@@ -4,11 +4,21 @@ set -eux
 
 GFDOCKER_SCRIPT_PATH="`dirname $0`"
 name=scitokens-cpp
+srcdir=/work/gfarm/${name}
+specpath="rpm/${name}.spec"
+
+# OpenSUSE does not have "%cmake3"* macros of RPM spec file
+if type lsb_release >/dev/null &&
+   [ "$(lsb_release -i | awk '{print $NF}')" = "openSUSE" ]
+then
+  sed 's/^%cmake3/%cmake/' "${srcdir}/${specpath}" >"/tmp/${name}.spec"
+  specpath="/tmp/${name}.spec"
+fi
 
 "${GFDOCKER_SCRIPT_PATH}/rpm-build.sh" -O \
 	-F	"/usr/include/sqlite3.h
 		 /usr/include/curl/curl.h
 		 /usr/include/uuid/uuid.h" \
 	-T "cmake c++" \
-	-s "rpm/${name}.spec" \
+	-s "${specpath}" \
 	${name}
