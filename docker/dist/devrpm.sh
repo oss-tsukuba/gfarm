c5306baaafafccc724f4ede475c47ecf26370c95
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2023-05-09 23:27:13 +0900

    (docker/dev) fix comment

--- docker/dist/devrpm.sh
+++ docker/dist/devrpm.sh
@@ -6,8 +6,8 @@ docker run -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN \
 	--device=/dev/fuse --name alma8 alma8
 docker exec -it alma8 sh -c "(cd && /bin/bash)"
 
-# cd
 # sh gfarm/docker/dist/mkrpm.sh
+# Ctrl-D
 
 # docker cp alma8:/root/rpmbuild/SRPMS/gfarm-$VER-1.src.rpm .
 # docker stop alma8
