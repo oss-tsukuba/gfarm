3dc9ad460aa8e7a61a5a2cb851069770608732da
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2023-05-18 14:04:11 +0900

    (docker/dist) docker compose down for sure

--- docker/dist/batchtest.sh
+++ docker/dist/batchtest.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 set -xeu
 status=1
-trap '[ $status = 1 ] && echo NG; exit $status' 0 1 2 15
+trap '[ $status = 0 ] && echo Done || echo NG; exit $status' 0 1 2 15
 
 test()
 {
 	script=$2
 
+	DIST=$1 docker compose build c1
 	DIST=$1 docker compose up -d
-	docker exec gfarm-c1 sudo -u $USER \
-		sh -c "(cd /home/$USER/gfarm/docker/dist && sh $script)"
+	docker exec -u $USER -w /home/$USER/gfarm/docker/dist gfarm-c1 \
+		sh $script
 	docker compose down
 }
+
+# clean up
+docker compose down
 
 # Ubuntu
 test ubuntu all.sh
 
+# AlmaLinux8 and CentOS7
 for d in almalinux8 centos7
 do
 	for s in all.sh all-rpm.sh
@@ -24,4 +29,3 @@ do
 	done
 done
 status=0
-echo Done
