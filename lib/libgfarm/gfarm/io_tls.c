oss-tsukuba/gfarm

commit f2303843415f01dbb0dc53cf9b05f0012a449bfb

auth TLS: make TLS negotiation more graceful

lib/libgfarm/gfarm/io_tls.c
@@ -321,8 +321,10 @@ gfp_xdr_tls_alloc(struct gfp_xdr *conn,	int fd, int flags)
 	static const char diag[] = "gfp_xdr_tls_alloc";
 
 	GFARM_MALLOC(io);
-	if (io == NULL)
+	if (io == NULL) {
+		/* It's OK to make all authentcation fail in this case */
 		return (GFARM_ERR_NO_MEMORY);
+	}
 	gfarm_mutex_init(&io->mutex, diag, mutex_what);
 
 	/*
@@ -343,14 +345,14 @@ gfp_xdr_tls_alloc(struct gfp_xdr *conn,	int fd, int flags)
 	gfarm_openssl_global_unlock(diag);
 #endif
 
-	if (unlikely(ret != GFARM_ERR_NO_ERROR)) {
-		/* do nothing */
-	} else if (unlikely(ctx == NULL)) {
+	if (unlikely(ret == GFARM_ERR_NO_ERROR && ctx == NULL)) {
+		gflog_error(GFARM_MSG_UNFIXED,
+		    "tls_session_create_ctx: ctx == NULL, should not happen");
 		ret = GFARM_ERR_INTERNAL_ERROR;
 	} else {
 		io->ctx = ctx;
 
-		ret = tls_session_establish(ctx, fd);
+		ret = tls_session_establish(ctx, fd, conn, ret);
 
 		if (likely(ret == GFARM_ERR_NO_ERROR))
 			gfp_xdr_set(conn, &gfp_xdr_tls_iobuf_ops, io, fd);

lib/libgfarm/gfarm/tls_funcs.h
@@ -2961,18 +2961,21 @@ done:
 }
 
 static inline gfarm_error_t
-tls_session_establish(struct tls_session_ctx_struct *ctx, int fd)
+tls_session_establish(struct tls_session_ctx_struct *ctx, int fd,
+	struct gfp_xdr *conn, gfarm_error_t prior_error)
 {
-	gfarm_error_t ret = GFARM_ERR_UNKNOWN;
+	gfarm_error_t e, ret = GFARM_ERR_UNKNOWN;
 	struct sockaddr sa;
 	socklen_t salen = sizeof(sa);
-	int pst = -1;
+	int eof, pst = -1;
+	bool negotiation_sent = false, negotiation_received = false;
+	gfarm_int32_t negotiation_error;
 	typedef int (*tls_handshake_proc_t)(SSL *ssl);
 	tls_handshake_proc_t p = NULL;
 	SSL *ssl = NULL;
 
 	errno = 0;
-	if (likely(fd >= 0 &&
+	if (likely(prior_error == GFARM_ERR_NO_ERROR && fd >= 0 &&
 		(pst = getpeername(fd, &sa, &salen)) == 0 &&
 		ctx != NULL)) {
 
@@ -2990,6 +2993,29 @@ tls_session_establish(struct tls_session_ctx_struct *ctx, int fd)
 			int st;
 			int ssl_err;
 			bool do_cont = false;
+
+			e = gfp_xdr_send(conn, "i",
+			    (gfarm_int32_t)GFARM_ERR_NO_ERROR);
+			if (e == GFARM_ERR_NO_ERROR)
+				e = gfp_xdr_flush(conn);
+			negotiation_sent = true;
+			if (e == GFARM_ERR_NO_ERROR) {
+				e = gfp_xdr_recv(conn, 0, &eof, "i",
+				    &negotiation_error);
+				negotiation_received = true;
+			}
+			if (e != GFARM_ERR_NO_ERROR) {
+				ret = e;
+				goto bailout;
+			}
+			if (eof) {
+				ret = GFARM_ERR_UNEXPECTED_EOF;
+				goto bailout;
+			}
+			if (negotiation_error != GFARM_ERR_NO_ERROR) {
+				ret = negotiation_error;
+				goto bailout;
+			}
 
 			ctx->is_handshake_tried_ = true;
 			p = (ctx->role_ == TLS_ROLE_SERVER) ?
@@ -3094,6 +3120,22 @@ retry:
 	}
 
 bailout:
+	if (ret != GFARM_ERR_NO_ERROR) {
+		/* to make negotiation graceful */
+		if (!negotiation_sent) {
+			e = gfp_xdr_send(conn, "i",
+			    (gfarm_int32_t)GFARM_ERR_NO_ERROR);
+			if (e == GFARM_ERR_NO_ERROR)
+				e = gfp_xdr_flush(conn);
+			negotiation_sent = true;
+		}
+		if (!negotiation_received) {
+			e = gfp_xdr_recv(conn, 0, &eof, "i",
+			    &negotiation_error);
+			negotiation_received = true;
+		}
+	}
+
 	return (ret);
 }
 

commit 004f8cea5404bbcb50507129a845a230a84aa9c2

auth TLS: fix deadlock in authentication
problem introduced in commit f2303843415f01dbb0dc53cf9b05f0012a449bfb
(auth TLS: make TLS negotiation more graceful)

pointed out by @otatebe san

lib/libgfarm/gfarm/io_tls.c
@@ -330,7 +330,9 @@ gfp_xdr_tls_alloc(struct gfp_xdr *conn,	int fd, int flags)
 	gfarm_openssl_global_unlock(diag);
 #endif
 
-	if (unlikely(ret == GFARM_ERR_NO_ERROR && ctx == NULL)) {
+	if (unlikely(ret != GFARM_ERR_NO_ERROR)) {
+		/* do nothing */
+	} else if (unlikely(ctx == NULL)) {
 		gflog_error(GFARM_MSG_UNFIXED,
 		    "tls_session_create_ctx: ctx == NULL, should not happen");
 		ret = GFARM_ERR_INTERNAL_ERROR;

lib/libgfarm/gfarm/tls_funcs.h
@@ -3113,6 +3113,8 @@ tls_session_establish(struct tls_session_ctx_struct *ctx, int fd,
 		 */
 		ret = tls_session_setup_ssl(ctx);
 		if (unlikely(ret != GFARM_ERR_NO_ERROR || ctx->ssl_ == NULL)) {
+			if (ret == GFARM_ERR_NO_ERROR)
+				ret = GFARM_ERR_INTERNAL_ERROR;
 			goto bailout;
 		}
 		ssl = ctx->ssl_;
@@ -3129,7 +3131,7 @@ tls_session_establish(struct tls_session_ctx_struct *ctx, int fd,
 				e = gfp_xdr_flush(conn);
 			negotiation_sent = true;
 			if (e == GFARM_ERR_NO_ERROR) {
-				e = gfp_xdr_recv(conn, 0, &eof, "i",
+				e = gfp_xdr_recv(conn, 1, &eof, "i",
 				    &negotiation_error);
 				negotiation_received = true;
 			}
@@ -3269,7 +3271,7 @@ bailout:
 			negotiation_sent = true;
 		}
 		if (!negotiation_received) {
-			e = gfp_xdr_recv(conn, 0, &eof, "i",
+			e = gfp_xdr_recv(conn, 1, &eof, "i",
 			    &negotiation_error);
 			negotiation_received = true;
 		}