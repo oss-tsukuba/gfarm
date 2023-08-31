oss-tsukuba/gfarm

commit 004f8cea5404bbcb50507129a845a230a84aa9c2

auth TLS: fix deadlock in authentication
problem introduced in commit f2303843415f01dbb0dc53cf9b05f0012a449bfb
(auth TLS: make TLS negotiation more graceful)

pointed out by @otatebe san

lib/libgfarm/gfarm/io_tls.c
@@ -330,7 +330,9 @@ gfp_xdr_tls_alloc(struct gfp_xdr *conn,	int fd, int flags)
 	gfarm_openssl_global_unlock(diag);
 #endif
 
-	if (unlikely(ret == GFARM_ERR_NO_ERROR && ctx == NULL)) {
+	if (unlikely(ret != GFARM_ERR_NO_ERROR)) {
+		/* do nothing */
+	} else if (unlikely(ctx == NULL)) {
 		gflog_error(GFARM_MSG_UNFIXED,
 		    "tls_session_create_ctx: ctx == NULL, should not happen");
 		ret = GFARM_ERR_INTERNAL_ERROR;

lib/libgfarm/gfarm/tls_funcs.h
@@ -3113,6 +3113,8 @@ tls_session_establish(struct tls_session_ctx_struct *ctx, int fd,
 		 */
 		ret = tls_session_setup_ssl(ctx);
 		if (unlikely(ret != GFARM_ERR_NO_ERROR || ctx->ssl_ == NULL)) {
+			if (ret == GFARM_ERR_NO_ERROR)
+				ret = GFARM_ERR_INTERNAL_ERROR;
 			goto bailout;
 		}
 		ssl = ctx->ssl_;
@@ -3129,7 +3131,7 @@ tls_session_establish(struct tls_session_ctx_struct *ctx, int fd,
 				e = gfp_xdr_flush(conn);
 			negotiation_sent = true;
 			if (e == GFARM_ERR_NO_ERROR) {
-				e = gfp_xdr_recv(conn, 0, &eof, "i",
+				e = gfp_xdr_recv(conn, 1, &eof, "i",
 				    &negotiation_error);
 				negotiation_received = true;
 			}
@@ -3269,7 +3271,7 @@ bailout:
 			negotiation_sent = true;
 		}
 		if (!negotiation_received) {
-			e = gfp_xdr_recv(conn, 0, &eof, "i",
+			e = gfp_xdr_recv(conn, 1, &eof, "i",
 			    &negotiation_error);
 			negotiation_received = true;
 		}

commit 98373c6951c05be1425e7c6eff751e3d51986b41

make TLS authentication graceful again
problem pointed out by @otatebe san

this bug was introduced in commit 004f8cea5404bbcb50507129a845a230a84aa9c2
(auth TLS: fix deadlock in authentication).

this version is more graceful than the version prior to commit 004f8cea5

lib/libgfarm/gfarm/io_tls.c
@@ -330,22 +330,19 @@ gfp_xdr_tls_alloc(struct gfp_xdr *conn,	int fd, int flags)
 	gfarm_openssl_global_unlock(diag);
 #endif
 
-	if (unlikely(ret != GFARM_ERR_NO_ERROR)) {
-		/* do nothing */
-	} else if (unlikely(ctx == NULL)) {
-		gflog_error(GFARM_MSG_UNFIXED,
-		    "tls_session_create_ctx: ctx == NULL, should not happen");
-		ret = GFARM_ERR_INTERNAL_ERROR;
+	/*
+	 * to make TLS authentication graceful,
+	 * always call tls_session_establish()
+	 * even if ret != GFARM_ERR_NO_ERROR or ctx == NULL.
+	 * tls_session_establish() can handle such case.
+	 */
+	io->ctx = ctx;
+
+	ret = tls_session_establish(ctx, fd, conn, ret);
+
+	if (likely(ret == GFARM_ERR_NO_ERROR)) {
+		gfp_xdr_set(conn, &gfp_xdr_tls_iobuf_ops, io, fd);
 	} else {
-		io->ctx = ctx;
-
-		ret = tls_session_establish(ctx, fd, conn, ret);
-
-		if (likely(ret == GFARM_ERR_NO_ERROR))
-			gfp_xdr_set(conn, &gfp_xdr_tls_iobuf_ops, io, fd);
-	}
-
-	if (unlikely(ret != GFARM_ERR_NO_ERROR)) {
 		if (ctx != NULL)
 			tls_session_destroy_ctx(ctx);
 		gfarm_mutex_destroy(&io->mutex, diag, mutex_what);

lib/libgfarm/gfarm/tls_funcs.h
@@ -3204,8 +3204,8 @@ retry:
 					"the file descriptor %d: %s",
 					fd, gfarm_error_string(ret));
 			}
-		} else {
-			ret = GFARM_ERR_INVALID_ARGUMENT;
+		} else { /* fd < 0 || ctx == NULL */
+			ret = GFARM_ERR_INTERNAL_ERROR; /* shouldn't happen */
 			gflog_tls_error(GFARM_MSG_UNFIXED,
 				"The tls context is not initialized.");
 		}