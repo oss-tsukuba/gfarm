8695dcc6162bae00c29e9396e43097383f1bcd46
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2023-09-22 23:55:12 +0900

    (docker/dist) createuser.sh - register to Gfarm

--- docker/dist/createuser.sh
+++ docker/dist/createuser.sh
@@ -5,11 +5,16 @@ PROG=$(basename $0)
 trap '[ $status = 0 ] && echo Done || echo NG: $PROG; exit $status' 0 1 2 15
 
 u=$1
-gfarm-prun -a -p sudo useradd -m $u
+SUDO="sudo -u $u"
+gfarm-prun -a -p sudo useradd -m -s /bin/bash $u
 gfarm-prun -a -p sudo passwd -d $u
-sudo -u $u sh ./setup.sh
+$SUDO sh ./setup.sh
+$SUDO gfarm-pcp /home/$u/.nodelist .
 
 sh ./userkey.sh $u
 sh ./usercert.sh $u
+
+SUB=$($SUDO grid-proxy-info -identity)
+gfuser -c $u $u / $SUB
 
 status=0
