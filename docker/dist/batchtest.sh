fc00341ef89e3f7dd8b66f30b50ff50f096449b8
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2023-10-02 14:40:53 +0900

    (docker/dist) batchtest.sh - include oauth2 test

--- docker/dist/batchtest.sh
+++ docker/dist/batchtest.sh
@@ -3,22 +3,34 @@ set -xeu
 status=1
 trap '[ $status = 0 ] && echo Done || echo NG; exit $status' 0 1 2 15
 
+DOCKEREXEC="docker exec -u $USER -w /home/$USER/gfarm/docker/dist gfarm-c1"
+
 test()
 {
 	script="$2"
+	JWT=false
+	[ $# -gt 2 ] && JWT=true
 
 	DIST=$1 docker compose build --build-arg UID=$(id -u) c1
 	DIST=$1 docker compose up -d
-	docker exec -u $USER -w /home/$USER/gfarm/docker/dist gfarm-c1 \
-		sh $script -min
+
+	# JWT-Server
+	$JWT && (cd jwt-server && docker compose up -d && make setup)
+
+	# execute a script
+	$DOCKEREXEC sh $script -min
+
+	# SASL XOAUTH2 test
+	$JWT && $DOCKEREXEC sh ./check-oauth.sh
+
 	docker compose down
 }
 
 # clean up
-docker compose down
+make down
 
-# Ubuntu
-test ubuntu all.sh
+## Ubuntu
+test ubuntu all.sh jwt
 
 # AlmaLinux8 and CentOS7
 for d in almalinux8 centos7
@@ -28,4 +40,5 @@ do
 		test $d "$s"
 	done
 done
+
 status=0
