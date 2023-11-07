03d023e6ee2272b43bed7f5affcdebd51e667678
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2023-11-07 16:24:32 +0900

    (regress) gflsof-G - more robust

--- regress/gftool/gflsof/gflsof-G.sh
+++ regress/gftool/gflsof/gflsof-G.sh
@@ -12,10 +12,14 @@ trap 'gfrm -f $gftmp; exit $exit_trap' $trap_sigs
 if host=`gfsched -n 1 -w`; then
   if gfreg -h $host $data/1byte $gftmp; then
     $gfs_pio_test -h $host -wa -O -P$SLEEP_TIME $gftmp <$data/1byte &
-    sleep 1
-    if gflsof -WG | grep "^${host}\$" >/dev/null; then
-      exit_code=$exit_pass
+    # need to wait for reopen
+    timeout=$(($(date +%s) + SLEEP_TIME))
+    while [ $(date +%s) -le $timeout ]; do
+      if gflsof -WG | grep ^$host$ > /dev/null; then
+	exit_code=$exit_pass
+	break
     fi
+    done
   fi
 fi
 
