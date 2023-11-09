a728cabdc84e5db6734b6b9739dff89f83d31058
SODA Noriyuki <n-soda@users.sourceforge.net>
2023-11-09 11:02:59 +0900

    docker/dist: make "docker exec -it" inherit $TERM environment if set

--- docker/dist/devrpm.sh
+++ docker/dist/devrpm.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -xeu
 
+TERM_INHERIT=${TERM+--env TERM=${TERM}}
+
 docker build -f Dockerfile ../.. -t alma8
 docker run -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN \
 	--device=/dev/fuse --name alma8 alma8
-docker exec -w /root -it alma8 /bin/bash
+docker exec ${TERM_INHERIT} -w /root -it alma8 /bin/bash
 
 # sh gfarm/docker/dist/mkrpm.sh
 # Ctrl-D
