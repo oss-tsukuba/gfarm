9b828493ec166e025cfe8df615beb3c3a311e58f
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2025-04-11 15:56:05 +0900

    (docker/dist) hpci.sh - GSI deprecated

--- docker/dist/hpci.sh
+++ docker/dist/hpci.sh
@@ -5,27 +5,12 @@ status=1
 trap '[ $status = 1 ] && echo NG; rm -f get_gfarm2conf.sh; exit $status' \
 	0 1 2 15
 
-GSICONF=/etc/grid-security/gsi.conf
-grep "NAME_COMPATIBILITY=HYBRID" $GSICONF || {
-	sudo mv $GSICONF $GSICONF.orig
-	awk '/^NAME_COMPATIBILITY=/{print "NAME_COMPATIBILITY=HYBRID";next}
-	     {print}' $GSICONF.orig | sudo tee $GSICONF > /dev/null
-}
+# delete proxy cert
+grid-proxy-destroy 2>/dev/null || :
 
 sudo mkdir -p /etc/grid-security/certificates
 OPWD=$PWD
 cd /etc/grid-security/certificates
-# HPCI CA (will be deprecated soon)
-HASH=61cd35bd
-for suf in signing_policy 0
-do
-	[ -f $HASH.$suf ] || {
-		[ -f $OPWD/hpci/$HASH.$suf ] &&
-			sudo cp $OPWD/hpci/$HASH.$suf . ||
-			sudo wget https://www.hpci.nii.ac.jp/ca/$HASH.$suf
-	}
-done
-
 # HPCI SS CA
 CERT=21d9c8b3.0
 [ -f $CERT ] || {
@@ -53,6 +38,5 @@ echo \# YOU NEED TO DO THE FOLLOWING
 echo mv ~/.globus ~/.globus.bak
 echo export GFARM_CONFIG_FILE=$HOME/.gfarm2rc.hpci
 echo jwt-agent -s https://elpis.hpci.nii.ac.jp/ -l HPCI_ID
-echo or myproxy-logon -s portal.hpci.nii.ac.jp -t 168 -l HPCI-ID
 status=0
 echo Done
