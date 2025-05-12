3f26b6568ed27c4ab5e7902436030f0b594f6e0b
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2025-05-12 14:01:36 +0900

    (docker/dist) hpc-manual - install orjson

--- docker/dist/hpci-manual/test.sh
+++ docker/dist/hpci-manual/test.sh
@@ -5,6 +5,11 @@ set -eu
 HPCIID=$1
 read PASS
 
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
+pip install orjson
+
 echo $PASS | jwt-logon -s https://elpis.hpci.nii.ac.jp/ -l $HPCIID
 sleep 3
 jwt-parse
