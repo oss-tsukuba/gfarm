adc5dab2f70e90d072b8f7ab5da04b45185e6337
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2025-05-16 14:44:59 +0900

    (docker/dist) hpci-manual - docopt and schema required

--- docker/dist/hpci-manual/test.sh
+++ docker/dist/hpci-manual/test.sh
@@ -8,7 +8,7 @@ read PASS
 python3 -m venv venv
 . venv/bin/activate
 pip install --upgrade pip
-pip install orjson
+pip install orjson docopt schema
 
 echo $PASS | jwt-logon -s https://elpis.hpci.nii.ac.jp/ -l $HPCIID
 sleep 3
@@ -18,7 +18,14 @@ echo $PASS | nohup jwt-agent -s https://elpis.hpci.nii.ac.jp/ -l $HPCIID
 sleep 3
 jwt-parse
 
+set -x
+
 gfstatus
 gfhost -lvu
+OUTD=test-$HPCIID-$$
+gfptar -c gfarm:/tmp/$OUTD-gz -C / hpci-manual
+gfptar -c gfarm:/tmp/$OUTD-bz2 -T bz2 -C / hpci-manual
+gfptar -c gfarm:/tmp/$OUTD-xz -T xz -C / hpci-manual
+gfrm -rf gfarm:/tmp/$OUTD-gz gfarm:/tmp/$OUTD-bz2 gfarm:/tmp/$OUTD-xz
 mount.hpci
 umount.hpci
