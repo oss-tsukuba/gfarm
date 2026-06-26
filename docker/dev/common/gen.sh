113c48c05e00d8067eb33e1f82a155a51488c4b0
Yuta Murakami <m-yuuta@sra.co.jp>
2026-06-26 17:26:48 +0900

    docker/dev: remove cgroup v2 detection
    * drop cgroup v2 detection and related code
    * remove legacy cgroup v1 handling as no supported
      distributions require it anymore

--- docker/dev/common/gen.sh
+++ docker/dev/common/gen.sh
@@ -23,14 +23,6 @@ set -eu
 #ADMIN_DN="/O=Grid/OU=GlobusTest/OU=GlobusSimpleCA/CN=${GFDOCKER_PRIMARY_USER}"
 ADMIN_DN="/O=Gfarm/OU=GfarmDev/OU=GfarmCA/CN=${GFDOCKER_PRIMARY_USER}"
 
-IS_CGROUP_V2_COMMAND="${TOP}/docker/dev/common/is_cgroup_v2.sh"
-
-if ${IS_CGROUP_V2_COMMAND}; then
-    IS_CGROUP_V2=true
-else
-    IS_CGROUP_V2=false
-fi
-
 gen_gfservicerc() {
   cat <<EOF
 # This file was automatically generated.
@@ -88,7 +80,6 @@ COMPOSE_YAML="${TOP}/docker/dev/docker-compose.yml"
 
 gen_gfservicerc > "${TOP}/docker/dev/common/rc.gfservice"
 
-IS_CGROUP_V2="${IS_CGROUP_V2}" \
 "${TOP}/docker/dev/common/gen_docker_compose_conf.py" \
   > "${COMPOSE_YAML}.tmp"
 mv "${COMPOSE_YAML}.tmp" "${COMPOSE_YAML}"
