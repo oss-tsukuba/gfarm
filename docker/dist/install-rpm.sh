526ac4b9acc58c19166cdc7bc368fc777f78a170
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2026-07-19 00:46:39 +0900

    docker/dist:install-rpm.sh: support aarch64

--- docker/dist/install-rpm.sh
+++ docker/dist/install-rpm.sh
@@ -4,10 +4,11 @@ status=1
 trap '[ $status = 1 ] && echo NG; rm -rf ~/local/rpms; exit $status' 0 1 2 15
 
 : ${PKG:=gfarm}
+: ${ARCH:=$(uname -m)}
 
-sudo rpm -Uvh --force ~/rpmbuild/RPMS/x86_64/$PKG-*
+sudo rpm -Uvh --force ~/rpmbuild/RPMS/$ARCH/$PKG-*
 mkdir -p ~/local/rpms
-cp -p ~/rpmbuild/RPMS/x86_64/$PKG-* ~/local/rpms
+cp -p ~/rpmbuild/RPMS/$ARCH/$PKG-* ~/local/rpms
 gfarm-prun -p sudo rpm -Uvh --force local/rpms/$PKG-*
 
 status=0
